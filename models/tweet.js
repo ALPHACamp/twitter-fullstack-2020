703f3ace14da5f75f16b05ec46920c6b65cb05b5
DESKTOP-9TBTLD8\SAM LAI <b540915@gmail.com>
2020-07-17 17:12:36 +0800

    fix: modify Tweet & User association through Like

--- models/tweet.js
+++ models/tweet.js
@@ -13,6 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Tweet.belongsTo(models.User)
       Tweet.hasMany(models.Reply)
+      Tweet.belongsToMany(models.User, {
+        through: models.Like,
+        foreignKey: 'tweetId',
+        as: 'LikedUser'
+      })
     }
   };
   Tweet.init({
