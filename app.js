fb840edb0b3a998df5472e363e5f8b39894217eb
leey611 <leey611@newschool.edu>
2020-07-17 11:41:37 -0400

    basic appjs setup and view engine

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
-const express = require('express')
+const express = require('express');
 const helpers = require('./_helpers');
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//iew engine
+app.engine(
+  'hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+  })
+);
+app.set('view engine', 'hbs');
+// body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//method override
+app.use(methodOverrice('_method'));
+// session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+//flash
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages');
+  res.locals.error_messages = req.flash('error_messages');
+  next();
+});
 
-module.exports = app
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+module.exports = app;
