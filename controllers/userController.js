28a6e1dfe7857c3ae05808adf972b9d5b3a902c0
leey611 <leey611@newschool.edu>
2020-07-18 13:43:24 -0400

    test commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,47 @@
+const db = require('../models');
+const User = db.User;
+const bcrypt = require('bcryptjs');
+
 let userController = {
   loginPage: (req, res) => {
     return res.render('login');
   },
   signupPage: (req, res) => {
     return res.render('signup');
+  },
+  signup: (req, res) => {
+    //password and confirmPassword must be the same
+    console.log(req.body.password);
+    console.log(req.body.confirmPassword);
+    console.log(req.body);
+    //return;
+    if (req.body.password !== req.body.confirmPassword) {
+      req.flash('error_messages', 'Please check your confirm password');
+      return res.redirect('/signup');
+    }
+    //check if this email has been registered
+    User.findOne({ where: { email: req.body.email } }).then((user) => {
+      if (user) {
+        return req.flash('error_messages', 'This email has been registered');
+      }
+      User.create({
+        name: req.body.name,
+        account: req.body.account,
+        email: req.body.email,
+        password: bcrypt.hashSync(
+          req.body.password,
+          bcrypt.genSaltSync(10, null)
+        ),
+        avatar:
+          'https://loremflickr.com/cache/resized/65535_49534054127_ef7d5544b9_320_240_nofilter.jpg',
+        role: 'user'
+      })
+        .then((user) => {
+          req.flash('success_messages', 'Signuped successfully');
+          return res.redirect('/login');
+        })
+        .catch((err) => console.log(err));
+    });
   }
 };
 
--- routes/routes.js
+++ routes/routes.js
@@ -8,5 +8,6 @@ const userController = require('../controllers/userController');
 router.get('/admin/login', adminController.adminLoginPage);
 router.get('/login', userController.loginPage);
 router.get('/signup', userController.signupPage);
+router.post('/signup', userController.signup);
 
 module.exports = router;
