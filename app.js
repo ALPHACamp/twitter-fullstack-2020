c805a23cdc5dcff80a2cc3ddabce660ef711a029
carey <schiafang@gmail.com>
2020-07-19 22:47:19 +0800

    init

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
+const exhbs = require('express-handlebars')
+const bodyPaser = require('body-parser')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const app = express()
 const port = 3000
@@ -7,7 +12,14 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use(bodyPaser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(session({ secret: 'twittercat', resave: false, saveUninitialized: false }))
+app.use(flash())
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-module.exports = app
+require('./routes/index')(app)
