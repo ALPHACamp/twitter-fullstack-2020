c6026b3b1117e2f1b59f8a7d295f307f225bd351
Robert <robert913152750@gmail.com>
2020-07-20 15:15:50 +0800

    set setting.json to follow js standard

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,8 @@
-const db = require("../models");
-const User = db.User;
-const Tweet = db.Tweet;
-const Reply = db.Reply;
-const Like = db.Like;
+const db = require('../models')
+const User = db.User
+const Tweet = db.Tweet
+const Reply = db.Reply
+const Like = db.Like
 
 const userController = {
   getUser: (req, res) => {
@@ -10,23 +10,21 @@ const userController = {
       include: [
         { model: Tweet, include: [Reply] },
         { model: Tweet, include: [Like] },
-        { model: User, as: "Followers" },
-        { model: User, as: "Followings" },
-      ],
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ]
     }).then((user) => {
-      let results = user.toJSON();
-      results["followingCount"] = results.Followings.length;
-      results["followerCount"] = results.Followers.length;
-      console.log(results["Tweets"][0]["Replies"].length);
+      const results = user.toJSON()
+      results.followingCount = results.Followings.length
+      results.followerCount = results.Followers.length
+      console.log(results.Tweets[0].Replies.length)
 
-      for (i = 0; i < results["Tweets"].length; i++) {
-        results["Tweets"][i]["repliesCount"] =
-          results["Tweets"][i]["Replies"].length;
-        results["Tweets"][i]["likeCount"] =
-          results["Tweets"][i]["Likes"].length;
+      for (let i = 0; i < results.Tweets.length; i++) {
+        results.Tweets[i].repliesCount = results.Tweets[i].Replies.length
+        results.Tweets[i].likeCount = results.Tweets[i].Likes.length
       }
 
-      return res.json(results);
+      return res.json(results)
     })
   },
   userSigninPage: (req, res) => {
