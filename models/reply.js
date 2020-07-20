39ce3321122a160a04d193e2174d35d504d02b9c
Marc <g40419@gmail.com>
2020-07-20 14:59:09 +0800

    建立user & reply 的關聯

--- models/reply.js
+++ models/reply.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Reply.associate = function(models) {
     Reply.belongsTo(models.Tweet)
+    Reply.belongsTo(models.User)
   };
   return Reply;
 };
--- models/user.js
+++ models/user.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   User.associate = function(models) {
     User.hasMany(models.Tweet)
+    User.hasMany(models.Reply)
   };
   return User;
 };
