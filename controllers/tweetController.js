2a1a3d4d9875967ec18d76689cdcc961edf58401
Marc <g40419@gmail.com>
2020-07-20 17:36:04 +0800

    更動seeder 加account 欄位，完成getTweet controller

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,5 +1,8 @@
 const db = require('../models')
 const Tweet = db.Tweet
+const User = db.User
+const Reply = db.Reply
+const Like = db.Like
 
 const tweetController = {
     getTweets: (req, res) => {
@@ -7,9 +10,25 @@ const tweetController = {
     },
     getTweet: async (req, res) => {
       const id = req.params.id
-      const tweet = await Tweet.findOne({id})
-      console.log(tweet)
-      res.render('tweet')
+      const tweet = await Tweet.findOne({
+        where: { id },
+        include: [
+          User, 
+          {model: Reply, include: [User]}
+        ]
+      })
+
+      const totalLike = await Like.count({
+        where: { UserId: id }
+      })
+      const totalComment = tweet.toJSON().Replies.length
+
+      const totalCount = {
+        totalLike, totalComment
+      }
+
+      console.log(totalLike, totalComment)
+      res.render('tweet',{ tweet: tweet.toJSON(), totalCount })
     }
   }
   module.exports = tweetController
