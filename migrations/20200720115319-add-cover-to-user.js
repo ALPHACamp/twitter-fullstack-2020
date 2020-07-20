6b7e34ee8a26abb45000a7f6e18c15ed25c781ae
carey <schiafang@gmail.com>
2020-07-20 22:09:22 +0800

    modify: change signin feature, user can use email or account to sign in

--- migrations/20200720115319-add-cover-to-user.js
+++ migrations/20200720115319-add-cover-to-user.js
@@ -3,11 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('users', 'cover', {
-<<<<<<< HEAD
-      type: Sequelize.STRING
-=======
       type: Sequelize.STRING,
->>>>>>> 2de11848b17de0e41c8b66f3d22984ad02dfe5dd
     })
   },
 
