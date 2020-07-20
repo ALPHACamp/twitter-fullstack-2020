9955a742f443c89dea21adde07a3eb2372c23b3a
leey611 <leey611@newschool.edu>
2020-07-20 11:29:15 -0400

    feat: userController.signup and view

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,71 @@
+const db = require('../models');
+const User = db.User;
+const bcrypt = require('bcryptjs');
+
 let userController = {
   loginPage: (req, res) => {
     return res.render('login');
+  },
+  signUpPage: (req, res) => {
+    return res.render('signup');
+  },
+  signup: (req, res) => {
+    //password and confirmPassword must be the same
+    if (req.body.password !== req.body.confirmPassword) {
+      return res.render('signup', {
+        error_messages: 'Please check your confirm password',
+        name: req.body.name,
+        account: req.body.account,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+      });
+    }
+    //check if account name has been used
+    User.findOne({ where: { account: req.body.account } }).then((user) => {
+      // if the account name has been used
+      if (user) {
+        return res.render('signup', {
+          error_messages: 'This account name is not available',
+          name: req.body.name,
+          account: req.body.account,
+          email: req.body.email,
+          password: req.body.password,
+          confirmPassword: req.body.confirmPassword
+        });
+      } else {
+        // the account name is available, check if email has been used
+        User.findOne({ where: { email: req.body.email } }).then((user) => {
+          if (user) {
+            return res.render('signup', {
+              error_messages: 'This email has been registered',
+              name: req.body.name,
+              account: req.body.account,
+              email: req.body.email,
+              password: req.body.password,
+              confirmPassword: req.body.confirmPassword
+            });
+          }
+          User.create({
+            name: req.body.name,
+            account: req.body.account,
+            email: req.body.email,
+            password: bcrypt.hashSync(
+              req.body.password,
+              bcrypt.genSaltSync(10, null)
+            ),
+            avatar: 'https://i.imgur.com/73A1th4.png',
+            role: 'user'
+          })
+            .then((user) => {
+              req.flash('success_messages', 'Signup successfully');
+              return res.redirect('/login');
+            })
+            .catch((err) => console.log(err));
+        });
+      }
+    })
+ 
   }
 };
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const tweetController = require('../controllers/tweetController')
-const adminController = require('../controllers/adminController.js')
+const tweetController = require('../controllers/tweetController');
+const adminController = require('../controllers/adminController.js');
 
+router.get('/', (req, res) => res.redirect('/tweets'));
 
-router.get('/', (req, res) => res.redirect('/tweets'))
-
-router.get('/tweets', tweetController.getTweets)
-router.get('/tweets/:id', tweetController.getTweet)
-
+router.get('/tweets', tweetController.getTweets);
+router.get('/tweets/:id', tweetController.getTweet);
+router.get('/admin/login', adminController.adminLoginPage);
 router.get('/login', userController.loginPage);
+router.get('/signup', userController.signUpPage);
+router.post('/signup', userController.signup);
+router.get('/admin', (req, res) => res.redirect('/admin/tweets'));
 
+router.get('/admin/tweets', adminController.getTweets);
 
-router.get('/admin', (req, res) => res.redirect('/admin/tweets'))
-
-router.get('/admin/tweets', adminController.getTweets)
-
-router.delete('/admin/tweets/:id', adminController.deleteTweet)
+router.delete('/admin/tweets/:id', adminController.deleteTweet);
 
 module.exports = router;
