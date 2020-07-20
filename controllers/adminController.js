3931b23220d18468f97d3604ee3106650e6faa07
鄭又誠 <zxcy9980@gmail.com>
2020-07-20 15:56:30 +0800

    後台首頁刪除功能完成

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,17 +7,29 @@ let adminController = {
   adminLoginPage: (req, res) => {
     return res.render('admin/login');
   },
+
   getTweets: (req, res) => {
-   Tweet.findAll({ raw: true, nest: true, include: User, }).then(tweets => {          
-      const data = tweets.map(r => ({               
+    Tweet.findAll({ raw: true, nest: true, include: User, }).then(tweets => {
+      const data = tweets.map(r => ({
+        ...r,
         userName: r.User.name,
         userAvatar: r.User.avatar,
         description: r.description.substring(0, 50),
         createdAt: r.createdAt
-      }))
-      console.log(data)
+      }))      
       return res.render('admin/tweetsHome', { tweets: data })
     })
+  },
+
+  deleteTweet: (req, res) => {
+    Tweet.findByPk(req.params.id)
+      .then((Tweet) => {
+        Tweet.destroy()
+          .then((Tweet) => {
+            req.flash('success_messages', '刪除成功！')
+            return res.redirect('/admin/tweets')
+          })
+      })
   },
 }
 
