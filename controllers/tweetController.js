2da2940c4cb24b80a553122d5ad04696f2dfa0b5
Marc <g40419@gmail.com>
2020-07-20 14:39:42 +0800

    test

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,6 +1,15 @@
+const db = require('../models')
+const Tweet = db.Tweet
+
 const tweetController = {
     getTweets: (req, res) => {
       return res.render('tweetsHome')
+    },
+    getTweet: async (req, res) => {
+      const id = req.params.id
+      const tweet = await Tweet.findOne({id})
+      console.log(tweet)
+      res.render('tweet')
     }
   }
   module.exports = tweetController
