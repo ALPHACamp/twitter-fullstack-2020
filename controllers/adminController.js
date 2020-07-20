a627e4ad5dece413025d406a7deac758ca419be0
Marc <g40419@gmail.com>
2020-07-20 14:09:17 +0800

    將models 加入初始值

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,7 +2,6 @@ let adminController = {
   adminLoginPage: (req, res) => {
     return res.render('admin/login');
   },
-
   getTweets: (req, res) => {
     return res.render('admin/tweetsHome')
   }
