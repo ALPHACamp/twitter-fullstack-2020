1030ecb0b8943169e11fae627bb35246aa665906
Robert <robert913152750@gmail.com>
2020-07-20 15:58:35 +0800

    fix getUser bug (very strange problem..)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,61 +1,38 @@
-<<<<<<< HEAD
-=======
 const bcrypt = require('bcryptjs')
 const { Sequelize } = require('../models')
 const { or } = Sequelize.Op
-
->>>>>>> d062623133ff0dd1fdda43ef9bf196f316af7f7f
 const db = require('../models')
 const User = db.User
 const Tweet = db.Tweet
 const Reply = db.Reply
-<<<<<<< HEAD
-const Like = db.Like
-=======
 const Followship = db.Followship
->>>>>>> d062623133ff0dd1fdda43ef9bf196f316af7f7f
 
 const userController = {
   getUser: (req, res) => {
     return User.findByPk(req.params.id, {
       include: [
-<<<<<<< HEAD
-        { model: Tweet, include: [Reply] },
-        { model: Tweet, include: [Like] },
-=======
         {
           model: Tweet,
-          include: { model: User, as: 'LikedUser' },
+          include: { model: User, as: 'LikedUser' }
         },
         { model: Tweet, include: Reply },
->>>>>>> d062623133ff0dd1fdda43ef9bf196f316af7f7f
         { model: User, as: 'Followers' },
         { model: User, as: 'Followings' }
       ]
-    }).then((user) => {
-      const results = user.toJSON()
-      results.followingCount = results.Followings.length
-      results.followerCount = results.Followers.length
-<<<<<<< HEAD
-      console.log(results.Tweets[0].Replies.length)
-
-      for (let i = 0; i < results.Tweets.length; i++) {
-        results.Tweets[i].repliesCount = results.Tweets[i].Replies.length
-        results.Tweets[i].likeCount = results.Tweets[i].Likes.length
-      }
-
-=======
-      console.log(results['Tweets'][0]['Replies'].length)
-
-      for (let i = 0; i < results['Tweets'].length; i++) {
-        results['Tweets'][i]['repliesCount'] =
-          results['Tweets'][i]['Replies'].length
-        results['Tweets'][i]['likeCount'] =
-          results['Tweets'][i]['Likes'].length
-      }
->>>>>>> d062623133ff0dd1fdda43ef9bf196f316af7f7f
-      return res.json(results)
     })
+      .then((user) => {
+        const results = user.toJSON()
+        results.followingCount = results.Followings.length
+        results.followerCount = results.Followers.length
+
+        for (let i = 0; i < results.Tweets.length; i++) {
+          results.Tweets[i].repliesCount = results.Tweets[i].Replies.length
+          results.Tweets[i].likeCount = results.Tweets[i].LikedUser.length
+        }
+
+        return res.json(results)
+      })
+      .catch((err) => res.send(err))
   },
   addFollowing: (req, res) => {
     const userId = req.params.userId
@@ -64,15 +41,16 @@ const userController = {
       followingId: userId
     })
       .then(() => res.redirect('back'))
-      .catch(err => res.send(err))
+      .catch((err) => res.send(err))
   },
   removeFollowing: (req, res) => {
-    return Followship.findOne({ where: { followerId: req.user.id, followingId: req.params.userId } })
-      .then(followship => {
-        followship.destroy()
-          .then(() => res.redirect('back'))
+    return Followship.findOne({
+      where: { followerId: req.user.id, followingId: req.params.userId }
+    })
+      .then((followship) => {
+        followship.destroy().then(() => res.redirect('back'))
       })
-      .catch(err => res.send(err))
+      .catch((err) => res.send(err))
   },
   userSigninPage: (req, res) => {
     res.render('userSigninPage')
@@ -99,18 +77,24 @@ const userController = {
     // 必填檢查
     if (!account || !name || !email || !password || !checkPassword) {
       return res.render('userSignupPage', {
-        account, name, email, error_messages: '別偷懶~全部欄位均為必填呦！'
+        account,
+        name,
+        email,
+        error_messages: '別偷懶~全部欄位均為必填呦！'
       }) // 密碼因安全性問題，要重新填寫
     }
     // 密碼 & 確認密碼檢查
     if (password !== checkPassword) {
       return res.render('userSignupPage', {
-        account, name, email, error_messages: '密碼與確認密碼不符，請重新確認！'
+        account,
+        name,
+        email,
+        error_messages: '密碼與確認密碼不符，請重新確認！'
       })
     }
     // 檢查 account & email 是否為唯一值
     User.findOne({ where: { [or]: { account, email } }, raw: true })
-      .then(user => {
+      .then((user) => {
         if (!user) {
           return User.create({
             account,
@@ -123,20 +107,26 @@ const userController = {
             role: 'user'
           })
             .then(() => res.redirect('/signin'))
-            .catch(err => res.send(err))
+            .catch((err) => res.send(err))
         }
         if (user.account === account) {
           return res.render('userSignupPage', {
-            account, name, email, error_messages: '帳號已存在，請更改成其他帳號！'
+            account,
+            name,
+            email,
+            error_messages: '帳號已存在，請更改成其他帳號！'
           })
         }
         if (user.email === email) {
           return res.render('userSignupPage', {
-            account, name, email, error_messages: 'Email已存在，請更改成其他Email！'
+            account,
+            name,
+            email,
+            error_messages: 'Email已存在，請更改成其他Email！'
           })
         }
       })
-      .catch(err => res.send(err))
+      .catch((err) => res.send(err))
   },
   accountSettingPage: (req, res) => {
     res.render('accountSettingPage')
