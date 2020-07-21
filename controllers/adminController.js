068bb0355d0e8e62eb47e183abf3daec32a83b26
carey <schiafang@gmail.com>
2020-07-21 14:41:14 +0800

    count user associate data

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,11 +1,28 @@
 const db = require('../models')
 const User = db.User
+const Reply = db.Reply
+const Tweet = db.Tweet
+const Like = db.Like
 
 const adminController = {
   getUsers: (req, res) => {
-    return User.findAll({ raw: true, order: [['createdAt', 'DESC']] })
+    return User.findAll({
+      include: [
+        { model: User, as: 'Followings' },
+        { model: User, as: 'Followers' },
+        { model: Reply, include: Tweet },
+        // { model: Like, include: Tweet }
+      ],
+      order: [['createdAt', 'DESC']],
+      raw: true,
+      nest: true
+    })
       .then(users => {
-        res.render('admin/users', { users })
+        const followersCount = users.Followers.length
+        const followingsCount = users.Followings.length
+        const repliesCount = users.Replies.length
+
+        res.render('admin/users', { users, followersCount, followingsCount, repliesCount })
       })
   }
 }
