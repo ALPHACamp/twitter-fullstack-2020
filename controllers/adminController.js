90b7d7e025094de0bce7184d9a978eefc3cce999
carey <schiafang@gmail.com>
2020-07-21 19:45:45 +0800

    modify: admin users sort by number of tweets

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,21 +8,22 @@ const adminController = {
   getUsers: (req, res) => {
     return User.findAll({
       include: [
+        Tweet,
         { model: User, as: 'Followings' },
         { model: User, as: 'Followers' },
         { model: Reply, include: Tweet },
         { model: Like, include: Tweet }
-      ],
-      order: [['createdAt', 'DESC']],
+      ]
     })
       .then(result => {
-        const users = result.map(item => ({
+        const data = result.map(item => ({
           ...item.dataValues,
           followingsCount: item.Followings.length,
           followersCount: item.Followers.length,
           likesCount: item.Likes.length,
           repliesCount: item.Replies.length
         }))
+        const users = data.sort((a, b) => b.Tweets.length - a.Tweets.length)
 
         res.render('admin/users', { users })
       })
