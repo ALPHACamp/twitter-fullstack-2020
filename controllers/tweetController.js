830773bf9cdf02f903ed7fbc99424adbb1b252ec
鄭又誠 <zxcy9980@gmail.com>
2020-07-22 11:19:34 +0800

    排序功能

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -7,47 +7,47 @@ const Like = db.Like
 const tweetController = {
   getTweets: async (req, res) => {
     let tweets = await Tweet.findAll({
-       include: [
+      order: [['createdAt', 'DESC']],
+      include: [
         User,
         { model: User, as: 'TweetWhoLike' },
         { model: User, as: 'whoReply' },
       ]
     })
-    
-    
-       data = tweets.map( r => ({
-        ...r.dataValues,
-        userId:r.User.id,
-        userName: r.User.name,
-        userAvatar: r.User.avatar,
-        userAccount: r.User.account,
-        description: r.description,
-        createdA: r.createdAt,
-        likeCount:r.TweetWhoLike.length,
-        replayCount:r.whoReply.length,
-      }))
-      console.log(data[1])
-      return res.render('tweetsHome', { tweets: data })
-    
+
+    data = tweets.map(r => ({
+      ...r.dataValues,
+      userId: r.User.id,
+      userName: r.User.name,
+      userAvatar: r.User.avatar,
+      userAccount: r.User.account,
+      description: r.description,
+      createdA: r.createdAt,
+      likeCount: r.TweetWhoLike.length,
+      replayCount: r.whoReply.length,
+    }))
+    console.log(data[1])
+    return res.render('tweetsHome', { tweets: data })
+
   },
-    getTweet: async (req, res) => {
-      const id = req.params.id
-      const tweet = await Tweet.findOne({
-        where: { id },
-        include: [
-          User, 
-          { model: User, as: 'whoReply'}
-        ]
-      })
-      const totalLike = await Like.count({
-        where: { UserId: id }
-      })
-      
-      const totalComment = tweet.toJSON().whoReply.length
-      const totalCount = {
-        totalLike, totalComment
-      }
-      res.render('tweet',{ tweet: tweet.toJSON(), totalCount })
-    }   
+  getTweet: async (req, res) => {
+    const id = req.params.id
+    const tweet = await Tweet.findOne({
+      where: { id },
+      include: [
+        User,
+        { model: User, as: 'whoReply' }
+      ]
+    })
+    const totalLike = await Like.count({
+      where: { UserId: id }
+    })
+
+    const totalComment = tweet.toJSON().whoReply.length
+    const totalCount = {
+      totalLike, totalComment
+    }
+    res.render('tweet', { tweet: tweet.toJSON(), totalCount })
+  }
 }
 module.exports = tweetController
