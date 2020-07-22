7edea2308181e9acd0b13e4eaf7385d3bc313495
Robert <robert913152750@gmail.com>
2020-07-22 13:35:16 +0800

    Add follwerCount and followingCount to User model and updata addFollowing and removeFollowing deature

--- migrations/20200721161601-add-replyCount-to-tweets.js
+++ migrations/20200721161601-add-replyCount-to-tweets.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Tweets', 'replyCount', {
-      type: Sequelize.INTEGER,
+      type: Sequelize.INTEGER
     })
   },
 
   down: async (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Tweets', 'replyCount')
   }
-};
+}
