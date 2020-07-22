8e5e66e5a1010a66f489859b4e7500affd679901
carey <schiafang@gmail.com>
2020-07-22 13:45:25 +0800

    fix: adminController mistake

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,7 +8,8 @@ const adminController = {
   signInPage: (req, res) => res.render('admin/signin'),
   signIn: (req, res) => {
     req.flash('successMessages', '登入成功')
-    res.redirect('/admin/tweets'),
+    res.redirect('/admin/tweets')
+  },
   getUsers: (req, res) => {
     return User.findAll({
       include: [
