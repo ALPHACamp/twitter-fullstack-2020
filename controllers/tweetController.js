62fad4b2ea07b536310e33c2d9d95144ca1b70b6
Robert <robert913152750@gmail.com>
2020-07-22 15:56:11 +0800

    merge with develop

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,4 +1,4 @@
-const db = require("../models")
+const db = require('../models')
 const Tweet = db.Tweet
 const User = db.User
 const Reply = db.Reply
@@ -7,30 +7,30 @@ const tweetController = {
   getHomePage: (req, res) => {
     // 推薦追蹤名單 (follower人數top6)
     User.findAll({
-      include: [{ model: User, as: "Followers" }],
+      include: [{ model: User, as: 'Followers' }]
     }).then((users) => {
       users = users.map((user) => ({
         ...user.dataValues,
         followerCount: user.Followers.length,
-        isFollowed: user.Followers.map((er) => er.id).includes(req.user.id),
+        isFollowed: user.Followers.map((er) => er.id).includes(req.user.id)
       }))
       // 去除掉自己
-      users = users.filter(user => user.id !== req.user.id)
+      users = users.filter((user) => user.id !== req.user.id)
       users = users
         .sort((a, b) => b.followerCount - a.followerCount)
         .slice(0, 6)
       // 顯示已追蹤人的tweets
-      let followings = req.user.Followings.map((user) => user.id)
+      const followings = req.user.Followings.map((user) => user.id)
       followings.push(req.user.id)
       Tweet.findAll({
         where: { userId: followings },
         raw: true,
         nest: true,
         include: [User],
-        order: [["createdAt", "DESC"]],
+        order: [['createdAt', 'DESC']]
       })
         .then((tweets) => {
-          res.render("home", { tweets: tweets, recommendFollowings: users })
+          res.render('home', { tweets: tweets, recommendFollowings: users })
         })
         .catch((err) => res.send(err))
     })
@@ -43,7 +43,7 @@ const tweetController = {
     Tweet.findByPk(tweetId, {
       raw: true,
       nest: true,
-      include: [User, Reply],
+      include: [User, Reply]
     })
       .then((tweet) => {
         Reply.findAll({
@@ -51,16 +51,16 @@ const tweetController = {
           raw: true,
           nest: true,
           include: [User],
-          order: [["createdAt", "ASC"]],
+          order: [['createdAt', 'ASC']]
         }).then((replies) => {
-          res.render("reply", { tweet, replies })
+          res.render('reply', { tweet, replies })
         })
       })
       .catch((err) => res.send(err))
   },
   replyTweet: (req, res) => {
     res.send(req.body)
-  },
+  }
 }
 
 module.exports = tweetController
