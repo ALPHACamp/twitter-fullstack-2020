936f14b7ef1580c457f14c965c2d8a7e6a0af020
wei <fhaowei@yahoo.com.tw>
2020-07-26 01:05:26 +0800

    add get Top user feature on profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,9 +49,30 @@ const userController = {
   },
 
   getUser: (req, res) => {
-    return User.findByPk(req.params.id)
+    return User.findByPk(req.params.id, {
+      include: [
+        { model: User, as: 'Followings' }
+      ]
+    })
       .then(user => {
-        return res.render('profile', { user: user.toJSON() })
+        return User.findAll({
+          include: [
+            { model: User, as: 'Followers' }
+          ]
+        }).then(users => {
+          // 整理 users 資料
+          users = users.map(user => ({
+            ...user.dataValues,
+            //計算追蹤者人數
+            FollowerCount: user.Followers.length,
+            // // 判斷目前登入使用者是否已追蹤該 User 物件, passport.js加入 followship以取得req.user.Followings
+            isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
+          }))
+          console.log('users===>', users)
+          // 依追蹤者人數排序清單
+          users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
+          return res.render('profile', { user: user.toJSON(), users: users })
+        })
       })
   },
 
