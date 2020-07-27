66e1f753d7797d15522efbda1f32ed3b45ef8ed3
carey <schiafang@gmail.com>
2020-07-27 11:25:08 +0800

    refactor: 1. exchange putUserProfile and putUser name 2. modify likedUsers to LikedUsers 3. some layout

--- models/tweet.js
+++ models/tweet.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     Tweet.belongsToMany(models.User, {
       through: models.Like,
       foreignKey: 'TweetId',
-      as: 'likedUsers'
+      as: 'LikedUsers'
     })
     Tweet.hasMany(models.Like)
   };
