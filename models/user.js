425da0dc0e246ea3131f486467864efb2e7c0002
wei <fhaowei@yahoo.com.tw>
2020-07-28 23:15:01 +0800

    modify models files to fit test spec

--- models/user.js
+++ models/user.js
@@ -1,29 +1,60 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-
-    }
-  };
-  User.init({
+  const User = sequelize.define('User', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    role: DataTypes.BOOLEAN,
+    role: DataTypes.STRING,
     avatar: DataTypes.STRING,
     introduction: DataTypes.STRING,
     cover: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  }, {});
+  User.associate = function (models) {
+    User.hasMany(models.Reply)
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Like)
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
+  };
   return User;
 };
+
+
+
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+
+//     }
+//   };
+//   User.init({
+//     name: DataTypes.STRING,
+//     email: DataTypes.STRING,
+//     password: DataTypes.STRING,
+//     role: DataTypes.BOOLEAN,
+//     avatar: DataTypes.STRING,
+//     introduction: DataTypes.STRING,
+//     cover: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'User',
+//   });
+//   return User;
+// };
