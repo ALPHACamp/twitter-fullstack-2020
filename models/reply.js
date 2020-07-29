954db0e177c5d07a1fcc2cbb8ed32aff85990b6d
YenLai <b540915@gmail.com>
2020-07-29 13:48:28 +0800

    feat: modify model init to model define

--- models/reply.js
+++ models/reply.js
@@ -1,35 +1,62 @@
 'use strict'
-const { Model } = require('sequelize')
-module.exports = (sequelize, DataTypes) => {
-  class Reply extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate (models) {
-      // define association here
-      Reply.belongsTo(models.Tweet)
-      Reply.belongsTo(models.User)
-      Reply.hasMany(models.Secondreply)
-      Reply.belongsToMany(models.User, {
-        through: models.Like,
-        foreignKey: 'ReplyId',
-        as: 'LikedUsers'
-      })
-    }
-  }
-  Reply.init(
+const model = (sequelize, DataTypes) => {
+  const Reply = sequelize.define(
+    'Reply',
     {
       UserId: DataTypes.INTEGER,
       TweetId: DataTypes.INTEGER,
       comment: DataTypes.TEXT,
       likeCount: DataTypes.INTEGER
-    },
-    {
-      sequelize,
-      modelName: 'Reply'
     }
   )
+  Reply.associate = (models) => {
+    Reply.belongsTo(models.User)
+    Reply.belongsTo(models.Tweet)
+    Reply.hasMany(models.Secondreply)
+    Reply.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'ReplyId',
+      as: 'LikedUsers'
+    })
+  }
+
   return Reply
 }
+
+module.exports = model
+
+// ----- Model init ----
+// const { Model } = require('sequelize')
+// module.exports = (sequelize, DataTypes) => {
+//   class Reply extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate (models) {
+//       // define association here
+//       Reply.belongsTo(models.Tweet)
+//       Reply.belongsTo(models.User)
+//       Reply.hasMany(models.Secondreply)
+//       Reply.belongsToMany(models.User, {
+//         through: models.Like,
+//         foreignKey: 'ReplyId',
+//         as: 'LikedUsers'
+//       })
+//     }
+//   }
+//   Reply.init(
+//     {
+//       UserId: DataTypes.INTEGER,
+//       TweetId: DataTypes.INTEGER,
+//       comment: DataTypes.TEXT,
+//       likeCount: DataTypes.INTEGER
+//     },
+//     {
+//       sequelize,
+//       modelName: 'Reply'
+//     }
+//   )
+//   return Reply
+// }
--- test/models/Reply.spec.js
+++ test/models/Reply.spec.js
@@ -1,49 +1,46 @@
-const { expect } = require('chai')
-const { spy } = require('sinon')
-const proxyquire = require('proxyquire')
-const { sequelize, Sequelize } = require('sequelize-test-helpers')
-const chai = require('chai')
-const sinonChai = require('sinon-chai')
-const db = require('../../models')
-chai.use(sinonChai)
+var chai = require('chai')
+var sinon = require('sinon')
+chai.use(require('sinon-chai'))
 
-describe('../../models/Reply', () => {
-  const { DataTypes } = Sequelize
-  const ReplyFactory = proxyquire('../../models/Reply', {
-    sequelize: Sequelize
+const { expect } = require('chai')
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkUniqueIndex,
+  checkPropertyExists
+} = require('sequelize-test-helpers')
+
+const db = require('../../models')
+const ReplyModel = require('../../models/reply')
+
+describe('# Reply Model', () => {
+  before(done => {
+    done()
   })
-  let Reply
-  console.log(ReplyFactory(sequelize, DataTypes).associate)
-  before(() => {
-    Reply = ReplyFactory(sequelize, DataTypes)
-  })
-  after(() => {
-    Reply.init.resetHistory()
-  })
+
+  const Reply = ReplyModel(sequelize, dataTypes)
+  const like = new Reply()
+  checkModelName(Reply)('Reply')
 
   context('properties', () => {
-    it('called Followship.init with the correct parameters', () => {
-      expect(Reply.init).to.have.been.calledWith(
-        {
-          UserId: DataTypes.INTEGER,
-          TweetId: DataTypes.INTEGER,
-          comment: DataTypes.TEXT,
-          likeCount: DataTypes.INTEGER
-        },
-        {
-          sequelize,
-          modelName: 'Reply'
-        }
-      )
-    })
+    ;[
+    ].forEach(checkPropertyExists(like))
   })
-  context('associations', () => {
-    before(() => {
-      Reply.associate(db.User)
-    })
 
+  context('associations', () => {
+    const User = 'User'
+    const Tweet = 'Tweet'
+    before(() => {
+      Reply.associate({ User })
+      Reply.associate({ Tweet })
+    })
     it('should belong to user', (done) => {
-      expect(Reply.belongsTo).to.have.been.calledWith(db.User)
+      expect(Reply.belongsTo).to.have.been.calledWith(User)
+      done()
+    })
+    it('should belong to tweet', (done) => {
+      expect(Reply.belongsTo).to.have.been.calledWith(Tweet)
       done()
     })
   })
@@ -82,57 +79,65 @@ describe('../../models/Reply', () => {
   })
 })
 
-// var chai = require('chai');
-// var sinon = require('sinon');
-// chai.use(require('sinon-chai'));
-
+// -----Testing for Model.init----- //
 // const { expect } = require('chai')
-// const {
-//   sequelize,
-//   dataTypes,
-//   checkModelName,
-//   checkUniqueIndex,
-//   checkPropertyExists
-// } = require('sequelize-test-helpers')
-
+// const { spy } = require('sinon')
+// const proxyquire = require('proxyquire')
+// const { sequelize, Sequelize } = require('sequelize-test-helpers')
+// const chai = require('chai')
+// const sinonChai = require('sinon-chai')
 // const db = require('../../models')
-// const ReplyModel = require('../../models/reply')
+// const { Model } = require('sequelize/types')
+// chai.use(sinonChai)
 
-// describe('# Reply Model', () => {
-
-//   before(done => {
-//     done()
+// describe('../../models/Reply', () => {
+//   const { DataTypes } = Sequelize
+//   const ReplyFactory = proxyquire('../../models/Reply', {
+//     sequelize: Sequelize
 //   })
-
-//   const Reply = ReplyModel(sequelize, dataTypes)
-//   const like = new Reply()
-//   checkModelName(Reply)('Reply')
+//   let Reply
+//   before(() => {
+//     Reply = ReplyFactory(sequelize, DataTypes)
+//   })
+//   after(() => {
+//     Reply.init.resetHistory()
+//   })
 
 //   context('properties', () => {
-//     ;[
-//     ].forEach(checkPropertyExists(like))
+//     it('called Followship.init with the correct parameters', () => {
+//       expect(Reply.init).to.have.been.calledWith(
+//         {
+//           UserId: DataTypes.INTEGER,
+//           TweetId: DataTypes.INTEGER,
+//           comment: DataTypes.TEXT,
+//           likeCount: DataTypes.INTEGER
+//         },
+//         {
+//           sequelize,
+//           modelName: 'Reply'
+//         }
+//       )
+//     })
 //   })
-
 //   context('associations', () => {
 //     const User = 'User'
 //     const Tweet = 'Tweet'
 //     before(() => {
-//       Reply.associate({ User })
-//       Reply.associate({ Tweet })
+//       Reply.associate(User)
+//       Reply.associate(Tweet)
 //     })
 
 //     it('should belong to user', (done) => {
 //       expect(Reply.belongsTo).to.have.been.calledWith(User)
 //       done()
 //     })
-//     it('should belong to tweet', (done) => {
+//     it('should belong to user', (done) => {
 //       expect(Reply.belongsTo).to.have.been.calledWith(Tweet)
 //       done()
 //     })
 //   })
 
 //   context('action', () => {
-
 //     let data = null
 
 //     it('create', (done) => {
@@ -144,11 +149,11 @@ describe('../../models/Reply', () => {
 //     it('read', (done) => {
 //       db.Reply.findByPk(data.id).then((reply) => {
 //         expect(data.id).to.be.equal(reply.id)
-//           done()
-//         })
+//         done()
+//       })
 //     })
 //     it('update', (done) => {
-//       db.Reply.update({}, { where: { id: data.id }}).then(() => {
+//       db.Reply.update({}, { where: { id: data.id } }).then(() => {
 //         db.Reply.findByPk(data.id).then((reply) => {
 //           expect(data.updatedAt).to.be.not.equal(reply.updatedAt)
 //           done()
@@ -156,7 +161,7 @@ describe('../../models/Reply', () => {
 //       })
 //     })
 //     it('delete', (done) => {
-//       db.Reply.destroy({ where: { id: data.id }}).then(() => {
+//       db.Reply.destroy({ where: { id: data.id } }).then(() => {
 //         db.Reply.findByPk(data.id).then((reply) => {
 //           expect(reply).to.be.equal(null)
 //           done()
