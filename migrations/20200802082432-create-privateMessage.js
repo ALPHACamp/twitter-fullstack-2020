3e760ed296c072d9bf550c23868c86adba111bbe
Wanglala <sue4100035045@gmail.com>
2020-08-02 16:34:01 +0800

    feat: create privateMessage model

---
+++ migrations/20200802082432-create-privateMessage.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('PrivateMessages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      message: {
+        type: Sequelize.TEXT
+      },
+      senderId: {
+        type: Sequelize.INTEGER
+      },
+      receiverId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('PrivateMessages');
+  }
+};
