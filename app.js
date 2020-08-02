b5ec3dd96b97fe12e7170a6266919f555906f4ef
Marc <g40419@gmail.com>
2020-08-02 15:05:47 +0800

    process.env

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 } 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
