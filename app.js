3a4b5f924b51b5056853f4d66fd82eda896326d2
Corey <simon8888xsimon8888x@gmail.com>
2020-09-24 23:37:04 +0800

    dqwd

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
-console.log('test 777')
+console.log('test 777 8888')
