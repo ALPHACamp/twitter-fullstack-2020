718d99b45e4b23518a5369f7b2511baa30c995b6
Corey <simon8888xsimon8888x@gmail.com>
2020-09-26 17:20:26 +0800

    feat: add icon

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const helpers = require('./_helpers');
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: "main", extname: "hbs" }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
+app.use('/', (req, res) => {
+  res.render('test')
+})
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
