57254b6932310da50a9d6f331bef6e13af89b9a8
emily40830 <emily40830@gmail.com>
2020-09-26 21:14:32 +0800

    feat:Add user model

--- models/user.js
+++ models/user.js
@@ -1,12 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-  }, {});
-  User.associate = function(models) {
-    User.hasMany(models.Reply)
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      account: DataTypes.STRING,
+      cover: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+    },
+    {},
+  );
+  User.associate = function (models) {
+    User.hasMany(models.Reply);
   };
   return User;
 };
