761609b3d7ff3ac4439984ec53bbe7b608252eac
Josh Fang <joshuafang13@gmail.com>
2020-09-28 22:18:32 +0800

    define all model relationships

--- models/tweet.js
+++ models/tweet.js
@@ -4,6 +4,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Tweet.associate = function (models) {
     Tweet.belongsTo(models.User)
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
   };
   return Tweet;
 };
