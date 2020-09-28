6b5a079df3a0c00499aab81b9b52ac58471b72d2
MIchael Huang <michaelnctu@gmail.com>
2020-09-28 13:38:52 +0800

    signup updated

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ const db = require('./models')
 const app = express()
 const port = 3000
 
+const flash = require('connect-flash')
+const session = require('express-session')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
+
+// 把 req.locals
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
--- controllers/userController.js
+++ controllers/userController.js
@@ -6,15 +6,33 @@ const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
   },
+
   signUp: (req, res) => {
-    return User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))  //salt = bcrypt.genSaltSync(10)
-    }).then(user => {
-      return res.redirect('/signin')
-    })
+
+    if (!req.body.password === req.body.passwordCheck) {
+      req.flash('error_messages', '兩次密碼輸入不同')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user) {
+          return User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))  //salt = bcrypt.genSaltSync(10)
+          }).then(user => {
+            req.flash('success_messages', '成功註冊帳號!')
+            return res.redirect('/signin')
+          })
+        }
+      })
+    }
+  },
+
+  signIn: (req, res) => {
+    return res.render('signin')
   }
+
+
 }
 
 module.exports = userController
