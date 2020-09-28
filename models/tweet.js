64287e12eafb28575574a8a136dc944ebf66d1ac
icedike <icedike@gmail.com>
2020-09-29 02:10:53 +0800

    Create attributes and relationships of models

--- models/tweet.js
+++ models/tweet.js
@@ -2,7 +2,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
   }, {});
-  Tweet.associate = function(models) {
+  Tweet.associate = function (models) {
+    Tweet.belongsTo(models.User)
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
   };
   return Tweet;
 };
