68ec0f21bd4a512bc251280df3075c6f494341ff
Calvin Sung <calvinsungvin@gmail.com>
2020-09-29 16:38:43 +0800

    set up adminController + set up main & signin hbs

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const helpers = require('./_helpers');
-
+const handlebars = require('express-handlebars')
+const db = require('./models')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'})) 
+app.set('view engine', 'handlebars') 
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
