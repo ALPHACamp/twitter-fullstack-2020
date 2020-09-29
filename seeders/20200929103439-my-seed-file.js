2d71198c09114dc6a4686067132a2eda6d367052
Kay <kahachang@gmail.com>
2020-09-30 00:09:25 +0800

    add avatar, cover, interduction to user seeder

--- seeders/20200929103439-my-seed-file.js
+++ seeders/20200929103439-my-seed-file.js
@@ -19,8 +19,9 @@ module.exports = {
       role: 'user',
       name: 'user1',
       account: '@user1',
-      avatar: 'https://pickaface.net/gallery/avatar/unr_fake_190524_1549_9fgji7.png',
-      cover: 'https://images.alphacoders.com/689/689223.jpg',
+      avatar: faker.image.avatar(),
+      cover: faker.image.nature(),
+      introduction: faker.lorem.text(),
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -29,8 +30,9 @@ module.exports = {
       role: 'user',
       name: 'user2',
       account: '@user2',
-      avatar: 'https://pickaface.net/gallery/avatar/20160625_050020_889_FAKE.png',
-      cover: 'https://mcdn.wallpapersafari.com/medium/7/92/bmNBFo.jpg',
+      avatar: faker.image.avatar(),
+      cover: faker.image.nature(),
+      introduction: faker.lorem.text(),
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -39,8 +41,9 @@ module.exports = {
       role: 'user',
       name: 'user3',
       account: '@user3',
-      avatar: 'https://pickaface.net/gallery/avatar/Charlie123527d772fa51a7.png',
-      cover: 'https://images2.alphacoders.com/719/719339.jpg',
+      avatar: faker.image.avatar(),
+      cover: faker.image.nature(),
+      introduction: faker.lorem.text(),
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -49,8 +52,9 @@ module.exports = {
       role: 'user',
       name: 'user4',
       account: '@user4',
-      avatar: 'https://pickaface.net/gallery/avatar/unr_adrian_180215_0407_2oo0yrq.png',
-      cover: 'https://images.alphacoders.com/558/558963.jpg',
+      avatar: faker.image.avatar(),
+      cover: faker.image.nature(),
+      introduction: faker.lorem.text(),
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
@@ -59,8 +63,9 @@ module.exports = {
       role: 'user',
       name: 'user5',
       account: '@user5',
-      avatar: 'https://pickaface.net/gallery/avatar/ninas4001566c31994ff65.png',
-      cover: 'https://images4.alphacoders.com/875/875696.jpg',
+      avatar: faker.image.avatar(),
+      cover: faker.image.nature(),
+      introduction: faker.lorem.text(),
       createdAt: new Date(),
       updatedAt: new Date()
     }], {})
