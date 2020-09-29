6c16cf9c90c5bce3e3feff1643b7bcdf548048fc
cotool <tusky.saga@gmail.com>
2020-09-29 15:39:49 +0800

    new passport.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const methodOverride = require('method-override')
 const helpers = require('./_helpers');
 
@@ -23,6 +24,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
 
+// setup passport
+app.use(passport.initialize())
+app.use(passport.session())
 
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
