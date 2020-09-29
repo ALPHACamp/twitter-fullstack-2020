13171eb2c08ee69650e47ef38be20cdccbf26bc9
Calvin Sung <calvinsungvin@gmail.com>
2020-09-29 18:24:35 +0800

    Add userController and signup page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 
 module.exports = app => {
  app.get('/admin', (req, res) => res.redirect('/admin/tweets'))
-
- // 在 /admin/restaurants 底下則交給 adminController.getRestaurants 處理
  app.get('/admin/tweets', adminController.getTweets)
+
+ app.get('/signup', userController.signUpPage)
+ app.post('/signup', userController.signUp)
 }
