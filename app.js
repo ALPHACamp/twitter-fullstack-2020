8154cfa61fc6216caeea84ed37831169cc5d10fc
emily40830 <emily40830@gmail.com>
2020-09-29 20:14:40 +0800

    fix:fix some conflict

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
+const express = require('express');
+const exphbs = require('express-handlebars');
 const helpers = require('./_helpers');
-const exphbs = require('express-handlebars')
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.engine('hbs', exphbs({ defaultLayout: "main", extname: "hbs" }))
-app.set('view engine', 'hbs')
-app.use(express.static('public'))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
 
 app.use('/', (req, res) => {
-  res.render('test')
-})
+  res.render('test');
+});
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 app.get('/', (req, res) => {
-  res.render('tweets')
-})
+  res.render('tweets');
+});
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-module.exports = app
+module.exports = app;
