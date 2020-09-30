9de4256f710232128dafe161a71d49d2bc6640b9
Yao <capslock0921@gmail.com>
2020-09-30 16:33:00 +0800

    update app.js

--- app.js
+++ app.js
@@ -30,24 +30,6 @@ app.use((req, res, next) => {
   next()
 })
 
-const flash = require('connect-flash')
-const session = require('express-session')
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-
-// setup session and flash
-app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-app.use(flash())
-
-// 把 req.locals
-app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')
-  res.locals.error_messages = req.flash('error_messages')
-  next()
-})
-
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 app.engine('handlebars', handlebars({ defaultLayout: 'main', helpers: require('./config/handlebars-helpers') }))
@@ -55,12 +37,8 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-
-
-
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
 
 require('./routes')(app, passport) // passport 傳入 routes
 
