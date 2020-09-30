f452257335d5e003e7c1e6b3fa67581b1e662584
Pony <ponyma2100@gmail.com>
2020-09-30 18:58:46 +0800

    feat: Add authentication before rendering  pages

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,65 @@ const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
 
-  app.get('/', (req, res) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.role === 'user') {
+        return next()
+      } else {
+        req.flash('error_messages', '請登入正確帳號!')
+        return res.redirect('/login')
+      }
+    }
+    res.redirect('/login')
+  }
+
+  const authenticatedAdmin = (req, res, next) => {
+    console.log('SDFSDF')
+    if (req.isAuthenticated()) {
+      if (req.user.role === 'admin') {
+        return next()
+      } else {
+        req.flash('error_messages', '請登入正確帳號!')
+        return res.redirect('/admin/login')
+      }
+    }
+    res.redirect('/admin/login')
+  }
+
+  app.get('/', authenticated, (req, res) => {
     res.redirect('/tweets')
   })
 
   // tweet首頁
-  app.get('/tweets', tweetController.getTweets)
+  app.get('/tweets', authenticated, tweetController.getTweets)
 
   // 註冊頁
   app.get('/register', userController.registerPage)
   app.post('/register', userController.register)
   // 前台登入頁
   app.get('/login', userController.loginPage)
-
+  // 前台登入
   app.post('/login', passport.authenticate('local', {
     failureRedirect: '/login',
     failureFlash: true
   }), userController.login)
 
   app.get('/logout', userController.logout)
+
+
   // 後台登入頁
   app.get('/admin/login', adminController.loginPage)
+  // 後台登入 
+  app.post('/admin/login', passport.authenticate('local', {
+    failureRedirect: '/admin/login',
+    failureFlash: true
+  }), adminController.login)
 
+  app.get('/admin', authenticatedAdmin, (req, res) => {
+    res.redirect('/admin/tweets')
+  })
+  // 後台推文清單
+  app.get('/admin/tweets', authenticatedAdmin, adminController.getTweets)
+
+  app.get('/admin/logout', adminController.logout)
 }
