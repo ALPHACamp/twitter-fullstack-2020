7dfab3321de1d7a6eb31e150f505302991d485cd
hsiyu1121 <bluefish1121@gmail.com>
2020-09-30 09:50:46 +0800

    save routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
-const userController = require('../controllers/userController')
-const tweetController = require('../controllers/tweetController')
+const userController = require("../controllers/userController");
+const tweetController = require("../controllers/tweetController");
 
+module.exports = (app) => {
+  app.get("/signup", userController.signUpPage);
+  app.post("/signup", userController.signUp);
+  app.get("/signin", userController.signIn);
 
-module.exports = app => {
-  app.get('/signup', userController.signUpPage)
-  app.post('/signup', userController.signUp)
-  app.get('/signin', userController.signIn)
+  app.get("/", (req, res) => res.redirect("/tweets"));
+  app.get("/tweets", tweetController.getTweets);
+  app.get("/tweets/:id", tweetController.getTweet);
 
-
-  app.get('/', (req, res) => res.redirect('/tweets'))
-  app.get('/tweets', tweetController.getTweets)
-  app.get('/tweets/:id', tweetController.getTweet)
-  app.post('/like/:tweetId', tweetController.addLike)
-  app.delete('/like/:tweetId', tweetController.removeLike)
-
-}
+  app.post("/like/:tweetId", tweetController.addLike);
+  app.delete("/like/:tweetId", tweetController.removeLike);
+};
