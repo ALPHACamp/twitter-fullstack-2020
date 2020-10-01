1dc3d07bb8588c035fa7b967cef54ca363f660cb
Josh Fang <joshuafang13@gmail.com>
2020-10-02 01:03:29 +0800

    update admin tweets page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,7 @@
 const db = require('../models')
 const User = db.User
 const Tweet = db.Tweet
+const pageLimit = 30
 
 
 const adminControllers = {
@@ -12,6 +13,11 @@ const adminControllers = {
       include: [{ model: User }]
     })
       .then(tweets => {
+        console.log(tweets)
+        tweets = tweets.map(tweet => ({
+          ...tweet,
+          description: tweet.description.substring(0, 200),
+        }))
         console.log(tweets)
         return res.render('admin/tweets', { tweets })
       })
