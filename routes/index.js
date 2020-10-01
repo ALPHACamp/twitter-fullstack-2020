648709ed2ce285262c4394e8cd6ad52f956732a6
Calvin Sung <calvinsungvin@gmail.com>
2020-10-01 16:37:30 +0800

    debugging admin login issue

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,8 @@
-// const adminController = require('../controllers/adminController.js')
-// const userController = require('../controllers/userController.js')
-
-// module.exports = (app, passport) => {
-
-//     const authenticated = (req, res, next) => {
-//         if (req.isAuthenticated()) {  
-//           return next()
-//         }
-//         res.redirect('/signin')
-//       }
-    
-//       const authenticatedAdmin = (req, res, next) => {
-//         if (req.isAuthenticated()) {
-//           if (req.user.isAmin) { return next() }  
-//           return res.redirect('/') 
-//         }
-//         res.redirect('/signin')
-//       }
-
-// app.get('/admin', (req, res) => { res.redirect('/admin/tweets') })
-// app.get('/admin/tweets', adminController.getTweets)
-// app.post('/admin/tweets/:id', adminController.deleteTweet)
-
-//  app.get('/signup', userController.signUpPage)
-//   app.post('/signup', userController.signUp)
-//   app.get('/signin', userController.signInPage)
-//   app.post('/signin', passport.authenticate('local', {
-//     failureRedirect: '/signin',
-//     failureFlash: true
-//   }), userController.signIn)
-//   app.get('/logout', userController.logout)
-//   app.get('/main', (req, res) => res.render('mainpage'))
-
-// }
-
 const userController = require('../controllers/userController')
 const adminController = require('../controllers/adminController')
 
 
-
 module.exports = (app, passport) => {
-
 
   const authenticated = (req, res, next) => {
     if (req.isAuthenticated()) {  
@@ -77,3 +39,51 @@ module.exports = (app, passport) => {
   app.get('/admin/signin', adminController.signinPage)
   app.post('/admin/signin', adminController.signIn)
 }
+
+// const userController = require('../controllers/userController')
+// const adminController = require('../controllers/adminController')
+// module.exports = (app, passport) => {
+
+//   const authenticated = (req, res, next) => {
+//     if (req.isAuthenticated()) {  
+//       if (req.user.role === "1") { return next() }
+//       return res.redirect('/')
+//     }
+//     res.redirect('/signin')
+//   }
+
+//   const authenticatedAdmin = (req, res, next) => {
+//     if (req.isAuthenticated()) {
+//       if (req.user.role === "0") { return next() } 
+//       return res.redirect('/') 
+//     }
+//     res.redirect('/signin')
+//   }
+//   //user login
+//   app.get('/signup', userController.signUpPage)
+//   app.post('/signup', userController.signUp)
+//   app.get('/signin', userController.signInPage)
+//   app.post('/signin', passport.authenticate('local', {
+//     failureRedirect: '/signin',
+//     failureFlash: true
+//   }), userController.signIn)
+//   app.get('/main', (req, res) => res.render('mainpage'))
+//   app.get('/main', authenticated, (req, res) => res.render('mainpage'))
+
+
+//   // adminController
+  
+//   app.get('/admin', authenticatedAdmin, (req, res) => { res.redirect('/admin/tweets') })
+//   app.get('/admin/tweets', authenticatedAdmin, adminController.getTweets)
+//   app.post('/admin/tweets/:id', authenticatedAdmin, adminController.deleteTweet)
+
+//   app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
+
+//   app.get('/admin/signin', adminController.signinPage)
+//   app.post('/admin/signin', passport.authenticate('local', {
+//     failureRedirect: '/signin',
+//     failureFlash: true
+//   }), adminController.signIn)
+//   app.get('/logout', userController.logout)
+
+// } 
