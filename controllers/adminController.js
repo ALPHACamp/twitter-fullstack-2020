4b5a2f4909cf8897a79ce9e4f08e4eca0e3f592c
Josh Fang <joshuafang13@gmail.com>
2020-10-01 23:25:49 +0800

    add admin tweet page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,11 +1,20 @@
 const db = require('../models')
 const User = db.User
+const Tweet = db.Tweet
+
 
 const adminControllers = {
-
   getTweets: (req, res) => {
-
-    return res.render('admin/tweets')
+    return Tweet.findAll({
+      raw: true,
+      nest: true,
+      order: [['createdAt', 'DESC']],
+      include: [{ model: User }]
+    })
+      .then(tweets => {
+        console.log(tweets)
+        return res.render('admin/tweets', { tweets })
+      })
   }
 }
 
