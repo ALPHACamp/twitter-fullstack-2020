d371a47f84e7a42c68e63f89c4725b302f5f14a1
emily40830 <emily40830@gmail.com>
2020-10-01 10:16:06 +0800

    fix:some err

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const helpers = require('./_helpers');
-const session = require('express-session')
-const passport = require('./config/passport')
-const bodyParser = require('body-parser')
+const session = require('express-session');
+const passport = require('./config/passport');
+const bodyParser = require('body-parser');
 const flash = require('connect-flash');
-
-
 
 const app = express();
 const port = 3000;
@@ -14,18 +12,16 @@ const port = 3000;
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use(session({ secret: 'test', resave: false, saveUninitialized: true }))
-app.use(flash())
-app.use(passport.initialize())
-app.use(passport.session())
-
+app.use(session({ secret: 'test', resave: false, saveUninitialized: true }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
-
 
 // const db = require('./models')
 // const User = db.User
@@ -39,12 +35,12 @@ app.use(passport.session())
 //   console.log('success to create account')
 // })
 app.use((req, res, next) => {
-  res.locals.user = helpers.getUser(req)
-  res.locals.isAuthenticated = helpers.ensureAuthenticated(req)
-  next()
-})
+  res.locals.user = helpers.getUser(req);
+  res.locals.isAuthenticated = helpers.ensureAuthenticated(req);
+  next();
+});
 
-require('./routes/index')(app)
+require('./routes/index')(app);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
