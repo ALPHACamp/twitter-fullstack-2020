602821206346111e24d43edca2cec46cc1956782
Corey <simon8888xsimon8888x@gmail.com>
2020-10-03 01:02:22 +0800

    feat: add userSetting-page and modify user info function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const passport = require('./config/passport');
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
+const methodOverride = require('method-override')
 
 const app = express();
 const port = 3000;
@@ -19,6 +20,7 @@ app.engine(
 );
 app.set('view engine', 'hbs');
 
+app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
@@ -30,17 +32,6 @@ app.use(passport.session());
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-// const db = require('./models')
-// const User = db.User
-// const bcrypt = require('bcryptjs')
-// User.create({
-//   account: 'good2@example.com',
-//   password: bcrypt.hashSync("good2", bcrypt.genSaltSync(10)),
-//   isAdmin: false
-// })
-// .then(() => {
-//   console.log('success to create account')
-// })
 app.use((req, res, next) => {
   res.locals.user = helpers.getUser(req)
   res.locals.isAuthenticated = helpers.ensureAuthenticated(req)
