45f4b8e9af92d620292f14701c73843133cbe8b5
emily40830 <emily40830@gmail.com>
2020-10-02 11:46:35 +0800

    feat:user page v1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,16 @@
-
-
 const userController = {
-
   getSigninPage: (req, res) => {
-    return res.render('signin')
-
+    return res.render('signin');
   },
 
   signin: (req, res) => {
-    return res.redirect('/tweets')
+    return res.redirect('/tweets');
   },
+  getUser: (req, res) => {
+    let user = req.user;
+    console.log(user);
+    return res.render('user', user);
+  },
+};
 
-}
-
-module.exports = userController
+module.exports = userController;
