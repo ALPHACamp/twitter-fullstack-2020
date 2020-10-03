da2c0ea6f1be2cf67020d3e43e7d42b1f9cd4aaa
Kay <kahachang@gmail.com>
2020-10-03 18:26:09 +0800

    add getUserTweets and getUserLikes to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,11 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
+const tweetController = require('./tweetController')
 const User = db.User
 const Tweet = db.Tweet
 const Reply = db.Reply
 const Followship = db.Followship
+const helpers = require('../_helpers')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -33,7 +35,6 @@ const userController = {
               }).then(() => {
                 return res.redirect('/signin')
               })
-
             }
           })
         }
@@ -57,20 +58,51 @@ const userController = {
     res.redirect('/signin')
   },
 
-  getUser: (req, res) => {
+  getUserTweets: (req, res) => {
     return User.findByPk(req.params.id, {
       include: [
         Tweet,
+        { model: Tweet, as: 'LikedTweets' },
         { model: User, as: 'Followers' },
         { model: User, as: 'Followings' }
-      ]
+      ], order: [[Tweet, 'createdAt', 'DESC']]
     }).then(user => {
-      return res.render('user/userPage', { user: user.toJSON() })
+      const pageUser = user.toJSON()
+      const currentUserId = helpers.getUser(req).id
+      pageUser.isFollowed = helpers.getUser(req).Followers.map(item => item.id).includes(user.id)
+
+      // // 不確定能否撈到 tweet 資料
+      // pageUser.Tweets.forEach(tweet => {
+      //   tweet.isLiked = tweet.Likes.map(item => item.id).includes(currentUserId)
+      // })
+
+      return res.render('user/userPage', {
+        user: pageUser,
+        currentUserId
+      })
     })
+  },
 
+  getUserLikes: (req, res) => {
+    return User.findByPk(req.params.id, {
+      include: [
+        Tweet,
+        { model: like, include: [{ model: Tweet, include: User }] },
+        { model: Tweet, as: 'LikedTweets' },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ], order: [[LikedTweets, 'createdAt', 'DESC']]
+    }).then(user => {
+      const pageUser = user.toJSON()
+      const currentUserId = helpers.getUser(req).id
+      pageUser.isFollowed = helpers.getUser(req).Followers.map(item => item.id).includes(currentUserId)
 
+      // 缺少 like 清單
 
+      return res.render('user/userLikesPage')
+    })
   }
+
 }
 
 module.exports = userController
