75c00ae8b256d5727930ee6abd95d95ca054037a
Corey <simon8888xsimon8888x@gmail.com>
2020-10-03 17:14:01 +0800

    feat: show db data in admin/tweets-page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,11 @@
 const adminService = require('../services/adminService')
+// const bcrypt = require('bcryptjs');
+const db = require('../models');
+const User = db.User;
+const Like = db.Like;
+const Tweet = db.Tweet;
+const Reply = db.Reply;
+
 
 const adminController = {
 
@@ -7,9 +14,19 @@ const adminController = {
   },
 
   getTweets: (req, res) => {
-    adminService.getTweets(req, res, (data) => {
-      return res.render('admin/tweets', data)
+    Tweet.findAll({
+      include: User,
+      order: [['createdAt', 'DESC']],
+      limit: 10,
+      raw: true,
+      nest: true,
     })
+      .then(tweets => {
+        return res.render('admin/tweets', { tweets })
+      })
+    // adminService.getTweets(req, res, (data) => {
+    //   return res.render('admin/tweets', data)
+    // })
   },
 
   signin: (req, res) => {
