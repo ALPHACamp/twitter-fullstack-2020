54f9cc5973d2937de4dee08dcf20ff01e250cfaf
Josh Fang <joshuafang13@gmail.com>
2020-10-04 12:42:11 +0800

    fix bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
