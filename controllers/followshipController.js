ade38aaf80b441bf5899e1cd59d00b427497791b
emily40830 <emily40830@gmail.com>
2020-10-04 09:59:08 +0800

    feat:add follow-unfollow controller

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -4,6 +4,44 @@ const Followship = db.Followship;
 const helpers = require('../_helpers');
 
 const followshipController = {
-  follow: (req, res) => {},
-  unfollow: (req, res) => {},
+  addFollow: (req, res) => {
+    const followerId = helpers.getUser(req).id;
+    const followingId = Number(req.body.id);
+
+    if (followerId === followingId) {
+      req.flash('errorMessage', '無效的追蹤');
+      res.redirect(`/users/${followerId}`);
+    }
+
+    Followship.findOne({ where: { followerId, followingId } }).then(
+      (followship) => {
+        if (followship === null) {
+          Followship.create({ followerId, followingId }).then(() => {
+            req.flash('successMessage', '追蹤成功！');
+            return res.redirect(`/users/${followerId}`);
+          });
+        } else {
+          req.flash('errorMessage', '不得重複追蹤同一位使用者！');
+          return res.redirect(`/users/${followerId}`);
+        }
+      },
+    );
+  },
+  unFollow: (req, res) => {
+    const followerId = helpers.getUser(req).id;
+    const followingId = Number(req.body.id);
+
+    return Followship.findAll({ where: { followerId, followingId } }).then(
+      (followship) => {
+        if (followship !== null) {
+          followship.destroy().then(() => {
+            return res.redirect('back');
+          });
+        } else {
+          req.flash('errorMessage', '無效的操作！');
+          return res.redirect('back');
+        }
+      },
+    );
+  },
 };
