15335d7f08820782179b0e7bfad821bfe45066b6
emily40830 <emily40830@gmail.com>
2020-10-04 11:36:52 +0800

    fix:pass the followship.spec test

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -10,39 +10,42 @@ const followshipController = {
 
     if (followerId === followingId) {
       req.flash('errorMessage', '無效的追蹤');
-      res.redirect(`/users/${followingId}/tweets}`);
+      return res.render('error');
     }
 
-    Followship.findOne({ where: { followerId, followingId } }).then(
+    return Followship.findOne({ where: { followerId, followingId } }).then(
       (followship) => {
         if (followship === null) {
           Followship.create({ followerId, followingId }).then(() => {
             req.flash('successMessage', '追蹤成功！');
-            return res.redirect(`/users/${followingId}/tweets`);
+            return res.redirect('back');
+            //return res.redirect(`/users/${followingId}/tweets`);
           });
         } else {
           req.flash('errorMessage', '不得重複追蹤同一位使用者！');
-          return res.redirect(`/users/${followingId}/tweets`);
+          return res.redirect('back');
+          //return res.redirect(`/users/${followingId}/tweets`);
         }
       },
     );
   },
-  unFollow: (req, res) => {
+  unFollow: async (req, res) => {
     const followerId = helpers.getUser(req).id;
-    const followingId = Number(req.body.id);
+    const followingId = Number(req.params.id);
 
-    return Followship.destroy({ where: { followerId, followingId } }).then(
-      () => {
-        // if (followship !== null) {
-        //   followship.destroy().then(() => {
-        //     return res.redirect('back');
-        //   });
-        // } else {
-        // req.flash('errorMessage', '無效的操作！');
-        return res.redirect('back');
-        // }
-      },
-    );
+    await Followship.destroy({ where: { followerId, followingId } });
+    return res.redirect('back');
+    // .then(() => {
+    // if (followship !== null) {
+    //   followship.destroy().then(() => {
+    //     return res.redirect('back');
+    //   });
+    // } else {
+    // req.flash('errorMessage', '無效的操作！');
+
+    // }
+    // },
+    // );
   },
 };
 
