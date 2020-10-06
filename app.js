d1477879852272818ce6281646be9226f96abdca
STTSAI <shinting.tsai@gmail.com>
2020-10-06 23:56:11 +0800

    feat: add apiPostUserInfo and router POST:/api/users/:userid

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require('express-session')
-const passport = require('./config/passport')
+
+const app = express()
+const port = process.env.PORT || 3000
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const app = express()
-const port = process.env.PORT || 3000
+const passport = require('./config/passport')
 
 app.use(express.static('public'))
 
@@ -38,7 +39,7 @@ app.use('/upload', express.static(__dirname + '/upload'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
-  res.locals.user = req.user
+  res.locals.user = helpers.getUser(req)
   next()
 })
 
