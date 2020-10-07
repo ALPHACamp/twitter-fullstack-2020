e160cffffb06bbedd466149813fc63657903b006
Kay <kahachang@gmail.com>
2020-10-07 16:38:25 +0800

    modify heroku condition

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const db = require('./models')
 const session = require('express-session')
 const flash = require('connect-flash')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
