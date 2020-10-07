8258f3b18ba53e1a8415676b9cf096c37b338a6d
nonoLin <denkanono@gmail.com>
2020-10-07 16:35:45 +0800

    add preview edit pic

--- public/javascripts/spinner.js
+++ public/javascripts/spinner.js
@@ -1,4 +1,25 @@
-$(window).load(function () {
-  // Animate loader off screen
-  $(".se-pre-con").fadeOut("slow");;
+$("#cover").change(function () {
+  readCoverURL(this);
 });
+function readCoverURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $("#cover_img").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#avatar").change(function () {
+  readAvatarURL(this);
+});
+function readAvatarURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $("#avatar_img").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
