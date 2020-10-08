3cdee11d9fb5a9c56ad3a4bb06abead4a3af06f2
nonoLin <denkanono@gmail.com>
2020-10-08 18:08:29 +0800

    update

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -181,9 +181,8 @@ const tweetController = {
   },
 
   postReplies: (req, res) => {
-    let tweetId = req.params.id
     let replyId = req.params.rid
-    let replyText = req.body.replyText.trim()
+    let replyText = req.body.comment.trim()
     if (!replyText.length) {
       return res.redirect('back')
     } else {
