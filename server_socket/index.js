f4e0bffb71352b19746e9f6b399b3507615e450c
icedike <icedike@gmail.com>
2020-10-11 00:13:52 +0800

    Add chatroom feature

--- server_socket/index.js
+++ server_socket/index.js
@@ -1,10 +1,11 @@
 const { User } = require('../models')
 let chatroomList = []
 
-async function connectedUsers(newUserId, socketId, io, socket) {
+async function joinChatroomList(newUserId, socketId, io, socket) {
   if (newUserId) {
     const user = await User.findByPk(newUserId)
     const chatUserInfo = {
+      id: newUserId,
       name: user.name,
       account: user.account,
       avatar: user.avatar,
@@ -17,7 +18,14 @@ async function connectedUsers(newUserId, socketId, io, socket) {
 }
 
 function disconnectUser(socketId, io) {
-  chatroomList = chatroomList.filter(d => (d.socketId !== socketId))
+  // disconnect from chatroom
+  chatroomList = chatroomList.filter(d => {
+    if (d.socketId !== socketId) {
+      return true
+    }
+    io.emit('person leaving', d.name)
+    return false
+  })
   console.log('length of the chtroomList', chatroomList.length)
   io.emit('chat list', chatroomList)
 }
@@ -26,9 +34,8 @@ module.exports = io => {
   io.on('connection', (socket) => {
     console.log('user connect socketid=', socket.id)
     socket.on('join chat room', userId => {
-      connectedUsers(userId, socket.id, io, socket)
+      joinChatroomList(userId, socket.id, io, socket)
     })
-    connectedUsers()
     socket.on('disconnect', () => {
       disconnectUser(socket.id, io)
     })
