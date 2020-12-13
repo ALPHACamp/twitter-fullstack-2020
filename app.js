6e91f8c62b6ecd75af847b6ba36de22f1e05d726
yongde2900 <com414141@gmail.com>
2020-12-14 00:49:49 +0800

    Project init

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const exhbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const helpers = require('./_helpers')
+const session = require('express-session')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(methodOverride('_method'))
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+})
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
