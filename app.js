b8fffa7c5f7c5453a8252aff0b2b1b59937cb829
yongde2900 <com414141@gmail.com>
2020-12-14 14:52:10 +0800

    feat: create config/passport

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const flash = require('connect-flash')
 const helpers = require('./_helpers')
 const routes = require('./routes')
 const session = require('express-session')
+const passport = require('./config/passport')
 // if (process.env.NODE_ENV !== 'production') {
 //     require('dotenv').config()
 // }
@@ -17,6 +18,8 @@ app.engine('hbs', exhbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'simpleTweetSecret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(methodOverride('_method'))
 app.use(flash())
 app.use((req, res, next) => {
@@ -26,7 +29,6 @@ app.use((req, res, next) => {
 })
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
-
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
