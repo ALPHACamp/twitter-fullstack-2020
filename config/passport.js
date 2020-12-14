836b9827e1878791fc7213525fee35d03e8eb33d
yongde2900 <com414141@gmail.com>
2020-12-14 19:17:46 +0800

    fix: typo

--- config/passport.js
+++ config/passport.js
@@ -5,12 +5,12 @@ const db = require('../models')
 const User = db.User
 const bcrypt = require('bcryptjs')
 
-passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, passwrd, done) => {
+passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ where: { email: email } }).then(user => {
         if (!user) {
             return done(null, false, req.flash('error_msg', 'That email is not registered!'))
         }
-        if (!bcrypt.compareSync(passwrd, user.passwrd)) {
+        if (!bcrypt.compareSync(password, user.password)) {
             return done(null, false, req.flash('error_msg', 'Email or password incorrect!'))
         }
         return done(null, user)
