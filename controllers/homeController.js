0d0e60519e327049f713626381ff4dcc84af54f4
Lin <mt5718214@gmail.com>
2020-12-15 14:13:10 +0800

    feat: add user signin page & route

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,5 +2,11 @@
 
 
 module.exports = {
-    
+  signInPage: (req, res) => {
+    return res.render('signin')
+  },
+
+  signUpPage: (req, res) => {
+    return res.render('signup')
+  }
 }
--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/homeController')
 
-
+router.get('/signin', homeController.signInPage)
+router.get('/signup', homeController.signUpPage)
 
 module.exports = router
