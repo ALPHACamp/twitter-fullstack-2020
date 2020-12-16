85844195975fba70824449ccf9117a11bfacd52f
yongde2900 <com414141@gmail.com>
2020-12-16 19:47:38 +0800

    fix: modify tweet and user association

--- models/tweet.js
+++ models/tweet.js
@@ -7,11 +7,7 @@ module.exports = (sequelize, DataTypes) => {
   Tweet.associate = function (models) {
     Tweet.hasMany(models.Reply)
     Tweet.belongsTo(models.User)
-    Tweet.belongsToMany(models.User, {
-      through: models.Like,
-      foreignKey: 'TweetId',
-      as: 'LikedUser'
-    })
+    Tweet.hasMany(models.Like)
   };
   return Tweet;
 };
