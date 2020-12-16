506334d08f387946192e0fdc13d3f40ce1ff7815
Lin <mt5718214@gmail.com>
2020-12-16 10:05:51 +0800

    feat: add user signup function

--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,6 @@ const homeController = require('../controllers/homeController')
 
 router.get('/signin', homeController.signInPage)
 router.get('/signup', homeController.signUpPage)
+router.post('/signup', homeController.signup)
 
 module.exports = router
