a3ea376993a1d5af5e0275ef1dd00749a250616c
RoiWu <roih1h116@gmail.com>
2020-12-16 21:57:13 +0800

    signin page is done

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,16 @@ const db = require('../models')
 const User = db.User //input the user schema
 
 const userController = {
-signInPage: (req, res) => {
-    return res.render('signin')
+    signInPage: (req, res) => {
+        return res.render('signin')
+    },
+
+    signIn: (req, res) => {
+        req.flash('success_messages', '成功登入！')
+        res.redirect('/tweets')
+    },
+    signUpPage: (req, res) => {
+        return res.render('signup')
     },
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../controllers/adminController.js')
-const userController  = require('../controllers/userController.js')
+const userController = require('../controllers/userController.js')
+const twitterController = require('../controllers/twitterController.js')
 
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
@@ -11,7 +12,22 @@ const helplers = require('../_helpers')
 
 const passport = require('../config/passport')
 
+///////
+// admin
+///////
+router.get('/admin/signin', adminController.signin)
+
+///////
+// User
+///////
 router.get('/', userController.signInPage)
+router.get('/signup', userController.signUpPage)
 router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+
+///////
+// tweet
+///////
+router.get('/tweets', twitterController.getTwitters)
 
 module.exports = router
