90f3ac16d3fa9ebcb0af01226b0dfb01b37b6437
Sharon-liu-go <innovate72095@gmail.com>
2020-12-16 23:35:38 +0800

    add admin getTweets  feature

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,21 @@
+const db = require('../models')
+const { User, Tweet, Reply, Like } = db
+
 
 module.exports = (app) => {
   app.get('/', (req, res) => res.send('Hello World!'))
+
+  app.get('/admin/tweets', (req, res) => {
+    Tweet.findAll({
+      include: [User], order: [['createdAt', 'DESC']]
+    }).then(tweets => {
+      console.log(tweets)
+      tweets = tweets.map(tweet => ({
+        ...tweet.dataValues,
+        description: tweet.dataValues.description.substring(0, 50),
+      }))
+      return res.render('admin/tweets', { tweets: tweets })
+    }
+    )
+  })
 }
