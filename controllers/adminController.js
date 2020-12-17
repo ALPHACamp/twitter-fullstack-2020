5cdbdc9ea4649f82665df4acb73a558394e2c7c2
Lin <mt5718214@gmail.com>
2020-12-17 12:39:53 +0800

    feat: add admin users page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,7 @@
 const db = require('../models')
 const User = db.User
 const Tweet = db.Tweet
+const Like = db.Like
 
 
 module.exports = {
@@ -26,6 +27,25 @@ module.exports = {
       .then(tweet => {
         return tweet.destroy()
           .then(tweet => res.redirect('/admin/tweets'))
+      })
+  },
+
+  getUsers: (req, res) => {
+    User.findAll({
+      include: [Tweet, Like]
+    })
+      .then(users => {
+        users = users.map(user => ({
+          ...user.dataValues,
+          NumberOfTweets: user.Tweets.length,
+          NumberOfLikes: user.Likes.length,
+          NumberOfFollowers: req.user.Followers.length,
+          NumberOfFollowings: req.user.Followings.length
+        }))
+
+        res.render('admin/users', {
+          users
+        })
       })
   }
 }
