7c51ed9d2f15b8899110553a7e3a2dccfcb27ebe
Lin <mt5718214@gmail.com>
2020-12-18 16:52:04 +0800

    feat: add user followings page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,34 @@
+const db = require('../models')
+const User = db.User
 
+const helper = require('../_helpers')
 
 
 module.exports = {
+  getFollowings: (req, res) => {
+    User.findOne({
+      where: { id: req.params.id },
+      include: [{
+        model: User,
+        as: 'Followings',
+        through: {
+          attributes: ['createdAt']
+        }
+      }]
+    })
+      .then(user => {
+        // console.log(user.dataValues.Followings)
+        // console.log(user.toJSON().Followings[0].Followship)
+        // console.log(user.dataValues.Followings[0])
+        const followings = user.dataValues.Followings.map(f => ({
+          ...f.dataValues,
+          introduction: f.introduction.substring(0, 150),
+          isFollowed: helper.getUser(req).Followings.map(v => v.id).includes(f.id)
+        }))
 
+        res.render('followship', {
+          followings
+        })
+      })
+  }
 }
