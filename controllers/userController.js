9653f1ea163d33457358ae0a23d1883812448e09
Derek Hsu <derek800109@gmail.com>
2020-12-20 09:55:00 +0800

    chore: reomve content of userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,65 +6,6 @@ const Reply = db.Reply
 const Like = db.Like
 
 const userController = {
-  getUser: async (req, res) => {
-    // const user = await User.findByPk(req.params.id, {
-    //   raw: true,
-    //   nest: true,
-    //   include: [
-    //     { model: User, as: 'Followers' },
-    //     { model: User, as: 'Followings' },
-    //     { model: Tweet, include: [Reply, Like] }
-    //   ]
-    // })
-
-    // const users = await User.findAll({ raw: true, nest: true })
-
-    // await console.log('hahahaha   ' + user)
-
-    // await res.render('profile', { user: user.toJSON() })
-    return Promise.all([
-      User.findByPk(req.params.id, {
-        include: [
-          { model: User, as: 'Followers' },
-          { model: User, as: 'Followings' }
-        ]
-      }),
-      Tweet.findAll({
-        // raw: true,
-        // nest: true,
-        where: { UserId: req.params.id },
-        include: [User, Reply, { model: Like, include: [User] }],
-        order: [['createdAt', 'DESC']],
-      })
-      ,
-      User.findAll({
-        include: [{ model: User, as: 'Followers' }]
-      })
-    ]).then(([user, tweets, users]) => {
-      tweets = tweets.map(tweet => ({
-        ...tweet.dataValues,
-        likeLength: tweet.Likes.length,
-        replyLength: tweet.Replies.length,
-        User: tweet.User.dataValues,
-        isLiked: tweet.Likes.map(d => d.UserId).includes(Number(req.params.id)),
-      }))
-      console.log(tweets)
-
-      users = users.map(user => ({
-        ...user.dataValues,
-        isFollowed: user.Followers.map(d => d.id).includes(Number(req.params.id))
-      }))
-      // console.log(users)
-
-      return res.render('profile', {
-        user: user.toJSON(),
-        FollowersLength: user.dataValues.Followers.length,
-        FollowingsLength: user.dataValues.Followings.length,
-        tweets: tweets,
-        users: users
-      })
-    })
-  }
 }
 
 module.exports = userController
