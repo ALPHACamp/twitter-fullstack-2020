5f7db900dbf498af49e5a8c47808affadf10a4f8
yongde2900 <com414141@gmail.com>
2020-12-20 15:19:21 +0800

    feat: create momentFormat function

--- config/handlebars-helper.js
+++ config/handlebars-helper.js
@@ -8,5 +8,9 @@ module.exports = {
     },
     moment: function(a){
         return moment(a).fromNow()
+    },
+    momentFormat: function(a){
+        moment.locale('zh-tw')
+        return moment(a).format('mm:ss a ． YYYY MMMM Do')
     }
 }
