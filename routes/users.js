ab69f7fc87c7db069d78a84fcf41edd5ddce4f4d
yongde2900 <com414141@gmail.com>
2020-12-21 15:10:10 +0800

    fix: modify get user's tweets path

--- routes/users.js
+++ routes/users.js
@@ -8,8 +8,9 @@ const upload = multer({ dest: 'temp/' })
 
 // -----------------------------------------------------------------------------------
 
-router.get('/:id', userController.getUser)
-router.put('/:id', upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), userController.putUser)
+router.get('/:id/tweets', userController.getUser)
+router.put('/:id/tweets', upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), userController.putUser)
+router.get('/:id/edit', userController.getEdit)
 
 // -----------------------------------------------------------------------------------
 
