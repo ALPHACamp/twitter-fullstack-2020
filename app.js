2214bf33a510f31ac4327c110e4fb080fd1145e4
King LEE <a0988698776@gmail.com>
2020-12-21 22:30:24 +0800

    fix userController

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const db = require('./models') // 引入資料庫
-
+const app = express()
 const port = process.env.PORT || 3000
 
 const passport = require('./config/passport')
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,18 @@
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const User = db.User //input the user schema
+const User = db.User // input the user schema
 const Tweet = db.Tweet
 const Reply = db.Reply
 const Like = db.Like
 
 const userController = {
 
-  ////////
+  /// /////
   // login
-  ////////
+  /// /////
   signInPage: (req, res) => {
     return res.render('signin')
   },
@@ -53,96 +53,118 @@ const userController = {
             }
           })
         }
-      },
+      })
+    }
+  },
+  /// /////
+  // setting
+  /// /////
+  getSetting: (req, res) => {
+    return res.render('setting')
+  },
+  updateSetting: (req, res) => {
+    if (!req.body.account) {
+      req.flash('error_messages', "account didn't exist")
+      return res.redirect('back')
+    }
 
-        ////////
-        // setting
-        ////////
-        getSetting: (req, res) => {
-          return res.render('setting')
-        },
-        updateSetting: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
 
-          if (!req.body.account) {
-            req.flash('error_messages', "account didn't exist")
+    if (!req.body.email) {
+      req.flash('error_messages', "email didn't exist")
+      return res.redirect('back')
+    }
+
+    if (req.body.password !== req.body.checkPassword) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('back')
+    }
+
+    User.findOne({ where: { id: { [Op.ne]: req.user.id }, email: req.body.email } }).then(mailuser => {
+      if (mailuser) {
+        req.flash('error_messages', '信箱重複！')
+        return res.redirect('back')
+      } else {
+        User.findOne({ where: { id: { [Op.ne]: req.user.id }, account: req.body.account } }).then(user => {
+          if (user) {
+            req.flash('error_messages', '帳號重複！')
             return res.redirect('back')
-          }
-
-          if (!req.body.name) {
-            req.flash('error_messages', "name didn't exist")
-            return res.redirect('back')
-          }
-
-          if (!req.body.email) {
-            req.flash('error_messages', "email didn't exist")
-            return res.redirect('back')
-          }
-
-          if (req.body.password !== req.body.checkPassword) {
-            req.flash('error_messages', "兩次密碼輸入不同！")
-            return res.redirect('back')
-          }
-
-          User.findOne({ where: { id: { [Op.ne]: req.user.id }, email: req.body.email } }).then(mailuser => {
-            if (mailuser) {
-              req.flash('error_messages', '信箱重複！')
-              return res.redirect('back')
+          } else {
+            if (!req.body.password) {
+              return User.findByPk(req.user.id)
+                .then((user) => {
+                  user.update({
+                    account: req.body.account,
+                    name: req.body.name,
+                    email: req.body.email
+                  })
+                    .then((user) => {
+                      req.flash('success_messages', 'setting infomation was successfully to update')
+                      res.redirect('setting')
+                    })
+                })
             } else {
-              User.findOne({ where: { id: { [Op.ne]: req.user.id }, account: req.body.account } }).then(user => {
-                if (user) {
-                  req.flash('error_messages', '帳號重複！')
-                  return res.redirect('back')
-                } else {
-                  if (!req.body.password) {
-                    return User.findByPk(req.user.id)
-                      .then((user) => {
-                        user.update({
-                          account: req.body.account,
-                          name: req.body.name,
-                          email: req.body.email,
-                        })
-                          .then((user) => {
-                            req.flash('success_messages', 'setting infomation was successfully to update')
-                            res.redirect('setting')
-                          })
-                      })
-                  } else {
-                    return User.findByPk(req.user.id)
-                      .then((user) => {
-                        user.update({
-                          account: req.body.account,
-                          name: req.body.name,
-                          email: req.body.email,
-                          password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-                        })
-                          .then((user) => {
-                            req.flash('success_messages', 'setting infomation was successfully to update')
-                            res.redirect('setting')
-                          })
-                      })
-                  }
-                }
-              })
+              return User.findByPk(req.user.id)
+                .then((user) => {
+                  user.update({
+                    account: req.body.account,
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+                  })
+                    .then((user) => {
+                      req.flash('success_messages', 'setting infomation was successfully to update')
+                      res.redirect('setting')
+                    })
+                })
             }
-          })
-        },
+          }
+        })
+      }
+    })
+  },
 
-        //////////////
-        //Profile
-        //////////////
-        getUserProfile: async (req, res) => {
-          let profileUser = await User.findByPk(req.params.id, {
-            include: [
-              { model: Reply, include: [Tweet] },
-              { model: Tweet, include: [User, Like, Reply] },
-              { model: User, as: 'Followers' },
-              { model: User, as: 'Followings' },
-            ]
-          })
-          profileUser = profileUser.dataValues
-          const isFollowed = req.user.Followings.map(d => d.id).includes(profileUser.id)
-          return res.render('userProfile', { profileUser, isFollowed })
-        },
+  /// ///////////
+  // Profile
+  /// ///////////
+  getUserProfile: async (req, res) => {
+    let profileUser = await User.findByPk(req.params.id, {
+      include: [
+        { model: Reply, include: [Tweet] },
+        { model: Tweet, include: [User, Like, Reply] },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ]
+    })
+    profileUser = profileUser.dataValues
+
+    const target = req.query.target || 'tweets'
+    if (target === 'replies') {
+      profileUser.Tweets = await Tweet.findAll({
+        include: [User, Like, Reply]
+      })
+      profileUser.Tweets.filter(tweet => tweet.dataValues.Replies.map(d => d.dataValues.UserId).includes(profileUser.id))
+    }
+    if (target === 'likes') {
+      profileUser.Tweets = await Tweet.findAll({
+        include: [User, Like, Reply]
+      })
+      profileUser.Tweets = profileUser.Tweets.filter(tweet => tweet.dataValues.Likes.map(d => d.dataValues.UserId).includes(profileUser.id))
+    }
+
+    profileUser.Tweets = profileUser.Tweets.map(tweet => ({
+      ...tweet.dataValues,
+      tweetLiked: tweet.Likes.filter(like => like.likeOrNot === true).length,
+      tweetDisliked: tweet.Likes.filter(unlike => unlike.likeOrNot === false).length
+    }))
+
+    const isFollowed = req.user.Followings.map(d => d.id).includes(profileUser.id)
+    profileUser.Tweets = profileUser.Tweets.sort((a, b) => b.updatedAt - a.updatedAt)
+    return res.render('userProfile', { profileUser, isFollowed, target })
+  }
 }
 
-    module.exports = userController
+module.exports = userController
