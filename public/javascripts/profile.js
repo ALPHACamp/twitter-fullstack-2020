045a35d742aaa0f22c01b18f19396aa3ec03ad18
Sharon-liu-go <innovate72095@gmail.com>
2020-12-24 14:00:06 +0800

    add profile count words feature

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,3 +1,19 @@
+const nameCount = document.querySelector('.nameCount')
+const nameInput = document.querySelector('.nameInput')
+
+const descriptionInput = document.querySelector('.descriptionInput')
+const descriptionCount = document.querySelector('.descriptionCount')
+
+
+function countWords(name, wordslimit) {
+  if (name === "name") {
+    let total = nameInput.value.length;
+    return nameCount.innerHTML = `${total}/${wordslimit}`
+  } else if (name === "introduction") {
+    let total = descriptionInput.value.length;
+    return descriptionCount.innerHTML = `${total}/${wordslimit}`
+  }
+}
 
 
 
