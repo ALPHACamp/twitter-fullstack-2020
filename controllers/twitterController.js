fcd6bf051866c79a74ebf98b7dfd1f40cd3e9260
King LEE <a0988698776@gmail.com>
2020-12-24 15:56:26 +0800

    Add pagination function in the tweets.hbs

--- controllers/twitterController.js
+++ controllers/twitterController.js
@@ -3,27 +3,32 @@ const User = db.User // input the user schema
 const Like = db.Like
 const Tweet = db.Tweet
 const Reply = db.Reply
+const pageLimit = 10
 
 const twitterController = {
   getTwitters: (req, res) => {
-    Tweet.findAll({
-      raw: true,
-      nest: true,
-      include: [User],
-      order: [['createdAt', 'DESC']]
-    }).then(tweets => {
-      // console.log('tweets result', tweets)
-      tweets = tweets.map(tweet => ({
-        ...tweet,
-        description: tweet.description.substring(0, 50)
+    let offset = 0
+
+    if (req.query.page) {
+      offset = (Number(req.query.page) - 1) * pageLimit
+    }
+
+    Tweet.findAndCountAll({
+      include: [User], raw: true, nest: true, order: [['createdAt', 'DESC']], offset: offset, limit: pageLimit
+    }).then(result => {
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / pageLimit)
+      const totalPages = Array.from({ length: pages }).map((item, index) => index + 1)
+      const prev = page - 1 <= 0 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+      const tweets = result.rows.map(t => ({
+        ...t,
+        description: t.description.substring(0, 50),
+        User: t.User
       }))
-      return res.status(200).render('tweets', { tweets: tweets })
+      return res.render('tweets', { tweets, totalPages, prev, next, page })
     }
     )
-      .catch(error => {
-        console.log(error)
-        res.sendStatus(400)
-      })
   },
   createTwitters: (req, res, next) => {
     const description = req.body.description
@@ -50,7 +55,7 @@ const twitterController = {
     })
       .then(tweet => {
         tweet = tweet.dataValues
-        tweet.tweetLiked    = tweet.Likes.filter(like => like.likeOrNot === true).length
+        tweet.tweetLiked = tweet.Likes.filter(like => like.likeOrNot === true).length
         tweet.tweetDisliked = tweet.Likes.filter(like => like.likeOrNot === false).length
         return res.render('tweet', { tweet })
       })
