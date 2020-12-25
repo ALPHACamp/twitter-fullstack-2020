cb97075ef91d2e79051d7a3c260b8bd3a0256efc
King LEE <a0988698776@gmail.com>
2020-12-25 16:50:07 +0800

    Make pic to connect to profile

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,31 +1,25 @@
-//編輯個人檔案：名字
+// 編輯個人檔案：名字
 const nameCount = document.querySelector('.nameCount')
 const nameInput = document.querySelector('.nameInput')
 
-//編輯個人檔案：自我介紹
+// 編輯個人檔案：自我介紹
 const introductionInput = document.querySelector('.introductionInput')
 const introductionCount = document.querySelector('.introductionCount')
 
-//首頁左欄推文
+// 首頁左欄推文
 const descriptionCount = document.querySelector('.descriptionCount')
 const descriptionInput = document.querySelector('.descriptionInput')
 
-//字數統計
-function countWords(name, wordslimit) {
-  if (name === "name") {
-    let total = nameInput.value.length;
+// 字數統計
+function countWords (name, wordslimit) {
+  if (name === 'name') {
+    const total = nameInput.value.length
     return nameCount.innerHTML = `${total}/${wordslimit}`
-  } else if (name === "introduction") {
-    let total = introductionInput.value.length;
+  } else if (name === 'introduction') {
+    const total = introductionInput.value.length
     return introductionCount.innerHTML = `${total}/${wordslimit}`
-  } else if (name === "description") {
-    let total = descriptionInput.value.length;
+  } else if (name === 'description') {
+    const total = descriptionInput.value.length
     return descriptionCount.innerHTML = `${total}/${wordslimit}`
   }
 }
-
-
-
-
-
-
