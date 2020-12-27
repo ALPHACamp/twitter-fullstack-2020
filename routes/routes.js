b239a012b51925801e498b83361206b5bb3073e1
King LEE <a0988698776@gmail.com>
2020-12-27 16:03:40 +0800

    personChat version1

--- routes/routes.js
+++ routes/routes.js
@@ -15,6 +15,8 @@ const { authenticatedUser, authenticatedAdmin, beSigned } = require('../middlewa
 const passport = require('../config/passport')
 const user = require('../models/user.js')
 
+router.get('/', (req, res) => res.render('personChat'))
+
 // admin
 /// ////
 router.get('/admin/signin', beSigned, adminController.signinPage)
@@ -26,7 +28,8 @@ router.get('/admin/users', authenticatedAdmin, adminController.getUsers)
 /// ////
 // User
 /// ////
-router.get('/', (req, res) => res.redirect('/tweets'))
+// router.get('/', (req, res) => res.render('/personChat'))
+// router.get('/', (req, res) => res.redirect('/tweets'))
 router.get('/signup', beSigned, userController.signUpPage)
 router.post('/signup', beSigned, userController.signUp)
 router.get('/signin', beSigned, userController.signInPage)
@@ -70,6 +73,6 @@ router.post('/tweets/:id/unlike', authenticatedUser, twitterController.postTwitt
 // chat
 /// ////
 router.get('/globalChat', authenticatedUser, chatController.getGlobalChat)
-
+// router.get('/personChat')
 
 module.exports = router
