45f5e6c0e684270818578d0b1c75ab2d2f822074
Thumper <shaunwhl1991@gmail.com>
2021-02-26 20:00:06 +1100

    add user seeder
    add user seeder

---
+++ seeders/1-users-seed-file.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcryptjs');
+const faker = require('faker');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = [{
+      id          : 1,
+      email       : 'root@example.com',
+      password    : bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name        : faker.name.findName(),
+      avatar      : `https://loremflickr.com/300/300/portrait/?lock=${Math.random() * 100}`,
+      introduction: faker.lorem.text(),
+      role        : 'admin',
+      createdAt   : new Date(),
+      updatedAt   : new Date(),
+    }];
+    [21, 31, 41, 51, 61].forEach((e) => {
+      users.push({
+        id          : e,
+        email       : `user${e}@example.com`,
+        password    : bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        name        : faker.name.findName(),
+        avatar      : `https://loremflickr.com/300/300/portrait/?lock=${Math.random() * 100}`,
+        introduction: faker.lorem.text(),
+        role        : 'user',
+        createdAt   : new Date(),
+        updatedAt   : new Date(),
+      });
+    });
+    await queryInterface.bulkInsert('Users', users, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
