3cd4ba34b2a15dd9fc894c891ecbfaff8b56e6b1
newush <ushneway@gmail.com>
2021-02-26 16:27:31 -0500

    add config passport

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,34 @@
-module.exports = (app) => {
-  
-}
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+
+  (req, username, password, cb) => {
+    User.findOne({ where: { email: username } })
+      .then(user => {
+        if (!user) return cb(null, false)
+        if (!bcrypt.compareSync(password, user.password)) return cb(null, false)
+        return cb(null, user)
+      })
+  }
+))
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id).then(user => {
+    user = user.toJSON()
+    return cb(null, user)
+  })
+})
+
+module.exports = passport
