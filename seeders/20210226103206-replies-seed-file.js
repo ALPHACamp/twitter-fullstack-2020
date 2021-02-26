3507d52a911d7f6c8ceb80970e31a20f285a8c65
yun <n856839@gmail.com>
2021-02-26 19:29:14 +0800

    Add seeders

---
+++ seeders/20210226103206-replies-seed-file.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Replies',
+      Array.from({ length: 150 }).map((_, i) => ({
+        id: i * 10 + 1,        
+        UserId: Math.floor(Math.random() * 5) * 10 + 1,
+        TweetId: (i % 50) * 10 + 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      ), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', null, {})
+  }
+};
