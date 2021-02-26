ee6badab6f3d32af24d9b5cb2e5c42b2cffbb8d3
ddssads <tw0937608360@gmail.com>
2021-02-26 20:32:06 +0800

    fixed routes/admin/home.js

--- routes/admin/home.js
+++ routes/admin/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { authenticatedAdmin } = require('../../middleware/auth')
+const authenticatedAdmin = require('../../middleware/auth').authenticatedAdmin
 //admin/login 
 
 router.get('/', authenticatedAdmin, (req, res) => res.redirect('/admin/tweets'))
