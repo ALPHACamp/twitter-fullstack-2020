965e26cd74706861f033c5d9ba103e14b35abdbe
newush <ushneway@gmail.com>
2021-02-27 13:13:39 -0500

    fix: check existing user in sessionsController register

--- controllers/sessionsController.js
+++ controllers/sessionsController.js
@@ -14,24 +14,26 @@ const sessionsController = {
     User.findOne({
       where: { [Op.or]: [{ email: req.body.email }, { account: req.body.account }] },
     }).then((user) => {
-      if (user.account === req.body.account) {
-        req.flash('error messages', '帳號已經有人用了');
-        return res.redirect('/regist');
+      if (user) {
+        if (user.account === req.body.account) {
+          req.flash('error messages', '帳號已經有人用了');
+          return res.redirect('/regist');
+        }
+        if (user.email === req.body.email) {
+          req.flash('error_messages', '此信箱已存在');
+          return res.redirect('/regist');
+        }
       }
-      if (user.email === req.body.email) {
-        req.flash('error_messages', '此信箱已存在');
-        return res.redirect('/regist');
-      }
-
-      User.create({
+      return User.create({
         email   : req.body.email,
         password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
         name    : req.body.name,
         account : req.body.account,
       }).then((user) => {
         req.flash('success_message', '成功註冊帳號');
-        return res.redirect('/login');
-      });
+        res.redirect('/login');
+      })
+      .catch((error) => console.log('register error', error));
     });
   },
   loginPage: (req, res) => {
