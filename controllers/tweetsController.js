cd82a2728f9e058f816c9c969ea76fa0addb6152
linchuaccount <younchu123@gmail.com>
2021-02-28 15:00:34 +0800

    feat: add tweet create feature

--- controllers/tweetsController.js
+++ controllers/tweetsController.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const { Tweet, Reply, Like, User } = db;
+const helpers = require('../_helpers');
 
 const tweetsController = {
   getIndexPage: (req, res) => {
@@ -12,9 +13,30 @@ const tweetsController = {
         ReplyCount: tweet.Replies.length,
         LikeCount : tweet.Likes.length,
       }));
-      console.log(tweetsObj)
       return res.render('index', { tweets: tweetsObj });
     });
   },
+
+  createTweet: (req, res) => {
+    const { description } = req.body
+    if (!description.trim()) {
+      console.log('error')
+      req.flash('error_messages', '請輸入文字再送出推文');
+      return res.redirect('/');
+    }
+    if (description.length > 140) {
+      console.log('error')
+      req.flash('error_messages', '推文字數不能超過140字');
+      return res.redirect('/');
+    }
+    return Tweet.create({
+      description: req.body.description,
+      UserId: helpers.getUser(req).id
+    }).then(tweet => {
+      res.redirect('/');
+    })
+     
+    
+  }
 };
 module.exports = tweetsController;
--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -6,5 +6,6 @@ const tweetsController = require('../../controllers/tweetsController');
 
 /* GET home page. */
 router.get('/', tweetsController.getIndexPage);
+router.post('/', tweetsController.createTweet);
 
 module.exports = router;
