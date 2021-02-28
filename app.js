19c0eba988026fb6f7c258efb6c522c83679874a
jefflin <linjef0711@gmail.com>
2021-02-28 15:34:23 +0800

    feat : add session, flash-message, dotenv

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const helpers = require('./_helpers')
 const routers = require('./routes')
 
@@ -9,6 +13,8 @@ const port = 3000
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
@@ -20,8 +26,21 @@ app.use(bodyParser.json())
 
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => res.send('test'))
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
 
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
+app.get('/', (req, res) => res.send('test'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 routers(app)
