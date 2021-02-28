d01a6119f9e9a446a6b8c19b8822de3ca4dc9f4c
jefflin <linjef0711@gmail.com>
2021-02-28 15:02:12 +0800

    feat : add body-parser

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ const routers = require('./routes')
 const port = 3000
 
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main', helpers: require('./config/handlebars-helpers') }))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => res.send('test'))
 
