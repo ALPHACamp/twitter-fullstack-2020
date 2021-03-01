09a18ac7957cca9fb1c332b0a4c6a0950222d6c4
linchuaccount <younchu123@gmail.com>
2021-03-01 19:48:03 +0800

    refactor: get tweets function

--- controllers/tweetsController.js
+++ controllers/tweetsController.js
@@ -1,15 +1,19 @@
 const db = require('../models');
-const { Tweet, Reply, Like, User } = db;
+
+const {
+  Tweet, Reply, Like, User,
+} = db;
 const helpers = require('../_helpers');
 
 const tweetsController = {
   getIndexPage: (req, res) => {
-    Tweet.findAll({ 
-      order: [['createdAt', 'DESC']],
-      include: [ User, Reply, Like ] })
+    Tweet.findAll({
+      order  : [['createdAt', 'DESC']],
+      include: [User, Reply, Like],
+    })
     .then((tweets) => {
-      const tweetsObj = JSON.parse(JSON.stringify(tweets, null, 2)).map((tweet) => ({
-        ...tweet,
+      const tweetsObj = tweets.map((tweet) => ({
+        ...tweet.dataValues,
         ReplyCount: tweet.Replies.length,
         LikeCount : tweet.Likes.length,
       }));
@@ -18,25 +22,21 @@ const tweetsController = {
   },
 
   createTweet: (req, res) => {
-    const { description } = req.body
+    const { description } = req.body;
     if (!description.trim()) {
-      console.log('error')
       req.flash('error_messages', '請輸入文字再送出推文');
       return res.redirect('/');
     }
     if (description.length > 140) {
-      console.log('error')
       req.flash('error_messages', '推文字數不能超過140字');
       return res.redirect('/');
     }
     return Tweet.create({
       description: req.body.description,
-      UserId: helpers.getUser(req).id
-    }).then(tweet => {
+      UserId     : helpers.getUser(req).id,
+    }).then((tweet) => {
       res.redirect('/');
-    })
-     
-    
-  }
+    });
+  },
 };
 module.exports = tweetsController;
