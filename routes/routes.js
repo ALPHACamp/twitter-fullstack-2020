c3d35635bafa03402cf65722249b5295f8e0290b
chuni-lin <william811566@gmail.com>
2021-03-03 14:23:05 +0800

    feat: add edit user profile & view followers, followings functions

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,8 @@ const passport = require('passport')
 
 const auth = require('../config/auth')
 const userController = require('../controllers/userController')
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
 
 router.get('/', (req, res) => res.redirect('/signin'))
 router.get('/signin', userController.signInPage)
@@ -18,12 +20,22 @@ router.post('/admin/signin', passport.authenticate('local', {
 }), userController.AdminSignIn)
 router.get('/signout', userController.logout)
 
-router.get('/tweets', auth.authenticatedUser, (req, res) => res.render('test'))
+router.get('/tweets', auth.authenticatedUser, (req, res) => res.render('tweets'))
 router.get('/admin/tweets', auth.authenticatedAdmin, (req, res) => res.render('tweets'))
-
 
 router.get('/followships', auth.authenticatedUser, userController.addFavorite)
 router.delete('/followships/:id', auth.authenticatedUser, userController.removeFavorite)
+
+// user edit 相關路由
+router.get('/api/users/:id', auth.authenticatedUser, userController.editUser)
+router.put('/users/:id/edit', auth.authenticatedUser,
+  upload.fields([{ name: 'cover', maxCount: 1 }, { name: 'avatar', maxCount: 1 }]), userController.putUserInfo)
+router.get('/users/:id/followers', auth.authenticatedUser, userController.getRecommendedFollowings, userController.getUserFollowers) // 被追蹤
+router.get('/users/:id/followings', auth.authenticatedUser, userController.getRecommendedFollowings, userController.getUserFollowings) // 追蹤人
+
+// setting 相關路由
+router.get('/users/:id/setting', auth.authenticatedUser, userController.getSetting)
+router.post('/api/users/:id', auth.authenticatedUser, userController.putSetting)
 
 router.post('/tweets/:tweetId/like', auth.authenticatedUser, userController.addLike)
 router.delete('/tweets/:tweetId/unlike', auth.authenticatedUser, userController.removeLike)
