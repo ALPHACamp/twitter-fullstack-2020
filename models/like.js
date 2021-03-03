c5a1ab344217ec756c0208542519c8c8c492bc17
Thumper <shaunwhl1991@gmail.com>
2021-03-04 01:35:17 +1100

    Refactor: re-declared like and user model
    Refactor: re-declared like and user model

--- models/like.js
+++ models/like.js
@@ -1,5 +1,15 @@
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
+    id: {
+      allowNull    : false,
+      autoIncrement: true,
+      primaryKey   : true,
+      type         : DataTypes.INTEGER,
+    },
+    UserId   : DataTypes.INTEGER,
+    TweetId  : DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
   }, {});
   Like.associate = function (models) {
     Like.belongsTo(models.Tweet);
--- models/user.js
+++ models/user.js
@@ -1,5 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: {
+      allowNull    : false,
+      autoIncrement: true,
+      primaryKey   : true,
+      type         : DataTypes.INTEGER,
+    },
     email: {
       type  : DataTypes.STRING,
       unique: true,
@@ -14,13 +20,24 @@ module.exports = (sequelize, DataTypes) => {
     introduction: DataTypes.TEXT,
     cover       : DataTypes.STRING,
     role        : DataTypes.STRING,
-    createdAt   : DataTypes.DATE,
-    updatedAt   : DataTypes.DATE,
+    createdAt   : {
+      allowNull: false,
+      type     : DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type     : DataTypes.DATE,
+    },
   }, {});
   User.associate = (models) => {
     User.hasMany(models.Tweet);
     User.hasMany(models.Reply);
     User.hasMany(models.Like);
+    User.belongsToMany(models.Tweet, {
+      through   : models.Like,
+      foreignKey: 'UserId',
+      as        : 'LikedTweets',
+    });
     User.belongsToMany(User, {
       through   : models.Followship,
       foreignKey: 'followingId',
