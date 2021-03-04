21e01d902b84ceeadfe11fb1698765590b95c51d
linchuaccount <younchu123@gmail.com>
2021-03-05 01:16:33 +0800

    fix: prevent user follow themself

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -4,12 +4,16 @@ const { Followship } = db;
 
 const followshipController = {
   addFollowship: (req, res) => {
+    if ( req.user.id === req.params.userId ) {
+      req.flash('error_messages', '無法跟隨此用戶');
+      res.redirect('back');
+    }
     Followship.create({
       followerId: req.user.id,
       followingId: req.params.userId,
     })
     .then(() => {
-      return res.redirect('back')
+      return res.redirect('back');
     });
   },
 
@@ -23,7 +27,7 @@ const followshipController = {
     .then((followship) => {
       followship.destroy()
       .then(() => {
-        return res.redirect('back')
+        return res.redirect('back');
       });
     });
   },
