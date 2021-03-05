a15def25bbe1ed548d1808a731bc4a41e6104ba8
Thumper <shaunwhl1991@gmail.com>
2021-03-05 16:17:06 +1100

    Refactor: type casting to check no user self following, and coding style due to eslint issue on @younchu's machine
    Refactor: type casting to check no user self following, and coding style due to eslint issue on @younchu's machine

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -4,32 +4,28 @@ const { Followship } = db;
 
 const followshipController = {
   addFollowship: (req, res) => {
-    if ( req.user.id === req.params.userId ) {
+    if (req.user.id === Number(req.params.userId)) {
       req.flash('error_messages', '無法跟隨此用戶');
       res.redirect('back');
     }
     Followship.create({
-      followerId: req.user.id,
+      followerId : req.user.id,
       followingId: req.params.userId,
     })
-    .then(() => {
-      return res.redirect('back');
-    });
+    .then(() => res.redirect('back'));
   },
 
   removeFollowship: (req, res) => {
     Followship.findOne({
       where: {
-        followerId: req.user.id,
+        followerId : req.user.id,
         followingId: req.params.userId,
       },
     })
     .then((followship) => {
       followship.destroy()
-      .then(() => {
-        return res.redirect('back');
-      });
+      .then(() => res.redirect('back'));
     });
   },
 };
-module.exports = followshipController
+module.exports = followshipController;
