b5ca144d82069815c4d39f8cfc2bddf475d4eedd
linchuaccount <younchu123@gmail.com>
2021-03-11 23:27:18 +0800

    feat: add chatroom and socket.io

---
+++ public/javascripts/socket.js
@@ -0,0 +1,20 @@
+ const socket = io();
+ const messages = document.querySelector('#chat-messages');
+ const chatForm = document.querySelector('#chat-form');
+ const chatInput = document.querySelector('#chat-input');
+
+      chatForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        if (chatInput.value) {
+          socket.emit('chat message', chatInput.value);
+          chatInput.value = '';
+        }
+      });
+      
+      socket.on('chat message', function(msg) {
+        let item = document.createElement('li');
+        item.textContent = msg;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+      });
+      
