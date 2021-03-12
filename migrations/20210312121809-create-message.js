3cc4c67c672f29612d0bb9ba3714944f01560622
Thumper <shaunwhl1991@gmail.com>
2021-03-12 23:53:10 +1100

    Add model to save message
    Add model to save message

---
+++ migrations/20210312121809-create-message.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      senderId: {
+        type: Sequelize.INTEGER
+      },
+      receiverId: {
+        type: Sequelize.INTEGER
+      },
+      message: {
+        type: Sequelize.TEXT
+      },
+      isPublic: {
+        type: Sequelize.BOOLEAN
+      },
+      isRead: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Messages');
+  }
+};
