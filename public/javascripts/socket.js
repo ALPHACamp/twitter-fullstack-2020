a9720d34457253bc5cda13bf2900652a37155617
linchuaccount <younchu123@gmail.com>
2021-03-12 21:15:44 +0800

    feat: add chatroom user list and user online notice

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -2,20 +2,50 @@ const socket = io();
 const messages = document.querySelector('#chat-messages');
 const chatForm = document.querySelector('#chat-form');
 const chatInput = document.querySelector('#chat-input');
+const chatUserList = document.querySelector('#chat-user-list');
 
 // Temporary only, demonstrate the login connection workability
 socket.on('connect', () => {
   console.log('socket.id', socket.id);
 });
 
-// 單一上線使用者資料
-socket.on('joined', (user) => {
-  console.log('user joined', user);
-});
-
 // 使用者已上線, 會同時推送上線的使用者，以及這個使用者加入的房間裡的用戶 array
 socket.on('userJoined', (userObj) => {
-  console.log('userObj', userObj);
+  
+  const usersInRoom = userObj.usersInRoom;
+  document.querySelector('#chatroom-user-count').innerHTML = usersInRoom.length;
+
+  //使用者上線提示
+  const item = document.createElement('li');
+  item.innerHTML = ` ${userObj.user.name} 上線 `;
+  // item.setAttribute('class','mx-auto');
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+
+
+  //傳送上線使用者資料
+  const userItem = document.createElement('div');
+  let rawHTML = '';
+  usersInRoom.forEach((user) => {
+    rawHTML += `
+    <div class="d-flex flex-row no-wrap align-items-star pb-1">
+      <a class="profile-img mr-3" href="/users/${user.id}/tweets"> 
+        <img class="img-fluid rounded-circle" src="${user.avatar}" alt=""> 
+      </a>
+      <div class="item-header d-flex d-column no-wrap justify-content-start align-items-center">
+        <div class="name w-100 pr-2">
+          <a href="/users/${user.id}/tweets class="text-dark text-decoration-none">${user.name}</a>
+        </div>
+        <div class="item-username">
+          <a href="/chatroom/${user.id}" class="text-dark text-decoration-none">
+            <span class="username text-lightgrey">@${user.account}</span>
+          </a>
+        </div>
+      </div>
+    </div>`
+  });
+    userItem.innerHTML = rawHTML;
+  chatUserList.appendChild(userItem);
 });
 
 // 如果是公開聊天室，會向後端要求 'join' 'public'這個房間
@@ -33,13 +63,15 @@ if (chatForm !== null) {
   });
 }
 
-socket.on('chat message', (msg) => {
+//傳送使用者聊天訊息
+socket.on('newMessage', (message) => {
+  console.log(message)
   const item = document.createElement('li');
   item.innerHTML = `
     <div class="d-flex align-items-end">
-      <img id="chat-user-avatar" class="rounded-circle mr-2" src="https://i.imgur.com/AoWNOVG.jpg" alt="">
+      <img id="chat-user-avatar" class="rounded-circle mr-2" src="${message.sender.avatar}" alt="">
         <div id="chat-bubble" class="item-desc pt-2 pb-1">
-          ${msg}
+          ${message.message}
         </div>
        </div>
       <div id="chat-createdAt" class="ml-5">下午5:00</div>
