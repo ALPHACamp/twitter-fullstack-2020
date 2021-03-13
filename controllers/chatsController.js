3c8fca2c9019f73774ee471b2521e214f8053289
Thumper <shaunwhl1991@gmail.com>
2021-03-13 17:24:04 +1100

    Add pm routes and private chat page controller function
    Add pm routes and private chat page controller function

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -1,5 +1,9 @@
 const db = require('../models');
 
+const { Message, User } = db;
+
+const { getUser } = require('../_helpers');
+
 const chatsController = {
   getPublicChatPage: (req, res) => {
     res.render('chatroom', {
@@ -8,6 +12,26 @@ const chatsController = {
       },
     });
   },
+  getPrivateChatPage: (req, res) => {
+    let userMessaging;
+    if (req.params.receiverId) {
+      req.app.set('privateMessageReceiverId', Number(req.params.receiverId));
+      userMessaging = Number(req.params.receiverId);
+    }
 
+    // TODO: get list of users the current user has sent PM to, assign it back as usersPMSent
+    const usersPMSent = [
+      getUser(req),
+      getUser(req),
+    ];
+
+    res.render('chatroom', {
+      title: {
+        text: '私人訊息',
+      },
+      usersPMSent,
+      userMessaging,
+    });
+  },
 };
 module.exports = chatsController;
