a940b7989bc4d3e7949e73e90a4d3138d6c09b8e
Thumper <shaunwhl1991@gmail.com>
2021-03-14 01:35:13 +1100

    Add load private page to include interacted User List
    Add load private page to include interacted User List

--- controllers/chatsController.js
+++ controllers/chatsController.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const db = require('../models');
 
 const { Message, User } = db;
@@ -14,19 +15,72 @@ const chatsController = {
   },
   getPrivateChatPage: (req, res) => {
     const userMessaging = (req.params.receiverId) ? Number(req.params.receiverId) : null;
+    const privateMessageSenderId = getUser(req);
 
-    // TODO: get list of users the current user has sent PM to, assign it back as usersPMSent
-    const usersPMSent = [
-      getUser(req),
-      getUser(req),
-    ];
+    Promise.all([
+      Message.findAll({
+        raw  : true,
+        nest : true,
+        where: {
+          [Op.or]: [
+            {
+              senderId: privateMessageSenderId,
+            },
+            {
+              receiverId: privateMessageSenderId,
+            },
+          ],
+          isPublic: 0,
+        },
+        include: [{
+          model: User,
+          as   : 'Sender',
+        }, {
+          model: User,
+          as   : 'Receiver',
+        }],
+        order: [
+          // Will escape title and validate DESC against a list of valid direction parameters
+          ['createdAt', 'DESC'],
+        ],
+      }),
+    ])
+    .then(([interactionMessages]) => {
+      // Modify interacted user list
+      const userList = [];
+      interactionMessages.forEach((message) => {
+        if (message.Sender.id === Number(privateMessageSenderId)) {
+          const receiverObj = message.Receiver;
+          Object.assign(receiverObj, {
+            lastMessage: message.message,
+            createdAt  : message.createdAt,
+          });
+          userList.push(receiverObj);
+        } else {
+          const senderObj = message.Sender;
+          delete senderObj.password;
+          Object.assign(senderObj, {
+            lastMessage: message.message,
+            createdAt  : message.createdAt,
+          });
+          userList.push(senderObj);
+        }
+      });
 
-    res.render('chatroom', {
-      title: {
-        text: '私人訊息',
-      },
-      usersPMSent,
-      userMessaging,
+      const interactedUserList = userList
+      .filter((v, i, a) => a.findIndex((t) => (t.id === v.id)) === i)
+      .map((user) => ({
+        ...user,
+        createdAt: `${moment(user.createdAt).format('a h:mm')}`,
+      }));
+
+      return res.render('chatroom', {
+        title: {
+          text: '私人訊息',
+        },
+        usersPMSent: interactedUserList,
+        userMessaging,
+      });
     });
   },
 };
