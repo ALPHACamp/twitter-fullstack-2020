59ce3065a38f737886bc390ebc894e23a1ebe601
LeeChan <lee79102@hotmail.com>
2021-04-19 22:47:15 +0800

    feat: add express-handlebars and some pages

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const helpers = require('./_helpers');
 
 const app = express()
-const port = 3000
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) // Handlebars 註冊樣板引擎
+app.set('view engine', 'hbs') // 設定使用 Handlebars 做為樣板引擎, 使用縮寫hbs
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/users/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+    res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+    res.render('register')
+})
+
+app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
+})
 
 module.exports = app
