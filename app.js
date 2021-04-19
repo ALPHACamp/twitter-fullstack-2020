c36a8ca1b7fcc9d4e41913e1523460d2a6f9ed0d
LeeChan <lee79102@hotmail.com>
2021-04-19 23:06:47 +0800

    feat: set router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+
 const helpers = require('./_helpers');
 
 const app = express()
@@ -14,22 +15,9 @@ app.use(methodOverride('_method'))
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.get('/users/login', (req, res) => {
-    res.render('login')
-})
-
-app.post('/users/login', (req, res) => {
-    res.send('login')
-})
-
-app.get('/users/register', (req, res) => {
-    res.render('register')
-})
 
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`)
 })
 
-module.exports = app
+require('./routes')(app)
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,20 @@
+// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
+// 準備引入路由模組
+router.get('/', (req, res) => res.send('Hello World!'))
 
+router.get('/users/login', (req, res) => {
+    res.render('login')
+})
+
+router.post('/users/login', (req, res) => {
+    res.send('login')
+})
+
+router.get('/users/register', (req, res) => {
+    res.render('register')
+})
+
+// 匯出路由器
 module.exports = router
