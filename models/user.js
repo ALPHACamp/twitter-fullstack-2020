bb01b9a0f25330c9b39425def1fce80045462529
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-04-20 15:33:01 +0800

    feat:add followers and followings to users

--- models/user.js
+++ models/user.js
@@ -6,6 +6,16 @@ module.exports = (sequelize, DataTypes) => {
     User.hasMany(models.Tweet)
     User.hasMany(models.Reply)
     User.hasMany(models.Like)
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
   };
   return User;
 };
