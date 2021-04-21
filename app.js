5dc55262f19db9536bdba7dcd5394347a133c495
Ace1862020 <kyds1862015@gmail.com>
2021-04-21 21:37:50 +0800

    Comfirm user password and unique email & account and add message.handlebars

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const bodyParser = require('body-parser')
-
+const { urlencoded } = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 const helpers = require('./_helpers');
-const { urlencoded } = require('body-parser');
 
 const app = express()
 const port = 3000
@@ -15,6 +15,13 @@ app.set('view engine', 'handlebars')
 
 // body-parser
 app.use(urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,22 +9,41 @@ const userController = {
     return res.render('signup')
   },
 
-  signUp: (req, res) => {
+  signUp: async (req, res) => {
     const { account, name, email, password, passwordCheck } = req.body
-    if (!account || !name || !email || !password || !passwordCheck) {
-      return res.redirect('back')
-    }
+    const errors = []
     if (password !== passwordCheck) {
-      return res.redirect('back')
+      errors.push({ message: '密碼與確認密碼不相符' })
     }
-    User.create({
-      account: account,
-      name: name,
-      email: email,
-      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
+    if (errors.length > 0) {
+      return res.render('signup', { account, name, email, password, passwordCheck, errors })
+    }
+
+    try {
+      const userAccount = await User.findOne({ where: { account } })
+      const userEmail = await User.findOne({ where: { email } })
+
+      if (userAccount) {
+        req.flash('error_msg', '帳號已被註冊過了')
+        return res.redirect('/signup')
+      }
+      if (userEmail) {
+        req.flash('error_msg', 'Email已被註冊過了')
+        return res.redirect('/signup')
+      }
+
+      await User.create({
+        account: account,
+        name: name,
+        email: email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+      })
+
+      req.flash('success_msg', '帳號註冊成功')
       return res.redirect('/signin')
-    })
+    } catch (error) {
+      console.log(error)
+    }
   },
 
   signInPage: (req, res) => {
