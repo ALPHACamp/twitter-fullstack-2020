63f95e7965789ed57aa7863859115bceac9ba801
Ace1862020 <kyds1862015@gmail.com>
2021-04-21 16:21:50 +0800

    Add signup controller & signup page

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+
 const helpers = require('./_helpers');
+const { urlencoded } = require('body-parser');
 
 const app = express()
 const port = 3000
-
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser, urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const helpers = require('./_helpers');
+const User = db.User
+
+const userController = {
+
+  signUpPage: (req, res) => {
+    return res.render('signup')
+  },
+
+  signUp: (req, res) => {
+
+  },
+
+}
+
+module.exports = userController
