47d7c32d29a3593c8527e71089aa58adc1a7b0bc
Ace1862020 <kyds1862015@gmail.com>
2021-04-21 17:58:34 +0800

    Add signup feature and Get signin route

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,8 +10,26 @@ const userController = {
   },
 
   signUp: (req, res) => {
-
+    const { account, name, email, password, passwordCheck } = req.body
+    if (!account || !name || !email || !password || !passwordCheck) {
+      return res.redirect('back')
+    }
+    if (password !== passwordCheck) {
+      return res.redirect('back')
+    }
+    User.create({
+      account: account,
+      name: name,
+      email: email,
+      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+    }).then(user => {
+      return res.redirect('/signin')
+    })
   },
+
+  signInPage: (req, res) => {
+    return res.render('signin')
+  }
 
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,8 @@ const userController = require('../controllers/userController')
 
 
 router.get('/signup', userController.signUpPage)
-
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
 
 
 module.exports = router
