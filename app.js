384119d41bf92ac2e4315acf7a38f17ca5e93f2b
Ace1862020 <kyds1862015@gmail.com>
2021-04-22 16:25:44 +0800

    Add authenticated to determine user or Admin

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg')
   res.locals.error_msg = req.flash('error_msg')
+  res.locals.user = helpers.getUser(req)
   next()
 })
 
