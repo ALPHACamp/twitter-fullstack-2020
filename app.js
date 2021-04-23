9c387263fb353c0440944159ae378dcda46bb4f0
Bob Huo <bob83080@gmail.com>
2021-04-23 19:12:56 +0800

    Confirm user password, unique account & email

--- app.js
+++ app.js
@@ -14,7 +14,17 @@ const port = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: require('./config/handlebars-helpers') })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'hbs') // 設定使用 Handlebars 做為樣板引擎, 使用縮寫hbs
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+
 app.use(methodOverride('_method'))
+
+app.use((req, res, next) => {
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    next()
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -9,18 +9,47 @@ let userController = {
   loginPage: (req, res) => {
     return res.render('login')
   },
+
+
+
   registerPage: (req, res) => {
     return res.render('register')
   },
   userRegister: (req, res) => {
-    User.create({
-      account: req.body.account,
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
-      return res.redirect('/login')
-    })
+    if (req.body.confirmPassword !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/register')
+    }
+    else {
+      // confirm unique account
+      User.findOne({ where: { account: req.body.account } }).then(user => {
+        if (user) {
+          req.flash('error_messages', '帳號重複！')
+          return res.redirect('/register')
+        }
+        else {
+          // confirm unique user
+          User.findOne({ where: { email: req.body.email } }).then(user => {
+            if (user) {
+              req.flash('error_messages', '信箱重複！')
+              return res.redirect('/register')
+            } else {
+              User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
+                image: null
+              }).then(user => {
+                req.flash('success_messages', '成功註冊帳號！')
+                return res.redirect('/login')
+              })
+            }
+          })
+
+        }
+
+      })
+    }
   },
 
   settingPage: (req, res) => {
