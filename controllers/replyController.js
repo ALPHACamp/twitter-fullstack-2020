c946efcad23ad1efb03a3646d141c1812395d288
Bob Huo <bob83080@gmail.com>
2021-04-26 05:35:40 +0800

    modify post reply limit and correct user profile view

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -5,6 +5,15 @@ const helpers = require('../_helpers')
 
 const replyController = {
   postReply: (req, res) => {
+    const comment = req.body.description
+    if (!comment.length) {
+      req.flash('error_messages', '回覆不可為空白!')
+      res.redirect('back')
+    }
+    if (comment.length > 140) {
+      req.flash('error_messages', '回覆不可超過140字!')
+      res.redirect('back')
+    }
     return Reply.create({
       UserId: req.user.id,
       TweetId: req.params.id,
@@ -13,6 +22,7 @@ const replyController = {
       .then((reply) => {
         res.redirect(`/tweets/${req.params.id}`)
       })
+
   },
 
 }
