4c4330c434f48b5966dfea00dab26b73fcec028c
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-04-26 20:25:48 +0800

    fixed: add Tweet db in passport

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ const LocalStrategy = require('passport-local')
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
+const Tweet = db.Tweet
 
 // setup passport strategy
 passport.use(new LocalStrategy(
