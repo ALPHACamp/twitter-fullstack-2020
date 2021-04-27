f8a21cbfef8706e22175a518166c798ff2505f5f
Bob Huo <bob83080@gmail.com>
2021-04-28 07:29:16 +0800

    message modify and admin twwets list order

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -26,6 +26,7 @@ let adminController = {
             raw: true,
             nest: true,
             include: [User],
+            order: [['createdAt', 'DESC']],
         }).then((tweets) => {
             console.log(tweets)
             return res.render('admin/tweets', { tweets })
