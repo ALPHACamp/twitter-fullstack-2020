4a43e191d0d92f3ac89cfec2c2366f5362780c76
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-04-29 17:13:23 +0800

    merge getTweet conflit

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -26,11 +26,7 @@ const tweetController = {
       })
   },
 
-<<<<<<< HEAD
   getTweet: (req, res) => {
-=======
-  getTweet: (req, res ) => {
->>>>>>> ac8007b939d72d9bfcc524de182880f2d427f63c
     Tweet.findByPk(req.params.id, {
       include: [User,
         { model: Reply, include: [User] },
@@ -43,14 +39,8 @@ const tweetController = {
         return res.render('tweet', {
           tweet: tweet.toJSON(),
           isLiked: isLiked
-<<<<<<< HEAD
-        }),
-          done()
-=======
         })
->>>>>>> ac8007b939d72d9bfcc524de182880f2d427f63c
       })
-
   },
 
   getUser: async (req, res) => {
