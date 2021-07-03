18d12dc8d0c051f9efd54d4049f2b3202157a0ea
Nina19980108 <ninagirl8787@gmail.com>
2021-07-03 21:44:56 +0800

    feat: modify main.hbs

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,5 @@ const adminController = require('../controller/adminController')
 
 module.exports = (app) => {
   app.get('/', adminController.getAdminTweets)
+  app.get('/admin/tweets', adminController.getAdminTweets)
 }
