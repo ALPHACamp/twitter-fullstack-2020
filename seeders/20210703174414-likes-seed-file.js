4909e5d45d9e873ab071d4025aacb6f9345b8009
MOMOJMOGG <momomacaca@gmail.com>
2021-07-04 11:51:14 +0800

    Add likes seed data

--- seeders/20210703174414-likes-seed-file.js
+++ seeders/20210703174414-likes-seed-file.js
@@ -1,26 +1,21 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Likes',
+      Array.from({ length: 30 })
+        .map((item, index) =>
+        ({
+          id: index * 10 + 1,
+          UserId: (Math.floor(Math.random() * 5) + 1) * 10 + 1,
+          TweetId: Math.floor(Math.random() * 50) * 10 + 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+        ), {})
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Likes', null, {})
   }
 };
