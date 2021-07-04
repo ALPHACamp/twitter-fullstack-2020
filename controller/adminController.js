6f8936258fd2eb01b54ed21d920066e7bc3dfd81
Nina19980108 <ninagirl8787@gmail.com>
2021-07-04 13:03:04 +0800

    feat: add seed to getAdminTweets function

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,6 +1,17 @@
+const db = require('../models')
+const User = db.User
+const Tweet = db.Tweet
+
 const adminController = {
   getAdminTweets: (req, res) => {
-    res.render('admin/tweets')
+    return Tweet.findAll({
+      raw: true,
+      nest: true,
+      include: [User]
+    }).then(tweets => {
+      console.log(tweets)
+      return res.render('admin/tweets', { tweets })
+    })
   }
 }
 module.exports = adminController
