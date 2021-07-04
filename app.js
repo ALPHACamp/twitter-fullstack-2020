cc728311fdfde5d44e2a00e10e7728b7d3da2f5e
afwyou <afwyousenge@gmail.com>
2021-07-05 07:08:50 +0800

    add views and hbs-helper, setup app.js, but can't get "/"

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const session = require('express-session')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const helpers = require('./_helpers');
-
+const passport = require('./config/passport')
+const db = require('./models') // 引入資料庫
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers: require('./config/handlebars-helpers')
+}))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(methodOverride('_method'))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = helpers.getUser(req)
+  next()
+})
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+require('./routes')(app, passport)
 
 module.exports = app
