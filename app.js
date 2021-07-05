54bd8c249b1f0ebb2683feabc9fbd0ece7c74eea
hazelisles <zhccj19@gmail.com>
2021-07-06 00:49:45 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express')
-const helpers = require('./_helpers');
-
+const handlebars = require('express-handlebars')
+const helpers = require('./_helpers')
+const db = require('./models')
+const methodOverride = require('method-override')
+const passport = require('./config/passport')
+const session = require('express-session')
+const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-// use helpers.getUser(req) to replace req.user
-// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs', helpers: require('./config/hbs-helpers') }))
+app.use('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use('/upload', express.static(__dirname + '/upload'))
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'ssseeecccrrreett',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(methodOverride('_method'))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = helpers.getUser(req)
+  next()
+})
 
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+require('./routes')(app)
 
 module.exports = app
