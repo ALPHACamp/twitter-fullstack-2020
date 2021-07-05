94aef7560d67ec7ce1ab76df547198b2e0ab3b9c
changjandam <changjandamtw@gmail.com>
2021-07-05 19:18:29 +0800

    signup layout done

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const helpers = require('./_helpers');
-
+const hbs = require('express-handlebars')
 const app = express()
 const port = 3000
+
+// set public file
+app.use(express.static('public'))
+// set handlebars
+app.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+app.set('view engine', 'hbs')
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('signup'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
