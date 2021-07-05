00ea2767375b51937addd0dfd416703ee2cd807a
Nina19980108 <ninagirl8787@gmail.com>
2021-07-05 20:21:55 +0800

    feat: modify userTweet layout

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,15 @@
+const db = require('../models')
+const User = db.User
+
 const userController = {
   getUserTweets: (req, res) => {
-    res.render('user/tweets')
+    return User.findOne({
+      where: {
+        id: req.params.userId
+      }
+    }).then(user => {
+      return res.render('user/tweets', { user: user.toJSON() })
+    })
   }
 }
 module.exports = userController
