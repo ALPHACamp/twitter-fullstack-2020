8e726570db662b2fc8330e18c46ef841053e3d3f
Nina19980108 <ninagirl8787@gmail.com>
2021-07-05 14:54:25 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const helpers = require('./_helpers');
+
+// use helpers.getUser(req) to replace req.user
+// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 const app = express()
 const port = 3000
@@ -12,8 +16,7 @@ app.engine('hbs', exphbs({
 }))
 app.set('view engine', 'hbs')
 
-// use helpers.getUser(req) to replace req.user
-// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+app.use(methodOverride('_method'))
 
 require('./routes/index')(app)
 // app.get('/', (req, res) => res.send('Hello World!'))
