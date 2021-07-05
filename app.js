ea4a0ec5593f8d0f07eaad973f66d779b754e2cf
lettuce <lettuce.lu@lettucelu.com>
2021-07-05 22:23:43 +0800

    testing

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  console.log('1234')
+  res.send('Hello World!')
+}
+
+)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
