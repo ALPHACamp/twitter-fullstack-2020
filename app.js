9632bcebbf8897c4afb2cce1b1e19604a92daab2
DennisWei9898 <denniseasycard@gmail.com>
2021-07-05 22:13:37 +0800

    update package.json 、 model & migration

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const helpers = require('./_helpers');
-
+const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.engine('hbs', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
+
+app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
+
+require('./routes')(app)
 
 module.exports = app
--- migrations/20190115071418-create-tweet.js
+++ migrations/20190115071418-create-tweet.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Replies', {
+    return queryInterface.createTable('Tweets', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -11,11 +11,11 @@ module.exports = {
       UserId: {
         type: Sequelize.INTEGER
       },
-      TweetId: {
-        type: Sequelize.INTEGER
-      },
-      comment: {
+      content: {
         type: Sequelize.TEXT
+      },
+      likes: {
+        type: Sequelize.INTEGER
       },
       createdAt: {
         allowNull: false,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Replies');
+    return queryInterface.dropTable('Tweets');
   }
 };
