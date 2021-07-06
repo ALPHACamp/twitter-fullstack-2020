e239677f480bdfa219f8781785ec782032d4a9e5
wei <a1234567045@yahoo.com.tw>
2021-07-06 15:58:11 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const helpers = require('./_helpers');
 
 const app = express()
 const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
@@ -14,5 +18,7 @@ app.get('/', (req, res) => {
 
 )
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//require('./routes')(app)
 
 module.exports = app
