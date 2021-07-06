dec86b60f7f2bbb10b8c38d6105075005ac38525
hazelisles <zhccj19@gmail.com>
2021-07-07 03:32:47 +0800

    feat: add admin signin page and route

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,15 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
+
 const adminController = {
   getTweets: (req, res) => {
+    return res.render('admin/tweets')
+  },
+  signInPage: (req, res) => {
+    return res.render('admin/signin')
+  },
+  signIn: (req, res) => {
     return res.render('admin/tweets')
   }
 }
