75b0e533df0a7edd76d5523c8071ba966d0639ba
elliotyou <m9402311@mail.ntust.edu.tw>
2021-07-06 16:44:28 +0800

    modify some typos

--- seeders/20210706012643-followships-seed-file.js
+++ seeders/20210706012643-followships-seed-file.js
@@ -1,20 +1,5 @@
 'use strict';
 
-const mappingTable = [
-  [1, 2],
-  [1, 3],
-  [1, 4],
-  // [2,1],
-  [2, 3],
-  // [2,4],
-  [3, 1],
-  [3, 2],
-  [3, 4],
-  // [4,1],
-  [4, 2],
-  // [4,3]
-]
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Followships', [{
@@ -51,12 +36,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    await queryInterface.bulkDelete('Followships', null, {})
   }
 };
