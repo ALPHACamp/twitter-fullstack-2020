46eae66f8d7fe2e107f6d85e7879abf7136cb30a
Nina19980108 <ninagirl8787@gmail.com>
2021-07-06 15:15:10 +0800

    feat: send data to render user/tweets

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,53 @@
 const db = require('../models')
 const User = db.User
+const Tweet = db.Tweet
+const Like = db.Like
+const Followship = db.Followship
+const Reply = db.Reply
 
 const userController = {
+  // getUserTweets: (req, res) => {
+  //   return User.findOne({
+  //     where: {
+  //       id: req.params.userId
+  //     }
+  //   }).then(user => {
+  //     return res.render('user/tweets', { user: user.toJSON() })
+  //   })
+  // },
+
   getUserTweets: (req, res) => {
     return User.findOne({
       where: {
         id: req.params.userId
       }
     }).then(user => {
-      return res.render('user/tweets', { user: user.toJSON() })
+      Followship.findAndCountAll({
+        raw: true,
+        nest: true,
+        where: { followerId: user.id },
+      }).then(following => {
+        Followship.findAndCountAll({
+          raw: true,
+          nest: true,
+          where: { followingId: user.id },
+        }).then(follower => {
+          Tweet.findAll({
+            raw: true,
+            nest: true,
+            where: { userId: user.id },
+          }).then(tweets => {
+            return res.render('user/tweets', {
+              user,
+              followingCount: following.count,
+              followerCount: follower.count,
+              tweets
+            })
+          })
+        })
+      })
     })
   }
+
 }
 module.exports = userController
