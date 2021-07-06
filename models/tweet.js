6c0e0c776ea6ccc98cd4663d0968c78bf36ba2f8
changjandam <changjandamtw@gmail.com>
2021-07-06 19:12:57 +0800

    fix missing file

---
+++ models/tweet.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Tweet = sequelize.define('Tweet', {
+    UserId: DataTypes.INTEGER,
+    description: DataTypes.STRING
+  }, {});
+  Tweet.associate = function (models) {
+    // associations can be defined here
+    Tweet.belongsTo(models.User)
+    Tweet.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'TweetId',
+      as: 'LikedUsers'
+    })
+    Tweet.hasMany(model.Reply)
+  };
+  return Tweet;
+};
