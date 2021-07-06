9266f365a4f9939f5b026fa892fdc1ea2840d40c
Nina19980108 <ninagirl8787@gmail.com>
2021-07-06 15:58:54 +0800

    feat: add connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
+const flash = require('connect-flash')
 
 const app = express()
 const port = 3000
@@ -15,8 +16,14 @@ app.set('view engine', 'hbs')
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.top_following = req.flash('top_following')
+  next()
+})
+
+
 require('./routes/index')(app)
-// app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
