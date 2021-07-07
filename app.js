7075806e78634fd86f2455cea5ea64e2659de50c
hazelisles <zhccj19@gmail.com>
2021-07-07 17:01:09 +0800

    update setting

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.use((req, res, next) => {
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-require('./routes')(app, passport)
+require('./routes')(app)
 
 module.exports = app
