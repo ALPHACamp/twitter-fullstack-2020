492d2a0b42bb2fa6edf9cabd5c07c52ac15035bc
MOMOJMOGG <momomacaca@gmail.com>
2021-07-07 12:38:22 +0800

    add get Tweets feature, but yet finished

---
+++ controller/tweetController.js
@@ -0,0 +1,68 @@
+const { sequelize } = require('../models')
+const db = require('../models')
+const User = db.User
+const Tweet = db.Tweet
+const Like = db.Like
+const Followship = db.Followship
+const Reply = db.Reply
+
+const pageLimit = 10
+
+const tweetController = {
+  getTweets: async (req, res) => {
+    try {
+      const user = await User.findByPk(11, { attributes: ['id', 'avatar'] })
+      const tweets = await Tweet.findAndCountAll({
+        raw: true,
+        nest: true,
+        attributes: ['id', 'description', 'updatedAt'],
+        include: [
+          { model: User, attributes: ['id', 'avatar', 'name', 'account'] }
+        ],
+        order: [['updatedAt', 'DESC']]
+      })
+
+      let Data = []
+      Data = tweets.rows.map(async (tweet, index) => {
+        const [replyCount, likeCount] = await Promise.all([
+          Reply.findAndCountAll({
+            raw: true,
+            nest: true,
+            where: { TweetId: tweet.id },
+          }),
+          Like.findAndCountAll({
+            raw: true,
+            nest: true,
+            where: { TweetId: tweet.id },
+          }),
+        ])
+        return {
+          ...tweet,
+          replyCount: replyCount.count,
+          likeCount: likeCount.count
+        }
+      })
+
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(tweets.count / pageLimit)
+      const totalPage = Array.from({ length: pages }, (item, index) => index + 1)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+
+      Promise.all(Data).then(data => {
+        return res.render('index', {
+          data,
+          user,
+          page,
+          totalPage,
+          prev,
+          next
+        })
+      })
+    } catch (err) {
+      console.warn(err)
+      // return res.redirect('/') // 假定回到首頁
+    }
+  }
+}
+module.exports = tweetController
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../controller/adminController')
+const tweetController = require('../controller/tweetController')
 
 module.exports = (app) => {
-  app.get('/', adminController.getAdminTweets)
+  app.get('/', tweetController.getTweets)
   app.get('/admin/tweets', adminController.getAdminTweets)
   app.get('/admin/users', adminController.getAdminUsers)
   app.delete('/admin/tweets/:tweetId', adminController.deleteAdminTweet)
