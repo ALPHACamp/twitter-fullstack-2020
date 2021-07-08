d5f1363dc2fc643ca523e553b42d9aa0af20ae0f
wei <a1234567045@yahoo.com.tw>
2021-07-08 15:01:30 +0800

    n2

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,28 @@ module.exports = app => {
   app.get('/twitters', twitController.getTwitters)
 
   //admin
+
   app.get('/admin', (req, res) => res.redirect('/admin/twitters'))
   app.get('/admin/twitters', adminController.getTwitters)
-  app.get('/admin/signin', adminController.adminsignin)
+
+  app.get('/admin/signin', adminController.adminSignin)
+
+
+  app.get('/user/self', (req, res) => {
+    res.render('user')
+  })
+
+  app.get('/signin', (req, res) => {
+    res.render('signin')
+  })
+
+  app.get('/signup', (req, res) => {
+    res.render('signup')
+  })
+
+  app.get('/setting', (req, res) => {
+    res.render('setting')
+  })
 
 }
 
