a379794c64878973ba9a30d846483671ecae2cf0
Kou <ioiowax2020.jp@gmail.com>
2021-07-10 01:45:52 +0900

    feat: puclic file, add link css at main.hbs , modify followership.hbs

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const port = process.env.PORT || 3000
 
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs', helpers: require('./config/hbs-helpers') }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use('/upload', express.static(__dirname + '/upload'))
 app.use(session({
