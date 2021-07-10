406cc0f1caa9d9480f557fdaad8ac8b3f053271f
hazelisles <zhccj19@gmail.com>
2021-07-11 00:06:35 +0800

    feat: add admin profile page and update feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
+const imgur = require('imgur-node-api')
 const { User, Tweet, sequelize } = require('../models')
 const { Op } = require('sequelize')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const adminController = {
   getTweets: (req, res) => {
@@ -71,6 +73,57 @@ const adminController = {
       return res.render('admin/users', { admin })
     })
   },
+  getProfile: (req, res) => {
+    return res.render('admin/profile')
+  },
+  getEditProfile: (req, res) => {
+    const edit = true
+    return User.findByPk(req.user.id).then(theuser => {
+      theuser = theuser.toJSON()
+      const { name, account, email } = theuser
+      return res.render('admin/profile', { edit, name, account, email })
+    })
+  },
+  putProfile: async (req, res) => {
+    const { name, account, email } = req.body
+    const { file } = req
+    const edit = true
+    const errors = []
+    try {
+      console.log(req.body, '------', req.file)
+      if (!name || !account || !email) {
+        req.flash('error_messages', '皆必填，不可以空白')
+        return res.redirect('back')
+      }
+
+      const [a, e] = await Promise.all([User.findOne({ raw: true, nest: true, where: { account } }), User.findOne({ raw: true, nest: true, where: { email } })])
+      if (a) {
+        errors.push({ msg: '此帳號已有人使用。' })
+      }
+      if (e) {
+        errors.push({ msg: '此Email已有人使用。' })
+      }
+      if (a || e) {
+        return res.render('admin/profile', { errors, edit, name, account, email })
+      }
+
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, async (err, img) => {
+        const admin = await User.findByPk(req.user.id)
+        await admin.update({
+          name, account, email,
+          img: file ? img.date.link : user.img
+        })
+        req.flash('success_messages', '成功更新個人資料！')
+        return res.redirect('/admin/profile')
+      })
+    } catch (error) {
+      console.warn(error)
+    }
+  },
+  // check: (req, res) => {
+  //   return res.json({ status: 200 })
+  // }
   signInPage: (req, res) => {
     return res.render('admin/signin')
   },
