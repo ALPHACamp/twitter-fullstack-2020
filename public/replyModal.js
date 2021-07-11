c59996d58f6de8321fd433a1d864143144a7d0bd
elliotyou <m9402311@mail.ntust.edu.tw>
2021-07-12 00:48:50 +0800

    complete reply function in tweets.hbs, including the corresponding modal

--- public/replyModal.js
+++ public/replyModal.js
@@ -3,32 +3,30 @@ const INDEX_URL = BASE_URL + '/api/tweets/'
 
 const switchModal = document.querySelectorAll('.switch-modal')
 
-function showReplyModal(id) {
-  const replyUserName = document.querySelector('#modal-reply-user-name')
-  const replyUserAccountAndTime = document.querySelector('#modal-reply-user-accountAndTime')
-  const replyUserAvatar = document.querySelector('#modal-reply-user-avatar')
-  const replyDescription = document.querySelector('#modal-reply-description')
+function showReplyModal(DOM, id) {
+  // console.log('line29...DOM', DOM)
+  const nameAndTimeAndDescription = document.querySelector('#nameAndTimeAndDescription')
+  const applicantAvatar = document.querySelector('#modal-reply-user-avatar')
   const replyTo = document.querySelector('#modal-reply-to')
-  const replyForm = document.querySelector('#modal-reply-form')
-  const replierAvatar = document.querySelector('#modal-replier-avatar')
+  const formGroup = document.querySelector('#modal-reply-form')
 
-  axios.get(INDEX_URL + id)
-    .then(res => {
-      const { User, createdAt, description, id } = res.data.tweet
-      const userAvatart = User.avatar || 'https://i.imgur.com/3P9xRqb.jpg'
-      replyUserName.innerText = User.name
-      replyUserAccountAndTime.innerText = User.account + '·' + createdAt
-      replyUserAvatar.innerHTML = `<img src="${userAvatart}" alt="" style="max-width: 50px; border-radius: 50%">`
-      replyForm.setAttribute('action', `/tweets/${id}/replies`)
-      replyDescription.innerText = description
-      replyTo.innerText = User.name
-    })
+  const nameAndTimeAndDescriptionInnerHTML = DOM.previousElementSibling.innerHTML;
+  const applicantAvatarInnerHTML = DOM.parentElement.previousElementSibling.innerHTML;
+  let replyToInnerHTML = '回覆給 ';
+  replyToInnerHTML += DOM.previousElementSibling.firstElementChild.firstElementChild.nextElementSibling.innerHTML;
+
+  // console.log('into public/replyModal/line15...replyToInnerHTML', DOM.previousElementSibling.firstElementChild.firstElementChild.innerHTML)
+  nameAndTimeAndDescription.innerHTML = nameAndTimeAndDescriptionInnerHTML
+  applicantAvatar.innerHTML = applicantAvatarInnerHTML
+  replyTo.innerHTML = replyToInnerHTML
+  formGroup.setAttribute('action', `/tweets/${id}/replies`)
 }
-
-
 
 switchModal.forEach(tweet => {
   tweet.addEventListener('click', function onSwitchModalClicked(event) {
-    showReplyModal(Number(event.target.dataset.id))
+    const currentDOM = event.target.tagName === 'svg' ? event.target.parentElement : event.target
+    const id = currentDOM.dataset.id;
+    const newDOM = currentDOM.parentElement.parentElement;
+    showReplyModal(newDOM, id)
   })
 })
