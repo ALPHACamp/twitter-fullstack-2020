fb49d4f6b25160e6f1ed990e8262af60510d4717
Kizuki199 <82953069+Kizuki199@users.noreply.github.com>
2021-07-11 14:37:04 +0800

    adding condtion

--- routes/routes.js
+++ routes/routes.js
@@ -13,9 +13,6 @@ const passport = require('../config/passport');
 
 router.get('/', authenticated, (req, res) => res.redirect('/tweets'));
 
-router.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/tweets'));
-router.get('/admin/users', authenticatedAdmin, adminController.getUsers);
-
 router.get('/tweets', authenticated, tweetController.getTweets);
 router.post('/tweets', authenticated, tweetController.postTweet);
 router.get('/tweets/:id', authenticated, tweetController.getTweet);
@@ -29,14 +26,17 @@ router.post('/signup', userController.signUp);
 router.get('/signin', userController.signInPage);
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn);
 
+router.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/tweets'));
+router.get('/admin/users', authenticatedAdmin, adminController.getUsers);
+router.delete('/admin/tweet/:id', authenticatedAdmin, adminController.deleteTweet);
+
 router.get('/admin/signin', adminController.signInPage);
-router.post('/admin/signin', passport.authenticate('local', { failureRedirect: 'admin/signin', failureFlash: true }), adminController.signIn);
+router.post('/admin/signin', passport.authenticate('local', { failureRedirect: '/admin/signin', failureFlash: true }), adminController.signIn);
 router.get('/admin/tweets', authenticatedAdmin, adminController.getTweets);
 
-router.delete('/admin/tweet/:id', authenticatedAdmin, adminController.deleteTweet);
 router.get('/signout', userController.signOut);
 router.get('/admin/signout', adminController.signOut);
 
-router.get('/users/:id',authenticated, userController.userPage)
+// router.get('/users/:id',authenticated, userController.userPage)
 
 module.exports = router
