bebd103f9167d84d8e97b524a82f5d513a50e6ab
changjandam <changjandamtw@gmail.com>
2021-07-15 08:00:13 +0800

    pass get /api/users/:id

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -2,7 +2,14 @@ const userService = require('../../services/userService')
 const helpers = require('../../_helpers')
 const userController = {
   getUser: (req, res) => {
+    console.log(helpers.getUser(req).id.toString())
+    console.log()
+    if (helpers.getUser(req).id.toString() === req.params.id){
       userService.getUser(req, res, (data) => res.json(data))
+    }else{
+      return res.json({status:'error'})
+    }
+
   },
 
   getTop10: (req, res) => {
