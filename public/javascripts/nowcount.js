9f1b509cdc4138f539a40eeeaeaae5beb564d77f
hazelisles <zhccj19@gmail.com>
2021-07-15 22:40:20 +0800

    fix: jquery showing

--- public/javascripts/nowcount.js
+++ public/javascripts/nowcount.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
   let count1 = $(".nowcount1").val().length
   let count2 = $(".nowcount2").val().length
-  let count3 = $(".nowcount3").val().length
   $('.now1').text(count1 + '/50')
   $('.now2').text(count2 + '/160')
-  $('.now3').text(count3 + '/140')
 
   $("input").keyup(function () {
     count1 = $(".nowcount1").val().length
@@ -13,7 +11,5 @@ $(document).ready(function () {
   $("textarea").keyup(function () {
     count2 = $(".nowcount2").val().length
     $('.now2').text(count2 + '/160')
-    count3 = $(".nowcount3").val().length
-    $('.now3').text(count3 + '/140')
   })
 })
