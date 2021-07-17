120cfc51d4fa8a4342d5070896d04f7de04cd1e3
changjandam <changjandamtw@gmail.com>
2021-07-18 00:10:23 +0800

    all users chat room layout done

--- seeders/20210706132415-users-seed-file.js
+++ seeders/20210706132415-users-seed-file.js
@@ -12,7 +12,7 @@ const additionalUsers = additionalIds.map(id => ({
   email: faker.internet.email(),
   password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
   name: faker.name.findName(),
-  avatar: `https://loremflickr.com/320/240/headshot?lock=${id}`,
+  avatar: `https://loremflickr.com/50/50/headshot?lock=${id}`,
   introduction: faker.lorem.text().substring(0, 130),
   createdAt: new Date(),
   updatedAt: new Date()
