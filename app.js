b19f7f9a0c6ef33935b0c31f14d7eeb7f2576307
kyle5408 <kyle5408@yahoo.com.tw>
2021-09-10 14:48:31 +0800

    chore: add basic module

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const helpers = require('./_helpers')
 const express = require('express')
-const helpers = require('./_helpers');
+const exhbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
 
 const app = express()
 const port = 3000
@@ -7,7 +11,15 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+// app.use(routes)
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on http://localhost:${port}!`))
 
 module.exports = app
