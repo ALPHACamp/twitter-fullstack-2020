0861d293601e9f1c0ed956251e1f22286162d449
johnson <poshenc@gmail.com>
2021-09-11 00:51:03 +0800

    confirm user password and unique email

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 
@@ -17,10 +18,16 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
 
-
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = helpers.getUser(req) // 取代req.user
+  next()
+})
 
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`))
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,13 +8,24 @@ const userController = {
   },
 
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      account: req.body.account,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
-      return res.redirect('/signin')
+    if (req.body.checkPassword !== req.body.password) {
+      req.flash('error_messages', 'Passwords you entered were inconsistent')
+      return res.redirect('/signup')
+    }
+    User.findOne({ where: { email: req.body.email } }).then(user => {
+      if (user) {
+        req.flash('error_messages', 'This email address had already been registered!')
+        return res.redirect('/signup')
+      }
+      User.create({
+        name: req.body.name,
+        account: req.body.account,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+      }).then(user => {
+        req.flash('success_messages', 'Your account had been successfully registered!')
+        return res.redirect('/signin')
+      })
     })
   }
 }
