8073604ddeaea5953c1dc774c5c8c5108a2cf3be
chao69 <star010631@gmail.com>
2021-09-11 22:19:11 +0800

    change port for test

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ const port = 3000
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(2000, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
