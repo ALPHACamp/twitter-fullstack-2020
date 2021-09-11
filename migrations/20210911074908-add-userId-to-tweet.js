24c83dcf3bbf72efaf8b4675f1f9ba86e53eb862
zeqas <casperi52400@gmail.com>
2021-09-11 17:30:03 +0800

    feat: question with addConstraint method

--- migrations/20210911074908-add-userId-to-tweet.js
+++ migrations/20210911074908-add-userId-to-tweet.js
@@ -2,19 +2,22 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Tweets', 'UserId', {
-      type: Sequelize.INTEGER,
+    await queryInterface.addConstraint('Tweets', {
+      type: 'foreign key',
+      fields: ['UserId'],
       allowNull: false,
       references: {
-        model: 'Users',
-        key: 'id'
+        table: 'Users',
+        field: 'id'
       },
+      onDelete: 'cascade',
+      onUpdate: 'cascade'
     })
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Tweets',
-      'UserId'
+    await queryInterface.removeConstraint('Tweets',
+      'Tweets_UserId_Users_fk'
     )
   }
 }
