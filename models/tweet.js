6f700449289ca89e71680634b2dd4aa0ab097cb6
chao69 <star010631@gmail.com>
2021-09-11 12:27:53 +0800

    define tweet model data

--- models/tweet.js
+++ models/tweet.js
@@ -1,6 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
+  const Tweet = sequelize.define('Tweet', 
+  {
+    UserId: DataTypes.INTEGER,
+    content: DataTypes.TEXT,
+    likeCount: DataTypes.INTEGER,
+    replyCount: DataTypes.INTEGER,
+    
   }, {});
   Tweet.associate = function(models) {
   };
