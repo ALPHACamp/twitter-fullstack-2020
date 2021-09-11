c92a573f1349d08d8160ba8a879d22e84d3eccaf
zeqas <casperi52400@gmail.com>
2021-09-11 17:16:53 +0800

    feat: create tweet, user model and associations

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,27 @@
-'use strict';
+'use strict'
+const {
+  Model
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-  }, {});
-  Tweet.associate = function(models) {
+  class Tweet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Tweet.belongsTo(models.User) // 加入關聯
+    }
   };
-  return Tweet;
-};
+  Tweet.init({
+    content: DataTypes.TEXT,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Tweet'
+  })
+  return Tweet
+}
