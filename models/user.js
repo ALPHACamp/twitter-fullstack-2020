1c176530400309789b66b497fa916c83f29685c7
chao69 <star010631@gmail.com>
2021-09-11 12:24:08 +0800

    define user model data

--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    isAdmin: DataTypes.BOOLEAN
   }, {});
   User.associate = function(models) {
   };
