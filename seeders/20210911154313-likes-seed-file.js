373874b3ed7b2fe1704dc6b4133fe380000535a5
zeqas <casperi52400@gmail.com>
2021-09-12 01:04:15 +0800

    feat: Add likes-seeder and associations with User-model and Tweet-model

---
+++ seeders/20210911154313-likes-seed-file.js
@@ -0,0 +1,20 @@
+'use strict'
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Likes',
+      Array.from({ length: 50 }).map((d, i) =>
+      ({
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        UserId: Math.floor(Math.random() * 5) + 2,
+        TweetID: Math.floor(Math.random() * 50) + 1,
+      })
+      ), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Likes', null, {})
+  }
+}
