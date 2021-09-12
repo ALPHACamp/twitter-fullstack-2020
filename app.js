0368bf60dd46b4f14b051a2685e223e0e7ec2b94
zeqas <casperi52400@gmail.com>
2021-09-12 18:01:13 +0800

    feat: Install passport and passport-local

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const db = require('./models')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
@@ -16,9 +16,10 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-app.use(flash())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
@@ -28,6 +29,6 @@ app.use((req, res, next) => {
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
