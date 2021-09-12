2ce30b716a1cc6822ebc37ee0c5029e4e5cdcdcc
zeqas <casperi52400@gmail.com>
2021-09-12 17:30:37 +0800

    chore: install connect-flash and express-session

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const handlebars = require('express-handlebars')
+const db = require('./models')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 const port = 3000
@@ -8,9 +11,23 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+require('./routes')(app)
 
 module.exports = app
