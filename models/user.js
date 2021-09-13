3ed55b052172b41b7e165b2d12475ddd6eb3884a
Trevor Lin <trevor0966@gmail.com>
2021-09-13 15:45:13 +0800

    feat: add models all tables columns & association

--- models/user.js
+++ models/user.js
@@ -1,8 +1,34 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
-  };
-  return User;
-};
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.BOOLEAN,
+    account: DataTypes.STRING,
+    cover: DataTypes.STRING
+  }, {})
+  User.associate = function (models) {
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Reply)
+    User.hasMany(models.Like)
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweets'
+    })
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+  }
+  return User
+}
