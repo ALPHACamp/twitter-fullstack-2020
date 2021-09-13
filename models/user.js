7e347c6148eacf314eeaf5f5834df34bd6551c81
zeqas <casperi52400@gmail.com>
2021-09-13 18:17:36 +0800

    feat: add followers and followings associtaions to users

--- models/user.js
+++ models/user.js
@@ -12,6 +12,16 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Tweet)
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
     }
   };
   User.init({
