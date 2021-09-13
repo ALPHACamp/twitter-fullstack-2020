92900ab90c29abe11234088928ab9b1162277f77
Trevor Lin <trevor0966@gmail.com>
2021-09-14 01:00:26 +0800

    add replies seed file

--- seeders/20210913162623-replies-seed-file.js
+++ seeders/20210913162623-replies-seed-file.js
@@ -3,16 +3,17 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Tweets', Array.from({ length: 50 }).map((d, i) => ({
+    await queryInterface.bulkInsert('Replies', Array.from({ length: 150 }).map((d, i) => ({
       id: i * 5 + 5,
       UserId: (i % 5) * 5 + 5,
-      description: faker.lorem.text(),
+      TweetId: (i % 50) * 5 + 5,
+      comment: faker.lorem.text(),
       createdAt: new Date(),
       updatedAt: new Date()
     })))
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Tweets', null, {})
+    await queryInterface.bulkDelete('Replies', null, {})
   }
 }
