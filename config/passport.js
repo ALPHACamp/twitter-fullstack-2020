f6be97c236a8178395e035cd1339da57154789ea
wei <a1234567045@yahoo.com.tw>
2021-09-14 16:33:58 +0800

    add signin/signup founction

--- config/passport.js
+++ config/passport.js
@@ -16,9 +16,9 @@ passport.use(new LocalStrategy(
   },
   // authenticate user
   (req, username, password, cb) => {
-    User.findOne({ where: { email: username } }).then(user => {
+    User.findOne({ where: { account: username } }).then(user => {
       if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
-      if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+      if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '密碼輸入錯誤！'))
       return cb(null, user)
     })
   }
