348560c03dda8c37d720cc04ce05e2861fba7f11
chiahung <a0926223478@hotmail.com>
2021-09-14 15:41:51 +0800

    feat:leftColumn.handlebars

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const port = 3000
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 require('./routes')(app)
