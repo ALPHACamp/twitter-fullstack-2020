864bdab6c5968b4ec5fffe3853341ec968357982
konnono <konnono@gmail.com>
2021-09-15 00:50:29 +0800

    feat: add admin signin page and route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const helpers = require('../_helpers')
 
-const tweetController = require('../controllers/tweetController.js')
+// const tweetController = require('../controllers/tweetController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 
@@ -10,21 +10,30 @@ const authenticated = (req, res, next) => {
   }
   res.redirect('/signin')
 }
+
 const authenticatedAdmin = (req, res, next) => {
-  if (helpers.ensureAuthenticated(req)) {
-    if (helpers.getUser(req).isAdmin) { return next() }
-    return res.redirect('/')
+  if (helpers.ensureAuthenticated(req) && helpers.getUser(req).isAdmin) {
+    return next()
   }
-  res.redirect('/signin')
+  req.flash('error_messages', '帳號或密碼輸入錯誤')
+  res.redirect('/admin')
 }
 
 module.exports = (app, passport) => {
   // 前台首頁
   app.get('/', authenticated, (req, res) => res.redirect('/tweets'))
-  app.get('/tweets', authenticated, tweetController.getTweets)
+  // app.get('/tweets', authenticated, tweetController.getTweets)
+
+  // 後台登入
+  app.get('/admin', adminController.signInPage)
+  app.post('/admin', passport.authenticate('local', {
+    failureRedirect: '/admin',
+    failureFlash: true
+  }), adminController.signIn)
+
   // 後台首頁
-  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/tweets'))
-  app.get('/admin/tweets', authenticatedAdmin, adminController.adminGetTweets)
+  // app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/tweets'))
+  // app.get('/admin/tweets', authenticatedAdmin, adminController.adminGetTweets)
 
   // 追蹤
   app.post('/following/:userId', authenticated, userController.addFollowing)
