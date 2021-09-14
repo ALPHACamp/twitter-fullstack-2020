e177bf804240e29e4522c353a56109cbcda6ec34
Trevor Lin <trevor0966@gmail.com>
2021-09-14 23:43:32 +0800

    add tweets main view1

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
+const handlebars = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('tweets'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
@@ -16,6 +20,6 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 
-require('./routes')(app)
+// require('./routes')(app)
 
 module.exports = app
