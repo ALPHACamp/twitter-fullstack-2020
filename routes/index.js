8a79e33a5c28a5ff4d9852ce47897b0c4589fde2
steven4program <stevenchang421@gmail.com>
2021-09-14 21:59:33 +0800

    Add tweets & admin tweets index page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
+const tweetController = require('../controllers/tweetController.js')
 const userController = require('../controllers/userController.js')
+const adminController = require('../controllers/adminController.js')
 
 module.exports = (app) => {
+  app.get('/', (req, res) => res.redirect('/tweets'))
+  app.get('/tweets', tweetController.getTweets)
+  app.get('/admin', (req, res) => res.redirect('/admin/tweets'))
+  app.get('/admin/tweets', adminController.getTweets)
   app.get('/register', userController.registerPage)
   app.post('/register', userController.register)
 }
