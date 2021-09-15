3be3dc021d44dac3aa4f6fce07f38076a2e139b2
VanessaLin9 <vanessa7591@gmail.com>
2021-09-15 11:32:10 +0800

    feat: add tweet, like, followship associate

--- models/followship.js
+++ models/followship.js
@@ -1,8 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Followship = sequelize.define('Followship', {
+
+    followingId: DataTypes.INTEGER,
+    followerId: DataTypes.INTEGER
+
   }, {});
   Followship.associate = function(models) {
+
   };
   return Followship;
 };
