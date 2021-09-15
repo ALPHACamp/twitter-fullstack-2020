a90cee9975c437d331d62ebe31738f8548ff678d
wei <a1234567045@yahoo.com.tw>
2021-09-15 13:53:03 +0800

    modify views & add admin route

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,10 +5,17 @@ const db = require('../models')
 
 const adminController = {
   signInPage: (req, res) => {
-
+    return res.render('admin/signin')
   },
   signIn: (req, res) => {
+    if (!req.user.role) {
+      req.flash('error_messages', '帳號或密碼錯誤')
 
+      res.redirect('admin/signin')
+    } else {
+      req.flash('success_messages', '成功登入！')
+      res.redirect('signup')
+    }
   },
   logOut: (req, res) => {
 
