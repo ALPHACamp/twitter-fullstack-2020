04212ee3e45c7fcb6e95a2f402ce0faf14aa8874
xuan1106 <spu96508@gmail.com>
2021-09-15 19:11:05 +0800

    complete reply to user reply to tweet associations

--- models/tweet.js
+++ models/tweet.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
   }, {});
   Tweet.associate = function(models) {
+    Tweet.hasMany(models.Reply)
   };
   return Tweet;
 };
--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
   }, {});
   User.associate = function(models) {
+    User.hasMany(models.Reply)
   };
   return User;
 };
