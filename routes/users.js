98c0b3a0d4406fc2b21f27bd3905e045a7eb5b81
chao69 <star010631@gmail.com>
2021-09-15 18:48:58 +0800

    debug

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const app = require('../app')
 const router = express()
+const passport = require('../config/passport')
 
 const userController = require('../controllers/userController')
 
+// user register
 router.get('/signup', userController.getSignup)
 router.post('/signup', userController.postSignup)
+// user login
+router.get('/login', userController.getLogin)
+router.post('/login', passport.authenticate('local', { failureRedirect: '/users/login', failureFlash: true }), userController.postLogin)
+router.get('/logout', userController.logout)
 
 
-module.exports = router  
+module.exports = router
