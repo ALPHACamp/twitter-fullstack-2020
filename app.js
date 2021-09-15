2d48c234171a8315ca856989df29749983ad2b22
chao69 <star010631@gmail.com>
2021-09-15 18:43:37 +0800

    use flash message & session and passport

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const helpers = require('./_helpers')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
@@ -11,18 +14,23 @@ app.engine('handlebars', handlebars({
 }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 app.use((req, res, next) => {
-  // res.locals.success_messages = req.flash('success_messages')
-  // res.locals.error_messages = req.flash('error_messages')
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
   res.locals.user = helpers.getUser(req)
   next()
 })
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 app.get('/', (req, res) => res.render('twitter'))
+app.get('/admin/login', (req, res) => res.render('adminLogin', { layout: 'userMain' }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
