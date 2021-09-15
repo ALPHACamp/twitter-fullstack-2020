aa556e9d1b3d25951040cafe14b174d939665224
xuan1106 <spu96508@gmail.com>
2021-09-15 22:42:39 +0800

    fix conflict

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express')
-const helpers = require('./_helpers')
-const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
