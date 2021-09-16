91a6281d8fbc2b72ae6af29ffd6cdd24a1980164
Wayne <urrightguy@gmail.com>
2021-09-16 21:54:40 +0800

    create routes.js for portal

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+const routes = require('./routes')
 const users = require('./users')
 const twitters = require('./twitters')
 const admins = require('./admins')
 
+
 module.exports = (app) => {
+  app.use('/', routes)
   app.use('/admins', admins)
   app.use('/users', users)
   app.use('/twitters', twitters)
