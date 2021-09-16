b7d002adab526982f81b4707dba5856018a33ae6
Chao <81567144+Chao69@users.noreply.github.com>
2021-09-16 20:36:08 +0800

    create admins route

---
+++ routes/admins.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('../config/passport')
+
+const adminController = require('../controllers/adminController')
+
+const isAuthenticatedAdmin = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    if (req.user.isAdmin) { return next() }
+    req.flash('error_messages', '只有管理員可登入後台')
+  }
+  res.redirect('/admins/login')
+}
+
+router.get('/', (req, res) => res.redirect('/admins/self'))
+router.get('/self', isAuthenticatedAdmin, adminController.getAdmin)
+
+// admin login & logout
+router.get('/login', adminController.getLogin)
+router.post('/login', passport.authenticate('local', { failureRedirect: '/users/login', failureFlash: true }), adminController.postLogin)
+router.get('/logout', adminController.logout)
+
+
+
+module.exports = router
