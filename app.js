bcacf0cdf83ebde51814569ac0350a92c539c8b7
ioriayaka <ioriayaka@hotmail.com>
2021-09-16 12:57:39 +0800

    feat: add hbs pages

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const express = require('express')
 const helpers = require('./_helpers');
-
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('hbs', handlebars({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/tweets', (req, res) => {
+  res.render('tweets')
+})
+app.get('/profileTweets', (req, res) => {
+  res.render('profileTweets')
+})
+app.get('/profileReplies', (req, res) => {
+  res.render('profileReplies')
+})
+app.get('/profileLike', (req, res) => {
+  res.render('profileLike')
+})
+app.get('/follower', (req, res) => {
+  res.render('follower')
+})
+app.get('/following', (req, res) => {
+  res.render('following')
+})
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
