e0e2c26d322990515795372c26be2292eaa2398a
mush1200 <mush1200@hotmail.com>
2021-09-16 22:09:10 +0800

    feat: add tweets page, tweet page, follower page and following page

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const helpers = require('./_helpers');
-
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('hbs', handlebars({
+  defaultLayout: 'main',
+  extname: '.hbs'
+})) 
+app.set('view engine', 'hbs') 
+app.use(express.static('public'))
+
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/tweets', (req, res) => {
+  res.render('tweets')
+})
+app.get('/tweet', (req, res) => {
+  res.render('tweet')
+})
+app.get('/followers', (req, res) => {
+  res.render('follower')
+})
+app.get('/following', (req, res) => {
+  res.render('following')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
