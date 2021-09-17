c509a8d4df42f0d5e87b2e6002728806da6d3827
steven4program <stevenchang421@gmail.com>
2021-09-17 22:17:58 +0800

    Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
+const helpers = require('../_helpers')
+
 const tweetController = require('../controllers/tweetController.js')
 const userController = require('../controllers/userController.js')
 const adminController = require('../controllers/adminController.js')
 
 module.exports = (app, passport) => {
-  app.get('/', (req, res) => res.redirect('/tweets'))
-  app.get('/tweets', tweetController.getTweets)
-  app.get('/admin', (req, res) => res.redirect('/admin/tweets'))
-  app.get('/admin/tweets', adminController.getTweets)
+  const authenticated = (req, res, next) => {
+    if (helpers.ensureAuthenticated(req)) {
+      return next()
+    }
+    res.redirect('/login')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (helpers.ensureAuthenticated(req)) {
+      if (helpers.getUser(req).isAdmin) {
+        return next()
+      }
+      return res.redirect('/')
+    }
+    res.redirect('/login')
+  }
+
+  app.get('/', authenticated, (req, res) => res.redirect('/tweets'))
+  app.get('/tweets', authenticated, tweetController.getTweets)
+  app.get('/admin', authenticatedAdmin, (req, res) =>
+    res.redirect('/admin/tweets')
+  )
+  app.get('/admin/tweets', authenticatedAdmin, adminController.getTweets)
   app.get('/register', userController.registerPage)
   app.post('/register', userController.register)
   app.get('/login', userController.loginPage)
