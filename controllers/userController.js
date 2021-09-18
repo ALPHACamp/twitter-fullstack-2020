8c77be37cf38d90fbc6ab70e31924c20c9f427bc
steven4program <stevenchang421@gmail.com>
2021-09-18 23:46:05 +0800

    fix routes bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ let userController = {
   },
 
   signup: (req, res) => {
-    if (req.body.passwordCheck !== req.body.password) {
+    if (req.body.checkPassword !== req.body.password) {
       req.flash('error_messages', '兩次密碼輸入不同！')
       return res.redirect('/signup')
     } else {
@@ -55,6 +55,32 @@ let userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+
+  userSetting: (req, res) => {
+    res.render('/setting')
+  },
+  putUserSetting: (req, res) => {
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/setting')
+    } else {
+      User.findOne({ where: { account: req.body.account } }).then((user) => {
+        if (user && req.body.account !== user.account) {
+          req.flash('error_messages', '此帳號已有人使用！')
+          return res.redirect('/setting')
+        }
+      })
+      return User.findByPk(req.params.id).then((user) => {
+        user.update({
+          account: req.body.account,
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        })
+      })
+    }
   }
 }
+
 module.exports = userController
