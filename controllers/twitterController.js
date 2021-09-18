3e5302e58df7347dc9e0accfbaee1bd6d953720e
Wayne <urrightguy@gmail.com>
2021-09-18 20:29:35 +0800

    update users seed and twitterController

--- controllers/twitterController.js
+++ controllers/twitterController.js
@@ -1,9 +1,25 @@
 const db = require('../models')
 const User = db.User
+const Tweet = db.Tweet
+const Reply = db.Reply
+const Like = db.Like
 
 const twitterController = {
   getTwitters: (req, res) => {
-    return res.render('twitter')
+    return Promise.all([
+      Tweet.findAll({
+        limit: 10,
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'DESC']],
+        include: [User, Reply, Like]
+      }).then((tweets) => {
+        return res.render('twitter', {
+          tweets: tweets,
+          User: users
+        })
+      })
+    ])
   },
 }
 
