b5eb6a3e2b6e1961c09a11e05f9d6280bdc08f89
zeqas <casperi52400@gmail.com>
2021-09-19 02:21:24 +0800

    refactor: delete testing commands

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -11,25 +11,11 @@ const maxDescLen = 50
 const tweetController = {
   // 首頁
   getTweets: async (req, res) => {
-    // console.log(req.user)
-    // console.log('********************要注意********************')
-    // const id = req.params.id
-    // console.log(helper.getUser(req),'哈哈')
-    // const loginUserId = helper.getUser(req).id
-    // const whereQuery = {}
-    // // 如果推文的人在使用者的追隨名單內，就顯示推文
-    // // followship內 A使用者在 B使用者 的
-    // // 驗證使用者
-    // if (req.query.userId === loginUserId) {
-    //   userId = Number(req.query.userId)
-    //   whereQuery.userId = userId
-    // }
     return Promise.all([
       Tweet.findAndCountAll({
         raw: true,
         nest: true,
         include: [User],
-        // where: whereQuery // 尋找特定使用者的推文
       }),
       User.findAndCountAll({
         include: [
@@ -38,15 +24,6 @@ const tweetController = {
       })
     ]).then(([tweets, users]) => {
       // 列出 追隨數前十名的使用者
-      // console.log('********************')
-      // console.log(users.rows)
-      // console.log('********************注意********************')
-      console.log(users.rows.map(user => ({
-        ...user.dataValues,
-        FollowedCount: user.Followers.length,
-        isFollowed: req.user.Followers.map(d => d.id).includes(user.id)
-      })))
-      // console.log('********************注意!!!********************')
       const topUsers = 
       users.rows.map(user =>({
         ...user.dataValues,
@@ -55,12 +32,9 @@ const tweetController = {
       }))
       .sort((a, b) => b.FollowedCount - a.FollowedCount)
       .slice(0, 10)
-      // console.log(topUsers)
-      // console.log('********************LookOut********************')
       
       const data = tweets.rows.map(tweet => ({
         ...tweet.dataValues,
-        // repliedCount: req.user.CommentedTweets.length,
         likedCount: req.user.LikedTweets.length,
         description: tweet.description,
         createdAt: tweet.createdAt,
@@ -74,11 +48,6 @@ const tweetController = {
         include: [Tweet]
       })
         .then((user) => {
-          // console.log('---------觀察users---------')
-          // console.log(users)
-          // console.log('---------觀察users!---------')
-          // console.log(data)
-          // console.log('---------觀察data---------')
           return res.render('home', {
             tweets: data,
             users: topUsers
