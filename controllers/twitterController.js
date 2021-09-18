4fbda7c2f7e9635a55b07699c60e8bbd392ea36a
futurenowchen <futurenowchen@gmail.com>
2021-09-18 21:53:24 +0800

    add userProfile tweet reply and like count & fix like seed

--- controllers/twitterController.js
+++ controllers/twitterController.js
@@ -6,21 +6,12 @@ const Like = db.Like
 
 const twitterController = {
   getTwitters: (req, res) => {
-    return Promise.all([
-      Tweet.findAll({
-        limit: 10,
-        raw: true,
-        nest: true,
-        order: [['createdAt', 'DESC']],
-        include: [User, Reply, Like]
-      }).then((tweets) => {
-        return res.render('twitter', {
-          tweets: tweets,
-          User: users
-        })
-      })
-    ])
-  },
+
+    return res.render('twitter', {
+      tweets: tweets,
+      User: users
+    })
+  }
 }
 
 module.exports = twitterController
