7a6fc218743e461d7c4e5716bdc636319e994d05
zeqas <casperi52400@gmail.com>
2021-09-19 02:14:58 +0800

    feat: render popular user list in side-bar

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -11,6 +11,8 @@ const maxDescLen = 50
 const tweetController = {
   // 首頁
   getTweets: async (req, res) => {
+    // console.log(req.user)
+    // console.log('********************要注意********************')
     // const id = req.params.id
     // console.log(helper.getUser(req),'哈哈')
     // const loginUserId = helper.getUser(req).id
@@ -22,63 +24,69 @@ const tweetController = {
     //   userId = Number(req.query.userId)
     //   whereQuery.userId = userId
     // }
-
-    // Tweet.findAndCountAll({
-    //   // include: Followship,
-    //   where: whereQuery
-    // }).then(result => {
-    //   const data = result.rows.map(r => ({
-    //     ...r.dataValues,
-    //     isCommented: req.user.CommentedTweets.map(d => d.id).includes(r.id), // 被回覆過的推文
-    //     isLiked: req.user.LikedTweets.map(d => d.id).includes(r.id) // 被喜歡過的推文
-    //   }))
-    //     .findAll({
-    //       raw: true,
-    //       nest: true
-    //     }).then(() => {
-    //       return res.render('home', {
-    //         tweets: data,
-    //       })
-    //     })
-    // })
-    const tweets = await Tweet.findAll({
-      raw: true,
-      nest: true,
-      include: [User],
-      where: whereQuery
-    }).then(tweets => {
-      console.log(tweets)
-      console.log('---------觀察rows---------')
-      console.log(tweets.rows)
-      console.log('---------觀察---------')
+    return Promise.all([
+      Tweet.findAndCountAll({
+        raw: true,
+        nest: true,
+        include: [User],
+        // where: whereQuery // 尋找特定使用者的推文
+      }),
+      User.findAndCountAll({
+        include: [
+          { model: User, as: 'Followers' }
+        ]
+      })
+    ]).then(([tweets, users]) => {
+      // 列出 追隨數前十名的使用者
+      // console.log('********************')
+      // console.log(users.rows)
+      // console.log('********************注意********************')
+      console.log(users.rows.map(user => ({
+        ...user.dataValues,
+        FollowedCount: user.Followers.length,
+        isFollowed: req.user.Followers.map(d => d.id).includes(user.id)
+      })))
+      // console.log('********************注意!!!********************')
+      const topUsers = 
+      users.rows.map(user =>({
+        ...user.dataValues,
+        FollowedCount: user.Followers.length,
+        isFollowed: req.user.Followers.map(d => d.id).includes(user.id)
+      }))
+      .sort((a, b) => b.FollowedCount - a.FollowedCount)
+      .slice(0, 10)
+      // console.log(topUsers)
+      // console.log('********************LookOut********************')
+      
       const data = tweets.rows.map(tweet => ({
         ...tweet.dataValues,
-        // TweetCommentedCount: tweet.dataValues.CommentedTweets.length,
-        // isCommented: req.user.CommentedTweets.map(d => d.id).includes(r.id), // 被回覆過的推文
-        //推文被喜歡的次數
-        TweetLikedCount: tweet.dataValues.LikedTweets.length,
+        // repliedCount: req.user.CommentedTweets.length,
+        likedCount: req.user.LikedTweets.length,
+        description: tweet.description,
+        createdAt: tweet.createdAt,
+        userName: tweet.User.name,
+        userAccount: tweet.User.account,
         isLiked: req.user.LikedTweets.map(d => d.id).includes(tweet.id) // 推文是否被喜歡過
       }))
-        .then(() => {
-          console.log(data)
-          console.log('---------觀察data---------')
+      User.findAll({
+        raw: true,
+        nest: true,
+        include: [Tweet]
+      })
+        .then((user) => {
+          // console.log('---------觀察users---------')
+          // console.log(users)
+          // console.log('---------觀察users!---------')
+          // console.log(data)
+          // console.log('---------觀察data---------')
           return res.render('home', {
-            tweets: data
+            tweets: data,
+            users: topUsers
           })
         })
     })
 
-    // console.log(tweets)
-
-    if (helper.getUser(req).isAdmin) {
-      tweets.map(tweet => {
-        tweet.description = tweet.description.length <= 50 ? tweet.description : tweet.description.substring(0, maxDescLen) + "..."
-      })
-    }
-    console.log('有到這嗎')
-    const renderPage = helper.getUser(req).isAdmin ? 'admin/admin_main' : 'home'
-    return res.render(renderPage, { tweets })
-
+    
   },
   getTweet: async (req, res) => {
     // return Tweet.findByPk(req.params.id, {
@@ -111,27 +119,6 @@ const tweetController = {
 
 
     }
-  },
-  // 在此得列出最受歡迎的十個使用者
-  // 依照追蹤者人數降冪排序
-  // 目前先另外寫一個controller，之後需要合併至getTweets
-  getTopUsers: (req, res) => {
-    return User.findAll({
-      include: [
-        { model: User, as: 'Followers' }
-      ]
-    }).then(users => {
-      users = users.map(user => ({
-        ...user.dataValues,
-        FollowedCount: user.FollowedUsers.length,
-        isFollowed: req.user.FollowedUsers.map(d => d.id).includes(user.id)
-      }))
-
-      users = users
-        .sort((a, b) => b.FollowedCount - a.FollowedCount)
-        .slice(0, 10)
-      return res.render('topUsers', { users })
-    })
   },
 }
 
