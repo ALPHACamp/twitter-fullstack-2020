636e90030c137b0cd031a3ba5c4a3667bedcc085
Demilululu <demilululu@gmail.com>
2021-09-18 16:52:33 +0800

    modify like model

--- models/like.js
+++ models/like.js
@@ -1,9 +1,18 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Like = sequelize.define('Like', {
-    UserId: DataTypes.INTEGER,
-    TweetId: DataTypes.INTEGER
-  }, {})
+  const Like = sequelize.define(
+    'Like',
+    {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      UserId: DataTypes.INTEGER,
+      TweetId: DataTypes.INTEGER
+    },
+    {}
+  )
   Like.associate = function (models) {
     Like.belongsTo(models.Tweet)
     Like.belongsTo(models.User)
