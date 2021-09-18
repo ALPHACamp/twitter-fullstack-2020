3a1443213090a38bdb3a3175fe73071a0026cfbe
Wayne <urrightguy@gmail.com>
2021-09-18 16:11:59 +0800

    replies seeder and update tweet seeder

--- seeders/20210918070445-replies-seed-file.js
+++ seeders/20210918070445-replies-seed-file.js
@@ -1,8 +1,9 @@
 'use strict';
+
 const faker = require('faker')
 const db = require('../models')
 const User = db.User
-
+const Tweet = db.Tweet
 const getUserId = new Promise((resolve, reject) => {
   User.findAll({ raw: true, nest: true })
     .then(users => {
@@ -13,22 +14,34 @@ const getUserId = new Promise((resolve, reject) => {
       return resolve(userIds)
     })
 })
+const getTweetId = new Promise((resolve, reject) => {
+  Tweet.findAll({ raw: true, nest: true })
+    .then(tweets => {
+      const tweetIds = []
+      tweets.forEach(tweet => {
+        tweetIds.push(tweet.id)
+      })
+      return resolve(tweetIds)
+    })
+})
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const userIds = await getUserId
+    const tweetIds = await getTweetId
 
-    await queryInterface.bulkInsert('Tweets',
+    await queryInterface.bulkInsert('Replies',
       Array.from({ length: 10 }).map((d, i) =>
       ({
         UserId: userIds[Math.floor(Math.random() * userIds.length)],
-        content: faker.lorem.text(),
+        TweetId: tweetIds[Math.floor(Math.random() * tweetIds.length)],
+        content: faker.lorem.sentence(),
         createdAt: new Date(),
         updatedAt: new Date()
       })), {})
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Tweets', null, {})
+    await queryInterface.bulkDelete('Replies', null, {})
   }
-};
+}
