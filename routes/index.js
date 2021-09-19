efa610368c167579d3cb6c5ed0fe9528a3eb45cc
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-19 13:47:31 +0800

    feat: Add personalpage

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 module.exports = (app) => {
   app.get("/", (req, res) => res.render("index"));
   app.get("/posts", (req, res) => res.render("posts"));
+  app.get("/users/1/main", (req, res) => res.render("personalpage"));
 };
