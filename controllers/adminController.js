1dd25668f2ea525456e6a0d81b3ffbab55a401d1
chiahung <a0926223478@hotmail.com>
2021-09-19 21:56:18 +0800

    feat: add admin router

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,12 +3,12 @@ const Tweet = db.Tweet
 const User = db.User
 
 const adminController = {
-  getTweets: (req, res) => {
+  getPosts: (req, res) => {
     return Tweet.findAll({ raw: true }).then(tweets => {
       return res.json(tweets)
     })
   },
-  deleteAdminTweets: (req, res) => {
+  deletePost: (req, res) => {
     return Tweet.findByPk(req.params.id).then((tweet) => {
       tweet.destroy().then(() => {
         return res.json({ status: 'success', tweet})
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,5 +1,6 @@
 // This modules is for admin-related roters
 const express = require("express");
+const adminController = require("../../controllers/adminController");
 const router = express.Router();
 const db = require("../../models");
 const { User, Twitter, Reply } = db;
@@ -7,5 +8,9 @@ const { User, Twitter, Reply } = db;
 router.get("/", (req, res) => {
   return res.send("index");
 });
+
+router.get("/posts", adminController.getPosts)
+router.delete('/posts/:id', adminController.deletePost)
+router.get("/users", adminController.getUsers)
 
 module.exports = router;
