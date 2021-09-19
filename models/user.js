e1298e52bc4957ea56e923c726c95ec5769316e6
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-19 22:24:22 +0800

    fix: 修好Like資料測試問題

--- models/user.js
+++ models/user.js
@@ -1,30 +1,39 @@
-'use strict';
+"use strict";
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    name: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    account: DataTypes.TEXT,
-    introduction: DataTypes.STRING,
-    cover: DataTypes.TEXT,
-    role: DataTypes.STRING,
-  }, {});
-  User.associate = function(models) {
-    User.hasMany(models.Reply)
-    User.hasMany(models.Tweet)
-    User.hasMany(models.Like)
+  const User = sequelize.define(
+    "User",
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      account: DataTypes.TEXT,
+      introduction: DataTypes.STRING,
+      cover: DataTypes.TEXT,
+      role: DataTypes.STRING,
+    },
+    {}
+  );
+  User.associate = function (models) {
+    User.hasMany(models.Reply);
+    User.hasMany(models.Tweet);
+    User.hasMany(models.Like);
     User.belongsToMany(User, {
       through: models.Followship,
-      foreignKey: 'followingId',
-      as: 'Followers'
-    })
+      foreignKey: "followingId",
+      as: "Followers",
+    });
     User.belongsToMany(User, {
       through: models.Followship,
-      foreignKey: 'followerId',
-      as: 'Followings'
-    })
+      foreignKey: "followerId",
+      as: "Followings",
+    });
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: "UserId",
+      as: "LikedTweets",
+    });
   };
   return User;
 };
