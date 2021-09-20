a9c5e9050a67eb55cd330192699bfd4711adfc8e
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-20 23:22:21 +0800

    fix: 分析路由與檔案不同的地方做出修改

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,20 @@
 // This modules is for index view page
 const express = require("express");
+const userController = require("../../controllers/userController");
 const router = express.Router();
 const db = require("../../models");
 const { User, Tweet, Reply } = db;
 
 router.get("/", (req, res) => {
-  return res.render("index");
+  return res.redirect("profiles/:id/main");
+});
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
+router.get("/signin", userController.signInPage);
+router.post("/signin", userController.signIn);
+router.post("/signup", (req, res) => {
+  //send signup info
 });
 
 module.exports = router;
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,23 +3,23 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../models");
 const { User, Tweet, Reply } = db;
-//以下路由為方便測試使用 可刪
+
 router.get("/", (req, res) => {
-  return res.render("index");
-});
-router.get("/login", (req, res) => {
-  return res.render("login");
-});
-router.get("/post", (req, res) => {
-  return res.render("post");
+  return res.redirect(`users/${req.user.id}/main`);
 });
 router.get("/profile", (req, res) => {
   return res.render("profile");
 });
-router.get("/signin", (req, res) => {
-  return res.render("signin");
+router.post("/signout", (req, res) => {
+  //signout action
 });
-router.get("/setting", (req, res) => {
+router.get("/:id/edit", (req, res) => {
   return res.render("setting");
+});
+router.put("/:id", (req, res) => {
+  //revise account setting
+});
+router.get("/:id/main", (req, res) => {
+  return res.render("index");
 });
 module.exports = router;
