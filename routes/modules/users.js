781c1c39c3808db39fe41061bee4058d81a9996f
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-20 12:05:41 +0800

    feat: add admin/followships page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -25,4 +25,7 @@ router.get("/setting", (req, res) => {
 router.get("/followship", (req, res) => {
   return res.render("followship");
 });
+router.get("/followships", (req, res) => {
+  return res.render("admin/followships");
+});
 module.exports = router;
