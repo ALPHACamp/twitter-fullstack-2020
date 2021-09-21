8b3c529098d212c5ce8e0ef0680d98c130a6e1bf
johnson <poshenc@gmail.com>
2021-09-22 02:13:28 +0800

    add userController API

---
+++ controllers/api/userController.js
@@ -0,0 +1,21 @@
+const db = require('../../models')
+const User = db.User
+const Tweet = db.Tweet
+const Reply = db.Reply
+const Like = db.Like
+const Followship = db.Followship
+
+const userService = require('../../services/userService.js')
+
+const { Op } = require("sequelize")
+const sequelize = require('sequelize')
+
+let userController = {
+  getUserTweets: (req, res) => {
+    userService.getUserTweets(req, res, (data) => {
+      return res.status(200).json(data)
+    })
+  }
+}
+
+module.exports = userController
--- routes/apis.js
+++ routes/apis.js
@@ -4,9 +4,12 @@ const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
 const tweetController = require('../controllers/api/tweetController.js')
-// const userController = require('../controllers/api/userController.js')
+const userController = require('../controllers/api/userController.js')
 
- // //使用者新增一則貼文
- router.post('/tweets', tweetController.postTweets)
+// //使用者新增一則貼文
+router.post('/tweets', tweetController.postTweets)
 
- module.exports = router
+// //使用者顯示特定使用者頁面(使用者所有貼文)
+router.get('/users/:user_id/tweets', userController.getUserTweets)
+
+module.exports = router
