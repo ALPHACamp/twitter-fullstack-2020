09715ad3cf4a9dbe6d2b347e33ce37e5964a32f5
Demilululu <demilululu@gmail.com>
2021-09-22 17:32:23 +0800

    add follow/unfollow button to user profile

--- config/passport.js
+++ config/passport.js
@@ -5,29 +5,33 @@ const db = require('../models')
 const User = db.User
 const Tweet = db.Tweet
 
-
-
 // setup passport strategy
-passport.use(new LocalStrategy(
-  // customize user field
-  {
-    usernameField: 'account',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-  // authenticate user
-  (req, username, password, cb) => {
-    console.log('================')
-    console.log('進入passport authenticate user')
-    User.findOne({ where: { account: username } }).then(user => {
-      if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
-      if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '密碼輸入錯誤！'))
-      return cb(null, user)
-    })
-  }
-))
-
-
+passport.use(
+  new LocalStrategy(
+    // customize user field
+    {
+      usernameField: 'account',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    // authenticate user
+    (req, account, password, cb) => {
+      console.log('================')
+      console.log('進入passport authenticate user')
+      User.findOne({ where: { account } }).then(user => {
+        if (!user)
+          return cb(
+            null,
+            false,
+            req.flash('error_messages', '帳號或密碼輸入錯誤')
+          )
+        if (!bcrypt.compareSync(password, user.password))
+          return cb(null, false, req.flash('error_messages', '密碼輸入錯誤！'))
+        return cb(null, user)
+      })
+    }
+  )
+)
 
 // serialize and deserialize user
 passport.serializeUser((user, cb) => {
