0589c2e7fb00c29ea3a2362b8c04c69d00e78542
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-22 08:42:06 +0800

    refactor: refactor likeController and followshipController

--- routes/modules/posts.js
+++ routes/modules/posts.js
@@ -1,15 +1,14 @@
 // This modules is for posts and comments (tablename: twitter and reply)
 const express = require("express");
 const tweetController = require("../../controllers/tweetController");
+const likeController = require("../../controllers/likeController");
 const router = express.Router();
-const db = require("../../models");
-const { User, Tweet, Reply } = db;
+const db = require("../../models")
 
-// router.get("/", (req, res) => {
-//   return res.render("index");
-// });
 router.get("/" , tweetController.getPosts)
 router.get("/:id", tweetController.getPost)
 router.post("/posts", tweetController.postTweet)
+router.put("/:id/like", likeController.changeLike)
+
 
 module.exports = router;
