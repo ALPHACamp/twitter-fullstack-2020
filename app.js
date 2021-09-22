58f03f7dd90aae4fc3d124cac7a162275723b643
VanessaLin9 <vanessa7591@gmail.com>
2021-09-23 02:20:08 +0800

    add Procfile

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const axios = require('axios');
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
