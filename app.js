fc17adee4e59d36bc4bf261763cca57b8ea1d444
Wayne <urrightguy@gmail.com>
2021-09-23 00:03:18 +0800

    complete replyList view

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const methodOverride = require('method-override')
 const passport = require('./config/passport')
 const helpers = require('./_helpers')
 
-
 const app = express()
 const port = 3000
 
@@ -37,8 +36,6 @@ app.use(async (req, res, next) => {
 })
 
 app.use(methodOverride('_method'))
-// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
-
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
