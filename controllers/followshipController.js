ddd85560dbc5a12afadefe223584be2e61f2232b
Trevor Lin <trevor0966@gmail.com>
2021-09-23 17:12:57 +0800

    modify signup error message

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -1,6 +1,7 @@
 const db = require('../models')
 const Followship = db.Followship
 const User = db.User
+const helpers = require('../_helpers')
 
 
 
@@ -9,14 +10,14 @@ const followshipController = {
   addFollowing: async (req, res) => {
     try {
       console.log('into addFollowing')
-      console.log('req.user.id', typeof Number(req.user.id))
+      console.log('helpers.getUser(req).id', typeof Number(helpers.getUser(req).id))
       console.log('req.body.id', typeof Number(req.body.id))
-      if (Number(req.user.id) === Number(req.body.id)) {
+      if (Number(helpers.getUser(req).id) === Number(req.body.id)) {
         req.flash('error_messages', '使用者不可以追蹤自己')
         return res.redirect('back')
       }
       const followship = await Followship.create({
-        followerId: req.user.id,
+        followerId: helpers.getUser(req).id,
         followingId: req.body.id
       })
 
@@ -35,13 +36,13 @@ const followshipController = {
     try {
       await Followship.destroy({
         where: {
-          followerId: req.user.id,
+          followerId: helpers.getUser(req).id,
           followingId: req.params.userId
         }
       })
 
       return res.redirect('back')
-      
+
     } catch {
       console.log(err)
       console.log('removeFollowing err')
