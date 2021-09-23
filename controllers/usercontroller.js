16cd6f40d1fbffdbe7cc3de0304e606fb63b07fe
xuan1106 <spu96508@gmail.com>
2021-09-23 10:40:14 +0800

    complete signup feature

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
-const { urlencoded } = require("express");
+const db = require('../models')
+const { User } = db
 
 //for test only
 const helpers = require("../_helpers.js");
@@ -25,8 +26,37 @@ const userController = {
   },
 
   signup: (req, res) => {
-    return res.render("signup");
-    //put sign up function here
+    if (req.body.confirmPassword !== req.body.password) {
+      console.log('兩次密碼輸入不同！')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({ where: { email: req.body.email } })
+        .then(user => {
+          if (user) {
+            console.log('此信箱已被註冊！')
+            return res.redirect('/signup')
+          } else {
+            User.findOne({ where: { account: req.body.account } })
+              .then(user => {
+                if (user) {
+                  console.log('此帳號已被使用！')
+                  return res.redirect('/signup')
+                } else {
+                  User.create({
+                    name: req.body.name,
+                    account: "@" + req.body.account,
+                    role: "user",
+                    email: req.body.email,
+                    password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+                  }).then(user => {
+                    console.log('成功註冊帳號！')
+                    return res.redirect('/signin')
+                  })
+                }
+              })
+          }
+        })
+    }
   },
 };
 
