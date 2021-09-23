f93d4afebb61bdc20eb631e82e44dd6237194380
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-23 08:40:51 +0800

    feat: revise modal for postTweet

--- routes/modules/posts.js
+++ routes/modules/posts.js
@@ -3,12 +3,11 @@ const express = require("express");
 const tweetController = require("../../controllers/tweetController");
 const likeController = require("../../controllers/likeController");
 const router = express.Router();
-const db = require("../../models")
+const db = require("../../models");
 
-router.get("/:id/main" , tweetController.getPosts)
-router.get("/:id", tweetController.getPost)
-router.post("/posts", tweetController.postTweet)
-router.put("/:id/like", likeController.changeLike)
-
+router.get("/:id/main", tweetController.getPosts);
+router.get("/:id", tweetController.getPost);
+router.post("/add", tweetController.postTweet); //有做修改： /posts ==> /add (這樣整個路由會變成 /posts/add，比較符合Restful)
+router.put("/:id/like", likeController.changeLike);
 
 module.exports = router;
