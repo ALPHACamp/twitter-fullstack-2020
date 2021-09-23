6ae7a8ff73224d162ad959522ef33d1d49a6c17c
kyle5408 <kyle5408@yahoo.com.tw>
2021-09-23 18:01:28 +0800

    refactor: signin email to account

--- config/passport.js
+++ config/passport.js
@@ -11,12 +11,12 @@ const Reply = db.Reply
 
 passport.use(new LocalStrategy(
   {
-    usernameField: 'email',
+    usernameField: 'account',
     passwordField: 'password',
     passReqToCallback: true
   },
   (req, username, password, cb) => {
-    User.findOne({ where: { email: username } })
+    User.findOne({ where: { account: username } })
       .then(user => {
         if (!user) return cb(null, false, req.flash('error_messages', '帳號不存在！'))
         if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '密碼輸入錯誤!'))
