41479d6507e7bddc5cd4ef2d9d69b5b30609f22c
Bumble <nick1092387456@gmail.com>
2021-09-23 21:00:58 +0800

    feat: add userSetting.hbs

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,11 +26,7 @@ let userController = {
                 account: req.body.account,
                 name: req.body.name,
                 email: req.body.email,
-                password: bcrypt.hashSync(
-                  req.body.password,
-                  bcrypt.genSaltSync(10),
-                  null
-                ),
+                password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null),
               }).then((user) => {
                 req.flash('success_messages', '成功註冊帳號！')
                 return res.redirect('/signin')
@@ -59,33 +55,6 @@ let userController = {
 
   userSetting: (req, res) => {
     res.render('/setting')
-  },
-
-
-  getUserSetting: (req,res) => {
-
-  },
-
-  putUserSetting: (req, res) => {
-    if (req.body.passwordCheck !== req.body.password) {
-      req.flash('error_messages', '兩次密碼輸入不同！')
-      return res.redirect('/setting')
-    } else {
-      User.findOne({ where: { account: req.body.account } }).then((user) => {
-        if (user && req.body.account !== user.account) {
-          req.flash('error_messages', '此帳號已有人使用！')
-          return res.redirect('/setting')
-        }
-      })
-      return User.findByPk(req.params.id).then((user) => {
-        user.update({
-          account: req.body.account,
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-        })
-      })
-    }
   },
 }
 
