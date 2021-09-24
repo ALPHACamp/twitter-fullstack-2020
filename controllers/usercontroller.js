6165fb0f20a7cf094e68311722c9711496449621
xuan1106 <spu96508@gmail.com>
2021-09-25 04:45:16 +0800

    complete edit user setting feature

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -61,12 +61,53 @@ const userController = {
   },
   getUserSetting: (req, res) => {
     const user = getTestUser(req)
-    console.log(user)
     return res.render('setting', {user})
   },
- 
+  editUserSetting: (req, res) => {
+    if(req.body.confirmPassword !== req.body.password) {
+      console.log('兩次密碼不同')
+      return res.redirect('back')
+    } else {
+      User.findOne({ where: { email: req.body.email } })
+        .then(userEmail => {
+          if (userEmail) {
+            console.log('此信箱已被使用')
+            res.redirect('/tweets')
+          } else {
+            User.findByPk(req.params.id)
+              .then((user) => {
+                user.update({
+                  name: req.body.name,
+                  account: req.body.account,
+                  email: req.body.email,
+                  password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+                })
+                  .then((user) => {
+                    console.log('成功更新個人資料')
+                    res.redirect(`/users/${user.id}/edit`)
+                  })
+              })
+          }
+        })
+    }
+  }
     
   
 };
 
 module.exports = userController;
+
+// User.findByPk(req.params.id)
+//         .then((user) => {
+//           console.log(user)
+//           user.update({
+//             account: req.body.account,
+//             name: req.body.name,
+//             email:req.body.email,
+//             password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+//           })
+//             .then((user) => {
+//               console.log('成功更新個人資料')
+//               res.redirect(`/users/${user.id}/edit`)
+//             })
+//         })
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -14,8 +14,8 @@ router.get("/:id/comments", profileController.getComments);
 router.get("/:id/likes", profileController.getLikedPosts);
 router.get("/:id/followers", followshipController.getFollowers);
 router.get("/:id/followings", followshipController.getFollowings);
-router.get("/:id/edit",userController.getUserSetting)  
-// router.post("/:id/edit")
+router.get("/:id/edit", userController.getUserSetting)  
+router.post("/:id/edit",userController.editUserSetting)
 
 router.put("/:userId", (req, res) => {
   return res.redirect("back");
