36b5af33fcb3db5b110f6bb7c17c80cc9d160acf
ioriayaka <ioriayaka@hotmail.com>
2021-09-24 10:06:17 +0800

    Modify pages

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const flash = require('connect-flash') //  自訂訊息並存到 session 裡
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const session = require('express-session') 
+const session = require('express-session')
 const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.engine('handlebars', handlebars({ 
+app.engine('handlebars', handlebars({
   defaultLayout: 'main',
   helpers: require('./config/handlebars-helpers')
 })) // Handlebars 註冊樣板引擎
@@ -37,10 +37,10 @@ app.use(flash())
 app.use(methodOverride('_method'))
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
-    res.locals.success_messages = req.flash('success_messages')
-    res.locals.error_messages = req.flash('error_messages')
-    res.locals.user = helpers.getUser(req)
-    next()
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = helpers.getUser(req)
+  next()
 })
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
