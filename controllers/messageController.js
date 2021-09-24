7238ded65fc158c8dd5dd594faf24b625954187f
VanessaLin9 <vanessa7591@gmail.com>
2021-09-25 04:28:02 +0800

    add old message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -7,17 +7,24 @@ const { Server } = require("socket.io");
 const io = new Server(server, { cors: {origin: "*"}});
 const db = require('../models')
 const Message = db.Message
+const User = db.User
 
 const messageController = {
   renderPage: (req, res) => {
     const currentUser = helpers.getUser(req)
+    // console.log(currentUser)
+    Message.findAll({
+      include:[{model:User, attributes:['id', 'avatar', 'name', 'account']}],
+      order: [['createdAt', 'DESC']],
+      raw: true, nest: true
+    }).then(msg => {
+      console.log(msg)
+    })
     return res.render('message', {currentUser})
   },
   sendMsg: (user) => {
-    console.log('yo====',user.id)
-    console.log(user.msg)
     return Message.create({
-      userId: user.id,
+      UserId: user.id,
       content: user.msg
     })
   }
