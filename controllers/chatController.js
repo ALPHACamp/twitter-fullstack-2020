cbdb5c71f2b24441accbe0d7b026aeaebc4db992
wei <a1234567045@yahoo.com.tw>
2021-09-25 21:31:30 +0800

    add remove logout User

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,17 +1,24 @@
 const db = require('../models')
+const User = db.User
+
 const helpers = require('../_helpers')
 
 
 const chatController = {
-  getChat: (req, res) => {
-
-
-    res.render('chat')
+  getChat: async (req, res) => {
+    try {
+      const userId = req.user.id
+      await User.findByPk(userId).then(userData => {
+         res.render('chat', {userData})
+      })
+      //const userData = req.user
+    } catch (err) {
+      consolog('getChat err')
+      req.flash('error_messages', '聊天室進入失敗')
+      res.redirect('back')
+    }
+    
   }
-
-
-
-
 }
 
 
