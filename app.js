b22ea0b59c59a04ebb332d1839c9957e4fb5e329
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-25 22:04:35 +0800

    fix: fix env problem

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ host = process.env.BASE_URL || "http://localhost";
 const session = require("express-session");
 const db = require("./models");
 const methodOverride = require("method-override");
-const usePassport = require('./config/passport')
+const usePassport = require("./config/passport");
 const routes = require("./routes");
 const {
   allowInsecurePrototypeAccess,
@@ -13,10 +13,11 @@ const Handlebars = require("handlebars");
 const handlebars = require("express-handlebars");
 const helpers = require("./_helpers");
 const app = express();
-console.log(process.env)
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
+console.log(process.env.IMGUR_CLIENT_ID);
 // 設定在測試環境下使用 helpers.getUser(req) 作為 req.user
 if (process.env.NODE_ENV === "test") {
   app.use((req, res, next) => {
@@ -27,16 +28,24 @@ if (process.env.NODE_ENV === "test") {
 
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.engine(".hbs", handlebars({ extname: ".hbs", defaultLayout: "main", helpers: require('./config/handlebars-helpers'), handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+app.engine(
+  ".hbs",
+  handlebars({
+    extname: ".hbs",
+    defaultLayout: "main",
+    helpers: require("./config/handlebars-helpers"),
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+  })
+);
 app.set("view engine", ".hbs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
-app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
-app.use('/upload', express.static(__dirname + '/upload'))
-usePassport(app)
+app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
+app.use("/upload", express.static(__dirname + "/upload"));
+usePassport(app);
 // app.use((req, res, next) => {
 //   res.locals.user = helpers.getUser(req)
 //   next()
