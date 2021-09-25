29a08594bfefa72fd04766297a5a6df72bc847ff
Trevor Lin <trevor0966@gmail.com>
2021-09-25 12:15:16 +0800

    repair followship routes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const apis = require('./modules/apis')
 const users = require('./modules/users')
 const tweets = require('./modules/tweets')
 const chat = require('./modules/chat')
+const followship = require('./modules/followship')
 
 module.exports = app => {
   app.use('/', routes)
   app.use('/api', apis)
   app.use('/users', users)
   app.use('/tweets', tweets)
+  app.use('/followships', followship)
   app.use('/chat', chat)
 }
