a5b38d147e617f7bad291f428e5e288b586df655
wei <a1234567045@yahoo.com.tw>
2021-09-26 17:29:25 +0800

    modify

--- config/socketChat.js
+++ config/socketChat.js
@@ -8,12 +8,6 @@ const helpers = require('../_helpers')
 
 
 
-
-// let onlineCount = 0;// 加入線上人數計數
-// let onlineUsers = []
-// let logout = ''
-// let userId = ''
-
 module.exports = {
   Server(server) {
     io = new Server(server)
@@ -21,6 +15,7 @@ module.exports = {
   connect() {
     io.on('connection', (socket) => {
       console.log('有人進入公開聊天室囉！')
+
       socket.on("userIn", async (userMsg) => {
         try {
           //運用socket儲存值
@@ -48,6 +43,9 @@ module.exports = {
           console.log(err)
         }
       })
+
+
+
       socket.on('disconnect', async () => {
         console.log('socket.currentUserId', socket.currentUserId)
         const offUser = await User.findByPk(socket.currentUserId)
@@ -62,39 +60,10 @@ module.exports = {
         io.emit('publicRoomUsers', publicRoomUsers)
       })
       
-      // console.log('進入聊天室')
-      // console.log('客戶端成功連線服務器: ' + socket.id)
-
-      // onlineCount++;// 有連線發生時增加人數
-      // io.emit("online", onlineCount);   // 發送人數給前端
-
-
-      // socket.on('login', (userdata) => {
-      //   console.log('===================================================')
-      //   console.log('後端接收到使用者資料')
-      //   onlineUsers.push(userdata)
-      //   // userdata.socketId = socket.id
-      //   userId = userdata.userId
-
-      //   const set = new Set()
-      //   // 若 user.id 沒出現過 set 中, 將 user.id 推入 set 中, 後續若有重複 user.id, 會被 filter 略過
-      //   onlineUsers = onlineUsers.filter(user =>
-      //     !set.has(userId) ? set.add(userId) : false)
-
-      //   io.emit('onlineUser', { onlineUsers })// 發送資料給所有使用者
-      //   console.log(onlineUsers)
-      // });
-
-
 
       // 公開聊天室的行為
       socket.on('chat message', async (userMsg) => {
         try {
-          // if (userMsg.behavior === 'enter-public') {
-          //   // 廣播上線資訊給大家
-          //   io.emit('chat message', userMsg)
-          // }
-
           if (userMsg.behavior === 'public-chat') {
             await PublicChat.create({
               UserId: userMsg.senderId,
@@ -106,31 +75,7 @@ module.exports = {
         } catch (err) {
           console.log(err)
         }
-        
       });
-
-      // socket.on('disconnect', () => {
-      //   console.log('Bye~');  // 顯示 bye~
-
-      //   console.log('===================================================')
-      //   console.log('確認資料放進onlineUsers')
-      //   console.log(onlineUsers)
-
-      //   onlineCount = (onlineCount < 0) ? 0 : onlineCount -= 1;// 使用者離線，扣人
-
-      //   // 登出的使用者 => 資料傳到前端
-      //   logout = onlineUsers.filter(user => {
-      //     return user.socketId === socket.id
-      //   })
-      //   console.log('===================================================')
-      //   console.log('刪除登出使用者資料')
-      //   console.log(logout)
-
-      //   io.emit('logoutUser', { logout })
-
-      // });
-
-
     });
   }
 }
