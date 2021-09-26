b6b948d20c63289186675c910ef7309b3478def2
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-26 19:37:30 +0800

    change name on router...

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../models");
-const userController = require("../../controllers/userController");
+const userController = require("../../controllers/usercontroller");
 const followshipController = require("../../controllers/followshipController");
 const profileController = require("../../controllers/profileController");
-const multer = require('multer')
-const upload = multer({ dest: 'temp/' })
+const multer = require("multer");
+const upload = multer({ dest: "temp/" });
 
 router.get("/signout", userController.signOut);
 
@@ -15,9 +15,16 @@ router.get("/:id/comments", profileController.getComments);
 router.get("/:id/likes", profileController.getLikedPosts);
 router.get("/:id/followers", followshipController.getFollowers);
 router.get("/:id/followings", followshipController.getFollowings);
-router.get("/:id/edit", userController.getUserSetting)  
-router.post("/:id/edit",userController.editUserSetting)
-router.put('/:id/edit', upload.fields([{ name: 'cover', maxCount: 1 }, { name: 'avatar', maxCount: 1 }]), profileController.putUserpage)
+router.get("/:id/edit", userController.getUserSetting);
+router.post("/:id/edit", userController.editUserSetting);
+router.put(
+  "/:id/edit",
+  upload.fields([
+    { name: "cover", maxCount: 1 },
+    { name: "avatar", maxCount: 1 },
+  ]),
+  profileController.putUserpage
+);
 
 router.put("/:userId/notification", followshipController.putNotification);
 
