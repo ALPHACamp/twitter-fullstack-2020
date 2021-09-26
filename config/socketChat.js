81f3390bce7aea56abc471f090f60d7b7934e7b1
Trevor Lin <trevor0966@gmail.com>
2021-09-26 13:03:01 +0800

    refactor chat.hbs & socketChat code

--- config/socketChat.js
+++ config/socketChat.js
@@ -8,10 +8,10 @@ const helpers = require('../_helpers')
 
 
 
-let onlineCount = 0;// 加入線上人數計數
-let onlineUsers = []
-let logout = ''
-let userId = ''
+// let onlineCount = 0;// 加入線上人數計數
+// let onlineUsers = []
+// let logout = ''
+// let userId = ''
 
 module.exports = {
   Server(server) {
@@ -19,12 +19,12 @@ module.exports = {
   },
   connect() {
     io.on('connection', (socket) => {
-      console.log('有人公開進入聊天室囉！')
-      socket.on("userIn", async (currentUserId) => {
+      console.log('有人進入公開聊天室囉！')
+      socket.on("userIn", async (userId) => {
         try {
           //運用socket儲存值
-          socket.currentUserId = currentUserId
-          const currentUser = await User.findByPk(currentUserId)
+          socket.currentUserId = userId
+          const currentUser = await User.findByPk(userId)
           await currentUser.update({publicRoom: true})
           const publicRoomUsers = await User.findAll({
             raw: true,
@@ -46,31 +46,32 @@ module.exports = {
           nest: true,
           where: { publicRoom: 1 }
         })
+        // 因有使用者離線，重新更新使用者上線名單與人數
         io.emit('publicRoomUsers', publicRoomUsers)
       })
       
-      console.log('進入聊天室')
-      console.log('客戶端成功連線服務器: ' + socket.id)
+      // console.log('進入聊天室')
+      // console.log('客戶端成功連線服務器: ' + socket.id)
 
-      onlineCount++;// 有連線發生時增加人數
-      io.emit("online", onlineCount);   // 發送人數給前端
+      // onlineCount++;// 有連線發生時增加人數
+      // io.emit("online", onlineCount);   // 發送人數給前端
 
 
-      socket.on('login', (userdata) => {
-        console.log('===================================================')
-        console.log('後端接收到使用者資料')
-        onlineUsers.push(userdata)
-        // userdata.socketId = socket.id
-        userId = userdata.userId
+      // socket.on('login', (userdata) => {
+      //   console.log('===================================================')
+      //   console.log('後端接收到使用者資料')
+      //   onlineUsers.push(userdata)
+      //   // userdata.socketId = socket.id
+      //   userId = userdata.userId
 
-        const set = new Set()
-        // 若 user.id 沒出現過 set 中, 將 user.id 推入 set 中, 後續若有重複 user.id, 會被 filter 略過
-        onlineUsers = onlineUsers.filter(user =>
-          !set.has(userId) ? set.add(userId) : false)
+      //   const set = new Set()
+      //   // 若 user.id 沒出現過 set 中, 將 user.id 推入 set 中, 後續若有重複 user.id, 會被 filter 略過
+      //   onlineUsers = onlineUsers.filter(user =>
+      //     !set.has(userId) ? set.add(userId) : false)
 
-        io.emit('onlineUser', { onlineUsers })// 發送資料給所有使用者
-        console.log(onlineUsers)
-      });
+      //   io.emit('onlineUser', { onlineUsers })// 發送資料給所有使用者
+      //   console.log(onlineUsers)
+      // });
 
 
 
@@ -87,26 +88,26 @@ module.exports = {
         }
       });
 
-      socket.on('disconnect', () => {
-        console.log('Bye~');  // 顯示 bye~
+      // socket.on('disconnect', () => {
+      //   console.log('Bye~');  // 顯示 bye~
 
-        console.log('===================================================')
-        console.log('確認資料放進onlineUsers')
-        console.log(onlineUsers)
+      //   console.log('===================================================')
+      //   console.log('確認資料放進onlineUsers')
+      //   console.log(onlineUsers)
 
-        onlineCount = (onlineCount < 0) ? 0 : onlineCount -= 1;// 使用者離線，扣人
+      //   onlineCount = (onlineCount < 0) ? 0 : onlineCount -= 1;// 使用者離線，扣人
 
-        // 登出的使用者 => 資料傳到前端
-        logout = onlineUsers.filter(user => {
-          return user.socketId === socket.id
-        })
-        console.log('===================================================')
-        console.log('刪除登出使用者資料')
-        console.log(logout)
+      //   // 登出的使用者 => 資料傳到前端
+      //   logout = onlineUsers.filter(user => {
+      //     return user.socketId === socket.id
+      //   })
+      //   console.log('===================================================')
+      //   console.log('刪除登出使用者資料')
+      //   console.log(logout)
 
-        io.emit('logoutUser', { logout })
+      //   io.emit('logoutUser', { logout })
 
-      });
+      // });
 
 
     });
