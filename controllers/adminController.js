268fbc7c91584fcf598e7ce7961e5365ad9fa8f8
Bumble <nick1092387456@gmail.com>
2021-09-27 19:27:29 +0800

    feat: admin userlist.hbs

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,6 +6,10 @@ const adminController = {
     return res.render('admin/signin')
   },
 
+  getUsers: (req, res) => {
+    return res.render('admin/users')
+  },
+
   signin: (req, res) => {
     req.flash('success_messages', '成功登入！')
     res.redirect('/admin/tweets')
@@ -15,7 +19,7 @@ const adminController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/admin/signin')
-  }
+  },
 }
 
 module.exports = adminController
