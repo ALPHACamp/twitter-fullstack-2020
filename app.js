52e069247e1b67fb990c39c2953b5f96524e0239
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-27 22:29:36 +0800

    feat: remove html validation

--- app.js
+++ app.js
@@ -52,7 +52,6 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash("success_messages");
   res.locals.error_messages = req.flash("error_messages");
-  res.locals.tweet_message = req.flash("tweet_message");
   res.locals.user = helpers.getUser(req);
   next();
 });
