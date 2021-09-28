8a04ee8bf7b27c6fa1336ad178a3264cc8c47880
chiahung <a0926223478@hotmail.com>
2021-09-28 21:36:11 +0800

    fix: add authenticate in router

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,24 +1,24 @@
 // This modules is for user and profile routers
 const express = require("express");
 const router = express.Router();
-const db = require("../../models");
 const userController = require("../../controllers/userController");
 const followshipController = require("../../controllers/followshipController");
 const profileController = require("../../controllers/profileController");
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
+const { authenticated } = require('../../middleware/authenticationHelper');
 
-router.get("/signout", userController.signOut);
+router.get("/signout", authenticated, userController.signOut);
 
-router.get("/:id/tweets", profileController.getPosts);
-router.get("/:id/comments", profileController.getComments);
-router.get("/:id/likes", profileController.getLikedPosts);
-router.get("/:id/followers", followshipController.getFollowers);
-router.get("/:id/followings", followshipController.getFollowings);
-router.get("/:id/edit", userController.getUserSetting)
-router.post("/:id/edit", userController.editUserSetting)
-router.put('/:id/edit', upload.fields([{ name: 'cover', maxCount: 1 }, { name: 'avatar', maxCount: 1 }]), profileController.putUserpage)
+router.get("/:id/tweets", authenticated, profileController.getPosts);
+router.get("/:id/comments", authenticated, profileController.getComments);
+router.get("/:id/likes", authenticated, profileController.getLikedPosts);
+router.get("/:id/followers", authenticated, followshipController.getFollowers);
+router.get("/:id/followings", authenticated, followshipController.getFollowings);
+router.get("/:id/edit", authenticated, userController.getUserSetting)
+router.post("/:id/edit", authenticated, userController.editUserSetting)
+router.put('/:id/edit', authenticated, upload.fields([{ name: 'cover', maxCount: 1 }, { name: 'avatar', maxCount: 1 }]), profileController.putUserpage)
 
-router.put("/:id/notification", followshipController.putNotification);
+router.put("/:id/notification", authenticated, followshipController.putNotification);
 
 module.exports = router;
