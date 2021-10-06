a81d67020132d5ea9cb04503d8107d129be32999
rexlin <rexlin6245@gmail.com>
2021-10-06 11:51:48 -0700

    buildup dev environment build models build seeds

--- models/user.js
+++ models/user.js
@@ -1,8 +1,82 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING(189),
+      unique: true
+    },
+    password: DataTypes.STRING(189),
+    name: DataTypes.STRING(189),
+    avatar: DataTypes.STRING(189),
+    introduction: DataTypes.TEXT,
+    account: {
+      type: DataTypes.STRING(189),
+      unique: true
+    },
+    cover: DataTypes.STRING(189),
+    role: {
+      type: DataTypes.STRING(189),
+      defaultValue: 'user'
+    }
+  }, {
+    hooks: {
+      beforeCreate: async (User, next) => {
+        const password = User.dataValues.password
+        if (password) {
+          try {
+            const salt = await bcrypt.genSalt(10)
+            User.dataValues.password = await bcrypt.hash(password, salt)
+          }
+          catch (error) {
+            console.log(error)
+          }
+        }
+      },
+      beforeUpdate: async (User, next) => {
+        const password = User.dataValues.password
+        if (password) {
+          try {
+            const salt = await bcrypt.genSalt(10)
+            User.dataValues.password = await bcrypt.hash(password, salt)
+          }
+          catch (error) {
+            console.log(error)
+          }
+        }
+      }
+    }
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
+    User.hasMany(models.Tweet, {
+      foreignKey: 'UserId',
+      as: 'userTweets'
+    })
+    User.hasMany(models.Reply, {
+      foreignKey: 'UserId',
+      as: 'replies'
+    })
+    User.hasMany(models.Followship, {
+      foreignKey: 'followerId',
+      as: 'following'
+    })
+    User.hasMany(models.Followship, {
+      foreignKey: 'followingId',
+      as: 'follower'
+    })
+    User.hasMany(models.Like, {
+      foreignKey: 'UserId',
+      as: 'likes'
+    })
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
+    User.belongsToMany(models.User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
   };
   return User;
 };
