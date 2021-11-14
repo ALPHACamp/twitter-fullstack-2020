fa0b11cc7e90dacc3e5c69bc664fbd94445e4bcb
Amber Yan <freehourse2sh@gmail.com>
2021-11-14 16:41:46 +0800

    add comments on tests/models

--- test/models/Like.spec.js
+++ test/models/Like.spec.js
@@ -15,54 +15,62 @@ const db = require('../../models')
 const LikeModel = require('../../models/like')
 
 describe('# Like Model', () => {
-  
-  before(done => {
-    done()
-  })
-
+  // 使用寫好的 Like Model
   const Like = LikeModel(sequelize, dataTypes)
+  // 創建 like instance 
   const like = new Like()
+  // 檢查 Model name
   checkModelName(Like)('Like')
 
+  // 檢查 like 是否有 UserId, TweetId 屬性
   context('properties', () => {
     ;[
+      'UserId', 'TweetId'
     ].forEach(checkPropertyExists(like))
   })
 
+  // 檢查 like 的關聯是否正確
   context('associations', () => {
     const User = 'User'
     const Tweet = 'Tweet'
     before(() => {
+      // 將 Like model 對 User, Tweet 做關聯(呼叫 associate)
       Like.associate({ User })
       Like.associate({ Tweet })
     })
 
     it('should belong to user', (done) => {
+      // 檢查是否有呼叫 belongsTo(User)
       expect(Like.belongsTo).to.have.been.calledWith(User)
       done()
     })
     it('should belong to tweet', (done) => {
+      // 檢查是否有呼叫 belongsTo(Tweet)
       expect(Like.belongsTo).to.have.been.calledWith(Tweet)
       done()
     })
   })
 
+  // 檢查 model 的新增、修改、刪除、更新
   context('action', () => {
 
     let data = null
 
+    // 檢查 db.Like 是否真的可以新增一筆資料
     it('create', (done) => {
       db.Like.create({}).then((like) => {   
         data = like
         done()
       })
     })
+    // 檢查 db.Like 是否真的可以讀取一筆資料
     it('read', (done) => {
         db.Like.findByPk(data.id).then((like) => {  
           expect(data.id).to.be.equal(like.id)
           done()
         })
     })
+    // 檢查 db.Like 是否真的可以更新一筆資料
     it('update', (done) => {
       db.Like.update({}, { where: { id: data.id }}).then(() => {
         db.Like.findByPk(data.id).then((like) => { 
@@ -71,6 +79,7 @@ describe('# Like Model', () => {
         })
       })
     })
+    // 檢查 db.Like 是否真的可以刪除一筆資料 
     it('delete', (done) => {
       db.Like.destroy({ where: { id: data.id }}).then(() => {
         db.Like.findByPk(data.id).then((like) => { 
--- test/models/Tweet.spec.js
+++ test/models/Tweet.spec.js
@@ -15,61 +15,68 @@ const db = require('../../models')
 const TweetModel = require('../../models/tweet')
 
 describe('# Tweet Model', () => {
-  
-  before(done => {
-    done()
-
-  })
-
+  // 使用寫好的 Tweet Model
   const Tweet = TweetModel(sequelize, dataTypes)
+  // 創建 tweet instance 
   const like = new Tweet()
+  // 檢查 Model name
   checkModelName(Tweet)('Tweet')
 
+  // 檢查 tweet 是否有 UserId, description 屬性
   context('properties', () => {
     ;[
+      'description', 'UserId'
     ].forEach(checkPropertyExists(like))
   })
 
+  // 檢查 tweet 的關聯是否正確
   context('associations', () => {
     const Reply = 'Reply'
     const Like = 'Like'
     const User = 'User'
     before(() => {
+      // 將 Tweet model 對 User, Tweet, Reply 做關聯(呼叫 associate)
       Tweet.associate({ Reply })
       Tweet.associate({ Like })
       Tweet.associate({ User })
     })
 
     it('should have many replies', (done) => {
+      // 檢查是否有呼叫 hasMany(Reply)
       expect(Tweet.hasMany).to.have.been.calledWith(Reply)
       done()
     })
     it('should have many likes', (done) => {
+      // 檢查是否有呼叫 hasMany(Like)
       expect(Tweet.hasMany).to.have.been.calledWith(Like)
       done()
     })
     it('should belong to user', (done) => {
+      // 檢查是否有呼叫 belongsTo(User)
       expect(Tweet.belongsTo).to.have.been.calledWith(User)
       done()
     })
   })
 
+  // 檢查 model 的新增、修改、刪除、更新
   context('action', () => {
 
     let data = null
-
+    // 檢查 db.Tweet 是否真的可以新增一筆資料
     it('create', (done) => {
       db.Tweet.create({UserId: 1, description: 'hi'}).then((tweet) => {   
         data = tweet
         done()
       })
     })
+    // 檢查 db.Tweet 是否真的可以讀取一筆資料
     it('read', (done) => {
       db.Tweet.findByPk(data.id).then((tweet) => {  
         expect(data.id).to.be.equal(tweet.id)
           done()
         })
     })
+    // 檢查 db.Tweet 是否真的可以更新一筆資料
     it('update', (done) => {
       db.Tweet.update({}, { where: { id: data.id }}).then(() => {
         db.Tweet.findByPk(data.id).then((tweet) => { 
@@ -78,6 +85,7 @@ describe('# Tweet Model', () => {
         })
       })
     })
+    // 檢查 db.Tweet 是否真的可以刪除一筆資料
     it('delete', (done) => {
       db.Tweet.destroy({ where: { id: data.id }}).then(() => {
         db.Tweet.findByPk(data.id).then((tweet) => { 
