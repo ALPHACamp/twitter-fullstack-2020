b9f3746bb07cf410a5157669c58f7650ab7cfc5e
Stanney <freehourse2sh@gmail.com>
2021-11-19 14:48:47 +0800

    add comments on test/requests/reply.spec.js

--- test/requests/reply.spec.js
+++ test/requests/reply.spec.js
@@ -7,12 +7,17 @@ var should = chai.should()
 var expect = chai.expect;
 const db = require('../../models')
 
+// 測試留言相關功能
+// 1. 可否取得留言資料
+// 2. 可以新增留言
+// 3. 測試沒有相關 models 預先存在的話，新增留言會失敗
 describe('# reply request', () => {
 
   context('#index', () => {
+    // 可否取得留言資料
     describe('GET /tweets/:id/replies', () => {
       before(async() => {
-        
+        // 模擬驗證資料
         this.ensureAuthenticated = sinon.stub(
           helpers, 'ensureAuthenticated'
         ).returns(true);
@@ -20,29 +25,32 @@ describe('# reply request', () => {
           helpers, 'getUser'
         ).returns({id: 1, Followings: []});
 
+        // 在測試資料庫中，確保 db 資料清空了
         await db.User.destroy({where: {},truncate: true})
         await db.Tweet.destroy({where: {},truncate: true})
         await db.Reply.destroy({where: {},truncate: true})
-
+        // 在測試資料庫中，新增 mock 資料
         await db.User.create({})
         await db.Tweet.create({UserId: 1, description: 'test'})
         await db.Reply.create({UserId: 1, TweetId: 1, comment: 'Tweet1 的 comment'})
       })
 
       it('should render index', (done) => {
+        // 確認 GET /tweets/1/replies 可以顯示留言
         request(app)
           .get('/tweets/1/replies')
           .set('Accept', 'application/json')
           .expect(200)
           .end(function(err, res) {
             if (err) return done(err);
+            // 確認顯示的留言中有 Tweet1 的 comment 這個資訊
             res.text.should.include('Tweet1 的 comment')
             return done();
           });
       })
 
       after(async () => {
-        
+        // 清除驗證資料以及測試 db 資料 
         this.ensureAuthenticated.restore();
         this.getUser.restore();
         await db.User.destroy({where: {},truncate: true})
@@ -53,23 +61,26 @@ describe('# reply request', () => {
   })
 
   context('#post', () => {
+    // 可以新增留言     
     describe('POST /tweets/1/replies successfully', () => {
       before(async() => {
-        
+        // 模擬驗證資料  
         this.ensureAuthenticated = sinon.stub(
           helpers, 'ensureAuthenticated'
         ).returns(true);
         this.getUser = sinon.stub(
           helpers, 'getUser'
         ).returns({id: 1, Followings: []});
+        // 在測試資料庫中，新增 mock 資料
         await db.User.create({})
         await db.Tweet.create({UserId: 1, description: 'test'})
       })
 
-      it('will redirect to index', (done) => {
+      it('POST /tweets/1/replies', (done) => {
+        // 送出 request POST /tweets/1/replies
         request(app)
           .post('/tweets/1/replies')
-          .send('comment=comment')
+          .send('comment=comment') // 留言內容是 comment
           .set('Accept', 'application/json')
           .expect(302)
           .end(function(err, res) {
@@ -77,15 +88,18 @@ describe('# reply request', () => {
             return done();
           });
       })
+
       it('when successfully save', (done) => {
+        // 讀取測試資料庫的 Reply table 所有資料
         db.Reply.findOne({where: {userId: 1}}).then(reply => {
+          // 檢查這個 table 不是空的，表示有新增資料進去
           expect(reply).to.not.be.null
           done()
         })
       })
 
       after(async () => {
-        
+        // 清除驗證資料以及測試 db 資料
         this.ensureAuthenticated.restore();
         this.getUser.restore();
         await db.User.destroy({where: {},truncate: true})
@@ -94,12 +108,14 @@ describe('# reply request', () => {
       })
     })
 
+    // 測試沒有相關 models 預先存在的話，新增留言會失敗
     describe('POST /tweets/1/replies fail', () => {
       before(async() => {
-        
+        // 沒有預先新增驗證及測試資料庫資料
       })
 
-      it('will redirect index', (done) => {
+      it('POST /tweets/1/replies', (done) => {
+        // 送出 request POST /tweets/1/replies 
         request(app)
           .post('/tweets/1/replies')
           .set('Accept', 'application/json')
@@ -111,7 +127,7 @@ describe('# reply request', () => {
       })
 
       after(async () => {
-        
+        // 沒有資料需要清除
       })
     })
   })
