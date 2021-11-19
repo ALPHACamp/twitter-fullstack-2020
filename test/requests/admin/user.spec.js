9101961ca5f326e7ff8090ea0a9a24742ab3daef
Stanney <freehourse2sh@gmail.com>
2021-11-19 16:03:45 +0800

    add comments to /test/requests/admin/user.spec.js

--- test/requests/admin/user.spec.js
+++ test/requests/admin/user.spec.js
@@ -8,23 +8,26 @@ var app = require('../../../app')
 var helpers = require('../../../_helpers');
 const db = require('../../../models')
 
-
+// admin 使用者相關功能測試
+// 1. 如果一般 user 進入，就要回到 user root path
+// 2. 如果是 admin 進入，就可以看到所有 User 資料
 describe('# Admin::User request', () => {
 
   context('go to admin user page', () => {
     describe('if normal user log in', () => {
       before(async() => {
-        
+        // 模擬登入資料
         this.ensureAuthenticated = sinon.stub(
           helpers, 'ensureAuthenticated'
         ).returns(true);
         this.getUser = sinon.stub(
           helpers, 'getUser'
         ).returns({id: 1, Followings: []});
+        // 在測試資料庫中，新增 mock 資料
         await db.User.create({name: 'User1'})
       })
 
-
+      // 如果一般 user 進入，就要回到 user root path
       it('should redirect to root path', (done) => {
         request(app)
           .get('/admin/users')
@@ -36,7 +39,7 @@ describe('# Admin::User request', () => {
       })
 
       after(async () => {
-        
+        // 清除登入及測試資料庫資料 
         this.ensureAuthenticated.restore();
         this.getUser.restore();
         await db.User.destroy({where: {},truncate: true})
@@ -45,29 +48,32 @@ describe('# Admin::User request', () => {
     
     describe('if admin user log in', () => {
       before(async() => {
-        
+        // 模擬登入資料  
         this.ensureAuthenticated = sinon.stub(
           helpers, 'ensureAuthenticated'
         ).returns(true);
         this.getUser = sinon.stub(
           helpers, 'getUser'
         ).returns({id: 1, Followings: [], role: 'admin'});
+        // 在測試資料庫中，新增 mock 資料
         await db.User.create({name: 'User1'})
       })
 
+      // 如果是 admin 進入，就可以看到所有 User 資料
       it('should see all user list', (done) => {
         request(app)
           .get('/admin/users')
           .expect(200)
           .end(function(err, res) {
             if (err) return done(err);
+            // 檢查是否有 User1 資料
             res.text.should.include('User1')
             done();
           });
       })
 
       after(async () => {
-        
+        // 清除登入及測試資料庫資料
         this.ensureAuthenticated.restore();
         this.getUser.restore();
         await db.User.destroy({where: {},truncate: true})
