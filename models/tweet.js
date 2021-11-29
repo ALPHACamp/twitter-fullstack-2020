246588e41195b3a5afdba78f06c4f98dfa501b81
yourname <tree12132002@hotmail.com>
2021-11-30 07:31:25 +0800

    Create tweet model

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
   }, {});
   Tweet.associate = function(models) {
+    Tweet.belongsTo(models.User)
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
   };
   return Tweet;
 };
