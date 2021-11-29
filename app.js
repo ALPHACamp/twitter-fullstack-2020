8c901d20935b1e9040ba2a9f9a98c9b61202adb7
Lettuce Lu <lettuce.lu@lettucelu.com>
2021-11-29 22:56:18 +0800

    add flash message

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ const helpers = require('./_helpers');
 
 const app = express()
 const port = 3000
-
+const db = require('./models')
+const User = db.User
+const bcrypt = require('bcryptjs')
+const flash = require('connect-flash')
+const session = require('express-session')
 app.use(express.urlencoded({ extended: true }))
 app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
 
-// use helpers.getUser(req) to replace req.user
-// use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    next()
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -25,8 +35,27 @@ app.get('/signup', (req, res) => {
 })
 
 app.post('/signup', (req, res) => {
-    console.log(req.body)
-    res.send(req.body)
+    // confirm password
+    if (req.body.passwordCheck !== req.body.password) {
+        req.flash('error_messages', '兩次密碼輸入不同！')
+        return res.redirect('/signup')
+    } else {
+        User.findOne({ where: { email: req.body.email } }).then(user => {
+            if (user) {
+                req.flash('error_messages', '信箱重複！')
+                return res.redirect('/signup')
+            } else {
+                User.create({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+                }).then(user => {
+                    req.flash('success_messages', '成功註冊帳號！')
+                    return res.redirect('/signin')
+                })
+            }
+        })
+    }
 })
 // 後台
 app.get('/admin/signin', (req, res) => {
--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: 0
+    }
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
   };
   return User;
 };
