abc19c506e9452ba18cc0103b006499683dd9487
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-11-30 16:47:08 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -11,6 +12,11 @@ const PORT = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 
 require('./routes')(app)
 
