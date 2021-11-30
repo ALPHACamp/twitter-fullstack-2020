e4c261355645895b72a085780bcd01851497b855
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-11-30 10:56:02 +0800

    feat: initialize models

--- models/user.js
+++ models/user.js
@@ -1,6 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.STRING
   }, {});
   User.associate = function(models) {
   };
