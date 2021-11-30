32f98873b779ab1b40d3d9657b55a3edbe5bb3c9
Liao CY <liaochungyid@gmail.com>
2021-12-01 01:00:40 +0800

    Add seed: users-seed-file

---
+++ seeders/20211130154650-users-seed-file.js
@@ -0,0 +1,39 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+// all the email and name of users/admin list here
+const userSeed = [
+  { email: 'root@example.com', name: 'Admin' },
+  { email: 'user1@example.com', name: 'user1' },
+  { email: 'user2@example.com', name: 'user2' },
+  { email: 'user3@example.com', name: 'user3' },
+  { email: 'user4@example.com', name: 'user4' },
+  { email: 'user5@example.com', name: 'user5' }
+]
+// all users/admin use same password, do it once here
+const password = bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null)
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [
+      ...userSeed.map((user, index) => ({
+        id: index * 10 + 1,
+        email: user.email,
+        password: password,
+        name: user.name,
+        avatar: null,
+        introduction: faker.lorem.text().substring(0, 160),
+        role: user.name === 'Admin' ? 'Admin' : 'User',
+        account: user.email,
+        cover: null,
+        createdAt: new Date(new Date().setDate(new Date().getDate() - 10 + index / 5)),
+        updatedAt: new Date(new Date().setDate(new Date().getDate() - 10 + index / 5))
+      })
+      )], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+};
