f6e79fa7221cbeadf637fc01f75ddf9518661863
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-11-30 17:57:53 +0800

    feat: confirm password and unique email

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,14 +6,33 @@ const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
   },
+
   signUp: async (req, res) => {
-    const { name, email, password } = req.body
-    await User.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-    })
-    return res.redirect('/signin')
+    try {
+      const { name, email, password, passwordCheck } = req.body
+
+      if (passwordCheck !== password) {
+        req.flash('error_messages', '兩次密碼輸入不同！')
+        return res.redirect('/signup')
+      }
+
+      const user = await User.findOne({ where: { email } })
+      if (user) {
+        req.flash('error_messages', '電子信箱重複！')
+        return res.redirect('/signup')
+      }
+
+      await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+      })
+
+      req.flash('success_messages', '成功註冊帳號！')
+      return res.redirect('/signin')
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
 
