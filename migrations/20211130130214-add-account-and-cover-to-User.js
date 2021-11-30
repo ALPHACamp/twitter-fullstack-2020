7cbd9e848287a6c3f5bdd9203f843da06ecd3dc5
Liao CY <liaochungyid@gmail.com>
2021-11-30 21:27:30 +0800

    Add account & cover columns to User (migration & model)

---
+++ migrations/20211130130214-add-account-and-cover-to-User.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'account', {
+      allowNull: false,
+      type: Sequelize.STRING
+    });
+
+    await queryInterface.addColumn('Users', 'cover', {
+      type: Sequelize.STRING
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'account');
+    await queryInterface.removeColumn('Users', 'cover');
+  }
+};
