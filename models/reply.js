125d0c5f36a47bfcde286b417bee8d28c22bdc87
Sin0001 <bboyliljie@gmail.com>
2021-12-01 22:32:29 +0800

    Setup user model to pass test

--- models/reply.js
+++ models/reply.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
   }, {});
   Reply.associate = function(models) {
+    //Reply.belongsTo(models.User)
   };
   return Reply;
 };
