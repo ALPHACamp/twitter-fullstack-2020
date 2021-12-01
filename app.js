dec6be50859a0d9628bfb3c5d664214f1b762ccc
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-01 23:08:30 +0800

    feat: add static & add stylesheet.css

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
