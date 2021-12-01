efa8b4c3943b1bcaf92675c690a7954685463d3c
Liangni <light0104@gmail.com>
2021-12-01 18:24:02 +0800

    Add register page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const helpers = require('./_helpers');
-
+const handlebars = require('express-handlebars')
 
 const app = express()
 const port = 3000
@@ -8,7 +8,8 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-
+app.engine('handlebars', handlebars({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+
+
+const userController = {
+  signUpPage: (req, res) => {
+    res.render('signup')
+  }
+}
+
+module.exports = userController
