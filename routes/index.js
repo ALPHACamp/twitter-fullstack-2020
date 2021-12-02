932debca84b70e5d37c2dff0899e73b12186532c
xuan1106 <spu96508@gmail.com>
2021-12-03 01:44:55 +0800

    refactor authenticatedAdmin and authenticatedUser

--- routes/index.js
+++ routes/index.js
@@ -17,35 +17,42 @@ module.exports = (app, passport) => {
     res.redirect('/signin')
   }
   const authenticatedAdmin = (req, res, next) => {
-    if (helpers.ensureAuthenticated(req)) {
-      if (helpers.getUser(req).role === 'admin') {
-        return next()
-      }
-      return res.redirect('/')
+    if (helpers.getUser(req).role === 'user') {
+      req.flash('error_msg', '前台帳號不能登入後台')
+      return res.redirect('/admin/signin')
     }
-    res.redirect('/signin')
-  }
-  const authenticatedUser = (req, res, next) => {
-    if (helpers.ensureAuthenticated(req)) {
-      if (helpers.getUser(req).role === 'user') {
+    if (helpers.getUser(req).role === 'admin') {
+      if (helpers.ensureAuthenticated(req)) {
         return next()
       }
+    }
+    res.redirect('/admin/tweets')
+  }
+
+  const authenticatedUser = (req, res, next) => {
+    if (helpers.getUser(req).role === 'admin') {
+      req.flash('error_msg', '後台帳號不能登入前台')
       return res.redirect('/admin/tweets')
     }
-    res.redirect('/admin/signin')
+    if (helpers.getUser(req).role === 'user') {
+      if (helpers.ensureAuthenticated(req)) {
+        return next()
+      }
+    }
+    res.redirect('/signin')
   }
 
 
   // ADMIN
   app.get('/admin/signin', adminController.signInPage)
-  app.post('/admin/signin', passport.authenticate('local', { failureRedirect: '/admin/signin', failureFlash: true }), adminController.signIn)
+  app.post('/admin/signin', passport.authenticate('local', { failureRedirect: '/admin/signin', failureFlash: true }), authenticatedAdmin, adminController.signIn)
   app.get('/admin/tweets', authenticatedAdmin, adminController.getTweets)
   app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
   app.delete('/admin/tweets/:id', authenticatedAdmin, adminController.deleteTweets)
   // OTHERS
 
   // TWEET
-  app.get('/', authenticated,  (req, res) => res.redirect('/tweets'))
+  app.get('/', authenticated, authenticatedUser, (req, res) => res.redirect('/tweets'))
   app.get('/tweets', authenticatedUser, tweetController.getTweets)
   app.get('/tweets/:id', authenticatedUser, tweetController.getTweet)
   app.post('/tweets', authenticatedUser, tweetController.postTweet)
@@ -61,7 +68,7 @@ module.exports = (app, passport) => {
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', {
     failureRedirect: '/signin', failureFlash: true
-  }), userController.signIn)
+  }), authenticatedUser, userController.signIn)
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
   app.get('/logout', userController.logout)
