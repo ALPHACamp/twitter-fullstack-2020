37d1f129c4ead1c8cdf334d67362864b42b0ac78
Sin0001 <bboyliljie@gmail.com>
2021-12-02 17:26:47 +0800

    將.handlebars副檔名修改為短檔名.hbs

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
+app.engine('hbs', handlebars({ 
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
