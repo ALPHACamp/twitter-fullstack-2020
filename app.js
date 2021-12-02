fdb38c92cf6373e6e35a485d6b33cc4c4161898d
Jo <a0750318@gmail.com>
2021-12-03 00:41:31 +0800

    feat/indexCenter、popularRight、settingLeft partipopularRight、settingLeft partia、settingLeft partials

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
+const handlebars = require('express-handlebars')
 
 const app = express()
 const port = 3000
@@ -7,7 +8,19 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// 設定 view engine 使用 handlebars
+app.engine(
+	'hbs',
+	handlebars({
+		defaultLayout: 'main',
+		extname: '.hbs',
+	})
+)
+
+app.set('view engine', 'hbs')
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('signin'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
