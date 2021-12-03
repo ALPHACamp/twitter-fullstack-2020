f4a7a976031599bcab5905b7e9daec166615aafd
Liangni <light0104@gmail.com>
2021-12-03 18:39:25 +0800

    Use account to replace email in login form & modify login error message

--- config/passport.js
+++ config/passport.js
@@ -15,7 +15,7 @@ passport.use(new LocalStrategy(
   // authenticate user
   (req, username, password, cb) => {
     User.findOne({ where: { account: username } }).then(user => {
-      if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
+      if (!user) return cb(null, false, req.flash('error_messages', '帳號不存在!'))
       if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
       return cb(null, user)
     })
