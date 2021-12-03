c9bf2190667c1d3f73239ec277cc1534aca655ab
yourname <tree12132002@hotmail.com>
2021-12-03 19:11:19 +0800

    Add setting user data feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
+const helpers = require('../_helpers')
 
 const userController = {
   //註冊頁面
@@ -43,6 +44,56 @@ const userController = {
     req.flash('success_msg', '成功登入！')
     res.redirect('/tweets')
   },
+
+  editPage: (req, res) => {
+    return User.findByPk(helpers.getUser(req).id)
+      .then((user) => {
+        res.render('edit', { user: user.toJSON() })
+      })
+  },
+
+  editData: (req, res) => {
+    const { name, email, password, checkPassword } = req.body
+    const currentUser = helpers.getUser(req)
+    if (!email || !name || !password || !checkPassword) {
+      req.flash('error_msg', '所有欄位皆為必填')
+      return res.redirect('back')
+    }
+
+    if (checkPassword !== password) {
+      req.flash('error_msg', '兩次密碼輸入不同！')
+      return res.redirect('back')
+    }
+    if (email !== currentUser.email) {
+      return User.findOne({ where: { email: email }})
+        .then((user) => {
+          if (user) {
+            req.flash('error_msg', '信箱已存在')
+            return res.redirect('back')
+          }
+        })
+    }
+    if (account !== currentUser.account) {
+      return User.findOne({ where: { account: account } })
+        .then((user) => {
+          if (user) {
+            req.flash('error_msg', '帳號已存在')
+            return res.redirect('back')
+          }
+        })
+    }
+    return User.findByPk(currentUser.id)
+      .then((user) => {
+        user.update({
+          name: name,
+          account: account,
+          email: email,
+          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null),
+        })
+        return res.redirect('/tweets')
+      })
+  },
+
 
   logout: (req, res) => {
     req.flash('success_msg', '登出成功！')
