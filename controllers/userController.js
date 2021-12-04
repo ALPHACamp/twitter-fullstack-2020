dedef2266d44e92bfa0bba763fbe5ae768aed8f6
Lettuce Lu <lettuce.lu@lettucelu.com>
2021-12-05 03:47:55 +0800

    finish sort feature for Follower and Following

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,12 @@
 const bcrypt = require('bcryptjs')
+const { test } = require('mocha')
 const db = require('../models')
+const user = require('../models/user')
 const User = db.User
 const Followship = db.Followship
 const Tweet = db.Tweet
+const moment = require('moment')
+
 
 const userController = {
     getSignUpPage: (req, res) => {
@@ -107,21 +111,15 @@ const userController = {
         })
 
         const userFindAll = User.findAll({
-
             include: [
-
                 { model: User, as: 'Followers' }
             ]
         })
 
         Promise.all([tweetFindAll, userFindAll])
             .then(responses => {
-                // console.log(responses[0])
                 let tweets = responses[0]
                 let users = responses[1]
-                // console.log(users)
-                // console.log(responses[1])
-
                 users = users.map(user => ({
 
                     ...user.dataValues,
@@ -136,6 +134,7 @@ const userController = {
 
                 users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
 
+                // console.log(users)
                 return res.render('user', { tweets, users })
             }).catch(error => {
                 console.log(error)
@@ -147,20 +146,54 @@ const userController = {
         const usersFindAll = User.findAll({
             include: [
                 { model: User, as: 'Followers' },
-                { model: User, as: 'Followings' }
             ]
         })
-        Promise.all([usersFindAll])
+
+        let userFindAll2 = User.findAll({
+            include: [
+                { model: User, as: 'Followers' }
+            ]
+        })
+
+
+        Promise.all([usersFindAll, userFindAll2])
             .then(responses => {
                 let users = responses[0]
-
+                let users2 = responses[1]
                 users = users.map(user => ({
                     ...user.dataValues,
-                    isFollowed: req.user.Followers.map(d => d.id).includes(user.id)
+                    isFollowed: req.user.Followers.map(d => d.id).includes(user.id),
+                    followersTime: req.user.Followers.filter(d => { return (d.id === user.id) })
                 }))
 
-                console.log(users)
-                return res.render('follower', { users })
+                users2 = users2.map(user => ({
+                    ...user.dataValues,
+                    isUser: !user.Followers.map(d => d.id).includes(user.id),
+                    // 計算追蹤者人數
+                    FollowerCount: user.Followers.length,
+                    // 判斷目前登入使用者是否已追蹤該 User 物件
+                    isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
+                }))
+                // 依追蹤者人數排序清單
+                users2 = users2.sort((a, b) => b.FollowerCount - a.FollowerCount)
+
+                let noCreatedAt = []
+                let sortCreatedAt = []
+                users.forEach(user => {
+                    if (user.followersTime.length > 0) {
+                        user.followersTime = user.followersTime[0].Followship.createdAt
+                        sortCreatedAt.push(user)
+                    } else {
+                        user.followersTime = ''
+                        noCreatedAt.push(user)
+                    }
+                })
+
+                sortCreatedAt = sortCreatedAt.sort((a, b) => b.followersTime - a.followersTime)
+
+                users = sortCreatedAt.concat(noCreatedAt)
+
+                return res.render('follower', { users, users2 })
             }).catch(error => {
                 console.log(error)
             })
@@ -170,18 +203,54 @@ const userController = {
     getFollowings: (req, res) => {
         let usersFindAll = User.findAll({
             include: [
-                { model: User, as: 'Followings' }
+                { model: User, as: 'Followings' },
+            ],
+        })
+        let userFindAll2 = User.findAll({
+            include: [
+                { model: User, as: 'Followers' }
             ]
         })
-        Promise.all([usersFindAll])
+
+        Promise.all([usersFindAll, userFindAll2])
             .then(responses => {
                 let users = responses[0]
+                let users2 = responses[1]
                 users = users.map(user => ({
                     ...user.dataValues,
-                    isFollowings: req.user.Followings.map(d => d.id).includes(user.id)
+                    isFollowings: req.user.Followings.map(d => d.id).includes(user.id),
+                    followingsTime: req.user.Followings.filter(d => { return (d.id === user.id) })
                 }))
-                console.log(users)
-                return res.render('following', { users })
+
+                users2 = users2.map(user => ({
+                    ...user.dataValues,
+                    isUser: !user.Followers.map(d => d.id).includes(user.id),
+                    // 計算追蹤者人數
+                    FollowerCount: user.Followers.length,
+                    // 判斷目前登入使用者是否已追蹤該 User 物件
+                    isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
+                }))
+                // 依追蹤者人數排序清單
+                users2 = users2.sort((a, b) => b.FollowerCount - a.FollowerCount)
+
+                let noCreatedAt = []
+                let sortCreatedAt = []
+                users.forEach(user => {
+                    if (user.followingsTime.length > 0) {
+                        user.followingsTime = user.followingsTime[0].Followship.createdAt
+                        sortCreatedAt.push(user)
+                    } else {
+                        user.followingsTime = ''
+                        noCreatedAt.push(user)
+                    }
+                })
+
+                sortCreatedAt = sortCreatedAt.sort((a, b) => b.followingsTime - a.followingsTime)
+
+
+                users = sortCreatedAt.concat(noCreatedAt)
+
+                return res.render('following', { users, users2 })
             }).catch(error => {
                 console.log(error)
             })
