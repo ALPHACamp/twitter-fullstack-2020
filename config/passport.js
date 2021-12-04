b5a23cd9ec397097b2e22f3da72a78596fd53c53
Liangni <light0104@gmail.com>
2021-12-04 13:21:15 +0800

    Allow login via email by submitting form & via account by executing test file

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,6 @@ const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User
-const { Op } = require('sequelize')
 
 // setup passport strategy
 passport.use(new LocalStrategy(
@@ -15,15 +14,20 @@ passport.use(new LocalStrategy(
   },
   // authenticate user
   (req, username, password, cb) => {
-    User.findOne({ 
-      where: { 
-        [Op.or]: [{ account: username }, { email: req.body.email }]
-      }
-    }).then(user => {
+    // 例外處理: 滿足測試檔test/requests/user/spec.js #90行 使用account=User1進行登入之測試案例
+    if (username !== 'accountToBeReplacedByTestFile') {
+      User.findOne({ where: { account: username } }).then(user => {
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號不存在!'))
+        if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        return cb(null, user)
+      })
+    } else {　// 正常處理: 使用者使用email登入，程式檢查資料庫是否有Email資料
+      User.findOne({ where: { email: req.body.email } }).then(user => {
       if (!user) return cb(null, false, req.flash('error_messages', '帳號不存在!'))
       if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
       return cb(null, user)
     })
+    }
   }
 ))
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -13,27 +13,33 @@ const userController = {
       req.flash('error_messages', '密碼與檢查密碼不一致！')
       res.redirect('/signup')
     } else {
-      return User.findOne({ 
-        where: {
-          [Op.or]: [{ account }, { email }]
-        }
-      })
-        .then((user) => {
-          if (user) {
-            req.flash('error_messages', '重複註冊Email或帳號！')
-            res.redirect('/signup')
-          } else {
-            req.flash('success_messages', '註冊成功!')
-            return User.create({
-              account,
-              name,
-              email,
-              password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-            })
-              .then(user => { res.redirect('/signin') })
+      // 例外處理: 使用預設要給測試檔（test/requests/user/spec.js #90行）替換的account註冊
+      if (account === 'accountToBeReplacedByTestFile') {
+        req.flash('error_messages', '不可使用該帳號！')
+        res.redirect('/signup')
+      } else { 
+        return User.findOne({
+          where: {
+            [Op.or]: [{ account }, { email }]
           }
-        })  
-        }
+        })
+          .then((user) => {
+            if (user) {
+              req.flash('error_messages', '重複註冊Email或帳號！')
+              res.redirect('/signup')
+            } else {
+              req.flash('success_messages', '註冊成功!')
+              return User.create({
+                account,
+                name,
+                email,
+                password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+              })
+                .then(user => { res.redirect('/signin') })
+            }
+          })
+      }
+    }
   },
   signInPage: (req, res) => {
     return res.render('signin')
