e7b9ff931b079e25d59fdd41d2da2c65e04bca7e
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-06 07:31:39 +0800

    feat: add api/users/:userId route

---
+++ controllers/api/userController.js
@@ -0,0 +1,9 @@
+const helpers = require('../../_helpers')
+
+const userController = {
+  getEditModal: (req, res) => {
+    // return res.json(currentUser)
+  }
+}
+
+module.exports = userController
