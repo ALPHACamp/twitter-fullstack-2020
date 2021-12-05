0103de902c51641033f524acb1d8de372d6f6389
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-05 20:32:12 +0800

    chore: standard config ok!

--- config/passport.js
+++ config/passport.js
@@ -1,43 +1,50 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
-
 const db = require('../models')
 const { User } = db
 
-passport.use(new LocalStrategy(
-  {
-    usernameField: 'account',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-  async (req, account, password, done) => {
-    try {
-      const user = await User.findOne({ where: { account } })
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'account',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    async (req, account, password, done) => {
+      try {
+        const user = await User.findOne({ where: { account } })
 
-      if (!user
-        || user.role === 'admin' && !req.url.includes('admin')
-        || user.role === 'user' && req.url.includes('admin')) {
-        return done(null, false, req.flash('error_messages', '該帳號未註冊！'))
+        if (
+          !user ||
+          (user.role === 'admin' && !req.url.includes('admin')) ||
+          (user.role === 'user' && req.url.includes('admin'))
+        ) {
+          return done(null, false, req.flash('error_messages', '該帳號未註冊！'))
+        }
+
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false, req.flash('error_messages', '密碼錯誤'))
+        }
+
+        return done(null, user)
+      } catch (err) {
+        console.error(err)
       }
-
-      if (!bcrypt.compareSync(password, user.password)) {
-        return done(null, false, req.flash('error_messages', '密碼錯誤'))
-      }
-
-      return done(null, user)
-    } catch (err) {
-      console.error(err)
     }
-  }
-))
+  )
+)
 
 passport.serializeUser((user, done) => {
   return done(null, user.id)
 })
 passport.deserializeUser(async (id, done) => {
-  const user = await User.findByPk(id)
-  return done(null, user.toJSON())
+  try {
+    const user = await User.findByPk(id)
+    return done(null, user.toJSON())
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 module.exports = passport
