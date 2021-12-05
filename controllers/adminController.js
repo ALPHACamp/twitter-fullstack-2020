74ba0e814e505171de946f73b97d5b6af50c50ed
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-05 20:19:50 +0800

    chore: unify ***Count name

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,65 +4,70 @@ const { Op } = db.Sequelize
 const { User, Tweet, Reply, Like, Followship } = db
 
 const adminController = {
-  getTweets: async (req, res) => { 
+  getTweets: async (req, res) => {
     try {
-      let tweets = await Tweet.findAll({ 
-        raw: true, 
-        nest: true, 
-        order: [[ 'createdAt', 'DESC' ]], 
-        include: [ { model: User } ] 
+      let tweets = await Tweet.findAll({
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'DESC']],
+        include: [{ model: User }],
       })
 
-      tweets = tweets.map(tweet => ({
+      tweets = tweets.map((tweet) => ({
         ...tweet,
-        description: tweet.description.slice(0, 50)
+        description: tweet.description.slice(0, 50),
       }))
-      
+
       return res.render('admintweets', { tweets })
-    } catch (err) { console.error(err) }
+    } catch (err) {
+      console.error(err)
+    }
   },
 
   deleteTweet: async (req, res) => {
     try {
-      await Tweet.destroy({ where: { id: req.params.id }  })
+      await Tweet.destroy({ where: { id: req.params.id } })
       return res.redirect('/admin/tweets')
-    } catch (err) { console.error(err) }
+    } catch (err) {
+      console.error(err)
+    }
   },
 
   adminUsers: async (req, res) => {
-    try { 
-      let users =  await User.findAll({
+    try {
+      let users = await User.findAll({
         include: [
           { model: Tweet, include: [Like] },
           { model: User, as: 'followings' },
           { model: User, as: 'Followers' },
         ],
-        order: [[ Tweet, 'createdAt', 'ASC' ]],
+        order: [[Tweet, 'createdAt', 'ASC']],
       })
 
-      users = users.map(user => 
-        ({
+      users = users
+        .map((user) => ({
           ...user.dataValues,
-          tweetsCount: user.Tweets.length,
-          likesCount: adminController.sumLikes(
-            user.Tweets.map(tweet => tweet.Likes.length)
-          ), // 有寫工具function adminController.sumLikes(arr) 計算加總 
-          followingsCount: user.followings.length,
-          followersCount: user.Followers.length
-        })
-      ).sort((a, b) => b.tweetsCount - a.tweetsCount) // 根據tweet數排序
-      
+          tweetCount: user.Tweets.length,
+          likeCount: adminController.sumLikes(
+            user.Tweets.map((tweet) => tweet.Likes.length)
+          ), // 有寫工具function adminController.sumLikes(arr) 計算加總
+          followingCount: user.followings.length,
+          followerCount: user.Followers.length,
+        }))
+        .sort((a, b) => b.tweetCount - a.tweetCount) // 根據tweet數排序
 
       return res.render('adminusers', { users })
       // return res.json(users)
-    } catch (err) { console.error(err) }
+    } catch (err) {
+      console.error(err)
+    }
   },
 
   sumLikes: (arr) => {
     let likes = 0
-    arr.forEach(i => likes += i)
+    arr.forEach((i) => (likes += i))
     return likes
-  }
+  },
 }
 
 module.exports = adminController
