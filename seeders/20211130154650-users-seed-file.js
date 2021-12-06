35cfa894bdd1b1f216f938f6d203cd6ba11d131a
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-06 17:01:39 +0800

    chore: unify user name to be lower case

--- seeders/20211130154650-users-seed-file.js
+++ seeders/20211130154650-users-seed-file.js
@@ -6,12 +6,12 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     // all the email and name of users/admin list here
     const userSeed = [
-      { email: 'root@example.com', name: 'Admin' },
-      { email: 'user1@example.com', name: 'User1' },
-      { email: 'user2@example.com', name: 'User2' },
-      { email: 'user3@example.com', name: 'User3' },
-      { email: 'user4@example.com', name: 'User4' },
-      { email: 'user5@example.com', name: 'User5' }
+      { email: 'root@example.com', name: 'admin' },
+      { email: 'user1@example.com', name: 'user1' },
+      { email: 'user2@example.com', name: 'user2' },
+      { email: 'user3@example.com', name: 'user3' },
+      { email: 'user4@example.com', name: 'user4' },
+      { email: 'user5@example.com', name: 'user5' }
     ]
     // all users/admin use same password, do it once here
     const password = bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null)
@@ -24,8 +24,8 @@ module.exports = {
         name: user.name,
         avatar: `https://loremflickr.com/320/240/dog/?random=${Math.random() * 100}`,
         introduction: faker.lorem.text().substring(0, 160),
-        role: user.name === 'Admin' ? 'admin' : 'user',
-        account: user.email,
+        role: user.name === 'admin' ? 'admin' : 'user',
+        account: user.name,
         cover: `https://loremflickr.com/320/240/city/?random=${Math.random() * 100}`,
         createdAt: new Date(new Date().setDate(new Date().getDate() - 240 + index)),
         updatedAt: new Date(new Date().setDate(new Date().getDate() - 240 + index))
