b28014e573c4f602198f5ebcc0499b0481a6097a
Liangni <light0104@gmail.com>
2021-12-07 02:38:58 +0800

    Add preview avatar feature

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -2,6 +2,9 @@ const coverInput = document.querySelector('#cover-input')
 const coverWrapper = document.querySelector('#cover-wrapper')
 const currentCover = document.querySelector('#current-cover')
 const deleteCoverBtn = document.querySelector('#delete-cover-btn')
+const avatarInput = document.querySelector('#avatar-input')
+const currentAvatar = document.querySelector('#current-avatar')
+const avatarWrapper = document.querySelector('#avatar-wrapper')
 
 
 coverInput.addEventListener('change', function showCoverPreview(event) {
@@ -31,4 +34,24 @@ deleteCoverBtn.addEventListener('click', function deleteCoverPreview() {
   coverWrapper.lastElementChild.remove()
   currentCover.classList.remove('hide')
   deleteCoverBtn.classList.add('hide')
+})
+
+avatarInput.addEventListener('change', function showAvatarPreview(event) {
+  
+  if (event.target.files.length !== 0) {
+    const src = URL.createObjectURL(event.target.files[0])
+    let previewArea = document.createElement('div')
+    previewArea.innerHTML = `
+      <img src="${src}" class="avatar large" id="file-avatar-preview">
+    `
+    // 若是初次選擇檔案，要先隱藏使用者頭貼
+    if (!currentAvatar.classList.contains('hide')) {
+      currentAvatar.classList.add('hide')
+    }
+    //　若是重複選擇檔案，要先把前一個選擇的圖片移除
+    if (avatarWrapper.children.length > 1) {
+      avatarWrapper.lastElementChild.remove()
+    }
+    avatarWrapper.appendChild(previewArea)
+  }
 })
