9c920097b25fc78c0ab3c5a8cf7367d5cf942575
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-06 15:18:53 +0800

    feat: userController.getEditModal

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,8 +1,14 @@
 const helpers = require('../../_helpers')
+const db = require('../../models')
+const { User } = db
 
 const userController = {
-  getEditModal: (req, res) => {
-    // return res.json(loginUser)
+  getEditModal: async (req, res) => {
+    if (helpers.getUser(req) !== Number(req.params.userId)) {
+      return res.end()
+    }
+    const loginUser = await User.findByPk(req.params.userId)
+    return res.json(loginUser)
   }
 }
 
