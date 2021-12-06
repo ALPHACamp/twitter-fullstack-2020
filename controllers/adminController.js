744db6be1000fa7e52cc9386fb56492266875df6
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-06 13:24:20 +0800

    fix: typo

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -38,7 +38,7 @@ const adminController = {
       let users = await User.findAll({
         include: [
           { model: Tweet, include: [Like] },
-          { model: User, as: 'followings' },
+          { model: User, as: 'Followings' },
           { model: User, as: 'Followers' },
         ],
         order: [[Tweet, 'createdAt', 'ASC']],
@@ -51,7 +51,7 @@ const adminController = {
           likeCount: adminController.sumLikes(
             user.Tweets.map((tweet) => tweet.Likes.length)
           ), // 有寫工具function adminController.sumLikes(arr) 計算加總
-          followingCount: user.followings.length,
+          followingCount: user.Followings.length,
           followerCount: user.Followers.length,
         }))
         .sort((a, b) => b.tweetCount - a.tweetCount) // 根據tweet數排序
