8aa3f153d820b8988b76497fe1f0b49b392587b8
Liangni <light0104@gmail.com>
2021-12-07 00:33:30 +0800

    modify preview cover feature

--- public/javascripts/events.js
+++ public/javascripts/events.js
@@ -4,7 +4,7 @@ const currentCover = document.querySelector('#current-cover')
 const deleteCoverBtn = document.querySelector('#delete-cover-btn')
 
 
-coverInput.addEventListener('change', (event) => {
+coverInput.addEventListener('change', function showCoverPreview(event) {
   
   if (event.target.files.length !== 0) { 
     const src = URL.createObjectURL(event.target.files[0])
@@ -12,10 +12,12 @@ coverInput.addEventListener('change', (event) => {
     previewArea.innerHTML = `
       <img src="${src}" class="cover" id="file-cover-preview">
     `
+    // 若是初次選擇檔案，要先隱藏使用者封面，並加上刪除按鈕
     if (!currentCover.classList.contains('hide')) {
       currentCover.classList.add('hide')
       deleteCoverBtn.classList.remove('hide')
     }
+    //　若是重複選擇檔案，先把前一個選擇的圖片移除
     if (coverWrapper.children.length > 1) {
       coverWrapper.lastElementChild.remove()
     }
@@ -24,7 +26,8 @@ coverInput.addEventListener('change', (event) => {
 })
 
 
-deleteCoverBtn.addEventListener('click', (event) => {
+deleteCoverBtn.addEventListener('click', function deleteCoverPreview() {
+  coverInput.value =""
   coverWrapper.lastElementChild.remove()
   currentCover.classList.remove('hide')
   deleteCoverBtn.classList.add('hide')
