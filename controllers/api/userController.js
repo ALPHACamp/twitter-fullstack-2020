84faf2e141bdca1d68237952f052d4a53ed9373b
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-06 16:36:15 +0800

    fix: missing word

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -4,8 +4,8 @@ const { User } = db
 
 const userController = {
   getEditModal: async (req, res) => {
-    if (helpers.getUser(req) !== Number(req.params.userId)) {
-      return res.end()
+    if (helpers.getUser(req).id !== Number(req.params.userId)) {
+      return res.redirect('back')
     }
     const loginUser = await User.findByPk(req.params.userId)
     return res.json(loginUser)
