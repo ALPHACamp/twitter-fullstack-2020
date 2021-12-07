a9c9c18671df4e746dd95da7c619f383bc6498a9
xuan1106 <spu96508@gmail.com>
2021-12-07 21:36:22 +0800

    add event  listener

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -43,3 +43,75 @@ profileBtn.addEventListener('click', event => {
     return
   }
 })
+const profileModal = document.getElementById('profileModal')
+const profileForm = document.getElementById('profileForm')
+
+if (profileModal != null) {
+  const profileName = document.getElementById('profileName')
+  const profileIntro = document.getElementById('profileIntro')
+  const coverFile = document.getElementById('coverFile')
+  const avatarFile = document.getElementById('avatarFile')
+  const coverInput = document.getElementById('uploadCover')
+  const avatarInput = document.getElementById('uploadAvatar')
+
+  // Modal show init
+  profileModal.addEventListener('shown.bs.modal', e => {
+    nameLength.textContent = `${profileName.value.length}/50`
+    introLength.textContent = `${profileIntro.value.length}/140`
+    coverFile.style.color = "#FFFFFF"
+    avatarFile.style.color = "#FFFFFF"
+  })
+  // Modal hide reset
+  profileModal.addEventListener('hide.bs.modal', e => {
+    if (coverInput.value !== '') {
+      coverInput.value = ''
+      coverFile.style.color = "#FFFFFF"
+    }
+    if (avatarInput.value !== '') {
+      avatarInput.value = ''
+      avatarFile.style.color = "#FFFFFF"
+    }
+  })
+  // update typing length
+  profileName.addEventListener('input', e => {
+    const nameStrLen = profileName.value.length
+    nameLength.textContent = `${nameStrLen}/50`
+    if (nameStrLen >= 50) {
+      nameLength.style.color = "red"
+      profileModal.setCustomValidity('超過50字囉')
+    } else {
+      nameLength.style.color = "black"
+      profileModal.setCustomValidity('')
+    }
+  })
+  profileIntro.addEventListener('input', e => {
+    const introStrLen = profileIntro.value.length
+    introLength.textContent = `${introStrLen}/140`
+    if (introStrLen >= 140) {
+      introLength.style.color = "red"
+      profileIntro.setCustomValidity('超過140字囉')
+    } else {
+      introLength.style.color = "black"
+      profileIntro.setCustomValidity('')
+    }
+  })
+  coverInput.addEventListener('change', e => {
+    if (coverInput.value === '') {
+      coverFile.style.color = "#FFFFFF"
+    } else {
+      coverFile.style.color = "#40E0D0"
+    }
+  })
+  avatarInput.addEventListener('change', e => {
+    if (avatarInput.value === '') {
+      avatarFile.style.color = "#FFFFFF"
+    } else {
+      avatarFile.style.color = "#40E0D0"
+    }
+  })
+}
+
+function profileSubmitButtonClick(event) {
+  profileForm.submit()
+  profileModal.hide()
+}
