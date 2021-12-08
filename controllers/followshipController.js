477d5270f32c6646919ee269434f51554d1094e5
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-08 22:56:30 +0800

    hotfix: user can not follw self and correctly redirect page

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -10,7 +10,7 @@ const followshipController = {
 
       if (followerId === followingId) {
         req.flash('error_messages', '不可跟隨自己')
-        return res.end()
+        return res.redirect('/')
       }
 
       await Followship.findOrCreate({ where: { followerId, followingId } })
