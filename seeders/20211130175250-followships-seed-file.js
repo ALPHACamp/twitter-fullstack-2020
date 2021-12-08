cfb8cc9777ea5bc4e8e7d13afcbd5a7cf9a7ca66
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-08 17:39:38 +0800

    hotfix: update folowships seeder

--- seeders/20211130175250-followships-seed-file.js
+++ seeders/20211130175250-followships-seed-file.js
@@ -1,27 +1,38 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // random pair of user id, then filter out same id pair
-    const randomPair = Array
-      .from({ length: 14 })
-      .map((d) => ([
-        Math.ceil(Math.random() * 5) * 10 + 1,
-        Math.ceil(Math.random() * 5) * 10 + 1
-      ])).filter((pair) => pair[0] !== pair[1])
+    const pairs = []
+    for (let i = 0; i < 5; i++) {
+      const a = i * 10 + 11
+      for (let j = 0; j < i; j++) {
+        if (j === 5) {
+          break // 如果種子資料太多，會擋住
+        }
+        const b = j * 10 + 11
+        pairs.push([a, b])
+      }
+    }
 
-    await queryInterface.bulkInsert('Followships',
-      randomPair.map((pair, index) => ({
+    await queryInterface.bulkInsert(
+      'Followships',
+      pairs.map((pair, index) => ({
         id: index * 10 + 1,
         followerId: pair[0],
         followingId: pair[1],
-        createdAt: new Date(new Date().setDate(new Date().getDate() - 30 + index)),
-        updatedAt: new Date(new Date().setDate(new Date().getDate() - 30 + index))
-      })
-      ), {})
+        createdAt: new Date(
+          new Date().setDate(new Date().getDate() - 30 + index)
+        ),
+        updatedAt: new Date(
+          new Date().setDate(new Date().getDate() - 30 + index)
+        )
+      })),
+      {}
+    )
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Followships', null, {})
   }
-};
+}
