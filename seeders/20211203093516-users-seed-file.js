26161d73d1b6a77b6487063cc3e72fcb2fd295a6
Zin <tyulkeds159357@gmail.com>
2021-12-10 02:00:32 +0800

    modify seeders

--- seeders/20211203093516-users-seed-file.js
+++ seeders/20211203093516-users-seed-file.js
@@ -10,7 +10,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       name: 'root',
       role: 'admin',
-      account: 'Root',
+      account: 'root',
       avatar: `https://loremflickr.com/320/240/pepole/?random=${Math.random() * 100}`,
       createdAt: new Date(),
       updatedAt: new Date()
@@ -20,7 +20,7 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       name: 'user1',
       role: 'normal',
-      account: 'User',
+      account: 'user1',
       avatar: `https://loremflickr.com/320/240/pepole/?random=${Math.random() * 100}`,
       createdAt: new Date(),
       updatedAt: new Date()
@@ -30,7 +30,37 @@ module.exports = {
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       name: 'user2',
       role: 'normal',
-      account: 'User2',
+      account: 'user2',
+      avatar: `https://loremflickr.com/320/240/pepole/?random=${Math.random() * 100}`,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 4,
+      email: 'user3@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: 'user3',
+      role: 'normal',
+      account: 'user3',
+      avatar: `https://loremflickr.com/320/240/pepole/?random=${Math.random() * 100}`,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 5,
+      email: 'user4@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: 'user4',
+      role: 'normal',
+      account: 'user4',
+      avatar: `https://loremflickr.com/320/240/pepole/?random=${Math.random() * 100}`,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 6,
+      email: 'user5@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: 'user5',
+      role: 'normal',
+      account: 'user5',
       avatar: `https://loremflickr.com/320/240/pepole/?random=${Math.random() * 100}`,
       createdAt: new Date(),
       updatedAt: new Date()
--- seeders/20211203093629-tweets-seed-file.js
+++ seeders/20211203093629-tweets-seed-file.js
@@ -5,13 +5,13 @@ const faker = require('faker')
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Tweets',
-      Array.from({ length: 10 }).map((d, i) =>
+      Array.from({ length: 50 }).map((d, i) =>
       ({
-        id: i * 10 + 1,
+        id: i + 1,
         description: faker.lorem.text(),
         createdAt: new Date(),
         updatedAt: new Date(),
-        UserId: Math.floor(Math.random() * 3) + 1
+        UserId: i % 5 + 2
       })
 
       ), {})
