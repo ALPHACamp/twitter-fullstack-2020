06796f81ab22f65e9ec00cf02d09012e5b3d8e00
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-09 17:51:55 +0800

    chore: update reply seed

--- seeders/20211130172532-replies-seed-file.js
+++ seeders/20211130172532-replies-seed-file.js
@@ -7,7 +7,7 @@ module.exports = {
       'Replies',
       Array.from({ length: 150 }).map((d, index) => ({
         id: index * 10 + 1,
-        UserId: Math.ceil(Math.random() * 5) * 10 + 1,
+        UserId: (index % 5) * 10 + 11,
         TweetId: Math.floor(index / 3) * 10 + 1,
         comment: faker.lorem.sentences().substring(0, 140),
         createdAt: new Date(
