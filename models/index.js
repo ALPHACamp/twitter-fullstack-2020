583a9e95256028b2271d33b85660d9c209fb70d1
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-09 10:21:19 +0800

    chore: update models/index in standard style

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(__dirname + '/../config/config.json')[env]
+const config = require(path.join(__dirname, '/../config/config.json'))[env]
 const db = {}
 
 let sequelize
@@ -27,7 +27,7 @@ fs.readdirSync(__dirname)
     )
   })
   .forEach((file) => {
-    const model = sequelize['import'](path.join(__dirname, file))
+    const model = sequelize.import(path.join(__dirname, file))
     db[model.name] = model
   })
 
