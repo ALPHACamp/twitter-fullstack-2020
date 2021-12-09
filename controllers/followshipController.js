f646cdeac2b955a866f10604cba9e60331dbe85f
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-12-09 09:45:07 +0800

    hotfix: update frontend follow button

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -10,7 +10,7 @@ const followshipController = {
 
       if (followerId === followingId) {
         req.flash('error_messages', '不可跟隨自己')
-        return res.redirect(200, '/')
+        return res.redirect(200, 'back')
       }
 
       await Followship.findOrCreate({ where: { followerId, followingId } })
