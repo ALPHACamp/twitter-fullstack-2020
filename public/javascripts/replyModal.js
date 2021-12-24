2c4a8098f29b2f6f80a0fb3113881bcafe12ac6f
Zin <tyulkeds159357@gmail.com>
2021-12-24 15:17:23 +0800

    modfiy tweets and tweet handlebars

--- public/javascripts/replyModal.js
+++ public/javascripts/replyModal.js
@@ -3,11 +3,7 @@ const comment = document.querySelector('#comment-modal')
 const replyButton = document.querySelector('#replybtn')
 
 comment.addEventListener('input', function check(event) {
-  if (comment.value.length < 1) {
-    comment.classList.add('is-invalid')
-    comment.nextElementSibling.innerHTML = "內容不可為空白"
-  }
-  if (comment.value.length > 1) {
+  if (comment.value.length > 0) {
     comment.classList.remove('is-invalid')
   }
 })
