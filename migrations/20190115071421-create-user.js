bed3138aebc2a85119fd29457e0f36529d704327
snmo2546 <snmo2546@gmail.com>
2022-03-25 17:05:07 +0800

    feat: adjust User model column

--- migrations/20190115071421-create-user.js
+++ migrations/20190115071421-create-user.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      account: {
+        type: Sequelize.STRING
+      },
       email: {
         type: Sequelize.STRING
       },
@@ -15,9 +18,6 @@ module.exports = {
         type: Sequelize.STRING
       },
       name: {
-        type: Sequelize.STRING
-      },
-      account: {
         type: Sequelize.STRING
       },
       avatar: {
