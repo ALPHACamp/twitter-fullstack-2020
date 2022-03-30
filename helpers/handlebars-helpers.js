da50eae65cb26132601b7c7660ac105d2e6a3565
snmo2546 <snmo2546@gmail.com>
2022-03-30 16:22:46 +0800

    feat: add admin users page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -4,10 +4,11 @@ dayjs.extend(relativeTime)
 
 module.exports = {
   isSigninSignupPage: function (url, options) {
-    return ['/signin', '/signup', '/admin/signin'].includes(url) ? options.fn(this) : options.inverse(this)
+    return url && ['/signin', '/signup', '/admin/signin'].includes(url) ? options.fn(this) : options.inverse(this)
   },
   relativeTimeFromNow: a => dayjs(a).fromNow(),
   isAdminPage: function (url, options) {
     return url && url.includes('admin') ? options.fn(this) : options.inverse(this)
-  }
+  },
+  arrayLength: a => a.length
 }
