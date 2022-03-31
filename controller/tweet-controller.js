3fc0ea7799d42ce57259b40a025dc9383ba0ddbd
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2022-03-31 17:15:05 +0800

    feat: modify tweets handlebars

--- controller/tweet-controller.js
+++ controller/tweet-controller.js
@@ -1,6 +1,18 @@
+const { Tweet, User } = require('../models')
+const helpers = require('../_helpers')
+
 const tweetController = {
-  getTweets: (req, res) => {
-    return res.render('tweets')
+  getTweets: (req, res, next) => {
+    return Tweet.findAll({
+      raw: true,
+      nest: true,
+      include: [User],
+      order: [['createdAt', 'DESC']]
+    })
+      .then(tweets => {
+        console.log(helpers.getUser(req))
+        return res.render('tweets', { tweets, user: helpers.getUser(req) })
+      })
   }
 }
 module.exports = tweetController
