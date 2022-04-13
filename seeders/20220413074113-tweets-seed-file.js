528dafc404e2ff4fbeb9d960a041410a9000d803
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2022-04-13 16:49:18 +0800

    feat: add users & tweets seeders

---
+++ seeders/20220413074113-tweets-seed-file.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const tweets = []
+    users.forEach(user => {
+      for (let i = 0; i < 10; i++) {
+        tweets.push({
+          userId: user.id,
+          content: faker.lorem.text(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      }
+    })
+    await queryInterface.bulkInsert('Tweets', tweets)
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Tweets', null, {})
+  }
+}
