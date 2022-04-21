e4cde972922c2eaf5103822685875cefc83140f2
snmo2546 <36234793+snmo2546@users.noreply.github.com>
2022-04-21 15:23:17 +0800

    feat: add word count to tweet & reply modal

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,31 +1,53 @@
-const name = document.querySelector('#name')
+const inputs = ['name', 'content', 'comment']
 
-name.addEventListener('keyup', characterCount)
+inputs.forEach(inputField => {
+  const inputGroup = document.getElementsByName(inputField)
+  if (inputGroup) {
+    inputGroup.forEach(input => {
+      input.addEventListener('keyup', characterCount)
+    })
+  }
+})
 
 function characterCount() {
   const displayer = document.querySelector(`.${this.id}WordCount`)
-
   let wordLimit = 0
   if (this.id === 'name') {
     wordLimit = 50
-  }
 
-  if (document.querySelector(`.${this.id}CurrentCount`)) {
-    document.querySelector(`.${this.id}CurrentCount`).innerText = this.value.length
-  } else {
-    const newItem = document.createElement('span')
-    newItem.innerHTML = `
-    <span class="${this.id}CurrentCount" max="${wordLimit}">${this.value.length}</span><span>/${wordLimit}</span>
-  `
-    displayer.appendChild(newItem)
-  }
-  if (this.value.length === wordLimit && !document.querySelector(`.${this.id}LengthWarning`)) {
-    this.classList.add('border-danger')
-    const wordLengthLimit = document.createElement('span')
-    wordLengthLimit.innerHTML = `<span class="${this.id}LengthWarning">字數已達上限！</span>`
-    displayer.appendChild(wordLengthLimit)
-  } else if (this.value.length < wordLimit && document.querySelector(`.${this.id}LengthWarning`)) {
-    this.classList.remove('border-danger')
-    document.querySelector(`.${this.id}LengthWarning`).remove()
+    if (document.querySelector(`.${this.id}CurrentCount`)) {
+      document.querySelector(`.${this.id}CurrentCount`).innerText = this.value.length
+    } else {
+      const newItem = document.createElement('span')
+      newItem.innerHTML = `<span class="${this.id}CurrentCount" max="${wordLimit}">${this.value.length}</span><span>/${wordLimit}</   span>`
+      displayer.appendChild(newItem)
+    }
+    if (this.value.length === wordLimit && !document.querySelector(`.${this.id}LengthWarning`)) {
+      this.classList.add('border-danger')
+      const wordLengthLimit = document.createElement('span')
+      wordLengthLimit.innerHTML = `<span class="${this.id}LengthWarning">字數已達上限！</span>`
+      displayer.appendChild(wordLengthLimit)
+    } else if (this.value.length < wordLimit && document.querySelector(`.${this.id}LengthWarning`)) {
+      this.classList.remove('border-danger')
+      document.querySelector(`.${this.id}LengthWarning`).remove()
+    }
+  } else if (this.id === 'content') {
+    wordLimit = 140
+    if (this.value.length === wordLimit && !document.querySelector(`.${this.id}LengthWarning`)) {
+      const wordLengthLimit = document.createElement('span')
+      wordLengthLimit.innerHTML = `<span class="${this.id}LengthWarning">字數不可超過 140 字</span>`
+      displayer.appendChild(wordLengthLimit)
+    } else if (this.value.length < wordLimit && document.querySelector(`.${this.id}LengthWarning`)) {
+      document.querySelector(`.${this.id}LengthWarning`).remove()
+    }
+  } else if (this.name === 'comment') {
+    wordLimit = 140
+    if (this.value.length === wordLimit && !document.querySelector(`.${this.id}LengthWarning`)) {
+      const wordLengthLimit = document.createElement('span')
+      wordLengthLimit.innerHTML = `<span class="${this.id}LengthWarning">字數不可超過 140 字</span>`
+      displayer.appendChild(wordLengthLimit)
+    } else if (this.value.length < wordLimit && document.querySelector(`.${this.id}LengthWarning`)) {
+      document.querySelector(`.${this.id}LengthWarning`).remove()
+    }
   }
 }
