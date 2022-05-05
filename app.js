da476cefed333d1e21e18aab7436ff8604ce4378
Marcus Liu <me@mail.lmarcus.onl>
2022-05-05 12:09:56 +0800

    feat(infra): import body-parser

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const port = 3000
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extend: true }))
+
 app.get('/', (req, res) => res.render('index'))
 app.listen(port, () => console.log(`alphitter listening on port ${port}!`))
 
