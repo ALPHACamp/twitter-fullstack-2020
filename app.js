b51dc62853f1237e7f6df0351841504750240962
Marcus Liu <me@mail.lmarcus.onl>
2022-05-05 12:15:17 +0800

    feat(infra): import method-override

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const helpers = require('./_helpers')
 const handlebars = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
@@ -12,6 +13,8 @@ app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extend: true }))
+
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => res.render('index'))
 app.listen(port, () => console.log(`alphitter listening on port ${port}!`))
