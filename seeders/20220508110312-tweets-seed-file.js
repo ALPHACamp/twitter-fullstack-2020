f59eae73352617c13afad3f4971c12a05209a011
LouisChen-TW <yue841206@gmail.com>
2022-05-08 22:35:01 +0800

    feature: add tweets seeder

---
+++ seeders/20220508110312-tweets-seed-file.js
@@ -0,0 +1,27 @@
+'use strict';
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const usersTweet = []
+
+    for(let i = 0; i < users.length; i++) {
+      const result = Array.from({length: 10}, () => ({
+        UserId: users[i].id,
+        description: faker.lorem.text().substring(0,140),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }))
+      usersTweet.push(...result)
+    }
+    await queryInterface.bulkInsert('Tweets', usersTweet, {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Tweets', null, {})
+  }
+};
