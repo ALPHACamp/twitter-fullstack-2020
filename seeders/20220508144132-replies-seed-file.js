576de6740b86260fa97796e27bd84455a5c102d5
LouisChen-TW <yue841206@gmail.com>
2022-05-08 23:56:15 +0800

    feature: add replies seeder

---
+++ seeders/20220508144132-replies-seed-file.js
@@ -0,0 +1,32 @@
+'use strict';
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const tweets = await queryInterface.sequelize.query(
+      'SELECT id FROM Tweets;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const tweetReplies = []
+
+    for (let i = 0; i < tweets.length; i ++) {
+      const result = Array.from({length:3}, () => ({
+        UserId: users[Math.floor(Math.random() * users.length)].id,
+        TweetId: tweets[i].id,
+        comment: faker.lorem.text().substring(0,140),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }))
+      tweetReplies.push(...result)
+    }
+    await queryInterface.bulkInsert('Replies', tweetReplies, {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', null, {})
+  }
+};
