c7e02bb8920fbe96889b059a3312ccf499b4f913
Marcus Liu <me@mail.lmarcus.onl>
2022-05-10 21:56:58 +0800

    feat: Index (#15)
    * feat: nNnavbar 與留言框試作
    * refactor: 更動 pull request 的訊息範本
    * feat: 修改 tweetsController 指到 index
    * feat: 首頁初版 query 製作
    * refactor: 修改 query 中 includes 抓的欄位
    * refactor: 通用 layout
    * refactor: 將 main layout 的左右兩側面板移除，改成有需要時呼叫
    * feat: 左右兩側面板
    * feat: 產生使用者試作
    * feat: top user controller 功能試做&載入資料
    * refactor: 修改 grid 的排版
    * refactor: 刪除不必要的 console.log 與 topUser 後續事項
    * example controller 功成身退

--- controller/exampleController.js
+++
@@ -1,7 +0,0 @@
-const exampleController = {
-  indexPage: (req, res) => {
-    res.render('index')
-  }
-}
-
-module.exports = exampleController
--- controller/tweetsController.js
+++ controller/tweetsController.js
@@ -1,6 +1,23 @@
+const { Tweet, User } = require('../models')
+
 const tweetsController = {
-  getTweets: (req, res) => {
-    return res.render('tweets')
+  getTweets: async (req, res, next) => {
+    // TODO: like 與 replies 數量
+    try {
+      const tweets = await Tweet.findAll({
+        include: {
+          model: User,
+          attributes: ['name', 'account', 'avatar']
+        },
+        raw: true,
+        nest: true
+      })
+      const topUsers = await User.findAll({ raw: true })
+      // TODO: topUsers 尚未完成，需要根據 like 術與 follower 數相加
+      return res.render('index', { tweets, topUsers })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = tweetsController
