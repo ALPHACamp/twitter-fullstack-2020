f692d6271b8465ab2ea99405d8646584e96cdd64
tkooooo123 <tkooooo123@gmail.com>
2022-05-11 00:17:31 +0800

    add views folder and pages

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const helpers = require('./_helpers');
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
