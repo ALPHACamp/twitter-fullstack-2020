744933eb1000f909271a1a4907078f7d09931b1c
tkooooo123 <tkooooo123@gmail.com>
2022-05-11 19:27:23 +0800

    add flash message

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 app.use(methodOverride('_method'))
+
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')  
+  res.locals.error_messages = req.flash('error_messages')  
+  next()
+})
 
 app.use(routes)
 
