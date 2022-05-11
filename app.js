b22262cbda62f37025c34d81153539a3c8a094fe
tkooooo123 <tkooooo123@gmail.com>
2022-05-11 12:04:19 +0800

    add bootstrap, modify main.hbs and express routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const routes = require('./routes')
 const helpers = require('./_helpers');
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override') 
 
 const app = express()
 const port = 3000
@@ -9,8 +11,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => res.render('index'))
+app.use(routes)
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
