ef48fbfbc3c7f041176e9a564a0ece16ebf040d2
tkooooo123 <tkooooo123@gmail.com>
2022-05-13 00:15:57 +0800

    Update 20220512075155-users-seed.js
    add id, avatar, cover, introduction values

--- seeders/20220512075155-users-seed.js
+++ seeders/20220512075155-users-seed.js
@@ -1,21 +1,29 @@
 'use strict';
 const bcript = require('bcryptjs')
 const userList = [{
+  id: 1,
   email: 'root@example.com',
   name: 'root',
   password: bcript.hashSync('12345678',10),
   createdAt: new Date(),
   updatedAt: new Date(),
-  role: 'admin'
+  role: 'admin',
+  avatar: 'https://loremflickr.com/320/240/human',
+  cover: 'https://loremflickr.com/320/240/',
+  introduction: faker.lorem.text()
 }]
 for(let i=0;i<5;i++){
   userList.push({
+    id: i + 2,
     email: `user${i+1}@example.com`,
     name: `user${i+1}`,
     password: bcript.hashSync('12345678',10),
     createdAt: new Date(),
     updatedAt: new Date(),
-    role: 'user'
+    role: 'user',
+    avatar: 'https://loremflickr.com/320/240/avatar',
+    cover: 'https://loremflickr.com/320/240/scenery',
+    introduction: faker.lorem.text()
   })
 }
 module.exports = {
