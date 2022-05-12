31314da96378c40ced71e2164b8eddc82e5483ed
LouisChen-TW <yue841206@gmail.com>
2022-05-12 15:04:19 +0800

    merge origin main

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -65,6 +65,69 @@ const userController = {
     } catch (err) {
       next(err)
     }
+  },
+  getTweets: async (req, res, next) => {
+    try {
+      const userId = req.params.id
+      const [user, tweets] = await Promise.all([
+        User.findByPk(userId, { raw: true }),
+        Tweet.findAll({
+          include: [Reply],
+          where: { userId }
+        })
+      ])
+      if (!user) throw new Error("User didn't exist!")
+
+      const data = tweets.map(tweet => ({
+        ...tweet.toJSON()
+      }))
+
+      res.render('user', { user, tweets: data, tab: 'getTweets' })
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  getReplies: async (req, res, next) => {
+    try {
+      const userId = req.params.id
+
+      const [user, replies] = await Promise.all([
+        User.findByPk(userId, { raw: true }),
+        Reply.findAll({
+          where: { userId },
+          include: [{ model: Tweet, include: User }]
+        })
+      ])
+      if (!user) throw new Error("User didn't exist!")
+
+      const data = replies.map(reply => ({
+        ...reply.toJSON()
+      }))
+
+      res.render('user', { user, replies: data, tab: 'getReplies' })
+    } catch (err) {
+      next(err)
+    }
+  },
+  getLikedTweets: async (req, res, next) => {
+    try {
+      const userId = req.params.id
+      const user = await User.findByPk(userId, {
+        include: [
+          {
+            model: Tweet,
+            as: 'LikedTweets',
+            include: [User, Reply, { model: User, as: 'LikedUsers' }]
+          }
+        ]
+      })
+      if (!user) throw new Error("User didn't exist!")
+
+      res.render('user', { user: user.toJSON(), tab: 'getLikedTweets' })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
