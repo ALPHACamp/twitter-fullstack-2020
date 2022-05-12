ba7e03cce37197c3dba380e36b2ac1a173a9896c
aziz0916 <aziz0916@yahoo.com.tw>
2022-05-12 09:22:12 +0800

    feat: modify app.js

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
+const path = require('path')
 const express = require('express')
-const helpers = require('./_helpers');
+const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./_helpers')
+const routes = require('./routes')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
+
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.loginUser = getUser(req)
+  next()
+})
+
+app.use(routes)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
