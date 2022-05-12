1ad17f5441cd23f8a29c306b9878e6d38258bda5
Lydia-09 <pasky0901@gmail.com>
2022-05-12 21:03:55 +0800

    feat: fix conflict

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -48,6 +48,85 @@ const userController = {
     } catch (err) {
       next(err)
     }
+  },
+  signIn: async (req, res, next) => {
+    try {
+      req.flash('success_messages', '成功登入！')
+      res.redirect('/tweets')
+    } catch (err) {
+      next(err)
+    }
+  },
+  logout: async (req, res, next) => {
+    try {
+      req.flash('success_messages', '登出成功！')
+      req.logout()
+      res.redirect('/signin')
+    } catch (err) {
+      next(err)
+    }
+  },
+  getTweets: async (req, res, next) => {
+    try {
+      const userId = req.params.id
+      const [user, tweets] = await Promise.all([
+        User.findByPk(userId, { raw: true }),
+        Tweet.findAll({
+          include: [Reply],
+          where: { userId }
+        })
+      ])
+      if (!user) throw new Error("User didn't exist!")
+
+      const data = tweets.map(tweet => ({
+        ...tweet.toJSON()
+      }))
+
+      res.render('user', { user, tweets: data, tab: 'getTweets' })
+    } catch (err) {
+      next(err)
+    }
+  },
+  getReplies: async (req, res, next) => {
+    try {
+      const userId = req.params.id
+
+      const [user, replies] = await Promise.all([
+        User.findByPk(userId, { raw: true }),
+        Reply.findAll({
+          where: { userId },
+          include: [{ model: Tweet, include: User }]
+        })
+      ])
+      if (!user) throw new Error("User didn't exist!")
+
+      const data = replies.map(reply => ({
+        ...reply.toJSON()
+      }))
+
+      res.render('user', { user, replies: data, tab: 'getReplies' })
+    } catch (err) {
+      next(err)
+    }
+  },
+  getLikedTweets: async (req, res, next) => {
+    try {
+      const userId = req.params.id
+      const user = await User.findByPk(userId, {
+        include: [
+          {
+            model: Tweet,
+            as: 'LikedTweets',
+            include: [User, Reply, { model: User, as: 'LikedUsers' }]
+          }
+        ]
+      })
+      if (!user) throw new Error("User didn't exist!")
+
+      res.render('user', { user: user.toJSON(), tab: 'getLikedTweets' })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,4 +3,8 @@ const router = express.Router()
 
 const userController = require('../../controllers/user-controller')
 
+router.get('/:id/tweets', userController.getTweets)
+router.get('/:id/replies', userController.getReplies)
+router.get('/:id/likes', userController.getLikedTweets)
+
 module.exports = router
