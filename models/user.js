01cf407a645b125ae1bc54df58552209bdefd59b
aziz0916 <aziz0916@yahoo.com.tw>
2022-05-12 09:29:32 +0800

    feat: modify models

--- models/user.js
+++ models/user.js
@@ -1,8 +1,49 @@
-'use strict';
+'use strict'
+const {
+  Model
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      User.hasMany(models.Tweet, { foreignKey: 'userId' })
+      User.hasMany(models.Reply, { foreignKey: 'userId' })
+      User.hasMany(models.Like, { foreignKey: 'userId' })
+      User.belongsToMany(models.Tweet, {
+        through: models.Like,
+        foreignKey: 'userId',
+        as: 'LikedTweets'
+      })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
+    }
   };
-  return User;
-};
+  User.init({
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    cover: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.STRING,
+    account: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: true
+  })
+  return User
+}
