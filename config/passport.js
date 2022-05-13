e502b11fa7f92c6ac615fb01cf04b1c42b1d90fd
HO Chia Cung <john778300@gmail.com>
2022-05-13 18:49:32 +0800

    feat: login , adminlogin , signup

---
+++ config/passport.js
@@ -0,0 +1,33 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'account',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  (req, account, password, cb) => {
+    User.findOne({ where: { account } })
+      .then(user => {
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        bcrypt.compare(password, user.password).then(res => {
+          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          return cb(null, user)
+        })
+      })
+  }
+))
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id).then(user => {
+    user = user.toJSON()
+    return cb(null, user)
+  })
+})
+module.exports = passport
