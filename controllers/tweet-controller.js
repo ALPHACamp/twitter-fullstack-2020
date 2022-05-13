5abdcf6c6aed0b38b4de5994250a70ad2d584383
Tommy1023 <j.h.chen1023@gmail.com>
2022-05-13 18:07:34 +0800

    add top10 followers user

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,24 +1,39 @@
-const { User, Tweet } = require('../models')
+const { User, Tweet, Like, Reply } = require('../models')
 const { getUser } = require('../_helpers')
 
 const tweetController = {
   getTweets: (req, res, next) => {
-    const loginUser = getUser(req) ? getUser(req).id : 2
+    const loginUser = getUser(req) ? getUser(req).id : Number(2)
     return Promise.all([
       User.findByPk(loginUser, { raw: true, nest: true }),
       Tweet.findAll({
         include: User,
         raw: true,
         nest: true
+      }),
+      User.findAll({
+        where: { role: 'user' },
+        include: [
+          { model: User, as: 'Followers' },
+          { model: User, as: 'Followings' }
+        ]
       })
     ])
-      .then(([user, tweets]) => {
+      .then(([user, tweets, users]) => {
         if (!user) {
           req.flash('error_messages:', "User is didn't exist!")
-          res.redirect('/login')
+          return res.redirect('/login')
         }
-        console.log('tweets:', tweets)
-        return res.render('tweets', { user, tweets })
+        const LIMIT = 10
+        const userData = users
+          .map(user => ({
+            ...user.toJSON(),
+            followerCount: user.Followers.length,
+            isFollowed: user.Followings.some(f => f.id === loginUser)
+          }))
+          .sort((a, b) => b.followerCount - a.followerCount)
+          .slice(0, LIMIT)
+        return res.render('tweets', { user, tweets, users: userData })
       })
       .catch(err => next(err))
   }
