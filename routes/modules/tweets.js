ac643179f6406cdfe9088302d0f0d0b0762602f1
aziz0916 <aziz0916@yahoo.com.tw>
2022-05-13 21:45:55 +0800

    feat: add user controller

--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => res.render('tweet'))
+router.get('/', (req, res) => res.render('tweets'))
 
 module.exports = router
