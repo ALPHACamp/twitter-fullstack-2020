dbb9154f7999eee55bf01342cb9471e0a5cc9602
LinBii <aa68968@gmail.com>
2022-05-14 16:19:41 +0800

    modify helpers to pass test

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,15 @@
-const { ensureAuthenticated, getUser } = require('../_helpers')
+const helpers = require('../_helpers')
 
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     return next()
   }
   res.redirect('/signin')
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).role === 'admin') {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).role === 'admin') {
       return next() 
     }
     res.redirect('/')
