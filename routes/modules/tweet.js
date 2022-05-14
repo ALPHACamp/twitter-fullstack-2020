a38a55a34b721d3f4b7e2d5e07216c254cffa721
zxc58 <abc01234567889@gmail.com>
2022-05-14 21:18:25 +0800

    移動驗證功能

--- routes/modules/tweet.js
+++ routes/modules/tweet.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const {tweetValidation}=require('../../middleware/validation')
 const tweetController = require('../../controllers/tweet-controller')
 const likeController = require('../../controllers/like-controller')
 const replyController = require('../../controllers/reply-controller')
@@ -8,7 +7,7 @@ router.post('/:id/like', likeController.likeTweet)
 router.post('/:id/unlike', likeController.unlikeTweet)
 router.post('/:id/replies', replyController.postReply)
 router.get('/:id/replies', replyController.getReplies)
-router.post('/',tweetValidation, tweetController.postTweet)
+router.post('/', tweetController.postTweet)
 router.get('/', tweetController.getTweets)
 
 module.exports = router
