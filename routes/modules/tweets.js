2a5785520b45845d040d85fc238672c3b21868ae
jameshoo123 <hoosinyong@hotmail.com>
2022-05-16 15:06:29 +0800

    feat: merge newest main

--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -5,5 +5,7 @@ const tweetController = require('../../controllers/tweet-controller')
 
 router.get('/', tweetController.getTweets)
 router.post('/', tweetController.postTweets)
+router.post('/:tweetId/like', tweetController.likeTweets)
+router.post('/:tweetId/unlike', tweetController.unlikeTweets)
 
 module.exports = router
