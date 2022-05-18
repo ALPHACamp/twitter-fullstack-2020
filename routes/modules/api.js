5f2aef5120a93149975dd59c711c5b9fee4c47c3
tkooooo123 <tkooooo123@gmail.com>
2022-05-18 20:40:52 +0800

    修正User透過Modal編輯個人資料路由

--- routes/modules/api.js
+++ routes/modules/api.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user-controller')
+const { authenticated } = require('../../middleware/auth')
 
 
 
-router.get('/users/:id', userController.editUser)
-router.put('/users/:id', userController.putUser)
+router.get('/users/:id', authenticated, userController.editUser)
+router.post('/users/:id', authenticated, userController.putUser)
 
 module.exports = router
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -4,11 +4,11 @@ const userController = require('../../controllers/user-controller')
 const upload = require('../../middleware/multer')
 const { authenticated } = require('../../middleware/auth')
 
+router.put('/:id/edit', authenticated, upload.fields([{name: 'cover', maxCount: 1}, {name: 'avatar', maxCount: 1}]), userController.putUserProfile)
 router.get('/:id/tweets', authenticated, userController.getUser)
 router.get('/:id/replies', authenticated, userController.getReplies)
 router.get('/:id/likes', authenticated, userController.getLikes)
 router.get('/:id/followers', authenticated, userController.getFollowers)
 router.get('/:id/followings', authenticated, userController.getFollowings)
-router.put(':/id/edit', authenticated, upload.fields([{name: 'cover', maxCount: 1}, {name: 'avatar', maxCount: 1}]), userController.putUserProfile)
 
 module.exports = router
