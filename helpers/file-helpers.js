d9fafd26bbb37483c6123be62017cfa6028a966b
tkooooo123 <tkooooo123@gmail.com>
2022-05-18 20:27:20 +0800

    安裝imgur套件

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,5 +1,5 @@
 const fs = require('fs') // 引入 fs 模組
-const imgurFileHandler = file => { // file 是 multer 處理完的檔案
+const localFileHandler = file => { // file 是 multer 處理完的檔案
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null) 
     const fileName = `upload/${file.originalname}`
@@ -9,6 +9,17 @@ const imgurFileHandler = file => { // file 是 multer 處理完的檔案
       .catch(err => reject(err))
   })
 }
+const imgurFileHandler = file => {
+  return new Promise((resolve, reject) => {
+    if (!file) return resolve(null)
+    return imgur.uploadFile(file.path)
+      .then(img => {
+        resolve(img?.link || null)
+      })
+      .catch(err => reject(err))
+  })
+}
 module.exports = {
+  localFileHandler,
   imgurFileHandler
 }
