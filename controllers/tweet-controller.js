b8e5ec064af903b0049f7e7198ae39ce61efafb1
zxc58 <abc01234567889@gmail.com>
2022-05-20 07:14:05 +0800

    修改tweet.findAndCountAll

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -3,34 +3,33 @@ const db = require('../models')
 const { Tweet, User, Like, Reply, sequelize } = db
 const { catchTopUsers} = require('../helpers/sequelize-helper')
 const helpers = require('../_helpers')
-const tweet = require("../models/tweet")
 const tweetController = {
   getTweets: (req, res, next) => {
     const limit = Number(req.query.limit) || 20
     const page = Number(req.query.page) || 1
     const offset = getOffset(limit, page)
-    return Promise.all([ Tweet.findAll({
+    return Promise.all([ Tweet.findAndCountAll({
       include: [{
         model: User,
         attributes: ['id', 'name', 'avatar','account'],
       }, {
-        model: Like,
+        model: Like,attributes:[],duplicating:false
       }, {
-        model: Reply,
+        model: Reply,attributes:[],duplicating:false
       }],
-      order: [['createdAt','DESC']],limit,offset
-    }),
-    catchTopUsers(req),
-    Tweet.findAndCountAll({attributes:['id']})
-  ])
-    .then(([tweets,topUsers,T])  => {
-      const data=tweets.map(e=>({
-        ...e.toJSON(),
-        totalLike : e.Likes.length,
-        totalReply : e.Replies.length,
-        isLiked : e.Likes.some(f=>f.UserId===helpers.getUser(req).id)
-      }))
-      res.render('index',{tweets:data,topUsers,pagination: getPagination(limit, page, T.count)})
+      attributes:{include:[
+        [sequelize.fn('COUNT',sequelize.fn('DISTINCT',sequelize.col('Replies.id'))),'totalReply'],
+        [sequelize.fn('COUNT',sequelize.fn('DISTINCT',sequelize.col('Likes.id'))),'totalLike'],
+        [sequelize.fn('MAX',sequelize.fn('IF',sequelize.literal('`Likes`.`UserId`-'+helpers.getUser(req).id+'=0'),1,0)),"isLiked"]
+      ]},
+      distinct:true,group:'Tweet.id',
+      order: [['createdAt','DESC']],limit,offset,
+      raw:true,nest:true
+      }),
+      catchTopUsers(req)
+    ])
+    .then(([tweets,topUsers])  => {
+      res.render('index',{tweets:tweets.rows,topUsers,pagination: getPagination(limit, page, tweets.count.length)})
     }).catch(err => next(err))
   },
   postTweet: (req, res, next) => {
