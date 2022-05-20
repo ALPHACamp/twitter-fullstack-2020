c6ff758ab81ccf765d9fbb768ced7883b8ae7087
tkooooo123 <tkooooo123@gmail.com>
2022-05-20 22:36:20 +0800

    新增 點擊編輯按鈕時會顯示載入內容的字數

--- public/javascripts/editModal.js
+++ public/javascripts/editModal.js
@@ -3,8 +3,9 @@ const inputNameCounts = document.querySelector('.nameCounts')
 const inputIntroductionCounts = document.querySelector('.introductionCounts')
 const inputName = document.querySelector('#name')
 const inputIntroduction = document.querySelector('#introduction')
+const editButton = document.querySelector('#editButton')
 //計算按鍵放開時的字數
-editModal.addEventListener('keyup', function (event) {
+editModal.addEventListener('keyup', function(event) {
   const target = event.target
   const inputValue = target.value || ''
   if (target.matches('#name')) {
@@ -14,7 +15,7 @@ editModal.addEventListener('keyup', function (event) {
   }
 })
 //計算按下按鍵時的字數，顯示長按時的字數變化
-editModal.addEventListener('keydown', function (event) {
+editModal.addEventListener('keydown', function(event) {
   const target = event.target
   const inputValue = target.value || ''
   if (target.matches('#name')) {
@@ -22,6 +23,18 @@ editModal.addEventListener('keydown', function (event) {
   } else {
     inputIntroductionCounts.innerText = inputValue.length
   }
+})
+//點擊按鈕時，顯示內容之字數
+editButton.addEventListener('click', function(event){
+  const target = event.target
+  const UserId = target.dataset.id
+  axios
+  .get(`http://localhost:3000/api/users/${UserId}`)
+  .then(res => {
+    inputIntroductionCounts.innerText = res.data.introduction.length
+    inputNameCounts.innerText = res.data.name.length
+  })
+  .catch(err => console.log(err))
 })
 
 
