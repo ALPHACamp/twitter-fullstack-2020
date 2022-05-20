f1d0b9e233d2bfda4ddb08e7061a6318751b0bc6
aziz0916 <aziz0916@yahoo.com.tw>
2022-05-20 15:05:40 +0800

    feat: modify name and introduction count function

---
+++ public/javascripts/count.js
@@ -0,0 +1,8 @@
+function calNameWords() {
+  const length = document.getElementById('edit-name').value.length
+  document.getElementById('edit-name-value').innerHTML = length
+}
+function calIntroductionWords() {
+  const length = document.getElementById('edit-introduction').value.length
+  document.getElementById('edit-introduction-value').innerHTML = length
+}
