93adb0fdceef604874282ec7d4eeabf83640458f
LouisChen-TW <yue841206@gmail.com>
2022-05-20 20:00:48 +0800

    refactor: post likes and unlikes

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -1,7 +1,7 @@
 const BASE_URL = document.querySelector('#BASE-URL').textContent
 
 setTimeout(() => {
-  document.querySelector('.flash').classList.add('cb')
-  setTimeout(() => { document.querySelector('.flash').classList.remove('cb') },
+  document.querySelector('.flash')?.classList.add('cb')
+  setTimeout(() => { document.querySelector('.flash')?.classList.remove('cb') },
     2800)
 }, 500)
