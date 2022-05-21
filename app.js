2e791225e5f5f5e7a4ca9995623e0076611e4cf6
yc62897441 <yc62897441@gmail.com>
2022-05-21 13:14:14 +0800

    build basic routes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const helpers = require('./_helpers');
+const exphbs = require('express-handlebars')
 
 const app = express()
-const port = 3000
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const routes = require('./routes/index')
+routes(app)
+// require('./routes/index')(app)
 
 module.exports = app
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const router = express.Router()
+const port = 3000
+
+
+module.exports = (app) => {
+  app.get('/', (req, res) => { return res.render('index') })
+
+  // admin 用戶登入 A2-developer in charge
+  app.get('/admin/signin', (req, res) => { return res.render('admin/signin') })
+  app.post('/admin/signin', (req, res) => { })
+  // admin 用戶介面 A2-developer in charge
+  app.get('/admin/tweets', (req, res) => { return res.render('admin/index') })
+  app.delete('/admin/tweets/:id', (req, res) => { })
+  app.get('/admin/users', (req, res) => { })
+
+  // 一般用戶介面 A3-developer in charge
+  app.get('/tweets', (req, res) => { return res.render('index') })
+  app.post('/tweets', (req, res) => { })
+  app.get('/tweets/:id/replies', (req, res) => { })
+  app.post('/tweets/:id/replies', (req, res) => { })
+  app.post('/tweets/:id/like', (req, res) => { })
+  app.post('/tweets/:id/unlike', (req, res) => { })
+
+  // 一般用戶使用者資訊介面 A3-developer in charge
+  app.get('/users/:id/tweets', (req, res) => { })
+  app.get('/users/:id/followings', (req, res) => { })
+  app.get('/users/:id/followers', (req, res) => { })
+  app.get('/users/:id/likes', (req, res) => { })
+
+  // 使用者資訊介面修改 in charge 未定
+  app.get('/api/users/:id', (req, res) => { })
+  app.post('/api/users/:id', (req, res) => { })
+
+  // 追蹤功能 in charge 未定
+  app.post('/followships', (req, res) => { })
+  app.delete('/followships/:id', (req, res) => { })
+
+  // 註冊、登出、一般用戶登入 A2-developer in charge
+  app.get('/signin', (req, res) => { return res.render('signin') })
+  app.post('/signin', (req, res) => { })
+  app.get('/signup', (req, res) => { return res.render('signup') })
+  app.post('/signup', (req, res) => { })
+  app.get('/signout', (req, res) => { })
+
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+}
