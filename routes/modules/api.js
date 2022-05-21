a2947058b5670d543557c74b7ca25deb8ad0291e
tkooooo123 <tkooooo123@gmail.com>
2022-05-21 20:02:08 +0800

    修改api router 與modal，引用axios回傳更新資料

--- routes/modules/api.js
+++ routes/modules/api.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const apiController = require('../../controllers/api-controller')
 const { authenticated } = require('../../middleware/auth')
+const upload = require('../../middleware/multer')
 
 
 
 router.get('/users/:id', authenticated, apiController.getUser)
-router.post('/users/:id', authenticated, apiController.putUser)
+router.post('/users/:id', upload.fields([{name: 'cover', maxCount: 1}, {name: 'avatar', maxCount: 1}]), authenticated, apiController.putUser)
 
 module.exports = router
