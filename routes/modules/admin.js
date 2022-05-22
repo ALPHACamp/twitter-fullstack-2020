443d32697ef2b002b419697a4d51b78cf06e7619
Tommy1023 <j.h.chen1023@gmail.com>
2022-05-23 00:35:41 +0800

    feat:Debug.

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../../config/passport')
-const { authenticatedAdmin } = require('../../middleware/auth')
+const { authenticatedAdmin, authenticated } = require('../../middleware/auth')
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/signin', adminController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/admin/signin', failureFlash: true }), adminController.signIn)
 router.get('/logout', adminController.logout)
 
-router.get('/users', authenticatedAdmin, adminController.getUsers)
+router.get('/users', authenticated, authenticatedAdmin, adminController.getUsers)
 
-router.delete('/tweets/:id', authenticatedAdmin, adminController.deleteTweet)
-router.get('/tweets', authenticatedAdmin, adminController.getTweets)
+router.delete('/tweets/:id', authenticated, authenticatedAdmin, adminController.deleteTweet)
+router.get('/tweets', authenticated, authenticatedAdmin, adminController.getTweets)
 
 module.exports = router
