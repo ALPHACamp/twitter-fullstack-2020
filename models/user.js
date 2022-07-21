2ee760dbbad9074e9946a07d970ec529140e8c48
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-21 15:32:23 +0800

    feat: add 'cover' & 'account' column in user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,64 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: {
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER
+    },
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    account: {
+      type: DataTypes.STRING
+    },
+    name: DataTypes.STRING,
+    avatar: {
+      type: DataTypes.STRING
+    },
+    introduction: {
+      type: DataTypes.TEXT
+    },
+    cover: {
+      type: DataTypes.STRING
+    },
+    role: {
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
   }, {})
+
   User.associate = function (models) {
+    User.hasMany(models.Reply, { foreignKey: 'UserId' })
+    User.hasMany(models.Tweet, { foreignKey: 'UserId' })
+    User.hasMany(models.Like, { foreignKey: 'UserId' })
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikeTweets'
+    })
+    User.belongsToMany(User, { // 找出「被哪些人追蹤」
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+    User.belongsToMany(User, { // 找出「自己追蹤哪些人」
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
   }
+
   return User
 }
