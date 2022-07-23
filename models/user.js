57c854bd5d6103ce6e06fe5e832a23816131998d
Roger <winter30009@yahoo.com.tw>
2022-07-23 13:59:46 +0800

    set up user model

--- models/user.js
+++ models/user.js
@@ -1,36 +1,45 @@
 'use strict';
-const {
-  Model
-} = require('sequelize')
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
-  User.associate = function (models) {
-  };
-  return User;
-};
+const { Model } = require('sequelize')
 
-//預設的寫法跟餐廳專案的不一樣耶 @@
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      User.hasMany(models.Tweet, { foreignKey: 'UserId' })
+      User.hasMany(models.Like, { foreignKey: 'UserId' })
+      User.hasMany(models.Reply, { foreignKey: 'UserId' })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
+    }
+  };
+  User.init({
+    account: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: true
+  })
+  return User
+}
+
 // module.exports = (sequelize, DataTypes) => {
-//   class User extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
+//   const User = sequelize.define('User', {
+//   }, {});
+//   User.associate = function (models) {
 //   };
-//   User.init({
-//     name: DataTypes.STRING,
-//     email: DataTypes.STRING,
-//     password: DataTypes.STRING
-//   }, {
-//     sequelize,
-//     modelName: 'User',
-//     tableName: 'Users', // 新增這裡
-//     underscored: true
-//   })
-//   return User
-// }
+//   return User;
+// };
