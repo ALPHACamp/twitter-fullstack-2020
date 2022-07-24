67d246c7eb644d50217332218184939b3d044b3d
doug0849 <etaomonk@gmail.com>
2022-07-25 04:16:55 +0800

    feat: fix apis/tweet-controller.js

--- controllers/apis/tweet-controller.js
+++ controllers/apis/tweet-controller.js
@@ -3,25 +3,25 @@ const jwt = require('jsonwebtoken')
 
 const tweetController = {
   getTweetReplies: (req, res, next) => {
-    userServices.getTweetReplies(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    tweetServices.getTweetReplies(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   postTweetReply: (req, res, next) => {
-    userServices.postTweetReply(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    tweetServices.postTweetReply(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   postTweetLike: (req, res, next) => {
-    userServices.postTweetLike(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    tweetServices.postTweetLike(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   postTweetsUnlike: (req, res, next) => {
-    userServices.postTweetsUnlike(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    tweetServices.postTweetsUnlike(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   postTweetUnlike: (req, res, next) => {
-    userServices.editUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    tweetServices.editUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   postTweet: (req, res, next) => {
-    userServices.postTweet(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    tweetServices.postTweet(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   getTweets: (req, res, next) => {
-    userServices.getTweets(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    tweetServices.getTweets(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 
