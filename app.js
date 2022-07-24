4ac9c6ae794723a97808999b851c4fc0c3f0c488
doug0849 <etaomonk@gmail.com>
2022-07-25 00:57:22 +0800

    feat:initial folder & app.js

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
-const express = require('express')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const path = require('path')
 const helpers = require('./_helpers')
 
+const express = require('express')
+const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const passport = require('./config/passport')
+
+const handlebarsHelpers = require('./helpers/handlerbars-helpers')
+const { pages, apis } = require('./routes')
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const SESSION_SECRET = 'simpleTwitter'
+
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(session({ secret: SESSION_SECRET, resave: true, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  // res.locals.user = getUser(req)
+  next()
+})
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+
+app.use('/api', apis)
+app.use(pages)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
