43ea6c4e8d86021fda9382767d673ed64db3c45c
doug0849 <etaomonk@gmail.com>
2022-07-25 02:51:58 +0800

    feat: add public folder

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(passport.session())
 app.use(flash())
 app.use(methodOverride('_method'))
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use(express.static('public'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
