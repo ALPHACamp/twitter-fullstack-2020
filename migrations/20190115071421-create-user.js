9e8dcfa8174de70cbe9c5291720410b228d482a8
ChillDiver <yumingchang1991@gmail.com>
2022-07-25 14:47:05 +0800

    feat: add user model and migration file

--- migrations/20190115071421-create-user.js
+++ migrations/20190115071421-create-user.js
@@ -8,19 +8,24 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      account: {
+        type: Sequelize.STRING(50)
+      },
+      name: {
+        type: Sequelize.STRING(50)
+      },
       email: {
         type: Sequelize.STRING
       },
       password: {
         type: Sequelize.STRING
       },
-      name: {
-        type: Sequelize.STRING
-      },
       avatar: {
+        allowNull: true,
         type: Sequelize.STRING
       },
       introduction: {
+        allowNull: true,
         type: Sequelize.TEXT
       },
       role: {
--- models/user.js
+++ models/user.js
@@ -1,8 +1,45 @@
-'use strict';
+'use strict'
+const {
+  Model
+} = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
-  };
-  return User;
-};
+  class User extends Model {
+    static associate(models) {
+
+      User.hasMany(models.Tweet, { foreignKey: 'userId' })
+      User.hasMany(models.Reply, { foreignKey: 'userId' })
+      User.hasMany(models.Like, { foreignKey: 'userId' })
+
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
+    }
+  }
+
+  User.init({
+    account: DataTypes.STRING,
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    banner: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: true
+  })
+
+  return User
+
+}
