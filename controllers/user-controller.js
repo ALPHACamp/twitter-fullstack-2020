41f62e3879f105ec4be6e31ab9094c0e9511f3b3
MioRain <ericzero1593@hotmail.com>
2022-07-25 08:05:45 +0800

    fix rename columns of migrations & signup checkPassword & delete emailRule

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,12 +6,10 @@ const userController = {
     res.render('signup')
   },
   signUp: (req, res, next) => {
-    const { account, name, email, password, passwordCheck } = req.body
-    const emailRule = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
-    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!') // assert(req, 'Passwords do not match!')
-    if (!account || !name || !email || !password || !passwordCheck) throw new Error('Please fill in every blank.')
+    const { account, name, email, password, checkPassword } = req.body 
+    if (req.body.password !== req.body.checkPassword) throw new Error('Passwords do not match!') // assert(req, 'Passwords do not match!')
+    if (!account || !name || !email || !password || !checkPassword) throw new Error('Please fill in every blank.')
     if (name.length > 50) throw new Error('字數超出上限！')
-    if (emailRule.test(email)) throw new Error('Email is not in correct format.')
     User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
@@ -33,6 +31,7 @@ const userController = {
   },
   signIn: (req, res) => {
     req.flash('success_messages', '成功登入！')
+    res.status(302)
     res.redirect('/tweets')
   },
   logout: (req, res) => {
