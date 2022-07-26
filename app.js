a013a0715d8326c436c2c2b25d7432ef033e5083
yaahsin <brillanteoboe@gmail.com>
2022-07-26 16:50:45 +0800

    project init

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express')
-const helpers = require('./_helpers');
+// const helpers = require('./_helpers')
+const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+const session = require('express-session')
 
 const app = express()
 const port = 3000
 
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(express.json())
+app.use(flash())
+
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) =>
+  res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
