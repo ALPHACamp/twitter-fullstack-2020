3cb8140b43b75d72a8e71d5950d653b30f39d66a
chh817 <chh817@gmail.com>
2022-07-26 18:15:05 +0800

    feat: create Followship & Like models

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const handlebars = require('express-handlebars')
+const helpers = require('./_helpers')
 
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
