b3a346d07abd421c1204089f972b440de31369b3
MioRain <99007265+MioRain@users.noreply.github.com>
2022-07-26 23:00:19 +0800

    Unified use helpers.getUser(req)

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
 const helpers = require('./_helpers')
-
-const { getUser } = require('./_helpers')
 const app = express()
 const port = process.env.PORT || 3000
 if (process.env.NODE_ENV !== 'production') {
@@ -31,7 +29,7 @@ app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
-  res.locals.user = getUser(req)
+  res.locals.user = helpers.getUser(req)
   next()
 })
 app.use(express.static('public'))
