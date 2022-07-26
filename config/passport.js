dcb0bf90c14816489576d010209044b7f0cdd3f9
choyz1024 <nagatoyuki1024@gmail.com>
2022-07-26 11:42:02 +0800

    feat: add admin sign in

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,9 @@ const LocalStrategy = require('passport-local')
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
+
 passport.use(
+  'user-local',
   new LocalStrategy(
     {
       usernameField: 'account',
@@ -19,6 +21,25 @@ passport.use(
     }
   )
 )
+
+passport.use(
+  'admin-local',
+  new LocalStrategy(
+    {
+      usernameField: 'account',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    async (req, account, password, cb) => {
+      const user = await User.findOne({ where: { account } })
+      if (!user || user.role !== 'admin' || !bcrypt.compareSync(password, user.password)) {
+        return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+      }
+      return cb(null, user)
+    }
+  )
+)
+
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
