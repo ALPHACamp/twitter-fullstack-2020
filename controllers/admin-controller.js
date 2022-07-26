2aa91ade9b51302792a1ca5665041352e7acb1a5
AndyChienn <andy2002chien@hotmail.com.tw>
2022-07-27 03:26:26 +0800

    feat: modyfy admincontrollers with async/await and modify middleware/auth.js

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,6 @@
 // admin頁面各種 signin/ getuser/ gettweet/ deletetweet/ logout
-const { User, Tweet, Like } = require('../models')
-const { sequelize } = require('../models')
+const { User, Tweet, Like, Reply } = require('../models')
+
 const adminController = {
   signInPage: (req, res) => {
     res.render('admin/signin')
@@ -14,58 +14,71 @@ const adminController = {
     req.logout()
     res.redirect('/admin/signin')
   },
-  getAdminUsers: (req, res, next) => {
-    return User.findAll({
-      where: { role: 'user' },
-      include: [
-        { model: Tweet, include: [Like] },
-        { model: User, as: 'Followers' },
-        { model: User, as: 'Followings' },
-      ]
-    })
-      .then(users => {
-        const results = users.map(user => ({
-          ...user.toJSON(),
-        }))
-        results.forEach(r => {
-          r.TweetsCount = r.Tweets.length
-          r.FollowingsCount = r.Followings.length
-          r.FollowersCount = r.Followers.length
-          r.TweetsLikedCount = r.Tweets.reduce((num, tweet) => {
-            return num + tweet.Likes.length
-          }, 0)
-        })
-        results.sort((a, b) => b.TweetsCount - a.TweetsCount)
-        res.render('admin/users', { users: results })
+  getAdminUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({
+        include: [
+          { model: Tweet, include: [Like] },
+          { model: User, as: 'Followers' },
+          { model: User, as: 'Followings' },
+        ]
       })
-      .catch(err => next(err))
+      const results = users.map(user => ({
+        ...user.toJSON(),
+      }))
+      results.forEach(r => {
+        r.TweetsCount = r.Tweets.length
+        r.FollowingsCount = r.Followings.length
+        r.FollowersCount = r.Followers.length
+        r.TweetsLikedCount = r.Tweets.reduce((num, tweet) => {
+          return num + tweet.Likes.length
+        }, 0)
+      })
+      results.sort((a, b) => b.TweetsCount - a.TweetsCount)
+      return res.render('admin/users', { users: results })
+    } catch (err) {
+
+      next(err)
+
+    }
   },
-  getAdminTweets: (req, res, next) => {
-    return Tweet.findAll({
-      raw: true,
-      nest: true,
-      include: [User],
-      order: [['createdAt', 'DESC']],
-    })
-      .then(tweets => {
-        tweets = tweets.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50),
-        }))
-        return res.render('admin/tweets', { tweets })
+  getAdminTweets: async (req, res, next) => {
+    try {
+      const tweets = await Tweet.findAll({
+        raw: true,
+        nest: true,
+        include: [User],
+        order: [['createdAt', 'DESC']],
       })
-      .catch(err => next(err))
+      const rederedTweets = tweets.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50),
+      }))
+      return res.render('admin/tweets', { tweets: rederedTweets })
+    } catch (err) {
+
+      next(err)
+
+    }
   },
-  deleteTweet: (req, res, next) => {
-    return Tweet.findByPk(req.params.id)
-      .then(tweet => {
-        tweet.destroy()
-      })
-      .then(() => {
-        res.redirect('/admin/tweets')
-      })
-      .catch(err => next(err))
-  },
+  deleteTweet: async (req, res, next) => {
+    try {
+      const TweetId = req.params.id
+      const tweet = await Tweet.findByPk(req.params.id)
+      if (!tweet) throw new Error("Tweet didn't exist!")
+      await tweet.destroy()
+      await Reply.destroy({ where: { TweetId } })
+      await Like.destroy({ where: { TweetId } })
+
+      req.flash('success_messages', '成功刪除貼文及相關資訊！')
+      res.redirect('/admin/tweets')
+
+    } catch (err) {
+
+      next(err)
+
+    }
+  }
 }
 
 module.exports = adminController
