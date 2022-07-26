5c50ddc321e4360646f9367a582525f73e95c0e2
AlvinLee66 <ewqaz001@gmail.com>
2022-07-26 23:43:31 +0800

    feat: modify app.js

--- app.js
+++ app.js
@@ -10,19 +10,6 @@ const port = process.env.PORT || 3000
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
-app.set('view engine', 'hbs')
-
-app.use(session({
-  secret: 'ThisIsMySecret',
-  resave: false,
-  saveUninitialized: true
-}))
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-app.use(express.json())
-app.use(flash())
-
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
