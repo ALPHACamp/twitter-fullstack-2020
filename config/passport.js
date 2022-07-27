ebbc52b554ffcfd3022d031a487288b3c4f5f41b
ChillDiver <yumingchang1991@gmail.com>
2022-07-27 16:44:20 +0800

    feat: add Passport Authentioncation & User/Admin login

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,48 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-// const passportJWT = require('passport-jwt')
 const bcrypt = require('bcrypt-nodejs')
 
-// passport logic
+const { User, Tweet, Like } = require('../models')
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'account',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+
+  (req, account, password, cb) => {
+    User
+      .findOne({ where: { account } })
+      .then(user => {
+        if (!user) {
+          req.flash('error_message', '帳號輸入錯誤，或尚未註冊')
+          return cb(null, false)
+        }
+        const result = bcrypt.compareSync(password, user.password)
+        if (!result) return cb(new Error('密碼錯誤'), false)
+        return cb(null, user)
+      })
+      .catch(err => cb(err))
+  }
+))
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+
+passport.deserializeUser((id, cb) => {
+  User
+    .findByPk(id, {
+      include: [
+        Tweet,
+        Like,
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ]
+    })
+    .then(user => cb(null, user?.toJSON()))
+    .catch(err => cb(err))
+})
 
 module.exports = passport
---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const helpers = require('../_helpers')
+const ADMIN = 'admin'
+
+const authenticated = (req, res, next) => {
+  if (helpers.ensureAuthenticated(req)) {
+    return next()
+  }
+  res.redirect('/signin')
+}
+
+const authenticatedAdmin = (req, res, next) => {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(res).role === ADMIN) return next()
+    res.redirect('/')
+  } else {
+    res.redirect('/signin')
+  }
+}
+
+module.exports = {
+  authenticated,
+  authenticatedAdmin
+}
