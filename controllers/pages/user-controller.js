bc9468280ed9837a79812aa488224fe640a31690
bft7658 <bft7658@gmail.com>
2022-07-27 16:11:19 +0800

    feat: add signUp function

--- controllers/pages/user-controller.js
+++ controllers/pages/user-controller.js
@@ -1,4 +1,5 @@
 const { User } = require('../../models')
+const bcrypt = require('bcryptjs')
 
 const userConroller = {
   getSignin: (req, res) => {
@@ -6,6 +7,35 @@ const userConroller = {
   },
   signUpPage: (req, res) => {
     res.render('signup')
+  },
+  signUp: (req, res, next) => {
+    const { account, name, email, password, checkPassword } = req.body
+    // 查看不符合的條件
+    if (!account || !name || !email || !password || !checkPassword) throw new Error('請填寫每個欄位')
+    if (password !== checkPassword) throw new Error('密碼與確認密碼不相符!')
+    if (name.length > 50) throw new Error('名稱長度超出上限 50 字！')
+
+    // 並確認 email 與 account 不能重複
+    return Promise.all([
+      User.findOne({ where: { email } }),
+      User.findOne({ where: { account } })
+    ])
+      .then(([userEmail, userAccount]) => {
+        if (userEmail) throw new Error('Email 已經存在!')
+        if (userAccount) throw new Error('Account 已經存在!')
+        return bcrypt.hash(password, 10)
+      })
+      .then(hash => User.create({
+        account,
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號！')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/pages/index.js
+++ routes/pages/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const userController = require('../../controllers/pages/user-controller')
 
 router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/signin', userController.getSignin)
 router.get('/', (req, res) => res.send('Hello World!'))
 
