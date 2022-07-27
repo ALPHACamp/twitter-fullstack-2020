8262eed2d55dcfd9beb103e2eb4864cde9a1df9d
ChillDiver <yumingchang1991@gmail.com>
2022-07-27 20:45:39 +0800

    fix: change adminAuth to fallback to admin/signin

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,15 +5,13 @@ const authenticated = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
     return next()
   }
-  res.redirect('/signin')
+  return res.redirect('/signin')
 }
 
 const authenticatedAdmin = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
     if (helpers.getUser(req).role === ADMIN) return next()
-    res.redirect('/')
-  } else {
-    res.redirect('/signin')
+    return res.redirect('/admin/signin')
   }
 }
 
