9eac28934455e532d390d096ce061560238860e4
MioRain <ericzero1593@hotmail.com>
2022-07-28 19:29:10 +0800

    remove redundant code

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
-const sortObj = require('../helpers/sortObj')
-const { User, Followship } = require('../models')
 // set up Passport strategy
 passport.use(new LocalStrategy(
   // customize user field
