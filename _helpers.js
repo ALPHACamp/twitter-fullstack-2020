92ca4324619e0bbc0e8bb91eb7d2fd54f6bcdbf4
coldfish5630 <layan.schicksal@gmail.com>
2022-07-29 00:36:49 +0900

    feature/admin tweets & users page

--- _helpers.js
+++ _helpers.js
@@ -6,7 +6,16 @@ function getUser (req) {
   return req.user || null
 }
 
+function getThousands (num) {
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1) + 'k'
+  } else {
+    return num
+  }
+}
+
 module.exports = {
   ensureAuthenticated,
-  getUser
+  getUser,
+  getThousands
 }
