eaa6071134788319ea64a2058fcb4424f5fb32a9
ChillDiver <yumingchang1991@gmail.com>
2022-07-28 17:44:32 +0800

    feat: add admin-tweets route and controller

--- controllers/pages/admin-controller.js
+++ controllers/pages/admin-controller.js
@@ -1,4 +1,4 @@
-const { User } = require('../../models')
+const { sequelize } = require('../../models')
 
 const adminConroller = {
   getSignin: (req, res) => {
@@ -13,6 +13,17 @@ const adminConroller = {
     req.logout()
     req.flash('error_messages', '帳號不存在')
     return res.redirect('/admin/signin')
+  },
+  getTweets: async (req, res) => {
+    const dbResult = await sequelize.query('SELECT `Users`.`account`, `Users`.`name`, `Users`.`avatar`, substring(`Tweets`.`description`, 1, 50) AS `description`, `Tweets`.`createdAt`, `Tweets`.`id` FROM Tweets LEFT JOIN Users on`Users`.`id` = `Tweets`.`userId` ORDER BY `Tweets`.`createdAt` DESC'
+    )
+    const tweets = dbResult[0].map(tweet => tweet)
+    return res.render('admin/tweets', { tweets })
+  },
+  deleteTweet: async (req, res) => {
+    await sequelize.query('DELETE FROM Tweets WHERE `Tweets`.`id` = ' + req.params.tweetId + ';')
+    req.flash('error_messages', '成功刪除一筆推文')
+    return res.redirect('back')
   }
 }
 
--- routes/pages/modules/admin.js
+++ routes/pages/modules/admin.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const adminController = require('../../../controllers/pages/admin-controller')
 
 router.get('/tweets', adminController.getTweets)
+router.delete('/tweets/:tweetId', adminController.deleteTweet)
 
 module.exports = router
