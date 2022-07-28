ee31d48a9f0a205e9f0d045db52cf49b3dea41c8
ChillDiver <yumingchang1991@gmail.com>
2022-07-28 20:51:07 +0800

    fix: make user-auth to redirect admin to admin/tweets and add catch() in admin controller

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ const ADMIN = 'admin'
 
 const authenticated = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).role === ADMIN) return res.redirect('/admin/tweets')
     return next()
   }
   return res.redirect('/signin')
