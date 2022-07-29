3f0071cc716413c83e99b8b1165fd596cf0e029e
MioRain <ericzero1593@hotmail.com>
2022-07-29 10:24:56 +0800

    optimize getTweets function

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,55 +1,14 @@
 const assert = require('assert')
 const helpers = require("../_helpers")
-const { User, Tweet, Like, Reply } = require('../models')
-const { imgurFileHandler } = require('../helpers/file-helpers')
+const { User, Tweet, Like } = require('../models')
 
 const tweetController = {
-  // getTweets: (req, res, next) => {
-  //   console.log('here is tweets.')
-  //   const currentUser = getUser(req)
-  //   res.render('tweets') //, { currentUser }
-  //   Promise.all([User.findByPk(currentUser.id, {
-  //     include: [
-  //       // tweets
-  //       { model: Comment, include: Restaurant },
-  //       { model: Restaurant, as: 'FavoritedRestaurants' },
-  //       { model: User, as: 'Followings' }
-  //     ],
-  //     order: [
-  //       [{ model: Comment }, 'createdAt', 'DESC']
-  //     ]
-  //   }),
-  //   Comment.findAndCountAll({
-  //     where: { userId: req.params.id }
-  //   }),
-  //   Comment.findAll({
-  //     include: Restaurant,
-  //     where: { userId: req.params.id },
-  //     group: ['restaurant_id'],
-  //     nest: true,
-  //     raw: true
-  //   })
-  //   ])
-  //     .then(([targetUser, totalComments, commentedRestaurants]) => {
-  //       if (!targetUser) throw new Error("User doesn't exist!")
-  //       const isFollowed = req.user.Followings.some(f => f.id === targetUser.id)
-  //       res.render('tweets', {
-  //         targetUser: targetUser.toJSON(), // 查看其他使用者
-  //         user: currentUser, // 現在登入的使用者
-  //         totalComments,
-  //         commentedRestaurants,
-  //         isFollowed
-  //       })
-  //     })
-  //     .catch(err => next(err))
-  // },
   getTweets: async (req, res, next) => {
     try {
       const user = helpers.getUser(req)
       const tweets = await Tweet.findAll({
         include: [
-          User,
-          Like
+          User
         ],
         order: [
           ['created_at', 'DESC'],
@@ -58,7 +17,12 @@ const tweetController = {
         raw: true,
         nest: true
       })
-      return res.render('tweets', { tweets, user })
+      const likedTweetsId = req.user?.likedTweets.map(tweet => tweet.id)
+      const tweetsList = tweets.map(tweet => ({
+        ...tweet,
+        isLiked: likedTweetsId?.includes(tweet.id)
+      }))
+      return res.render('tweets', { tweetsList, user })
     }
     catch (err) {
       next(err)
