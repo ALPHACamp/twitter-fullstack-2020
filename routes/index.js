dbcc04a07f4a9136a1122f2b9cbf73bd7a68c4cc
AlvinLee66 <ewqaz001@gmail.com>
2022-07-29 09:50:05 +0800

    feat: add signin router

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 const userController = require('../controllers/user-controller')
 const followshipController = require('../controllers/followship-controller')
 const tweetController = require('../controllers/tweet-controller')
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 
 router.get('/users/:id/setting', userController.getSetting)
 router.get('/users', userController.getUsers)
