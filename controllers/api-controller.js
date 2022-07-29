24981515ddb4c7109acbc832a0cfb429f24f8651
june850713@gmail.com <june850713@gmail.com>
2022-07-29 15:56:08 +0800

    feat: add api put user feature

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -13,6 +13,21 @@ const apiController = {
         res.json({ status: 'success', data: user })
       })
       .catch(err => next(err))
+  },
+  postUser: (req, res, next) => {
+    const { cover, avatar, name, introduction } = req.body
+    const id = req.params.id
+    User.findByPk(id)
+      .then(data => {
+        if (!data) {
+          return res.json({ status: "user didn't exist" })
+        }
+        data.update({ cover, avatar, name, introduction })
+        const user = data.toJSON()
+        delete user.password
+        res.json({ status: 'success', data: user })
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/modules/api.js
+++ routes/modules/api.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const apiController = require('../../controllers/api-controller')
 
 router.get('/users/:id', apiController.getUserInfo)
+router.post('/users/:id', apiController.postUser)
 
 module.exports = router
