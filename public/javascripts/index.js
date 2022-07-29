257841631d765662f833b2dca88cefdeec0864d8
doug0849 <etaomonk@gmail.com>
2022-07-29 08:48:51 +0800

    整理檔案

--- public/javascripts/index.js
+++
@@ -1,5 +0,0 @@
-import { likeBtns, likeAddListener } from './like-btn-listener'
-import { replyBtns, replyAvatar, replyWindow, replyAddListener, replyBtnOnClick } from './reply-btn-listener'
-
-likeAddListener()
-replyAddListener()
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './public/javascripts/index.js',
-  output: {
-    path: path.resolve(__dirname, './public/javascripts/bundle'),
-    filename: 'main.js'
-  }
-}
