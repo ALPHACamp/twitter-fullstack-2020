137f3cdd7e47ce78f079e433697cb4d5cb462750
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-31 14:00:02 +0800

    fix: correct getTweets controller to pass test

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -3,7 +3,7 @@ const helpers = require('../_helpers')
 
 const tweetController = {
   getTweets: (req, res, next) => {
-    return Promise.all([
+    Promise.all([
       Tweet.findAll({
         include: [
           { model: User, attributes: ['id', 'avatar', 'name', 'account'] },
@@ -14,12 +14,20 @@ const tweetController = {
         nest: true
       }),
       User.findAll({
-        include: [
-          { model: User, as: 'Followers' }
-        ]
+        where: { role: 'user' },
+        attributes: ['id', 'avatar', 'name', 'account'],
+        include: [{
+          model: User,
+          attributes: ['id'],
+          as: 'Followers'
+        }],
+        nest: true
       })
     ])
       .then(([tweets, users]) => {
+        // 撈出loginUser，nav-left 使用
+        const user = helpers.getUser(req) ? JSON.parse(JSON.stringify(helpers.getUser(req))) : []
+
         // 整理 tweets
         const likedTweetsId = helpers.getUser(req)?.LikeTweets ? helpers.getUser(req).LikeTweets.map(lt => lt.id) : [] // 先確認 req.user 是否存在，若存在檢查 LikeTweets 是否存在。如果 LikeTweets 存在則執行 map 撈出 tweet id 。若上述兩個不存在，回傳空陣列
         tweets = JSON.parse(JSON.stringify(tweets))
@@ -29,24 +37,17 @@ const tweetController = {
           tweet.isLiked = likedTweetsId.includes(tweet.id)
         }
 
-        // 整理 users（要傳給 nav-right）
-        users = users.map(user => ({
-          ...user.toJSON(),
-          followerCount: user.Followers.length,
-          isFollowed: req.user.Followings.some(f => f.id === user.id)
-        }))
-        const filterSelfUser = []
-        users = users.forEach(user => {
-          if (user.id !== helpers.getUser(req).id && user.role !== 'admin') {
-            filterSelfUser.push(user)
-          }
-        })
-        users = filterSelfUser.sort((a, b) => b.followerCount - a.followerCount).slice(0, 10) // 只取排行前 10 的 users
+        // 整理 users 只留被追蹤數排行前 10 者，nav-right 使用
+        const followedUserId = helpers.getUser(req)?.Followings ? helpers.getUser(req).Followings.map(fu => fu.id) : [] // 先確認 req.user 是否存在，若存在檢查 Followings (該user追蹤的人) 是否存在。如果 Followers 存在則執行 map 撈出 user id 。若上述兩個不存在，回傳空陣列
+        users = JSON.parse(JSON.stringify(users))
+        for (const user of users) { // 以迴圈跑每一筆 user ，每一筆新增 numberOfFollowers、isFollowed 資訊
+          user.numberOfFollowers = user.Followers.length
+          user.isFollowed = followedUserId.includes(user.id)
+        }
+        users = users.sort((a, b) => b.numberOfFollowers - a.numberOfFollowers).slice(0, 10) // 只取排行前 10 的 users
 
-        const user = helpers.getUser(req)
-
-        return res.render('index', {
-          user: user.toJSON(),
+        res.render('index', {
+          user,
           tweets,
           users
         })
