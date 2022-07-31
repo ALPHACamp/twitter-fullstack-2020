7cfd1b5ba999fd965ca12118df40904939d4042f
yongsin0129 <yongsin0129@gmail.com>
2022-07-31 19:29:23 +0800

    fix : fix bug to fit test-file (user request : edit page) 2nd time

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -17,18 +17,19 @@ const apiController = {
     const userId = req.params.id
     const { files } = req
     const { name, introduction } = req.body
-    const user = await User.findByPk(userId)    
+
+    const user = await User.findByPk(userId)
     let avatarFilePath = user.dataValues.avatar
     let coverFilePath = user.dataValues.cover
+
     if (files?.image) {
       avatarFilePath = await localFileHandler(...files.image)
-      console.log('avatarFilePath : ', avatarFilePath)
     }
 
     if (files?.coverImage) {
       coverFilePath = await localFileHandler(...files.coverImage)
-      console.log('coverFilePath :', coverFilePath)
     }
+
     if (!user) throw new Error("user didn't exist")
     await user.update({
       name,
@@ -38,19 +39,6 @@ const apiController = {
     })
 
     return res.json({ status: 'success', ...user.toJSON() })
-    // res.sendStatus(200).redirect('back')
-
-    // User.findByPk(id)
-    //   .then(data => {
-    //     if (!data) throw new Error("user didn't exist")
-    //     return data.update({ cover, avatar, name, introduction })
-    //   })
-    //   .then(newData => {
-    //     const user = newData.toJSON()
-    //     delete user.password
-    //     res.json({ status: 'success', ...user })
-    //   })
-    //   .catch(err => next(err))
   }
 }
 
