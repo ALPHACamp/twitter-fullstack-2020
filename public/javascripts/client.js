d612dbadf829887a1a125eba1459682f9f618d6f
ChillDiver <yumingchang1991@gmail.com>
2022-08-02 22:20:26 +0800

    imp: render current count of input in signup form

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,17 @@
 const editUser = document.querySelector('#edit-user')
 const saveUser = document.querySelector('#user-modal-save')
 
+if (window.location.pathname === '/signup') {
+  addScript('/signup')
+}
+
+function addScript (route) {
+  const BODY = document.getElementsByTagName('body')[0]
+  const script = document.createElement('script')
+  script.src = `/javascripts${route}.js`
+  BODY.appendChild(script)
+}
+
 function showEditUserModal (data) {
   const modalForm = document.querySelector('#user-modal-form')
   const modalBanner = document.querySelector('#user-modal-banner')
