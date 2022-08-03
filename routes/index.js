b510eb8e6565a64d99a919ea7217946f5d10b037
忍者哈特利 <ks0dcongra@gmail.com>
2022-08-03 14:07:40 +0800

    feat: delete profile.hbs and move index.js /users/ router to user.js

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,9 @@ const admin = require('./modules/admin')
 const api = require('./modules/api')
 const tweets = require('./modules/tweets')
 const users = require('./modules/users')
+const followships = require('./modules/followships')
 
 const userController = require('../controllers/user-controller')
-const followships = require('./modules/followships')
-const likeController = require('../controllers/like-controller')
 
 const { authenticatedUser } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
@@ -21,15 +20,11 @@ router.use('/followships', authenticatedUser, followships)
 router.use('/tweets', authenticatedUser, tweets)
 router.use('/users', authenticatedUser, users)
 
-router.get('/users/:id/setting', authenticatedUser, userController.getUserSetting)
-router.get('/users/:userId', authenticatedUser, userController.getProfile)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', authenticatedUser, userController.logout)
-
-router.get('/users/:id/likes', authenticatedUser, likeController.getUserLike)
 
 router.use('/', (req, res) => {
   res.redirect('/tweets')
