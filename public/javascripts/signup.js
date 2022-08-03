e8b065393648d681d737914150d6be2d5b050c2e
ChillDiver <yumingchang1991@gmail.com>
2022-08-03 20:01:31 +0800

    imp: check password consistency in sign-up form and /setting

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,10 +1,18 @@
 const inputAccount = document.querySelector('input#account')
 const inputName = document.querySelector('input#name')
+const inputPassword = document.querySelector('input#password')
+const inputCheckPassword = document.querySelector('input#checkPassword')
+const msgCheckPassword = document.querySelector('span.checkPassword-msg')
 
 const COLOR_CODE = {
   WARNING: '#FC5A5A',
-  DARK: '#171725'
+  DARK: '#171725',
+  GREEN: '#82C43C'
 }
+
+// add event listeners
+inputPassword.addEventListener('input', verifyPassword)
+inputCheckPassword.addEventListener('input', verifyPassword)
 
 Array.from([inputAccount, inputName]).forEach(target => {
   const currentCount = target.value.length
@@ -24,6 +32,8 @@ Array.from([inputAccount, inputName]).forEach(target => {
   })
 })
 
+// function declaration
+
 function getCountElement (targetElement) {
   return targetElement.nextElementSibling.nextElementSibling.querySelector('span.current-word-count')
 }
@@ -38,4 +48,20 @@ function highlightCount (targetElement, highlight = true) {
   }
   COUNT_DIV.style.color = COLOR_CODE.DARK
   INPUT_DIV.style.color = COLOR_CODE.DARK
+}
+
+function verifyPassword () {
+  if (inputPassword.value.length === 0 && inputCheckPassword.value.length === 0) {
+    msgCheckPassword.textContent = ''
+    return
+  }
+  if (inputPassword.value.length > 0 && inputCheckPassword.value.length > 0) {
+    if (inputPassword.value !== inputCheckPassword.value) {
+      msgCheckPassword.textContent = '請確保輸入一致的密碼'
+      msgCheckPassword.style.color = COLOR_CODE.WARNING
+      return
+    }
+    msgCheckPassword.textContent = '密碼驗證成功'
+    msgCheckPassword.style.color = COLOR_CODE.GREEN
+  }
 }
