bbc0ec549536d08196182fcb1b23afbfc9b120f8
AlvinLee66 <ewqaz001@gmail.com>
2022-08-04 16:11:44 +0800

    feat: modify tweet migration

--- migrations/20190115071420-create-tweet.js
+++ migrations/20190115071420-create-tweet.js
@@ -8,10 +8,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-
-      user_id: {
-        type: Sequelize.INTEGER
-      },
       description: {
         type: Sequelize.TEXT
       },
