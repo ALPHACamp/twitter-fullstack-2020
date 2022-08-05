9376731394e107d21550aee77975a095379fc56d
yaahsin <brillanteoboe@gmail.com>
2022-08-05 19:44:44 +0800

    fix modal err messages

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,36 @@
 const postPanel = document.querySelector('.table')
 const replyPanel = document.querySelector('.reply-panel')
 const postModal = document.querySelector('.post-modal')
-const tweetModal = document.querySelector('.modal-body')
+const replyPost = document.querySelector('.reply-the-post')
+const tweetModal = document.querySelector('.add-post')
 const replyForm = document.querySelector('.reply-form')
+
+// tweet-modal驗證
+tweetModal.addEventListener('click', e => {
+  const tweet = e.target.parentElement.parentElement.parentElement.children[2].value
+  console.log(`回覆字數: ${tweet.length}`)
+  if (tweet.length === 0) {
+    document.getElementById('error').innerHTML = '內容不可空白'
+    e.preventDefault()
+  } else if (tweet.length > 140) {
+    document.getElementById('error').innerHTML = '限制140字數'
+    e.preventDefault()
+  }
+})
+
+// reply-modal驗證
+replyPost.addEventListener('click', e => {
+  e.preventDefault()
+  const reply = e.target.parentElement.parentElement.parentElement.children[1].children[2].value
+  console.log(`回覆字數: ${reply.length}`)
+  if (reply.length === 0) {
+    document.getElementById('error-msg').innerHTML = '內容不可空白'
+    e.preventDefault()
+  } else if (reply.length > 140) {
+    document.getElementById('error-msg').innerHTML = '限制140字數'
+    e.preventDefault()
+  }
+})
 
 // 特定貼文詳情: /tweets
 postPanel.addEventListener('click', e => {
@@ -57,32 +85,4 @@ replyPanel.addEventListener('click', e => {
 
   postModal.innerHTML = TweetInfo
   replyForm.setAttribute('action', `/tweets/${tweetId}/replies`)
-})
-
-// reply-modal驗證
-postModal.addEventListener('submit', e => {
-  const reply = e.target.children[0].children[0].children[0].children[5].children[2].value
-  console.log(`回覆字數: ${reply.length}`)
-  if (reply.length === 0) {
-    document.getElementById('error-msg').innerHTML = '內容不可空白'
-    e.preventDefault()
-  } else if (reply.length > 140) {
-    document.getElementById('error-msg').innerHTML = '限制140字數'
-    e.preventDefault()
-  }
-})
-
-// tweet-modal驗證
-tweetModal.addEventListener('submit', e => {
-  const tweet = e.target.children[1].value
-  console.log(e.target)
-  console.log(`回覆字數: ${tweet.length}`)
-  console.log(tweet)
-  if (tweet.length === 0) {
-    document.getElementById('error').innerHTML = '內容不可空白'
-    e.preventDefault()
-  } else if (tweet.length > 140) {
-    document.getElementById('error').innerHTML = '限制140字數'
-    e.preventDefault()
-  }
 })
