94982381beaa47557bbc2406aaedfe5de554949f
忍者哈特利 <ks0dcongra@gmail.com>
2022-08-05 21:20:58 +0800

    feat: add helper in nav-right

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -26,6 +26,45 @@ dayjs.extend(relativeTime)
 module.exports = {
   relativeTimeFromNow: theTime => dayjs(theTime).fromNow(),
 
+  sliceUserInfo: function (a, b, c) {
+    // 如果isFollowed存在
+    if (c) {
+      if (a.length > 4 && b === '') {
+        console.log('AAAAAAAA', a)
+        a = a.slice(0, 4) + ' ...'
+        return a
+      }
+      if (a.length <= 4 && b === '') {
+        return a
+      }
+      if (b.length > 5 && a === '') {
+        console.log('BBBBBBBB', b)
+        b = b.slice(0, 5) + ' ...'
+        return b
+      }
+      if (b.length <= 5 && a === '') {
+        return b
+      }
+    } else { // 如果isFollowed不存在
+      if (a.length > 5 && b === '') {
+        console.log('CCCCCCC', a)
+        a = a.slice(0, 5) + ' ...'
+        return a
+      }
+      if (a.length <= 5 && b === '') {
+        return a
+      }
+      if (b.length > 6 && a === '') {
+        console.log('DDDDDDDD', b)
+        b = b.slice(0, 6) + ' ...'
+        return b
+      }
+      if (b.length <= 6 && a === '') {
+        return b
+      }
+    }
+  },
+
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
