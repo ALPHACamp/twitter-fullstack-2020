3212e7d554eedcb3e48936feaee27a1323d890e6
yaahsin <brillanteoboe@gmail.com>
2022-08-06 14:37:28 +0800

    design: reply modal

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -36,7 +36,8 @@ postPanel.addEventListener('click', e => {
   const post = e.target.parentElement.parentElement.parentElement.children[0]
   const tweetId = e.target.dataset.id
   const userName = post.children[0].textContent
-  const userAccount = post.children[1].textContent
+  const userAccountTime = post.children[1].textContent
+  const userAccount = userAccountTime.split('．')[0]
   const description = post.children[2].textContent
   const avatar = post.parentElement.parentElement.children[0].children[0].children[0].src
 
@@ -46,11 +47,14 @@ postPanel.addEventListener('click', e => {
       <div class="px-2 tweet">
         <div class="reply-modal-tweet-right column mb-2">
           <img src="${avatar}" alt="post-icon" style="border-radius: 50%; width: 50px; height: 50px;"></td>
-          <a href="" class="user-name" style="left">${userName} </a><span class="user-account" style="color: #6C757D;">${userAccount}</span>
-          <div class="reply-modal-tweet-text m-2">${description}</div>
-          <span style="color: #6C757D;">回覆給  <span style="color: #FF6600;">${userAccount}</span></span>
+          <a href="" class="user-name" style="margin-left: 6px;">${userName}</a><span class="user-account"
+            style="color: #6C757D;">${userAccountTime}</span>
+          <div class="reply-modal-tweet-text" style="margin-left: 60px;">${description}</div>
+          <span style="color: #6C757D; margin-left: 60px;">回覆給 <span style="color: #FF6600;">${userAccount}</span></span>
         </div>
       </div>
+      <div class="vertical-line"
+        style="position: absolute; border-left: 2px solid #B5B5BE; height: 55px; left: 30px; top: 58px;"></div>
     </div>
       `
 
@@ -74,11 +78,14 @@ replyPanel.addEventListener('click', e => {
       <div class="px-2 tweet">
         <div class="reply-modal-tweet-right column mb-2">
           <img src="${avatar}" alt="post-icon" style="border-radius: 50%; width: 50px; height: 50px;"></td>
-          <a href="" class="user-name" style="left">${userName} </a><span class="user-account" style="color: #6C757D;">${userAccount}</span>
-          <div class="reply-modal-tweet-text m-2">${description}</div>
-          <span style="color: #6C757D;">回覆給  <span style="color: #FF6600;">${userAccount}</span></span>
+          <a href="" class="user-name" style="margin-left: 6px;">${userName}</a><span class="user-account"
+            style="color: #6C757D;">${userAccount}</span>
+          <div class="reply-modal-tweet-text" style="margin-left: 60px;">${description}</div>
+          <span style="color: #6C757D; margin-left: 60px;">回覆給 <span style="color: #FF6600;">${userAccount}</span></span>
         </div>
       </div>
+      <div class="vertical-line"
+        style="position: absolute; border-left: 2px solid #B5B5BE; height: 50px; left: 30px; top: 58px; z-index: 0;"></div>
     </div>
       `
 
