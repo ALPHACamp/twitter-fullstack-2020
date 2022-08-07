cd29300b23a003536c79366a56cb83629ba37673
yaahsin <brillanteoboe@gmail.com>
2022-08-07 20:39:33 +0800

    set env port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const routes = require('./routes')
 const path = require('path')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
