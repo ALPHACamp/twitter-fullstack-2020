1633db78dae507ca62a18d6a7fda35f58714edc7
Elliot <elliotcs30@gmail.com>
2022-10-02 18:49:58 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const routes = require('./routes')
+const handlebars = require('express-handlebars')
+const helpers = require('./_helpers')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+
+// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+// 設定使用 Handlebars 做為樣板引擎
+app.set('view engine', 'hbs')
+
+app.use(routes);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
