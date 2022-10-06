68e0366c4191de3a3dd5afbbfaed2c784c36ba12
Kris <krischiang0301@gmail.com>
2022-10-06 21:51:54 +0800

    chore: remove assiciate comment

--- models/user.js
+++ models/user.js
@@ -10,7 +10,6 @@ module.exports = (sequelize, DataTypes) => {
      */
 
     static associate (models) {
-      define association here
       User.hasMany(models.Reply)
       User.hasMany(models.Tweet)
       User.hasMany(models.Like)
