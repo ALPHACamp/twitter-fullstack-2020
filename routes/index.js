3a1c9b2e9926e15c6c093e07feaf29c3ace7458c
Elliot <elliotcs30@gmail.com>
2022-10-06 14:44:44 +0800

    fix: modify routes

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const passport = require('../config/passport')
 
-const twitterController = require('../controllers/twitter-controller')
+const tweetController = require('../controllers/tweet-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
 
 router.use('/admin', admin)
 router.get('/users', userController.getUser)
@@ -14,14 +16,15 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/tweets', authenticated, tweetController.getTweets)
 router.get('/logout', userController.logout)
 
 router.get('/setting', userController.getSetting)
 router.get('/other', userController.getOtherPage)
 router.get('/modals/reply', userController.getReply)
-router.get('/modals/self', twitterController.getModalsTabs)
-router.get('/twitters', twitterController.getTwitters)
+router.get('/modals/self', tweetController.getModalsTabs)
 
-router.use('/', (req, res) => res.redirect('/twitters'))
+router.use('/', (req, res) => res.redirect('/tweets'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
