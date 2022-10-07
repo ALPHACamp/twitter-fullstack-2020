897e7e2fdc6ac75ef2ddf0fefe04e3433e8e89e4
Elliot <elliotcs30@gmail.com>
2022-10-07 09:51:23 +0800

    fix: user and admin singin function

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const passport = require('./config/passport')
-const routes = require('./routes')
+const helpers = require('./_helpers')
+const methodOverride = require('method-override')
 const handlebars = require('express-handlebars')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
-const flash = require('connect-flash')
-
 const session = require('express-session')
-
+const passport = require('./config/passport')
+const flash = require('connect-flash')
+const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -18,19 +18,21 @@ app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public')) // 設定共用檔案
+app.use(express.json())
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+
 app.use(flash())
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 error 訊息
-  res.locals.error_messages = req.flash('warning_messages') // 設定 warning 訊息
+  res.locals.warning_messages = req.flash('warning_messages') // 設定 warning 訊息
   next()
 })
-
-// 設定共用檔案
-app.use(express.static('public'))
 
 app.use(routes)
 
