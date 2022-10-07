a1b357055379d9a04b40d32f959f748092e96699
Elliot <elliotcs30@gmail.com>
2022-10-07 21:19:36 +0800

    Merge branch 'dev'

---
+++ seeders/20221006143036-users-seed-file.js
@@ -0,0 +1,32 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const userSeed = Array.from({ length: 20 }).map((_, i) => ({
+      name: `user${i + 1}`,
+      email: `user${i + 1}@email.com`,
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      account: `user${i + 1}`,
+      avatar: faker.image.cats(),
+      cover: `https://loremflickr.com/630/200/landscape/?random=${Math.random() * 20}`,
+      introduction: faker.lorem.text().substring(0, 140),
+      role: 'user',
+      created_at: new Date(),
+      updated_at: new Date()
+    }))
+    await queryInterface.bulkInsert('Users', [{
+      name: 'root',
+      email: 'root@email.com',
+      password: await bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      account: 'root',
+      role: 'admin',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, ...userSeed], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
---
+++ seeders/20221007042310-reply-seed-file.js
@@ -0,0 +1,37 @@
+'use strict'
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const user = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const tweet = await queryInterface.sequelize.query(
+      'SELECT id FROM Tweets;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    await queryInterface.bulkInsert('Replies',
+      Array.from({ length: 600 }).map((_, i) => ({
+        user_id: user[(i % 20)].id,
+        tweet_id: tweet[Math.floor(i / 3)].id,
+        comment: faker.lorem.sentences(1),
+        created_at: new Date(),
+        updated_at: new Date()
+
+      })), {})
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', null, {})
+  }
+}
