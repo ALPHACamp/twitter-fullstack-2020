9c0c004c30013cb0e71201467d0129295b4e1127
Kris <krischiang0301@gmail.com>
2022-10-10 14:49:22 +0800

    feat: admin tweets list fix layout and modify seed data

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,12 +4,13 @@ const passport = require('../../config/passport')
 const adminController = require('../../controllers/admin-controller')
 const { authenticatedAdmin } = require('../../middleware/auth')
 
+router.delete('/tweets/:id', authenticatedAdmin, adminController.deleteTweet)
+
 router.get('/signin', adminController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/admin/signin', failureFlash: true }), authenticatedAdmin, adminController.signIn)
 router.get('/logout', adminController.logout)
 
 router.get('/tweets', authenticatedAdmin, adminController.getTweets)
-router.delete('/tweets/:id', authenticatedAdmin, adminController.deleteTweet)
 router.get('/users', authenticatedAdmin, adminController.getUsers)
 
 module.exports = router
