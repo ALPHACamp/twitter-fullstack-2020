b64efbe5ea8b4415338f24361d6ebec6f46d283b
Kris <krischiang0301@gmail.com>
2022-10-14 16:23:30 +0800

    feature:add pagination in tweets page

--- helpers/pagination-helper.js
+++ helpers/pagination-helper.js
@@ -1,5 +1,5 @@
 const getOffset = (limit = 10, page = 1) => (page - 1) * limit
-const getPagination = (limit = 10, page = 1, total = 10000) => {
+const getPagination = (limit = 10, page = 1, total = 50) => {
   const totalPage = Math.ceil(total / limit)
   const pages = Array.from({ length: totalPage }, (_, index) => index + 1)
   const currentPage = page < 1 ? 1 : page > totalPage ? totalPage : page
