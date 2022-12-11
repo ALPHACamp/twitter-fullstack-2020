c9fbc76cc44e10aa814f67fa988746cdde2b5c07
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-12-11 23:36:17 +0800

    add simple someing for view

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const helpers = require('./_helpers');
+const handlebars = require('express-handlebars')
+const routes = require('./routes')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+const db = require('./models')
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(routes)
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.get('/twitter', twitterController.getTwitters)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
