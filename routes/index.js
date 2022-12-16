cb93a079af69583742247834890e0f02ecb6be32
Rae7771653 <nicole831220@gmail.com>
2022-12-17 00:32:40 +0800

    feat:delete topFollowers partial page and add following function

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const userController = require('../controllers/user-controller')
 const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
