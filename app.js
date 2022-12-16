1dc07e4d8b242fb270d24d01776139ee90c493b9
Leon-Chun <s1235753@gmail.com>
2022-12-17 01:09:04 +0800

    feat: add admin users page

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const pages = require('./routes')
 const helpers = require('./_helpers')
 
 const app = express()
@@ -6,8 +9,14 @@ const port = 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(pages)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -10,7 +10,34 @@ const adminController = {
   },
   deleteTweet: (req, res, next) => {
   },
-  getUsers: (req, res, next) => {
+  getUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({
+        attributes: { exclude: ['password'] },
+        include: [
+          { model: Tweet, include: [Like] },
+          { model: User, as: 'Followers' },
+          { model: User, as: 'Followings' }
+        ]
+      })
+      const results = users
+        .filter(user => user.role !== 'admin')
+        .map(user => ({
+          ...user.toJSON()
+        }))
+      results.forEach(r => {
+        r.TweetsCount = r.Tweets.length
+        r.FollowingsCount = r.Followings.length
+        r.FollowersCount = r.Followers.length
+        r.TweetsLikedCount = r.Tweets.reduce((num, tweet) => {
+          return num + tweet.Likes.length
+        }, 0)
+      })
+      results.sort((a, b) => b.TweetsCount - a.TweetsCount)
+      return res.render('admin/users', { users: results })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
