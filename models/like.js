1e544a3075e956b651d3558d4565e78d9d09eadf
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-12-17 12:57:08 +0800

    feat : update model like

--- models/like.js
+++ models/like.js
@@ -1,6 +1,5 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-<<<<<<< HEAD
   class Like extends Model {
     static associate(models) {
       Like.belongsTo(models.User, { foreignKey: 'UserId'})
@@ -14,7 +13,6 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
       type: DataTypes.INTEGER
     },
-    isLike: DataTypes.BOOLEAN,
     UserId: DataTypes.INTEGER,
     TweetId: DataTypes.INTEGER,
     isLike: DataTypes.BOOLEAN
@@ -23,11 +21,5 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Like',
     tableName: 'Likes'
   })
-=======
-  const Like = sequelize.define('Like', {
-  }, {})
-  Like.associate = function (models) {
-  }
->>>>>>> 6e335317cb2566ef50768591a6f7ea99269dd2fd
   return Like
 }
