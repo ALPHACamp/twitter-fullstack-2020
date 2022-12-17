d525630315b023850c557a7b6322b206f22556dc
馬君 <87924804+Leon-Chun@users.noreply.github.com>
2022-12-17 13:11:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const exphbs = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
-const handlebars = require('express-handlebars')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
-const pages = require('./routes')
 const helpers = require('./_helpers')
 const routes = require('./routes')
 
@@ -13,7 +11,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' , helpers: handlebarsHelpers}))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
@@ -21,8 +19,6 @@ app.use(express.static('public'))
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
-app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
-app.set('view engine', 'hbs')
 
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
@@ -34,7 +30,6 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(pages)
 app.use(routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
