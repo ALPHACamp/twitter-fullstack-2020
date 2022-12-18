d58eed69765377bda565cc4d21bbcc31696745bc
Tzuminn <valuesu0326@gmail.com>
2022-12-18 17:24:16 +0800

    feat: add edit user setting function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcryptjs')
 const { Followship, Like, Reply, Tweet, User } = require('../models')
+const helpers = require('../_helpers')
+const { Op } = require('sequelize')
+
 const userController = {
   signUpPage: (req, res, next) => {
     res.render('signup')
@@ -27,11 +30,8 @@ const userController = {
           email,
           password: hash
         })
-          .then(() => {
-            req.flash('success_messages', '成功註冊帳號！')
-            res.redirect('/signin')
-          })
-          .catch(err => next(err))
+        req.flash('success_messages', '成功註冊帳號！')
+        res.redirect('/signin')
       }
     } catch (err) { next(err) }
   },
@@ -48,10 +48,57 @@ const userController = {
     res.redirect('/signin')
   },
   getSetting: (req, res, next) => {
+    return User.findByPk(helpers.getUser(req).id)
+      .then(currentUser => {
+        currentUser = currentUser.toJSON()
+        const { account, name, email } = currentUser
+        return res.render('settings', { account, name, email })
+      })
   },
-  putSetting: (req, res, next) => {
+  putSetting: async (req, res, next) => {
+    try {
+      const { account, name, email, password, checkPassword } = req.body
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(password, salt)
+      if (!account || !name || !email || !password || !checkPassword) {
+        throw new Error('所有資料都是必填!')
+      } else if (name.length >= 50) {
+        throw new Error('姓名不得超過50字!')
+      } else if (password !== checkPassword) {
+        throw new Error('密碼和確認密碼不一致!')
+      }
+      const theUser = helpers.getUser(req)
+      const userAccount = await User.findOne({
+        where: {
+          [Op.and]: [
+            { account },
+            { account: { [Op.notLike]: theUser.account } }
+          ]
+        }
+      })
+      if (userAccount) throw new Error('account 已重複註冊！')
+      const userEmail = await User.findOne({
+        where: {
+          [Op.and]: [
+            { email },
+            { email: { [Op.notLike]: theUser.email } }
+          ]
+        }
+      })
+      if (userEmail) throw new Error('email 已重複註冊！')
+      const user = await User.findByPk(theUser.id)
+      await user.update({
+        account,
+        name,
+        email,
+        password: hash
+      })
+      req.flash('success_messages', '成功修改帳戶設定！')
+      res.redirect('/tweets')
+    } catch (err) { next(err) }
   },
   getUserTweets: (req, res, next) => {
+
   },
   getUserReplies: (req, res, next) => {
   },
