7b706c4d89c540a719f415610bcd9114da3f9df0
linway035 <wayneck0138@gmail.com>
2022-12-19 19:25:30 +0800

    feat: add index, modify sidebars and tabs

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,9 +1,52 @@
 const { Followship, Like, Reply, Tweet, User } = require('../models')
+const helpers = require('../_helpers')
 const tweetController = {
   getIndex: (req, res, next) => {
-    res.render('tweets')
+    return Promise.all([
+      Tweet.findAll({
+        include: [{ model: User }],
+        where: { UserId: helpers.getUser(req).id },
+        order: [['createdAt', 'desc']],
+        raw: true,
+        nest: true
+      }),
+      User.findAll({
+        include: [{ model: User, as: 'Followers' }],
+        where: { role: 'user' } // id: !req.user.id,待補
+      })
+    ])
+      .then(([tweets, users]) => {
+        const result = users
+          .map(user => ({
+            ...user.toJSON(),
+            followerCount: user.Followers.length
+            // isFollowed: req.user.Followings.some(f => f.id === user.id) //req.user還未設定、root不該出現
+          }))
+          .sort((a, b) => b.followerCount - a.followerCount)
+        res.render('tweets', { tweets, users: result.slice(0, 10) })
+      })
   },
-  postTweet: (req, res, next) => {
+  postTweet: async (req, res, next) => {
+    try {
+      const { description } = req.body
+      if (description.length > 140) {
+        throw new Error('請以 140 字以內為限')
+      } else if (description.trim() === '') {
+        throw new Error('內容不可空白')
+      }
+      const UserId = helpers.getUser(req).id
+      const createdTweet = await Tweet.create({
+        UserId,
+        description
+      })
+      if (!createdTweet) {
+        throw new Error('發推失敗')
+      }
+      req.flash('success_messages', '發推成功！')
+      return res.redirect('back')
+    } catch (err) {
+      next(err)
+    }
   },
   getTweet: (req, res, next) => {
   },
--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -10,5 +10,8 @@ module.exports = {
   relativeTimeFromNow: a => dayjs(a).fromNow(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
+  },
+  ifNotCond: function (a, b, options) {
+    return a === b ? options.inverse(this) : options.fn(this)
   }
 }
