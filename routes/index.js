8fc5b5a2bac17562151e11021c506fe0da6fbfa5
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-12-21 22:23:43 +0800

    feat : create edit-page

--- routes/index.js
+++ routes/index.js
@@ -9,27 +9,36 @@ const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
+
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+
 router.get('/logout', userController.logout)
+
 // router.get('/users/:id', authenticated, userController.getTweets)
 router.get('/users/:id/followings', authenticated, userController.getFollowings)
 router.get('/users/:id/followers', authenticated, userController.getFollowers)
 // router.get('/users/:id/tweets', authenticated, userController.getTweets)
 // router.put('/users/:id', authenticated, userController.putUser)
 router.get('/users/:id', authenticated, userController.getTweets)
+
 router.get('/tweets/:id/replies', authenticated, tweetController.getReplies)
 router.post('/tweets/:id/replies', authenticated, tweetController.createReply)
 router.post('/tweets/:id/like', authenticated, tweetController.addLike)
 router.post('/tweets/:id/unlike', authenticated, tweetController.removeLike)
-// router.get('/edit', authenticated, userController.editUserPage)
-// router.put('/edit', authenticated, userController.editUser)
+
+router.get('/edit', authenticated, userController.editUserPage)
+router.put('/edit', authenticated, userController.editUser)
+
 router.post('/followships', authenticated, userController.addFollowship)
 router.delete('/followships/:id', authenticated, userController.removeFollowship)
+
 router.post('/tweets', authenticated, tweetController.postTweet)
 router.get('/tweets', authenticated, tweetController.getTweets)
+
 router.use('/', (req, res) => res.redirect('/tweets'))
 router.use('/', generalErrorHandler)
 
