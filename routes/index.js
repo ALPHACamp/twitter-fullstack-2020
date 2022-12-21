f3a2ece3c0a83b848d11385f1435aef45b453b49
lomo007 <eragon66997@gmail.com>
2022-12-21 20:43:57 +0800

    feat: temp-mixed

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,6 @@ router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
