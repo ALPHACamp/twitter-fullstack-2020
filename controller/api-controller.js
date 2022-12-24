d4d94e2e414b54546eb218339be30aaed8a35dbd
weitungstyle <114373709+weitungstyle@users.noreply.github.com>
2022-12-24 21:47:20 +0800

    Victor/fixbug 3 (#36)
    * fix: warning message, getUserLikes: order by likes createdAt
    
    * fix test error item
    
    * add profile limit & fix isFollowed problem

--- controller/api-controller.js
+++ controller/api-controller.js
@@ -22,18 +22,20 @@ const apiController = {
     const { files } = req
     if (editUserId !== loginUserId) throw new Error("You can't edit other's profile!")
     if (!name.trim()) throw new Error('User name is required!')
+    if (name.length > 50) throw new Error('暱稱字數超過上限!')
+    if (introduction?.length > 160) throw new Error('自我介紹字數超過上限!')
     return Promise.all([
       User.findByPk(editUserId),
       imgurFileHandler(files?.avatar && files.avatar[0]),
       imgurFileHandler(files?.coverImage && files.coverImage[0])
     ])
-      .then(([user, avatar, imageCover]) => {
+      .then(([user, avatar, coverImage]) => {
         if (!user) throw new Error("User didn't exist!")
         return user.update({
           name,
           introduction,
           avatar: avatar || user.avatar,
-          cover: imageCover || user.cover,
+          cover: coverImage || user.cover,
         })
           .then(() => res.redirect(200, 'back'))
           .catch(err => next(err))
