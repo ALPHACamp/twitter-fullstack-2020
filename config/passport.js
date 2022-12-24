a5b58013e41d2913881951b0016f214bf4f1fd76
Victor Cheng <weitungstyle@gmail.com>
2022-12-24 11:43:09 +0800

    fix: warning message, getUserLikes: order by likes createdAt

--- config/passport.js
+++ config/passport.js
@@ -16,10 +16,10 @@ passport.use(new LocalStrategy(
   (req, account, password, cb) => {
     User.findOne({ where: { account } })
       .then(user => {
-        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號不存在或密碼輸入錯誤！'))
         bcrypt.compare(password, user.password)
           .then(res => {
-            if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+            if (!res) return cb(null, false, req.flash('error_messages', '帳號不存在或密碼輸入錯誤！'))
             return cb(null, user)
           })
       })
