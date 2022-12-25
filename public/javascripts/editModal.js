0315e28fc6b6c312366c98ce7fb758c16e424e85
Tzuminn <valuesu0326@gmail.com>
2022-12-25 15:14:01 +0800

    feat: modify views and fix editModal

--- public/javascripts/editModal.js
+++ public/javascripts/editModal.js
@@ -24,7 +24,10 @@ editButton.addEventListener('click', function (event) {
 
 // 儲存modal資訊，刷新頁面
 profileModalForm.addEventListener('submit', function (event) {
-  event.preventDefault()
+  if (!profileModalForm.checkValidity()) {
+    event.preventDefault()
+    event.stopPropagation()
+  }
   const target = event.target
   const UserId = target.dataset.id
   const formData = new FormData()
@@ -41,4 +44,35 @@ profileModalForm.addEventListener('submit', function (event) {
       history.go(0)
     })
     .catch(err => console.log(err))
+})
+
+// 表單驗證
+inputName.addEventListener('input', function (event) {
+  const errName = document.querySelector('#err-name')
+  const nameTotal = inputName.value.trim().length
+  document.getElementById('text-count').innerHTML = nameTotal + '/50'
+  if (nameTotal >= 50) {
+    inputName.classList.add('disabled')
+    document.getElementById('err-name').innerHTML = '字數超出上限!'
+    errName.classList.remove('err-sec')
+  } else if (nameTotal === 0) {
+    document.getElementById('err-name').innerHTML = '名稱為必填'
+    errName.classList.remove('err-sec')
+  } else if (nameTotal < 50 || nameTotal >= 1) {
+    inputName.classList.remove('disabled')
+    errName.classList.add('err-sec')
+  }
+})
+
+inputIntroduction.addEventListener('input', function (event) {
+  const errIntro = document.querySelector('#err-intro')
+  const introductionTotal = document.getElementById('introduction').value.trim().length
+  document.getElementById('text-count-info').innerHTML = introductionTotal + '/160'
+  if (introductionTotal >= 160) {
+    inputIntroduction.classList.add('disabled')
+    errIntro.classList.remove('err-sec')
+  } else {
+    inputIntroduction.classList.remove('disabled')
+    errIntro.classList.add('err-sec')
+  }
 })
