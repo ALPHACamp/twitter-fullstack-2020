cd3edb4b3e3d547bbf9df66f46473eb6a0af8127
Leon180 <lioung860302@gmail.com>
2023-03-19 15:49:41 +0800

    feat: initialize models and seeders

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const helpers = require('./_helpers');
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
--- models/index.js
+++ models/index.js
@@ -3,24 +3,30 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+// eslint-disable-next-line no-undef
 const basename = path.basename(__filename);
+// eslint-disable-next-line no-undef
 const env = process.env.NODE_ENV || 'development';
+// eslint-disable-next-line no-undef
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  // eslint-disable-next-line no-undef
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
+  // eslint-disable-next-line no-undef
   .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
+    // eslint-disable-next-line no-undef
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
