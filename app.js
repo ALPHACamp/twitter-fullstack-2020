2ab0c160facd4efdf4bdd856eb80c8bdbf105f46
DESKTOP-NE7KTJ6\zell0 <zell0323@hotmail.com>
2023-03-20 09:58:05 +0800

    first commit

--- app.js
+++ app.js
@@ -4,14 +4,28 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const helpers = require('./_helpers');
-
+const handlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+app.engine('hbs', handlebars({
+  extname: '.hbs',
+  partialsDir: ['views/partials','views/partials/svgs']
+}));
+app.set('view engine', 'hbs')
+app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => res.render('home'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
