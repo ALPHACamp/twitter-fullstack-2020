b652a0deccd279bcb03540627365e0ec808ee934
Ellen <ddhkxxhxx@hotmail.com>
2023-03-24 21:43:01 +0800

    add getTweets & deleteTweet function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const routes = require('./routes')
 const methodOverride = require('method-override')
 const helpers = require('./_helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,7 +11,7 @@ const port = process.env.PORT || 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true })) // body-Parser
 app.use(methodOverride('_method')) // methodOverride
--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -6,8 +6,21 @@ const adminController = {
     res.render('admin/signin')
   },
   getTweets: (req, res) => { // 後台取得推文清單
-    res.render('admin/tweets')
-  },
+    return Tweet.findAll({
+      include: User,
+      nest: true,
+      raw: true
+    }).then((tweets) => {
+      const data = tweets.map(t => ({
+        ...t,
+        description: t.description.substring(0, 50)
+      }))
+      console.log(data)
+      return res.render('admin/tweets', { tweets: data })
+    })
+      .catch(err => console.log(err))
+  }
+,
   getUsers: (req, res) => { // 後台取得使用者列表
     return User.findAll({
       include: [
@@ -20,7 +33,7 @@ const adminController = {
       .then((users) => {
         users = users.map(user => ({
           ...user.toJSON(),
-          // 計算追蹤者人數
+          // 計算追蹤人數
           followerCount: user.Followers.length,
           followingCount: user.Followings.length,
           likeCount: user.LikedTweets.length,
@@ -29,8 +42,16 @@ const adminController = {
         return res.render('admin/users', { users })
       })
       .catch(err => console.log(err))
-  }
-,
+  },
+  deleteTweet: (req, res) => { // 後台刪除tweet
+    return Tweet.findByPk(req.params.id)
+      .then(tweet => {
+        if (!tweet) throw new Error("Tweet didn't exist!")
+        return tweet.destroy()
+      })
+      .then(() => res.redirect('/admin/tweets'))
+      .catch(err => console.log(err))
+  },
   logout: (req, res) => { // 後台登出
     res.redirect('signin')
   }
