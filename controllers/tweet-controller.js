a8281cd5c579ed287a268bc5e9f8f82b88ded49d
chachagof <chacha0932@gmail.com>
2023-03-24 17:44:50 +0800

    feat:modify tweet controller

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,9 +1,36 @@
+const { User, Tweet, Reply } = require('../models')
+
 const tweetController = {
-  getTweets:(req,res,next)=>{ // 進入推文清單
-    res.render('tweets')
+  getTweets: (req, res, next) => { // 進入推文清單
+    const id = 1
+    return Promise.all([
+      User.findByPk(id),
+      Tweet.findAll({
+        include: User,
+        raw: true,
+        nest: true,
+        order: [['createdAt', "desc"]],
+        limit: 5
+      })
+    ])
+      .then(([user, tweets]) => {
+        res.render('tweets', { user: user.toJSON(), tweets })
+      })
+      .catch(err => console.log(err))
   },
-  getTweet:(req,res,next)=>{ // 登入使用者頁面顯示個人推文清單
-    res.render('users')
+  getTweet: (req, res, next) => { // 登入使用者頁面顯示個人推文清單
+    const { id } = req.params
+    return Tweet.findByPk(id, {
+      include: [
+        User,
+        { model: Reply, include: User }
+      ]
+    })
+      .then(tweet => {
+        tweet = tweet.toJSON()
+        res.render('tweet', { tweet })
+      })
+      .catch(err => console.log(err))
   }
 }
 
