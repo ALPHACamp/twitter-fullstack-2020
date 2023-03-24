e0a7f23a2d9137f9b9fe6dbb1e8443ac993fd80b
Ellen <ddhkxxhxx@hotmail.com>
2023-03-24 20:23:51 +0800

    add getUsers at admin-controller

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,3 +1,6 @@
+const { UCS2_PERSIAN_CI } = require('mysql2/lib/constants/charsets')
+const { User, Tweet, Followship, Like } = require('../models')
+
 const adminController = {
   signInPage: (req, res) => { // 後台登入
     res.render('admin/signin')
@@ -6,8 +9,28 @@ const adminController = {
     res.render('admin/tweets')
   },
   getUsers: (req, res) => { // 後台取得使用者列表
-    res.render('admin/users')
-  },
+    return User.findAll({
+      include: [
+        { model: User, as: 'Followings' },
+        { model: User, as: 'Followers' },
+        { model: Tweet, as: 'LikedTweets'},
+        Tweet
+      ]
+    })
+      .then((users) => {
+        users = users.map(user => ({
+          ...user.toJSON(),
+          // 計算追蹤者人數
+          followerCount: user.Followers.length,
+          followingCount: user.Followings.length,
+          likeCount: user.LikedTweets.length,
+          tweetCount: user.Tweets.length
+        }))
+        return res.render('admin/users', { users })
+      })
+      .catch(err => console.log(err))
+  }
+,
   logout: (req, res) => { // 後台登出
     res.redirect('signin')
   }
