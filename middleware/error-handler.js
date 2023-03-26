ddf4967f4bb7f0df4e8b9a1a667952a9484b8ccf
chachagof <chacha0932@gmail.com>
2023-03-26 21:42:23 +0800

    feat:modify middleware error-handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,9 @@
 module.exports = {
   generalErrorHandler(err, req, res, next) {
     if (err instanceof Error) {
-      req.flash('wrong_messages', `${err.name}: ${err.message}`)
+      req.flash('error_messages', `${err.name}: ${err.message}`)
     } else {
-      req.flash('wrong_messages', `${err}`)
+      req.flash('error_messages', `${err}`)
     }
     res.redirect('back')
     next(err)
