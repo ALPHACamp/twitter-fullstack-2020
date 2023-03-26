4ff73a51a34fdf994f603a6c1317b5696374dae2
KeYunTinG <zaac2240632@yahoo.com.tw>
2023-03-26 22:59:23 +0800

    add passport  admin/signin

--- routes/modules/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const adminController = require('../../controllers/admin-controller')
-
-router.get('/users', adminController.getUsers)
-router.delete('/tweets/:tweetId', adminController.deleteTweet)
-router.get('/tweets', adminController.getTweets);
-
-module.exports = router
