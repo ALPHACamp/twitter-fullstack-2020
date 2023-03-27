e5d49c5e11d77fd42665cf4368e6a76032c47b75
KeYunTinG <115207806+KeYunTinG@users.noreply.github.com>
2023-03-27 09:45:46 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -16,5 +16,7 @@ router.get('/signin', userController.login_page)
 router.get('/tweets/id/replies', replyController.getReplies) //測試畫面用
 router.get('/tweets', tweetController.getTweets)
 router.get('/users/1/tweets', userController.getUser)
+router.get('/users/1/followers', userController.getFollowers)
+router.get('/users/1/followings', userController.getFollowings)
 
 module.exports = router
