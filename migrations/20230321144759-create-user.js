0c39949a56fc2b02bc8ea5876040b0ed1605d2bc
robert1074004 <abc50113@yahoo.com.tw>
2023-03-30 19:58:58 +0800

    feat: modify user's email length

--- migrations/20230321144759-create-user.js
+++ migrations/20230321144759-create-user.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       email: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING(20),
         unique: true,
         allowNull: true,
       },
