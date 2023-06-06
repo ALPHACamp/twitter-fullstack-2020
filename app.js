1278423b2f40ba4789db88ded3a837fbe75f3701
jamiennn <jasper07165@hotmail.com>
2023-06-06 11:03:36 +0800

    add errorHandler & messages partial

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const helpers = require('./_helpers');
 const exphbs = require('express-handlebars')
 const flash = require('connect-flash')
+const session = require('express-session')
 
+const router = require('./routes')
 const app = express()
 const port = 3000
 
@@ -14,19 +16,19 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+app.use(session({ secret: 'SECRET', resave: false, saveUninitialized: false }))
 
-app.get('/', (req, res) => res.render('example'))
-app.get('/signup', (req, res) => {
-  req.flash('danger_msg', 'error')
-  res.render('signup')
-})
-
-// Setting flash
+// Setting middleware
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.danger_msg = req.flash('danger_msg')
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.info_msg = req.flash('info_msg')
   next()
 })
+
+app.use(router)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
