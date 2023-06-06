21661371172b50a4ca2483f4c125930f92e6af43
jamiennn <jasper07165@hotmail.com>
2023-06-06 09:09:29 +0800

    setting handlebars & bootstrap

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const helpers = require('./_helpers');
+const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
 
 const app = express()
 const port = 3000
@@ -7,7 +9,25 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('example'))
+app.get('/signup', (req, res) => {
+  req.flash('danger_msg', 'error')
+  res.render('signup')
+})
+
+// Setting flash
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.danger_msg = req.flash('danger_msg')
+  next()
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
