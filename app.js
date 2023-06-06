3bb4be064992b94b1d8145e0c616b70351ba9943
LongXiangL <bfh1104@gmail.com>
2023-06-06 12:40:32 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const routes = require('./routes')
+const handlebars = require('express-handlebars')
 const helpers = require('./_helpers');
 
 const app = express()
@@ -6,8 +8,13 @@ const port = 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+app.engine('hbs', handlebars({ extname: '.hbs' }))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.set('view engine', 'hbs')
+app.use(routes)
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
