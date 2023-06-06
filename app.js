3aa0c2025152b709cfd458f657fb6bcc3ba07449
LongXiangL <bfh1104@gmail.com>
2023-06-06 14:39:23 +0800

    設定並串接 Sequelize

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
 
 const app = express()
-const port = 3000
+const port = process.env.Port || 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 
-
 app.set('view engine', 'hbs')
 app.use(routes)
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const tweetController = require('../controllers/tweet-controller') 
+const tweetController = require('../controllers/tweet-controller')
 const admin = require('./modules/admin')
 
-router.use('/admin', admin) 
+router.use('/admin', admin)
 
 router.get('/tweets', tweetController.getTweets)
 router.use('/', (req, res) => res.redirect('/tweets'))
