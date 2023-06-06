ec4d01b0fbc8696b97b4a9c64eaff04a50e650da
LongXiangL <bfh1104@gmail.com>
2023-06-06 16:03:29 +0800

    flash msg

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
 const helpers = require('./_helpers')
+const flash = require('connect-flash')
+const session = require('express-session')
+const SESSION_SECRET = 'secret'
 
 const app = express()
 const port = process.env.Port || 3000
@@ -11,6 +14,14 @@ const port = process.env.Port || 3000
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const tweetController = require('../controllers/tweet-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
 
 router.get('/tweets', tweetController.getTweets)
 router.use('/', (req, res) => res.redirect('/tweets'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
