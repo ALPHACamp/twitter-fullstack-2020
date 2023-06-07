8281e6d1d593efa9537857707dfbf1952a5468f7
HuangYiLun <wk5018102@gmail.com>
2023-06-07 10:57:09 +0800

    branch develop init

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const path = require('path')
 const express = require('express')
-const helpers = require('./_helpers');
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const flash = require('connect-flash')
+const session = require('express-session')
+
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
+app.engine('hbs', exphbs({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
+app.use(routes)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
--- migrations/20230606150143-create-user.js
+++ migrations/20230606150143-create-user.js
@@ -1,12 +1,18 @@
-'use strict';
+'use strict'
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      account: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
       },
       email: {
         type: Sequelize.STRING
@@ -14,29 +20,29 @@ module.exports = {
       password: {
         type: Sequelize.STRING
       },
-      name: {
-        type: Sequelize.STRING
-      },
       avatar: {
         type: Sequelize.STRING
       },
-      introduction: {
-        type: Sequelize.TEXT
-      },
-      role: {
+      cover: {
         type: Sequelize.STRING
       },
-      createdAt: {
+      introduction: {
+        type: Sequelize.STRING
+      },
+      is_admin: {
+        type: Sequelize.BOOLEAN
+      },
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users')
   }
-};
+}
