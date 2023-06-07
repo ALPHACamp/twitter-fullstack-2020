0c68b3e850bb15c65d7f56e29cb5125499a6eba3
Suz033 <susanamy2324@gmail.com>
2023-06-07 20:13:17 +0800

    feat: add likes seeder

---
+++ seeders/20230607115741-likes-seed-file.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const tweets = await queryInterface.sequelize.query(
+      'SELECT id FROM Tweets;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    await queryInterface.bulkInsert('Replies',
+      Array.from({ length: 50 }, () => ({
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+        tweet_id: tweets[Math.floor(Math.random() * tweets.length)].id,
+        created_at: new Date(),
+        updated_at: new Date()
+      }))
+    )
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', {})
+  }
+}
