820802a88b0a3a694d4b941a0e48cf045c7f0b03
goodjobhot401 <goodjobhot@gmail.com>
2023-06-07 18:45:49 +0800

    feat: adjust adminController getUsers

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,10 +1,19 @@
+const { User } = require('../models')
+
 const adminController = {
   getTweets: (req, res, next) => {
     res.render('admin/admin-tweets')
   },
 
   getUsers: (req, res, next) => {
-    res.render('admin/admin-users')
+    return User.findAll({
+      raw: true,
+      nest: true
+    })
+      .then(users => {
+        res.render('admin/admin-users', { users })
+      })
+      .catch(err => next(err))
   }
 }
 
