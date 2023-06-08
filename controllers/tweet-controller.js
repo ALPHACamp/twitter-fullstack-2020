25866a34fa6c179a6b2eea40a9d5613df18d27d9
rr37 <ay870527@gmail.com>
2023-06-08 17:32:34 +0800

    feat: add UI due to signIn function added

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,7 +1,10 @@
 const { Tweet, User } = require('../models')
+const { getUser } = require('../helpers/_helpers')
 
 const tweetController = {
+  // 獲得所有貼文
   getTweets: (req, res, next) => {
+    const userId = getUser(req).id
     Promise.all([
       Tweet.findAll({
         // limit: 2,
@@ -16,21 +19,23 @@ const tweetController = {
         // limit: 10,
         where: { isAdmin: 0 },
         raw: true
-      })
+      }),
+      User.findByPk(userId, { raw: true })
     ])
-      .then(([tweets, users]) => {
-        // console.log(users)
-        res.render('tweets', { tweets, users })
+      .then(([tweets, users, currentUser]) => {
+        console.log(currentUser)
+        res.render('tweets', { tweets, users, currentUser })
       })
       .catch(err => next(err))
   },
+  // 新增一則貼文
   postTweets: (req, res, next) => {
     const { description } = req.body
-    console.log(req.body)
+    const userId = getUser(req).id
     if (!description) throw new Error('Tweet is required!')
     if (description.length > 140) throw new Error('Tweet length must be under 140 character!')
     Promise.all([
-      Tweet.create({ description })
+      Tweet.create({ description, userId })
     ])
       .then(() => {
         res.redirect('/tweets')
--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const tweetController = require('../../controllers/tweet-controller')
 
+// 新增一則 tweet
+router.post('/', tweetController.postTweets)
+
 // 首頁 取得tweets
 router.get('/', tweetController.getTweets)
 
