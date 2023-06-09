e4f147a8f2213f7b8cb0385a9a65414e233ab23b
jamiennn <jasper07165@hotmail.com>
2023-06-09 16:39:09 +0800

    add user likes page and route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,5 +4,6 @@ const userController = require('../../controllers/user-controller')
 
 router.get('/:uid/tweets', userController.getUserTweets)
 router.get('/:uid/replies', userController.getUserReplies)
+router.get('/:uid/likes', userController.getUserLikes)
 
 module.exports = router
