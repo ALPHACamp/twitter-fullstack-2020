510f1c79d6a50b0d85f580b031b55f4ebbc7424e
Suz033 <susanamy2324@gmail.com>
2023-06-09 16:00:39 +0800

    refactor: user seeder

--- seeders/20230606163450-users-seed-file.js
+++ seeders/20230606163450-users-seed-file.js
@@ -1,101 +1,46 @@
 'use strict'
-const bcrypt = require('bcrypt-nodejs')
+
+const bcrypt = require('bcryptjs')
 const faker = require('faker')
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [
-      {
-        email: 'root@example.com',
-        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
-        account: 'root',
-        name: 'root',
-        role: 'admin',
-        created_at: new Date(),
-        updated_at: new Date()
-      },
-      {
-        email: 'user1@example.com',
-        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
-        account: 'user1',
-        name: 'user1',
+    const users = []
+
+    // admin
+    const admin = {
+      email: 'root@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      account: 'root',
+      name: 'root',
+      role: 'admin',
+      created_at: new Date(),
+      updated_at: new Date()
+    }
+    users.push(admin)
+
+    // users
+    const userPromises = Array.from({ length: 5 }).map(async (_, index) => {
+      const user = {
+        email: `user${index + 1}@example.com`,
+        password: await bcrypt.hash('12345678', 10),
+        account: `user${index + 1}`,
+        name: `user${index + 1}`,
         role: 'user',
-        avatar: `https://loremflickr.com/140/140/avatar/?lock=${
-          Math.random() * 100
-        }`,
-        cover: `https://loremflickr.com/640/200/landscape/?lock=${
-          Math.random() * 100
-        }`,
-        introduction: faker.lorem.words(({ min: 1, max: 3 })),
-        created_at: new Date(),
-        updated_at: new Date()
-      },
-      {
-        email: 'user2@example.com',
-        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
-        account: 'user2',
-        name: faker.name.findName(),
-        role: 'user',
-        avatar: `https://loremflickr.com/140/140/avatar/?lock=${
-          Math.random() * 100
-        }`,
-        cover: `https://loremflickr.com/640/200/landscape/?lock=${
-          Math.random() * 100
-        }`,
-        introduction: faker.lorem.words(({ min: 1, max: 3 })),
-        created_at: new Date(),
-        updated_at: new Date()
-      },
-      {
-        email: 'user3@example.com',
-        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
-        account: 'user3',
-        name: faker.name.findName(),
-        role: 'user',
-        avatar: `https://loremflickr.com/140/140/avatar/?lock=${
-          Math.random() * 100
-        }`,
-        cover: `https://loremflickr.com/640/200/landscape/?lock=${
-          Math.random() * 100
-        }`,
-        introduction: faker.lorem.words(({ min: 1, max: 3 })),
-        created_at: new Date(),
-        updated_at: new Date()
-      },
-      {
-        email: 'user4@example.com',
-        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
-        account: 'user4',
-        name: faker.name.findName(),
-        role: 'user',
-        avatar: `https://loremflickr.com/140/140/avatar/?lock=${
-          Math.random() * 100
-        }`,
-        cover: `https://loremflickr.com/640/200/landscape/?lock=${
-          Math.random() * 100
-        }`,
-        introduction: faker.lorem.words(({ min: 1, max: 3 })),
-        created_at: new Date(),
-        updated_at: new Date()
-      },
-      {
-        email: 'user5@example.com',
-        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
-        account: 'user5',
-        name: faker.name.findName(),
-        role: 'user',
-        avatar: `https://loremflickr.com/140/140/avatar/?lock=${
-          Math.random() * 100
-        }`,
-        cover: `https://loremflickr.com/640/200/landscape/?lock=${
-          Math.random() * 100
-        }`,
-        introduction: faker.lorem.words(({ min: 1, max: 3 })),
+        avatar: `https://loremflickr.com/140/140/avatar/?lock=${Math.random() * 100}`,
+        cover: `https://loremflickr.com/640/200/landscape/?lock=${Math.random() * 100}`,
+        introduction: faker.lorem.words(10),
         created_at: new Date(),
         updated_at: new Date()
       }
-    ])
-  },
+      return user
+    })
 
+    const user = await Promise.all(userPromises)
+    users.push(...user)
+
+    return queryInterface.bulkInsert('Users', users, {})
+  },
   down: async (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Users', null, {})
   }
