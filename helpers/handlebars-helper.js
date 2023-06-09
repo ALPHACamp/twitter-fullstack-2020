1214c76f27f4ddbddad13ee047069265a19129dd
sherryylin0719 <sherryylin0719@gmail.com>
2023-06-09 09:54:27 +0900

    feat: modify to prevent future conflict

--- helpers/handlebars-helper.js
+++ helpers/handlebars-helper.js
@@ -1,9 +1,12 @@
 const dayjs = require('dayjs')
 const relativeTime = require('dayjs/plugin/relativeTime')
-
-dayjs.extend(relativeTime)
+require('dayjs/locale/zh-tw')
+dayjs.extend(relativeTime).locale('zh-tw')
 
 module.exports = {
   currentYear: () => dayjs().year(),
-  relativeTimeFromNow: a => dayjs(a).fromNow(),
+  relativeTimeFromNow: a => dayjs(a).fromNow().slice(0, -1),
+  ifCond: (a, b, options) => {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
--- middleware/auth.js
+++ middleware/auth.js
@@ -2,11 +2,11 @@ const helpers = require('../_helpers')
 
 module.exports = {
   authenticated: (req, res, next) => {
-    if (helpers.ensureAuthenticated(req) && (helpers.getUser(req).role === 'user')) next()
+    if (helpers.ensureAuthenticated(req) && (helpers.getUser(req).role === 'user')) return next()
     return res.redirect('/signin')
   },
-  authenticatedAdmin: (rea, res, next) => {
-    if (helpers.ensureAuthenticated(req) && (helpers.getUser(req).role === 'admin')) next()
+  authenticatedAdmin: (req, res, next) => {
+    if (helpers.ensureAuthenticated(req) && (helpers.getUser(req).role === 'admin')) return next()
     return res.redirect('/admin/signin')
   }
 }
