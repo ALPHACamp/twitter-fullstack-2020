ba4f1f8a116d880060e404a8ea2c3f508a020080
Wei <awei1127@hotmail.com>
2023-06-10 01:49:07 +0800

    admin cannot use non-admin routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const helpers = require('../_helpers')
 module.exports = {
   authenticator: (req, res, next) => {
     // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
-    if (helpers.ensureAuthenticated(req)) {
+    if (helpers.ensureAuthenticated(req) && !helpers.getUser(req).isAdmin) {
       return next()
     }
     req.flash('warning_messages', '請先登入才能使用。')
