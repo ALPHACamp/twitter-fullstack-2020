16d3ab2113ede234c1c5d44e44c016dd92496985
eunicebibi <agjay410@gmail.com>
2023-06-11 06:09:27 +0800

    feat: add tweet.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-const admin = require('./modules/admin')
-const { generalErrorHandler } = require('../middleware/error-handler')
-const tweetController = require('../controllers/tweet-controller')
+// const admin = require('./modules/admin')
+// const { generalErrorHandler } = require('../middleware/error-handler')
+// const tweetController = require('../controllers/tweet-controller')
 
-router.use('/admin', admin)
+// router.use('/admin', admin)
 
-router.get('/tweets/:id/replies', tweetController.getTweet)
-router.get('/tweets', tweetController.getTweets) // 顯示全部推文
-router.post('/tweets', tweetController.createTweet) // 新增推文
+// router.get('/tweets/:id/replies', tweetController.getTweet)
+// router.get('/tweets', tweetController.getTweets) // 顯示全部推文
+// router.post('/tweets', tweetController.createTweet) // 新增推文
 
-router.get('/', (req, res) => res.render('tweets')) // 專案初始測試路由
-router.use('/', generalErrorHandler)
+// router.get('/', (req, res) => res.redirect('/tweets'))
+router.get('/', (req, res) => res.render('tweet')) // 專案初始測試路由
+// router.use('/', generalErrorHandler)
 
 module.exports = router
