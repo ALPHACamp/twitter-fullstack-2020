0d6bc2f7a40f9342a16cf44c32ab775e7a54e285
Wei <awei1127@hotmail.com>
2023-06-11 21:50:13 +0800

    add edit setting feature

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -109,6 +109,62 @@ const userController = {
       })
       .catch(err => next(err))
   },
+  // 取得自己的帳戶設定頁面
+  getSettingPage: (req, res, next) => {
+    const userId = req.params.id
+    // 檢查是不是自己本人
+    if (Number(userId) !== helpers.getUser(req).id) throw new Error('Permission denied.')
+    // 取得自己的帳戶資訊
+    User.findByPk(userId, { raw: true })
+      .then(user => {
+        if (!user) throw new Error('User did not exist.')
+        const { account, name, email } = user
+        res.render('setting', { account, name, email, userId })
+      })
+      .catch(err => next(err))
+  },
+  // 送出帳戶設定資訊
+  putSetting: (req, res, next) => {
+    const userId = req.params.id
+    const { account, name, email, password, confirmPassword } = req.body
+    const errors = []
+    // 檢查是不是自己本人
+    if (Number(userId) !== helpers.getUser(req).id) throw new Error('Permission denied.')
+
+    // 檢查註冊資訊是否正確 任一欄不得為空 密碼與確認密碼必須相同
+    if (!account || !name || !email || !password || !confirmPassword) {
+      errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmPassword) {
+      errors.push({ message: '密碼與確認密碼不一致。' })
+    }
+    if (errors.length) {
+      return res.render('setting', {
+        userId,
+        errors,
+        account,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    // 取得自己的帳戶資訊 同時生成密碼
+    Promise.all([
+      User.findByPk(userId),
+      bcrypt.hash(password, 10)
+    ])
+      .then(([user, hash]) => {
+        if (!user) throw new Error('User did not exist.')
+        // 更新到資料庫
+        return user.update({ account, name, email, password: hash })
+      })
+      .then(() => {
+        req.flash('success_messages', '帳戶設定完成。')
+        res.redirect(`/users/${userId}/setting`)
+      })
+      .catch(err => next(err))
+  },
   // 取得特定使用者頁面
   getUserPage: (req, res, next) => {
     const userId = req.params.id
