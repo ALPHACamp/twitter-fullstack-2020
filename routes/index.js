942f1c339055d751220e1b9d7426f989f49d9c70
Gary Lee <112674145+gary86442@users.noreply.github.com>
2023-06-11 17:53:56 +0800

    Ts- 48 (#12)
    * feat:add follow function
    
    * feat:add like function

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,25 @@ router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-router.use('/', authenticated, generalErrorHandler)
+router.post(
+  '/signin',
+  passport.authenticate('local', {
+    failureRedirect: '/signin',
+    failureFlash: true
+  }),
+  userController.signIn
+)
 
+//* 追蹤功能
+router.post('/followships/:userId', authenticated, userController.addFollowing)
+router.delete(
+  '/followships/:userId',
+  authenticated,
+  userController.removeFollowing
+)
+router.post('/tweets/:id/like', authenticated, userController.addLike)
+router.delete('/tweets/:id/unlike', authenticated, userController.removeLike)
+router.use('/', authenticated, generalErrorHandler)
 router.get('/user', userController.getOther)
 
 router.get('/tweets', tweetController.getTweets)
