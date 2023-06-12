c25acb996046a39e2900e3ac288cd771af08134b
Leechiayu <gary86442@gmail.com>
2023-06-12 15:52:58 +0800

    feat:update right sidebar render by top-10-followers user

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,11 +1,19 @@
 const { Tweet, User } = require('../models')
-
+const { getTop10Following } = require('../helpers/getTop10Following-helper')
 const tweetController = {
   getTweets: async (req, res, next) => {
     try {
-      const tweets = await Tweet.findAll({ raw: true, nest: true, include: [User] })
+      const tweets = await Tweet.findAll({
+        raw: true,
+        nest: true,
+        include: [User]
+      })
+      const top10Followers = await getTop10Following(req, next)
       const sortedTweets = tweets.sort((a, b) => b.createdAt - a.createdAt)
-      return res.render('tweets', { tweets: sortedTweets })
+      return res.render('tweets', {
+        tweets: sortedTweets,
+        topFollowers: top10Followers
+      })
     } catch (err) {
       next(err)
     }
