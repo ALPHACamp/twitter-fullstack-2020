1540a1d44926dfbbb3f9db51b14b4e303604ef92
Nero <karta0706566@gmail.com>
2023-06-13 14:20:47 +0800

    modify: adjust admin user likes

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -44,14 +44,28 @@ const adminController = {
         const userId = user.id
         const followings = await Followship.findAll({ raw: true, where: { followerId: userId } })
         const followers = await Followship.findAll({ raw: true, where: { followingId: userId } })
-        const tweets = await Tweet.findAll({ raw: true, where: { UserId: userId } })
-        const likes = await Like.findAll({ raw: true, where: { UserId: userId } })
 
+        // 找出每個user的所有tweets：
+        let tweets = await Tweet.findAll({ raw: true, where: { UserId: userId } })
+        tweets = await Promise.all(tweets.map(async (tweet) => {
+          const tweetId = tweet.id
+          // 找出每個tweet的所有likes：
+          const likes = await Like.findAndCountAll({ raw: true, where: { TweetId: tweetId }, group: ['TweetId'] })
+          tweet.totalTweetLikes = likes.count[0] ? likes.count[0].count : 0 // 若有like就回傳count，沒有就為0
+          return tweet
+        }))
 
-        user.totalTweets = tweets ? tweets.length : 0
-        user.totalLikes = likes ? likes.length : 0
+        // 找出屬於當前使用者的tweet並回傳該tweet的likes：
+        const userTweetLikes = tweets.map(tweet => {
+          if (tweet.UserId === user.id) return tweet.totalTweetLikes
+        })
+        let userTotalLikes = 0
+        userTweetLikes.forEach(like => userTotalLikes += like) // 計算使用者擁有的tweets的likes
+
         user.totalFollowings = followings ? followings.length : 0
         user.totalFollowers = followers ? followers.length : 0
+        user.totalTweets = tweets ? tweets.length : 0
+        user.totalLikes = userTotalLikes
         return user
       }))
 
