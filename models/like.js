1955d6e0014fa14a85398eb8b4bf142cd89480bb
LongXiangL <bfh1104@gmail.com>
2023-06-13 22:22:49 +0800

    likemodel測試通過

--- models/like.js
+++ models/like.js
@@ -17,6 +17,12 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Like.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     UserId: DataTypes.INTEGER,
     TweetId: DataTypes.INTEGER
   }, {
