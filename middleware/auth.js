3492ac383abbe672523cef6138a94593eec3ff41
alvin <ckuvn1854@gmail.com>
2023-06-14 22:32:48 +0800

    fix: 修改後臺嘗試登入前台畫面的redirect頁面

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,16 @@
 const helpers = require('../_helpers')
 
 const authenticated = (req, res, next) => {
-  const isUser = helpers.getUser(req)?.role === 'user'
+  const role = helpers.getUser(req)?.role
   if (helpers.ensureAuthenticated(req)) {
-    if (isUser) {
+    if (role === 'user') {
       return next()
+    } else if (role === 'admin') {
+      return res.redirect('/admin/tweets')
+    } else {
+      req.flash('error_messages', '帳號或密碼錯誤！')
+      return res.redirect('/signin')
     }
-    req.flash('error_messages', '帳號或密碼錯誤！')
-    return res.redirect('/signin')
   } else {
     req.flash('warning_messages', '請先登入！')
     return res.redirect('/signin')
