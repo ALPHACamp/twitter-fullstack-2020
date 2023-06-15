7044345f71d2912abe01901222b562c47dc2bec4
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-06-15 23:51:38 +0800

    fix:修復透過發文框使用者頭像查看使用者個人資料功能

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -50,7 +50,8 @@ const tweetController = {
         tweets,
         topFollowers: top10Followers,
         tweetRoute,
-        userAvatar
+        userAvatar,
+        id
       })
     } catch (err) {
       next(err)
