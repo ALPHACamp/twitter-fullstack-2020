c5763462c530233cab141174b5d91684f347d1c8
goodjobhot401 <goodjobhot@gmail.com>
2023-06-15 11:09:11 +0800

    feat: adjust admin-getUsers controller

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -29,7 +29,6 @@ const adminController = {
 
   // 取得所有使用者資訊
   getUsers: async (req, res, next) => {
-
     // 取出所有 role: 'user' 的使用者 id
     try {
       const userId = await User.findAll({
@@ -87,115 +86,9 @@ const adminController = {
       // console.log(usersData)
       res.render('admin/admin-users', { users: usersData })
     } catch (err) {
-      next(err);
+      next(err)
     }
   },
-
-  //   return User.findAll({
-  //     raw: true,
-  //     nest: true,
-  //     attributes: ['id'],
-  //     where: { role: 'user' }
-  //   })
-  //     .then(users => {
-  //       users.map(user => user.id)
-  //       const userDatas = []
-
-  //       for (const user of users) {
-  //         Promise.all([
-  //           User.findByPk(user, {
-  //             // raw: true,
-  //             nest: true,
-  //             include: [
-  //               { model: User, as: 'Followers', attributes: ['id', 'account'] },
-  //               { model: User, as: 'Followings', attributes: ['id', 'account'] },
-  //               { model: Tweet }
-  //             ]
-  //           }),
-  //           Like.findAll({
-  //             raw: true,
-  //             nest: true,
-  //             include: [
-  //               { model: Tweet, attributes: [], include: [{ model: User, attributes: ['id'], raw: true, nest: true }] }
-  //             ],
-  //             where: { userId: user }
-  //           })
-  //         ])
-  //           .then(([userData, likes]) => {
-  //             console.log('以下是 userData.toJSON()')
-  //             console.log(userData.toJSON())
-  //             console.log('以下是 likes.toJSON()')
-  //             console.log(likes.toJSON())
-  //             const userDataWithCounts = ({
-
-  //             })
-  //           })
-  //       }
-  //     })
-  // }
-
-  // getUsers: async (req, res, next) => {
-  //   try {
-  //     // 取出所有 role = 'user' 的使用者 id
-  //     const usersId = await User.findAll({
-  //       raw: true,
-  //       nest: true,
-  //       attributes: ['id'],
-  //       where: { role: 'user' }
-  //     })
-  //     // 必須轉成數字型態
-  //     const users = usersId.map(user => user.id)
-  //     // console.log('users 的 id 們為：', users)
-
-  //     // 使用 for of 迴圈將 users 裡每個 user 進行查詢
-  //     // 並存在 userDatas 中
-  //     const userDatas = []
-  //     for (const user of users) {
-  //       try {
-  //         // userData 為 user 的 followers, followings, tweets
-  //         // likes 為被按讚的 tweet 的 userId 是 user
-  //         const [userData, likes] = await Promise.all([
-  //           User.findByPk(user, {
-  //             nest: true,
-  //             include: [
-  //               { model: User, as: 'Followers' },
-  //               { model: User, as: 'Followings' },
-  //               { model: Tweet }
-  //             ]
-  //           }),
-  //           Like.findAll({
-  //             raw: true,
-  //             nest: true,
-  //             include: [
-  //               { model: Tweet, attributes: [], include: [{ model: User, attributes: ['id'], raw: true, nest: true }] }
-  //             ],
-  //             where: { userId: user }
-  //           })
-  //         ])
-
-  //         // 將 userData 與 likes 資料計算後, 存進新變數
-  //         const userDataWithCounts = {
-  //           ...userData.toJSON(),
-  //           tweetCount: userData.Tweets.length,
-  //           followingCount: userData.Followings.length,
-  //           followerCount: userData.Followers.length,
-  //           likeCount: likes.length
-  //         }
-  //         console.log('以下是 userData')
-  //         console.log(userData)
-  //         // 將新變數 push 到 userDatas
-  //         userDatas.push(userDataWithCounts)
-  //       } catch (error) {
-  //         console.error(`Error processing user with ID ${user}:`, error)
-  //       }
-  //     }
-  //     console.log('以下是 userDatas')
-  //     console.log(userDatas)
-  //     res.render('admin/admin-users', { users: userDatas })
-  //   } catch (err) {
-  //     next(err)
-  //   }
-  // },
 
   // 刪除貼文
   deleteTweet: (req, res, next) => {
