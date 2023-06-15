ac8df05d8dac96013f342cc809b6fe29e7520ba5
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-06-16 01:38:38 +0800

    fix:修復點擊首頁推文查看單一貼文頁

--- public/javascripts/reply-modal-eventListener.js
+++ public/javascripts/reply-modal-eventListener.js
@@ -6,7 +6,7 @@ replyBtns.forEach(btn => {
     const btn = e.target.classList.contains('reply-btn')
       ? e.target
       : e.target.parentElement
-    const { id, account, img, time, name, description, useravatar } =
+    const { id, account, img, time, name, description, userAvatar } =
       btn.dataset
     console.log(btn.dataset)
     replyModal.innerHTML = `<div class="modal-dialog">
