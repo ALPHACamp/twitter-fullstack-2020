385c6db800b37443a18b5c521f45890d601a5269
sherryylin0719 <sherryylin0719@gmail.com>
2023-06-17 10:12:48 +0900

    feat: improve like/unlike UX

--- public/javascripts/tweets.js
+++ public/javascripts/tweets.js
@@ -20,6 +20,21 @@ textareaInput.addEventListener('input', function () {
     }
 })
 
+window.addEventListener('scroll', function() {
+  // 將滾動位置存儲在localStorage中
+  localStorage.setItem('scrollPosition', window.scrollY);
+});
+
+window.addEventListener('load', function() {
+  // 從localStorage中讀取滾動位置
+  const scrollPosition = localStorage.getItem('scrollPosition');
+  if (scrollPosition) {
+    // 將頁面滾動到存儲的位置
+    window.scrollTo(0, scrollPosition);
+    // 清除localStorage中的滾動位置
+    localStorage.removeItem('scrollPosition');
+  }
+});
 
 
 
