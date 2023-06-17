37404f23998ec8863d4ae7dfdebba73fc9c283f4
shaochenn <hnhngtb1111@gmail.com>
2023-06-17 15:08:43 +0800

    fix time diplay issue in reply section & get top followed route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,13 +8,12 @@ const { getUser, getTopFollowedUsers } = require('../../middleware/general-data'
 
 // users
 router.use('/', authenticated)
-router.get('/', getTopFollowedUsers)
-router.get('/:userId/tweets', getUser, profileController.getUserTweets)
-router.get('/:userId/replies', profileController.getUserReplies)
-router.get('/:userId/likes', profileController.getUserLikes)
-router.get('/:userId/followings', profileController.getUserFollowings)
-router.get('/:userId/followers', profileController.getUserFollowers)
-router.get('/:userId', profileController.editUserAccount)
+router.get('/:userId/tweets', getTopFollowedUsers, getUser, profileController.getUserTweets)
+router.get('/:userId/replies', getTopFollowedUsers, profileController.getUserReplies)
+router.get('/:userId/likes', getTopFollowedUsers, profileController.getUserLikes)
+router.get('/:userId/followings', getTopFollowedUsers, profileController.getUserFollowings)
+router.get('/:userId/followers', getTopFollowedUsers, profileController.getUserFollowers)
+router.get('/:userId', getTopFollowedUsers, profileController.editUserAccount)
 router.put('/:userId', cpUpload, profileController.putUserAccount)
 
 module.exports = router
