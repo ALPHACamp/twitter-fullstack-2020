24f18097d370f8414a5693be2b7c1755d0d9c029
HuangYiLun <wk5018102@gmail.com>
2023-06-19 00:13:24 +0800

    feat: add signup valid message

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,7 +9,7 @@ const userController = {
   },
   signUp: (req, res, next) => {
     const { account, name, email, password, checkPassword } = req.body
-    if (!account || !name || !email || !password || !checkPassword) throw new Error('所有欄位都是必填!')
+    if (!account.trim() || !name.trim() || !email.trim() || !password.trim() || !checkPassword.trim()) throw new Error('所有欄位都是必填!')
     if (name.length > 50) throw new Error('暱稱不得超過50字')
     if (req.body.password !== req.body.checkPassword) throw new Error('密碼不相符!')
 
