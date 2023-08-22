cded7f660048ff8b724a644cb91102688648deca
scheng0718 <scheng0718@gmail.com>
2023-08-22 15:34:57 -0700

    feat: project init

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const helpers = require('./_helpers');
-
+const handlebars = require('express-handlebars')
+const routes = require('./routes')
+// const helpers = require('./_helpers')
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
-
-app.get('/', (req, res) => res.send('Hello World!'))
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(routes)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
