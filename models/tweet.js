7ec09418bedf470b4d064bf02a6ce2f6e7396f1e
pupss91090 <hsuchinzai@gmail.com>
2023-08-23 00:27:44 +0800

    add reply, tweet model

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,30 @@
-'use strict';
+'use strict'
+const {
+  Model
+} = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-  }, {});
-  Tweet.associate = function(models) {
+  class Tweet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+      Tweet.hasMany(models.Reply, { foreignKey: 'tweetId' })
+      Tweet.hasMany(models.Like, { foreignKey: 'tweetId' })
+      Tweet.belongsTo(models.User, { foreignKey: 'userId' })
+    }
   };
-  return Tweet;
-};
+  Tweet.init({
+    userId: DataTypes.INTEGER,
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Tweet',
+    tableName: 'Tweets',
+    underscored: true
+  })
+  return Tweet
+}
