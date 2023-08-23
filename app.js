f2951535a1e3f0b7b953b0ddc6502e90aebaa79e
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-23 17:30:43 +0800

    add notice msg

--- app.js
+++ app.js
@@ -31,6 +31,8 @@ app.use(flash())
 app.use((req, res, next) => {
     res.locals.success_messages = req.flash('success_messages')
     res.locals.error_messages = req.flash('error_messages')
+    res.locals.warning_messages = req.flash('warning_messages')
+    res.locals.notice_messages = req.flash('notice_messages')
     res.locals.user = getUser(req)
     next()
   })
