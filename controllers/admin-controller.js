8ad5225a79d588bb875994da62bd4de77c8fd23e
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-08-23 21:41:53 +0800

    feat: add user seeders

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,10 @@
 const adminController = {
+  signInPage: (req, res) => {
+    return res.render('admin/signin')
+  },
+  signIn: async (req, res) => {
+    return res.redirect('/admin/tweets')
+  },
   getTweets: (req, res) => {
     return res.render('admin/tweets')
   }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 
+// Controllers
 const adminController = require('../../controllers/admin-controller')
+
+// Sign in
+router.get('/signin', adminController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/admin/signin', failureFlash: true }), adminController.signIn)
 
 router.get('/tweets', adminController.getTweets)
 router.use('/', (req, res) => res.redirect('/admin/tweets'))
