fb00c7cc65a5e264b384a4a8028c07abc1c63f31
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-23 16:54:47 +0800

    feat:common-feature done

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
+// 引入模組
 const express = require('express')
-const helpers = require('./_helpers');
-
+const handlebars = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
+// 引入路由
+const routes = require('./routes/index')
+// 引入helper
+const helpers = require('./_helpers')
+const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const passport = require('./config/passport')
+const SESSION_SECRET = 'secret'
+// 建立app
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Handlebars 設定
+app.engine('hbs', handlebars({ extname: '.hbs', defaultLayout: 'main', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use((req, res, next) => {
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    res.locals.user = getUser(req)
+    next()
+  })
+// 使用路由器
+app.use(routes)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,20 @@
 const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
-  // if (req.isAuthenticated)
   if (helpers.ensureAuthenticated(req).role === 'user') {
+    if (helpers.getUser(req).role === 'admin') {
+      req.flash('error_messages', '後臺帳號不能進入前臺!')
+      res.redirect('/admin/tweets')
+    }
     return next()
   }
   res.redirect('/signin')
 }
 const authenticatedAdmin = (req, res, next) => {
-  // if (req.isAuthenticated)
   if (helpers.ensureAuthenticated(req)) {
     if (helpers.getUser(req).role === 'admin') return next()
-    res.redirect('/admin/tweets')
+    req.flash('error_messages', '前臺帳號不能進入後臺!')
+    res.redirect('/tweets')
   } else {
     res.redirect('/signin')
   }
