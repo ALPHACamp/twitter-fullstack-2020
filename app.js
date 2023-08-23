a4dceaae2feb6ae5e70a10417ad88e15eefb7ce5
hao0516hsu <chhsu0516@gmail.com>
2023-08-23 14:12:46 +0800

    project initialization

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+// 引入模組
 const express = require('express')
+const handlebars = require('express-handlebars')
+const methodOverride = require('method-override')
+
+// 引入路由
+const routes = require('./routes/index')
+// 引入helper
 const helpers = require('./_helpers');
 
+// 建立app
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Handlebars 設定
+app.engine('hbs', handlebars({ extname: '.hbs', defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
+
+// 使用路由器
+app.use(routes)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+// 引入Modules
+
+// 使用Modules
+
+
+module.exports = router
