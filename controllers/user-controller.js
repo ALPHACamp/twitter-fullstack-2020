f0a80e1ac6d31418ba30e584fd2eabacd6144887
Easonlu0425 <andy1234532@hotmail.com>
2023-08-24 01:13:00 +0800

    merge tweet-reply

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,8 @@
 const bcrypt = require("bcryptjs");
 const { Tweet, User, Followship } = require("../models");
-const { imgurFileHandler } = require('../helpers/file-helpers')
-const randomUsersHelper = require('../helpers/randomUsersHelper');
-const helpers = require('../_helpers')
+const { imgurFileHandler } = require("../helpers/file-helpers");
+const randomUsersHelper = require("../helpers/randomUsersHelper");
+const helpers = require("../_helpers");
 const userController = {
   signupPage: (req, res) => {
     res.render("signup");
@@ -76,18 +76,19 @@ const userController = {
       const user = await User.findByPk(followingUserId);
       const followship = await Followship.findOne({
         where: { followerId: currentUserId, followingId: followingUserId },
-      })
+      });
       if (!user) throw new Error("User didn't exist");
       if (!followship) throw new Error("You aren't following this user!");
-      await followship.destroy()
+      await followship.destroy();
       return res.redirect("back");
-    } catch(err) {
-      console.log(err)
+    } catch (err) {
+      console.log(err);
       // next(err)
     }
   },
   getUser: async (req, res, next) => {
-    const isUser = helpers.getUser(req).id === Number(req.params.id) ? true : false
+    const isUser =
+      helpers.getUser(req).id === Number(req.params.id) ? true : false;
     try {
       const userId = req.params.id;
       const user = await User.findByPk(userId);
@@ -99,41 +100,47 @@ const userController = {
         const dataToRender = {
           user: userData,
           recommend: tenRandomUsers,
-          isUser
+          isUser,
         };
 
-        res.render('user/user-tweets', dataToRender);
+        res.render("user/user-tweets", dataToRender);
       } else {
-        res.status(404).send('未找到用户');
+        res.status(404).send("未找到用户");
       }
     } catch (err) {
       console.error(err);
       res.status(500).send("获取用户数据时出错。");
     }
   },
-  putUser: (req, res, next) => { //修改使用者名稱、自我介紹
-    const { name, introduction } = req.body
-    const avatar = req.files ? req.files.avatar : null
-    const background = req.files ? req.files.background : null
+  putUser: (req, res, next) => {
+    //修改使用者名稱、自我介紹
+    const { name, introduction } = req.body;
+    const avatar = req.files ? req.files.avatar : null;
+    const background = req.files ? req.files.background : null;
     User.findByPk(req.params.id)
-      .then(async user => {
-        const avatarFilePath = avatar ? await imgurFileHandler(avatar[0]) : user.avatar
-        const backgroundFilePath = background ? await imgurFileHandler(background[0]) : user.background
+      .then(async (user) => {
+        const avatarFilePath = avatar
+          ? await imgurFileHandler(avatar[0])
+          : user.avatar;
+        const backgroundFilePath = background
+          ? await imgurFileHandler(background[0])
+          : user.background;
         console.log("Avatar File Path:", avatarFilePath);
         console.log("Background File Path:", backgroundFilePath);
         return user.update({
           name,
           introduction,
           avatar: avatarFilePath || user.avatar,
-          background: backgroundFilePath || user.background
-        })
+          background: backgroundFilePath || user.background,
+        });
       })
       .then(() => {
-        res.redirect(`/users/${req.params.id}/tweets`)
+        res.redirect(`/users/${req.params.id}/tweets`);
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
-  getFollower: async (req, res, next) => { // 跟隨者
+  getFollower: async (req, res, next) => {
+    // 跟隨者
     try {
       const userId = req.params.id;
       const user = await User.findByPk(userId);
@@ -148,7 +155,6 @@ const userController = {
         };
 
         res.render("user/user-tweets", dataToRender);
-
       } else {
         res.status(404).send("未找到用户");
       }
@@ -212,40 +218,50 @@ const userController = {
       res.status(500).send("获取用户数据时出错。");
     }
   },
-<<<<<<< HEAD
-  getSetting: (req, res,) => { // 取得帳戶設定頁面
-    return User.findByPk(helpers.getUser(req).id)
-      .then(user => {
-        user = user.toJSON()
-        const { name, account, email } = user
-        return res.render('settings', { name, account, email })
-      })
+  getSetting: (req, res) => {
+    // 取得帳戶設定頁面
+    return User.findByPk(helpers.getUser(req).id).then((user) => {
+      user = user.toJSON();
+      const { name, account, email } = user;
+      return res.render("settings", { name, account, email });
+    });
   },
-  putSetting: (req, res, next) => { // 編輯帳戶設定
-    const { account, name, email, password, passwordCheck } = req.body
-    if (!name || !email || !password || !account || !passwordCheck) throw new Error('所有欄位都是必填。')
-    if (password !== passwordCheck) throw new Error('密碼不相同')
+  putSetting: (req, res, next) => {
+    // 編輯帳戶設定
+    const { account, name, email, password, passwordCheck } = req.body;
+    if (!name || !email || !password || !account || !passwordCheck)
+      throw new Error("所有欄位都是必填。");
+    if (password !== passwordCheck) throw new Error("密碼不相同");
     return Promise.all([
       User.findByPk(helpers.getUser(req).id),
       User.findOne({ where: { email } }),
-      User.findOne({ where: { account } })
+      User.findOne({ where: { account } }),
     ])
       .then(async ([user, emailCheck, accountCheck]) => {
-        if (accountCheck && Number(accountCheck.id) !== Number(helpers.getUser(req).id)) throw new Error('帳號已有人註冊')
-        if (emailCheck && Number(emailCheck.id) !== Number(helpers.getUser(req).id)) throw new Error('信箱已有人註冊')
+        if (
+          accountCheck &&
+          Number(accountCheck.id) !== Number(helpers.getUser(req).id)
+        )
+          throw new Error("帳號已有人註冊");
+        if (
+          emailCheck &&
+          Number(emailCheck.id) !== Number(helpers.getUser(req).id)
+        )
+          throw new Error("信箱已有人註冊");
         return user.update({
           name,
           account,
           email,
-          password: password ? await bcrypt.hash(password, 10) : user.password
-        })
+          password: password ? await bcrypt.hash(password, 10) : user.password,
+        });
       })
       .then(() => {
-        req.flash('success_messages', '帳戶設定編輯成功!')
-        return res.redirect('settings')
+        req.flash("success_messages", "帳戶設定編輯成功!");
+        return res.redirect("settings");
       })
-      .catch(err => next(err))
-=======
+      .catch((err) => next(err));
+  },
+
   putUser: (req, res, next) => {
     //修改使用者名稱、自我介紹
     const { name, introduction } = req.body;
@@ -259,7 +275,6 @@ const userController = {
       .then(() => {
         res.redirect(`/users/${req.params.id}/tweets`);
       });
->>>>>>> tweet-reply
   },
 };
 
