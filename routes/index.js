29a5564f0f5135a0184e972af64c652612f350ed
hao0516hsu <chhsu0516@gmail.com>
2023-08-23 15:45:55 +0800

    feat: signup page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
+// 引入Controller
+const userController = require('../controllers/user-controller')
+
 // 引入Modules
 
 // 使用Modules
-
+router.get('/signup',userController.signUpPage)
 
 module.exports = router
