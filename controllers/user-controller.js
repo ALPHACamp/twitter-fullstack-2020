36876be99f4747a958ee90e9220f71f15220fb20
yang haotse <ha91252@gmail.com>
2023-08-23 16:15:35 +0800

    add right views

---
+++ controllers/user-controller.js
@@ -0,0 +1,8 @@
+const userController = {
+  getUserSetting: (req, res, next) => {
+    return res.render('user-setting')
+  }
+}
+
+
+module.exports = userController 
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 
+const userController = require('../controllers/user-controller')
+
 
 
 
 router.get('/', (req, res) => {
-  res.render('setting')
+  res.render('right')
 })
 module.exports = router
