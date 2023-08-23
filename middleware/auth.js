f6a2ef584a098d9dded52afaa6f03f1a83f6c595
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-23 22:53:14 +0800

    modify auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,23 @@
 const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
-  if (helpers.ensureAuthenticated(req).role === 'user') {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).role === 'user') return next()
     if (helpers.getUser(req).role === 'admin') {
       req.flash('error_messages', '後臺帳號不能進入前臺!')
       res.redirect('/admin/tweets')
     }
-    return next()
+  } else {
+    res.redirect('/signin')
   }
-  res.redirect('/signin')
 }
 const authenticatedAdmin = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
     if (helpers.getUser(req).role === 'admin') return next()
-    req.flash('error_messages', '前臺帳號不能進入後臺!')
-    res.redirect('/tweets')
+    if (helpers.getUser(req).role === 'user') {
+      req.flash('error_messages', '前臺帳號不能進入後臺!')
+      res.redirect('/tweets')
+    }
   } else {
     res.redirect('/signin')
   }
