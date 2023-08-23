a6af282c0a54ba7ef4f04167ae051677785b8dac
go2Cshop <shell800808@gmail.com>
2023-08-23 18:20:27 +0800

    feat: modify user-profile & add upload image

--- controllers/likes-controller.js
+++ controllers/likes-controller.js
@@ -1,7 +1,9 @@
 const { User } = require('../models')
 const randomUsersHelper = require('../helpers/randomUsersHelper');
+const helpers = require('../_helpers')
 const likeController = {
   getLikes: async (req, res, next) => {
+    const isUser = helpers.getUser(req).id === Number(req.params.id) ? true : false
     try {
       const userId = req.params.id;
       const user = await User.findByPk(userId);
@@ -11,8 +13,9 @@ const likeController = {
         const tenRandomUsers = await randomUsersHelper.getTenRandomUsers(10); 
 
         const dataToRender = {
-          users: userData,
+          user: userData,
           recommend: tenRandomUsers,
+          isUser
         };
 
         res.render('user/user-likes', dataToRender);
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,11 +2,13 @@ const bcrypt = require('bcryptjs')
 const { Tweet, User, Followship } = require("../models");
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const randomUsersHelper = require('../helpers/randomUsersHelper');
+const helpers = require('../_helpers')
+
 const userController = {
   signupPage: (req, res) => {
     res.render('signup')
   },
-  signup: (req, res) => {
+  signup: (req, res,) => {
     const { account, name, email, password, passwordCheck } = req.body
     // if (password !== passwordCheck) throw new Error('密碼與確認密碼不相符')
     return User.findOne({ where: { email } })
@@ -69,6 +71,7 @@ const userController = {
     }
   },
   getUser: async (req, res, next) => {
+    const isUser = helpers.getUser(req).id === Number(req.params.id) ? true : false
     try {
       const userId = req.params.id;
       const user = await User.findByPk(userId);
@@ -78,8 +81,9 @@ const userController = {
         const tenRandomUsers = await randomUsersHelper.getTenRandomUsers(10); // 使用 helper 模块获取10个随机用户
 
         const dataToRender = {
-          users: userData,
+          user: userData,
           recommend: tenRandomUsers,
+          isUser
         };
 
         res.render('user/user-tweets', dataToRender);
@@ -90,6 +94,28 @@ const userController = {
       console.error(err);
       res.status(500).send("获取用户数据时出错。");
     }
+  },
+  putUser: (req, res, next) => { //修改使用者名稱、自我介紹
+    const { name, introduction } = req.body
+    const avatar = req.files ? req.files.avatar : null
+    const background = req.files ? req.files.background : null
+    User.findByPk(req.params.id)
+      .then(async user => {
+        const avatarFilePath = avatar ? await imgurFileHandler(avatar[0]) : user.avatar
+        const backgroundFilePath = background ? await imgurFileHandler(background[0]) : user.background
+        console.log("Avatar File Path:", avatarFilePath);
+        console.log("Background File Path:", backgroundFilePath);
+        return user.update({
+          name,
+          introduction,
+          avatar: avatarFilePath || user.avatar,
+          background: backgroundFilePath || user.background
+        })
+      })
+      .then(() => {
+        res.redirect(`/users/${req.params.id}/tweets`)
+      })
+      .catch(err => next(err))
   },
   getFollower: async (req, res, next) => { // 跟隨者
       try {
@@ -113,12 +139,6 @@ const userController = {
         console.error(err);
         res.status(500).send("获取用户数据时出错。");
       }
-    // return User.findByPk(req.params.id)
-    //   .then(user => {
-    //     return res.render('user/user-follower', {
-    //       users: user.toJSON()
-    //     })
-    //   })
   },
   getFollowing: async (req, res, next) => { // 跟隨中
     try {
@@ -143,19 +163,39 @@ const userController = {
       res.status(500).send("获取用户数据时出错。");
     }
   },
-  putUser: (req, res, next) => { //修改使用者名稱、自我介紹
-    const { name, introduction } = req.body
-    return User.findByPk(req.params.id)
+  getSetting: (req, res,) => { // 取得帳戶設定頁面
+    return User.findByPk(helpers.getUser(req).id)
       .then(user => {
+        user = user.toJSON()
+        const { name, account, email } = user
+        return res.render('settings', { name, account, email })
+      })
+  },
+  putSetting: (req, res, next) => { // 編輯帳戶設定
+    const { account, name, email, password, passwordCheck } = req.body
+    if (!name || !email || !password || !account || !passwordCheck) throw new Error('所有欄位都是必填。')
+    if (password !== passwordCheck) throw new Error('密碼不相同')
+    return Promise.all([
+      User.findByPk(helpers.getUser(req).id),
+      User.findOne({ where: { email } }),
+      User.findOne({ where: { account } })
+    ])
+      .then(async ([user, emailCheck, accountCheck]) => {
+        if (accountCheck && Number(accountCheck.id) !== Number(helpers.getUser(req).id)) throw new Error('帳號已有人註冊')
+        if (emailCheck && Number(emailCheck.id) !== Number(helpers.getUser(req).id)) throw new Error('信箱已有人註冊')
         return user.update({
           name,
-          introduction
+          account,
+          email,
+          password: password ? await bcrypt.hash(password, 10) : user.password
         })
       })
       .then(() => {
-        res.redirect(`/users/${req.params.id}/tweets`)
+        req.flash('success_messages', '帳戶設定編輯成功!')
+        return res.redirect('settings')
       })
-  }
+      .catch(err => next(err))
+  },
 };
 
 module.exports = userController;
