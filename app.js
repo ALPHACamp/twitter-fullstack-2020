6832746387773599d67fc04977c18a6d28d009db
tingul4 <xpple413208@gmail.com>
2023-08-23 16:27:58 +0800

    feat: add auth middleware and helper and add passport

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
-// const helpers = require('./_helpers')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('./config/passport')
+const { getUser } = require('./helpers/auth-helpers')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -12,9 +18,17 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(flash())
 app.use(methodOverride('_method'))
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
-
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
