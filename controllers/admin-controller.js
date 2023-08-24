b825ca73b94b8cd6bf4690ea0bac58f089d7cdf5
tingul4 <xpple413208@gmail.com>
2023-08-24 16:48:20 +0800

    feat: complete admin login and pass test

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,7 +1,16 @@
-const { } = require('../models')
-
 const adminController = {
-//  add controller action here
+  signInPage: (req, res) => {
+    res.render('admins/signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '管理員成功登入！')
+    res.redirect('/admin/tweets')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '管理員登出成功！')
+    req.logout()
+    res.redirect('/admin/signin')
+  }
 }
 
 module.exports = adminController
