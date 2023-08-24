6b8f0192e55d92226de1cb6bd42081c5e840b6e1
gibbs-shih <shih.gibbs@gmail.com>
2023-08-24 18:12:52 +0800

    feat: add followship function and routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const { userController } = require('../controllers/user-controller')
 // const helpers = require('../_helpers')
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
+// authenticated還沒載入 還沒寫這功能
+router.post('/followships/:userId', authenticated, userController.addFollowing)
+router.delete('/followships/:userId', authenticated, userController.deleteFollowing)
+
 module.exports = router
