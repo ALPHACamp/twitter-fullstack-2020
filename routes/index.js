e044de5ee8c510170bbb52438cbd53e1f03f6295
tingul4 <xpple413208@gmail.com>
2023-08-25 01:46:42 +0800

    fix: add authenticate in tweet route

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
-const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 const tweetController = require('../controllers/tweet-controller')
-const userController = require('../controllers/user-controller')
-const replyController = require('../controllers/reply-controller')
+// const userController = require('../controllers/user-controller')
+// const replyController = require('../controllers/reply-controller')
 const loginController = require('../controllers/login-controller')
 
 router.use('/admin', admin)
 
-router.get('/tweets', tweetController.getTweets) // test
-router.get('/tweets/replies', tweetController.getTweetsReply) // test
+router.get('/tweets', authenticated, tweetController.getTweets) // test
+router.get('/tweets/replies', authenticated, tweetController.getTweetsReply) // test
 
 router.get('/signup', loginController.signUpPage)
 router.post('/signup', loginController.signUp)
