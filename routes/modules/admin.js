576b9cccb789aa331785ced438b6bbced8004e5e
scheng0718 <scheng0718@gmail.com>
2023-08-24 19:24:06 -0700

    fix admin's failure redirect

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -5,7 +5,7 @@ const adminController = require('../../controllers/admin-controller')
 
 // const helpers = require('../_helpers')
 router.get('/signin', adminController.signInPage)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), adminController.signIn)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/admin/signin', failureFlash: true }), adminController.signIn)
 router.get('/logout', adminController.logOut)
 router.use('/', (req, res) => { res.redirect('/admin/signin') })
 
