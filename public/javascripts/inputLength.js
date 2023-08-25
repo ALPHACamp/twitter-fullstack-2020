1c07f6e89656815bfa261f9ae07ac414e18e7cd3
iamcoolalan <iamalan1996@gmail.com>
2023-08-25 19:59:55 +0800

    feat: seprect userPage.js into two script

---
+++ public/javascripts/inputLength.js
@@ -0,0 +1,33 @@
+/* for input word count */
+const TEXT_INPUT_ClASS = '.text-input'
+const TEXT_INPUT_LENGTH_DISPLAY_CLASS = '.input-length'
+
+//檢查是否在sign up頁
+const isSignUp = document.querySelector('#sign-up-form')
+const textInputs = document.querySelectorAll(TEXT_INPUT_ClASS)
+
+textInputs.forEach(textInput => {
+  showInputLengthWhenRender(textInput)
+  textInput.addEventListener('input', event => {
+    displayInputLength(event)
+  })
+})
+
+function displayInputLength(event) {
+  if (event.target.matches(TEXT_INPUT_ClASS)) {
+    const currentLength = event.target.value.length
+    const displayDiv = event.target.nextElementSibling.querySelector(TEXT_INPUT_LENGTH_DISPLAY_CLASS)
+    const maxLength = Number(event.target.getAttribute('maxlength'))
+    displayDiv.textContent = `${currentLength}/${maxLength}`
+  }
+}
+
+function showInputLengthWhenRender(inputObject) {
+  // sign up 頁不想要直接顯示出來
+  if (!isSignUp) {
+    const currentLength = inputObject.value.length
+    const displayDiv = inputObject.nextElementSibling.querySelector(TEXT_INPUT_LENGTH_DISPLAY_CLASS)
+    const maxLength = Number(inputObject.getAttribute('maxlength'))
+    displayDiv.textContent = `${currentLength}/${maxLength}`
+  }
+}
--- public/javascripts/userPage.js
+++ public/javascripts/userPage.js
@@ -4,9 +4,7 @@ const USER_COVER_INPUT = '#user-cover-input'
 const USER_AVATAR_INPUT = '#user-avatar-input'
 const USER_AVATAR = '#user-setting-avatar'
 const REMOVE_COVER_INPUT = '#remove-cover-input'
-/* for input word count */
-const TEXT_INPUT_ClASS = '.text-input'
-const TEXT_INPUT_LENGTH_DISPLAY_CLASS = '.input-length'
+
 /* get original input src */
 const originCover = document.querySelector(USER_COVER).src
 /* user avatar upload in setting */
@@ -40,28 +38,4 @@ function removeInput (event, inputId, originCoverId) {
   input.value = ''
 }
 
-const textInputs = document.querySelectorAll(TEXT_INPUT_ClASS)
-
-textInputs.forEach(textInput => {
-  showInputLengthWhenRender(textInput)
-  textInput.addEventListener('input', event => {
-    displayInputLength(event)
-  })
-})
-
-function displayInputLength (event) {
-  if (event.target.matches(TEXT_INPUT_ClASS)) {
-    const currentLength = event.target.value.length
-    const displayDiv = event.target.nextElementSibling.querySelector(TEXT_INPUT_LENGTH_DISPLAY_CLASS)
-    const maxLength = Number(event.target.getAttribute('maxlength'))
-    displayDiv.textContent = `${currentLength}/${maxLength}`
-  }
-}
-
-function showInputLengthWhenRender (inputObject) {
-  const currentLength = inputObject.value.length
-  const displayDiv = inputObject.nextElementSibling.querySelector(TEXT_INPUT_LENGTH_DISPLAY_CLASS)
-  const maxLength = Number(inputObject.getAttribute('maxlength'))
-  displayDiv.textContent = `${currentLength}/${maxLength}`
-}
 /* user setting end */
