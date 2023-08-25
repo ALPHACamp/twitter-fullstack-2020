7a6d725a9e1d1f7cbfb7082187baedfa9f135df6
Annie <anniehuang0410@gmail.com>
2023-08-25 17:03:58 +0800

    feat: add left sidebar view and getTweet route for test

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,6 +1,9 @@
 // const { Tweet } = require('../models')
 
 const tweetController = {
-
+  getTweets: (req, res, next) => {
+    // test sidebar view
+    res.render('tweets')
+  }
 }
 module.exports = tweetController
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 // const { userController } = require('../controllers/user-controller')
+const tweetController = require('../controllers/tweet-controller')
 const admin = require('./modules/admin')
 // const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
 
+router.get('/tweets', tweetController.getTweets)
 // authenticated還沒載入 還沒寫這功能
 // router.post('/followships/:userId', authenticated, userController.addFollowing)
 // router.delete('/followships/:userId', authenticated, userController.deleteFollowing)
