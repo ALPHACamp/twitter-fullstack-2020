243c81caf108f0faa505030f809490411bc91882
scheng0718 <scheng0718@gmail.com>
2023-08-24 17:02:35 -0700

    feat: add admin signin page and flash message

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 const adminController = require('../../controllers/admin-controller')
 
 // const helpers = require('../_helpers')
 router.get('/signin', adminController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), adminController.signIn)
+router.get('/logout', adminController.logOut)
 router.use('/', (req, res) => { res.redirect('/admin/signin') })
 
 module.exports = router
