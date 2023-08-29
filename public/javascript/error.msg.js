a881e7b78779ade04a2234a7a48a352598c5a393
RobertOu0213 <starshining12345@gmail.com>
2023-08-29 17:13:34 +0800

    refactor: modal

--- public/javascript/error.msg.js
+++ public/javascript/error.msg.js
@@ -1,36 +1,38 @@
-// document.addEventListener('DOMContentLoaded', function () {
-const submitBtn = document.getElementById('submitBtn')
-const contentInput = document.getElementById('contentInput')
-const contentMessage = document.getElementById('emptyContentMessage')
+for (let i = 1; i <= 2; i++) {
+  const submitBtn = document.getElementById(`submitBtn${i}`)
+  const contentInput = document.getElementById(`contentInput${i}`)
+  const contentMessage = document.getElementById(`contentMessage${i}`)
 
-// const replySubmitBtn = document.querySelectorAll('.replySubmitBtn')
-// const replyContentInput = document.querySelectorAll('.replyContentInput')
-// const replyEmptyContentMessage = document.querySelectorAll('.replyEmptyContentMessage')
-// console.log(replyEmptyContentMessage)
+  if (submitBtn) {
+    submitBtn.addEventListener('click', function (event) {
+      if (contentInput.value.trim() === '') {
+        contentMessage.textContent = '內容不可空白'
+        event.preventDefault()
+      } else {
+        contentMessage.textContent = ''
+      }
+      if (contentInput.value.length > 140) {
+        contentMessage.textContent = '字數不可超過 140 字'
+        event.preventDefault()
+      }
+    })
+  }
+}
 
-submitBtn.addEventListener('click', function (event) {
-  if (contentInput.value.trim() === '') {
-    contentMessage.textContent = '內容不可空白'
-    event.preventDefault()
-  } else {
-    contentMessage.textContent = ''
-  }
-  if (contentInput.value.length > 140) {
-    contentMessage.textContent = '字數不可超過 140 字'
-    event.preventDefault()
-  }
+document.querySelectorAll('[id^=submitBtn]').forEach(btn => {
+  const id = btn.getAttribute('data-id')
+  const contentInput = document.querySelector(`#contentInput${id}`)
+  const contentMessage = document.querySelector(`#contentMessage${id}`)
+  btn.addEventListener('click', function (event) {
+    if (contentInput.value.trim() === '') {
+      contentMessage.textContent = '內容不可空白'
+      event.preventDefault()
+    } else {
+      contentMessage.textContent = ''
+    }
+    if (contentInput.value.length > 140) {
+      contentMessage.textContent = '字數不可超過 140 字'
+      event.preventDefault()
+    }
+  })
 })
-
-// replySubmitBtn[0].addEventListener('click', function (event) {
-//   if (replyContentInput[0].value.trim() === '') {
-//     replyEmptyContentMessage[0].textContent = '內容不可空白'
-//     event.preventDefault()
-//   } else {
-//     replyEmptyContentMessage.textContent = ''
-//   }
-//   if (replyContentInput.value.length > 140) {
-//     replyEmptyContentMessage.textContent = '字數不可超過 140 字'
-//     event.preventDefault()
-//   }
-// })
-// })
