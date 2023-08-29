9ec39e5eb5a027334d5f828b5b2f59b52f2aaa8b
TinyMurky <murky0830@gmail.com>
2023-08-29 16:34:08 +0800

    admin

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,13 +1,7 @@
 const express = require('express')
 
-<<<<<<< HEAD:routes/pages/modules/admin.js
 const userController = require('../../../controllers/pages/user-controller')
 const adminController = require('../../../controllers/pages/admin-controller')
-=======
-const { adminJWTAuth, sendToken } = require('../../middlewares/auth')
-const userController = require('../../controllers/pages/user-controller')
-const adminController = require('../../controllers/pages/admin-controller')
->>>>>>> user:routes/modules/admin.js
 
 // passport & auth
 const { adminLocalAuth, authenticatedAdmin } = require('../../../middlewares/auth')
@@ -22,21 +16,11 @@ router.get('/tweets', authenticatedAdmin, adminController.getTweets)
 router.get('/users', authenticatedAdmin, adminController.getUsers)
 
 // Signin
-<<<<<<< HEAD:routes/pages/modules/admin.js
 router.get('/signin', userController.getAdminSignInPage)
 router.get('/logout', userController.adminLogout)
 router.post('/signin', adminLocalAuth, userController.adminSignin)
 
 // Main route
 router.get('/', authenticatedAdmin, adminController.getAdminHomePage)
-=======
-router.get('/signin', adminJWTAuth, userController.getAdminSignInPage)
-router.post('/signin', adminJWTAuth, sendToken, userController.adminSignin)
-
-// Main route
-router.get('/logout', userController.adminLogout)
-
-router.get('/', adminJWTAuth, adminController.getAdminHomePage)
->>>>>>> user:routes/modules/admin.js
 
 module.exports = router
