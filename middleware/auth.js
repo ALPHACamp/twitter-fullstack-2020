1504d26be1dba2f50f08a509f2f25f5e4a85ae24
tingul4 <xpple413208@gmail.com>
2023-08-30 09:57:52 +0800

    fix: login authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,8 @@ const helpers = require('../_helpers')
 const authenticated = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
     if (helpers.getUser(req).role === 'user') return next()
-    req.flash('error_messages', '帳號不存在！')
-    res.redirect('/signin')
+    req.flash('error_messages', '沒有瀏覽權限！')
+    res.redirect('/admin/tweets')
   }
   req.flash('error_messages', '請先登入使用者！')
   res.redirect('/signin')
@@ -11,33 +11,29 @@ const authenticated = (req, res, next) => {
 const authenticatedAdmin = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
     if (helpers.getUser(req).role === 'admin') return next()
-    req.flash('error_messages', '帳號不存在！')
-    res.redirect('/admin/signin')
-  }
-  req.flash('error_messages', '請先登入管理員！')
-  res.redirect('/admin/signin')
-}
-const authenticatedTweet = (req, res, next) => {
-  if (helpers.ensureAuthenticated(req)) {
-    if (helpers.getUser(req).role === 'user') return next()
-    req.flash('error_messages', '管理員沒有瀏覽權限！')
-    res.redirect('/admin/tweets')
-  }
-  req.flash('error_messages', '請先登入使用者！')
-  res.redirect('/signin')
-}
-const authenticatedAdminTweet = (req, res, next) => {
-  if (helpers.ensureAuthenticated(req)) {
-    if (helpers.getUser(req).role === 'admin') return next()
-    req.flash('error_messages', '使用者沒有瀏覽權限！')
+    req.flash('error_messages', '沒有瀏覽權限！')
     res.redirect('/tweets')
   }
   req.flash('error_messages', '請先登入管理員！')
   res.redirect('/admin/signin')
 }
+const authenticatedUserLogin = (req, res, next) => {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).role === 'user') return next()
+    req.flash('error_messages', '帳號不存在！')
+    res.redirect('/signin')
+  }
+}
+const authenticatedAdminLogin = (req, res, next) => {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).role === 'admin') return next()
+    req.flash('error_messages', '帳號不存在！')
+    res.redirect('/admin/signin')
+  }
+}
 module.exports = {
   authenticated,
   authenticatedAdmin,
-  authenticatedTweet,
-  authenticatedAdminTweet
+  authenticatedUserLogin,
+  authenticatedAdminLogin
 }
