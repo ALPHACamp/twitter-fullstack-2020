20384a519b077758283eaf5085099cd88a890ed3
tingul4 <xpple413208@gmail.com>
2023-09-01 15:25:11 +0800

    fix: session secret

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(flash())
 app.use(methodOverride('_method'))
-app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(session({ secret: 'SESSION_SECRET', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use((req, res, next) => {
