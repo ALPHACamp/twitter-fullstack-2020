8df0383d748a2d4dbb653883f351ce45dd7c1ff0
Annie <anniehuang0410@gmail.com>
2023-09-03 02:08:30 +0800

    feat: add post api route

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,5 +1,6 @@
 const { User } = require('../models')
 const helpers = require('../_helpers')
+const imgurFileHandler = require('../helpers/file-helpers')
 
 const apiController = {
   apiEditUser: async (req, res, next) => {
@@ -13,6 +14,41 @@ const apiController = {
     } catch (err) {
       req.flash('error_messages', err.message)
       res.json({ status: 'error', message: err.message })
+      next(err)
+    }
+  },
+  apiPostUser: async (req, res, next) => {
+    try {
+      const currentUserId = helpers.getUser(req).id
+      const UserId = req.params.id
+      const { name } = req.body
+      const introduction = req.body.introduction || ''
+      const avatar = req.files ? req.files.avatar : ''
+      const cover = req.files ? req.files.cover : ''
+
+      let uploadAvatar = ''
+      let uploadCover = ''
+      if (avatar) {
+        uploadAvatar = await imgurFileHandler(avatar[0])
+      }
+      if (cover) {
+        uploadCover = await imgurFileHandler(cover[0])
+      }
+      const user = await User.findByPk(UserId)
+
+      if (user.id !== Number(currentUserId)) return res.json({ status: 'error', message: '不可編輯其他使用者資料！' })
+      if (!name) return res.json({ status: 'error', message: '名稱不可空白！' })
+      if (name.length > 50) return res.json({ status: 'error', message: '字數超出上限！' })
+      if (introduction.length > 160) return res.json({ status: 'error', message: '字數超出上限！' })
+
+      const data = await user.update({
+        name,
+        introduction,
+        avatar: uploadAvatar || user.avatar,
+        cover: uploadCover || user.cover
+      })
+      res.json({ status: 'success', message: '已成功更新!', data })
+    } catch (err) {
       next(err)
     }
   }
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,6 +5,7 @@ const modalProfileName = document.querySelector('#modal-profile-name')
 const modalProfileIntro = document.querySelector('#modal-profile-intro')
 const modalProfileCover = document.querySelector('#modal-profile-cover')
 const modalProfileAvatar = document.querySelector('#modal-profile-avatar')
+const postModalForm = document.querySelector('#post-modal-form')
 
 // 點擊edit button 取得資料
 editProfile.addEventListener('click', event => {
@@ -19,6 +20,27 @@ editProfile.addEventListener('click', event => {
       modalProfileIntro.value = res.data.introduction
       modalProfileCover.src = res.data.cover
       modalProfileAvatar.src = res.data.avatar
+    })
+    .catch(err => console.log(err))
+})
+
+// 點擊儲存
+postModalForm.addEventListener('submit', function (event) {
+  event.preventDefault()
+  const target = event.target
+  const UserId = target.dataset.id
+  const formData = new FormData()
+  formData.append('name', modalProfileName.value)
+  formData.append('introduction', modalProfileIntro.value)
+  formData.append('cover', modalProfileCover.files[0])
+  formData.append('avatar', modalProfileAvatar.files[0])
+
+  axios
+    .post(`/api/users/${UserId}.json`, formData, {
+      headers: { 'Content-Type': 'multipart/form-data' }
+    })
+    .then(res => {
+      history.go(0)
     })
     .catch(err => console.log(err))
 })
