dc31fe22e935eb7367c8e456d888358b6d709251
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-09-02 10:04:53 +0800

    feat: modify time format

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,13 +1,21 @@
-
 const dayjs = require('dayjs')
 const relativeTime = require('dayjs/plugin/relativeTime')
+require('dayjs/locale/zh-tw')
 
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime).locale('zh-tw')
 
 module.exports = {
   currentYear: () => dayjs().year(),
-  relativeTimeFromNow: a => dayjs(a).fromNow(true),
+  relativeTimeFromNow: a => dayjs(a).fromNow().slice(0, -1),
+  normalTimeForm: a => dayjs(a).format('YYYY年MM月DD日'),
+  normalHourForm: a => dayjs(a).format('Ahh:mm'),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
+  },
+  ifTrueAndNotEqual: (a, b, c, options) => {
+    return a && b !== c ? options.fn(this) : options.inverse(this)
+  },
+  ifFalseAndNotEqual: (a, b, c, options) => {
+    return !a && b !== c ? options.fn(this) : options.inverse(this)
   }
 }
