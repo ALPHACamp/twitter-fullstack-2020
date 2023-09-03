daa66a5656ffcc6f765f04ac49003044f7d96b02
gibbs-shih <shih.gibbs@gmail.com>
2023-09-03 23:39:12 +0800

    modify 前後台 登入無權限 訊息

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const helpers = require('../_helpers')
 const authenticated = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
     if (helpers.getUser(req).role === 'user') return next()
-    req.flash('error_messages', '此帳號為管理者帳號，不得登入前台')
+    req.flash('error_messages', '此帳號無權限！')
     return res.redirect('/admin/tweets')
   }
   req.flash('error_messages', '請先登入使用者帳密！')
@@ -11,7 +11,7 @@ const authenticated = (req, res, next) => {
 const authenticatedAdmin = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
     if (helpers.getUser(req).role === 'admin') return next()
-    req.flash('error_messages', '此帳號為使用者帳號，不得登入後台')
+    req.flash('error_messages', '此帳號無權限')
     return res.redirect('/tweets')
   }
   req.flash('error_messages', '請先登入後台管理者帳密！')
