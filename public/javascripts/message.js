647f727508c136f536e782621a66c4ff44e5615c
yang haotse <ha91252@gmail.com>
2023-09-03 23:07:10 +0800

    feat: add validation in reply-modal

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -5,15 +5,16 @@ const alertElement = document.getElementById('alert')
       }, 3000)
     }
 
-const submitButton = document.querySelector('#reply-button')
-const form  = document.querySelector('.needs-validation')
-const replyComment = document.querySelector('#reply-comment')
-submitButton.addEventListener('click', function onSubmitButtonClicked(event) {
-  // console.log(event.target)
-	if (!form.checkValidity()) {
+
+//reply-modal監聽
+const forms  = document.querySelectorAll('.needs-validation')
+Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
       }
-  form.classList.add('was-validated')
-  replyComment.classList.add('is-invalid')
-})
+
+      form.classList.add('was-validated')
+    }, false)
+  })
