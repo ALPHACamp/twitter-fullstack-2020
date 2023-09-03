816f0db047330233ad370453df1a55c6600a43b5
yang haotse <ha91252@gmail.com>
2023-09-03 19:04:49 +0800

    reply-modal validation test (invalid)

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -4,3 +4,16 @@ const alertElement = document.getElementById('alert')
         alertElement.style.display = 'none'
       }, 3000)
     }
+
+const submitButton = document.querySelector('#reply-button')
+const form  = document.querySelector('.needs-validation')
+const replyComment = document.querySelector('#reply-comment')
+submitButton.addEventListener('click', function onSubmitButtonClicked(event) {
+  // console.log(event.target)
+	if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+  form.classList.add('was-validated')
+  replyComment.classList.add('is-invalid')
+})
