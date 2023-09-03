73ca29425441bc996ed2d2cdb3a63478dd7e244c
user <carola1408@yahoo.com.tw>
2023-09-03 23:41:30 +0800

    fead: modify self.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,6 @@
 const pages = require('./pages')
 const apis = require('./apis')
 
-<<<<<<< HEAD
-// Controllers
-const tweetController = require('../controllers/tweet-controller')
-const userController = require('../controllers/user-controller')
-
-// middleware
-const { generalErrorHandler } = require('../middleware/error-handler')
-const { authenticated } = require('../middleware/auth')
-
-// Admin
-router.use('/admin', admin)
-
-// Sign up
-router.get('/signup', userController.signUpPage)
-router.post('/signup', userController.signUp)
-
-// Sign in
-router.get('/signin', userController.signInPage)
-router.post('/signin', passport.authenticate('userSignin', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-
-// Log out
-router.get('/logout', userController.logout)
-
-// admin route
-router.use('/admin', admin)
-
-// users route
-router.get('/users/:id/tweets', authenticated, userController.getUserTweets)
-router.get('/users/:id', authenticated, userController.getUser)
-
-router.use('/users', authenticated, users)
-
-
-// Tweets
-router.get('/tweets', authenticated, tweetController.getTweets)
-router.post('/tweets', authenticated, tweetController.postTweet)
-router.post('/tweets/:id/like', authenticated, tweetController.addLike)
-router.post('/tweets/:id/unlike', authenticated, tweetController.removeLike)
-
-router.use('/', (req, res) => res.redirect('/tweets'))
-router.use('/', generalErrorHandler)
-
-router.use('/', generalErrorHandler)
-
-module.exports = router
-=======
->>>>>>> origin/master
 module.exports = {
   pages,
   apis
