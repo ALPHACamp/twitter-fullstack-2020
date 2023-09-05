5aff983bde0b96b6bd00f621bf93a2039b7e65a8
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-09-05 19:27:25 +0800

    feat: add tweets based on users length

--- seeders/20230824153405-tweets-seed-file.js
+++ seeders/20230824153405-tweets-seed-file.js
@@ -4,27 +4,25 @@ const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     try {
+      const delayInMinutes = 5
       const users = await queryInterface.sequelize.query(
         'SELECT id FROM Users WHERE role = "user";',
         { type: queryInterface.sequelize.QueryTypes.SELECT }
       )
-      const delayInMinutes = 5
-      const tweetCount = 260
-      const tweets = []
 
-      for (let i = 0; i < tweetCount; i++) {
-        const userIndex = parseInt(i / 10)
-        const createdAt = new Date(Date.now() - i * delayInMinutes * 60000).toISOString().substring(0, 16)
+      await queryInterface.bulkDelete('Tweets',
+        Array.from({ length: users.length * 10 }, (_, i) => {
+          const createdAt = new Date(Date.now() - i * delayInMinutes * 60000).toISOString().substring(0, 16)
+          const updatedAt = createdAt
 
-        tweets.push({
-          User_id: users[userIndex].id,
-          description: faker.lorem.text().substring(1, 140),
-          created_at: createdAt,
-          updated_at: createdAt
-        })
-      }
-
-      await queryInterface.bulkInsert('Tweets', tweets, {})
+          return {
+            User_id: users[i % users.length].id,
+            description: faker.lorem.text().substring(1, 140),
+            created_at: createdAt,
+            updated_at: updatedAt
+          }
+        }), {}
+      )
       console.log('Tweets table seeding completed successfully.')
     } catch (error) {
       console.error('Error seeding Tweets.', error)
