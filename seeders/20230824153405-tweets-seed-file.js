1ba5a79beb0df86a960bb13cedfa265dff18d154
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-09-05 19:45:16 +0800

    feat: add tweets based on user length

--- seeders/20230824153405-tweets-seed-file.js
+++ seeders/20230824153405-tweets-seed-file.js
@@ -4,22 +4,24 @@ const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     try {
-      const delayInMinutes = 5
       const users = await queryInterface.sequelize.query(
         'SELECT id FROM Users WHERE role = "user";',
         { type: queryInterface.sequelize.QueryTypes.SELECT }
       )
 
-      await queryInterface.bulkDelete('Tweets',
-        Array.from({ length: users.length * 10 }, (_, i) => {
+      const delayInMinutes = 10
+      const tweetsPerUser = 10
+
+      await queryInterface.bulkInsert('Tweets',
+        Array.from({ length: users.length * tweetsPerUser }, (_, i) => {
           const createdAt = new Date(Date.now() - i * delayInMinutes * 60000).toISOString().substring(0, 16)
-          const updatedAt = createdAt
+          const maxTextLength = 140
 
           return {
             User_id: users[i % users.length].id,
-            description: faker.lorem.text().substring(1, 140),
+            description: faker.lorem.text().substring(0, maxTextLength),
             created_at: createdAt,
-            updated_at: updatedAt
+            updated_at: createdAt
           }
         }), {}
       )
